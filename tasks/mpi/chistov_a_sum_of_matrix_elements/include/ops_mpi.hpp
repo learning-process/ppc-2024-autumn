106d3bddafa9c2169739dc39987f70bc7ff58f10
Alexey2013 <alexey.chistov10@gmail.com>
2024-10-16 11:36:25 +0300

    one test check

--- tasks/mpi/chistov_a_sum_of_matrix_elements/include/ops_mpi.hpp
+++ tasks/mpi/chistov_a_sum_of_matrix_elements/include/ops_mpi.hpp
@@ -4,12 +4,6 @@
 #include <gtest/gtest.h>
 
 #include <boost/mpi/collectives.hpp>
-//#include <boost/mpi/communicator.hpp>
-//#include <memory>
-//#include <numeric>
-//#include <string>
-//#include <utility>
-//#include <vector>
 
 #include "core/task/include/task.hpp"
 
