d6184f28834d282443e875d4222d6e52500cc9c7
0xG00SE <61384845+DSolo03@users.noreply.github.com>
2024-10-18 14:09:34 +0300

    Add files via upload

--- tasks/mpi/solovyev_d_vector_max/src/source.cpp
+++ tasks/mpi/solovyev_d_vector_max/src/source.cpp
@@ -21,7 +21,7 @@ std::vector<int> solovyev_d_vector_max_mpi::getRandomVector(int sz) {
 
 int solovyev_d_vector_max_mpi::vectorMax(std::vector<int,std::allocator<int>> v){
 	int m=-214748364;
-	for(string::size_type i = 0; i < v.size(); i++)
+	for(std::string::size_type i = 0; i < v.size(); i++)
 	{
 		if(v[i] > m)
 		{
