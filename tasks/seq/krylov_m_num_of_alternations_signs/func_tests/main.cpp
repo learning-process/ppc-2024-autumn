5ccda78a241c8c20d48570dd0276ffa36dd9bc8e
kmichaelk <130953568+kmichaelk@users.noreply.github.com>
2024-10-19 22:28:54 +0000

    Reimplement tests

--- tasks/seq/krylov_m_num_of_alternations_signs/func_tests/main.cpp
+++ tasks/seq/krylov_m_num_of_alternations_signs/func_tests/main.cpp
@@ -2,47 +2,33 @@
 
 #include <cstddef>
 #include <numeric>
-#include <tuple>
-#include <type_traits>
 #include <vector>
 
-#include "seq/krylov_m_num_of_alternations_signs/include/ops_seq.hpp"
+#include "../include/ops_seq.hpp"
 
 #define EXPAND(x) x
-#define REMOVE_FIRST(...) EXPAND(REMOVE_FIRST_SUB(__VA_ARGS__))
-#define REMOVE_FIRST_SUB(X, ...) __VA_ARGS__
 
-#define T_DEF(macro)     \
-  EXPAND(macro(int16_t)) \
-  EXPAND(macro(int32_t)) \
-  EXPAND(macro(int64_t)) \
-  EXPAND(macro(float))
+#define T_DEF(macro, ...)             \
+  EXPAND(macro(int16_t, __VA_ARGS__)) \
+  EXPAND(macro(int32_t, __VA_ARGS__)) \
+  EXPAND(macro(int64_t, __VA_ARGS__)) \
+  EXPAND(macro(float, __VA_ARGS__))
 
 using CountType = uint32_t;
-#define X(type) , type
-using ElementTypes = ::testing::Types<REMOVE_FIRST(T_DEF(X))>;
-#undef X
 
-template <typename T>
-class krylov_m_num_of_alternations_signs_seq_test : public testing::Test {
- public:
-  // clang-format off
-  using TestParamsT = std::tuple<
-    CountType      /* count */, 
-    std::vector<T> /* shift_indices */,
-    CountType      /* num */
-  >;
-  // clang-format on
-  static std::vector<TestParamsT> testParamsSet_;
-};
+// clang-format off
+using PredefParam = std::tuple<
+  CountType              /* count */, 
+  std::vector<CountType> /* shift_indices */,
+  CountType              /* num */
+>;
+// clang-format on
 
-TYPED_TEST_SUITE(krylov_m_num_of_alternations_signs_seq_test, ElementTypes);
-
-TYPED_TEST(krylov_m_num_of_alternations_signs_seq_test, yields_correct_result) {
-  using ElementType = TypeParam;
-
-  for (const auto &params : this->testParamsSet_) {
-    const auto &[count, shift_indices, num] = params;
+class krylov_m_num_of_alternations_signs_seq_test : public ::testing::TestWithParam<PredefParam> {
+ protected:
+  template <typename ElementType>
+  void PT_yields_correct_result() {
+    const auto &[count, shift_indices, num] = GetParam();
 
     //
     std::vector<ElementType> in(count);
@@ -63,35 +49,56 @@ TYPED_TEST(krylov_m_num_of_alternations_signs_seq_test, yields_correct_result) {
 
     //
     krylov_m_num_of_alternations_signs_seq::TestTaskSequential<ElementType, CountType> testTaskSequential(taskDataSeq);
-    ASSERT_EQ(testTaskSequential.validation(), true);
+    ASSERT_TRUE(testTaskSequential.validation());
     testTaskSequential.pre_processing();
     testTaskSequential.run();
     testTaskSequential.post_processing();
     ASSERT_EQ(num, out);
   }
-}
 
-#define DECL_TEST_PARAMS(TypeParam)                                                                              \
-  template <>                                                                                                    \
-  std::vector<krylov_m_num_of_alternations_signs_seq_test<TypeParam>::TestParamsT>                               \
-      krylov_m_num_of_alternations_signs_seq_test<TypeParam>::testParamsSet_{                                    \
-          std::make_tuple(129, std::vector<TypeParam>{0, 1, /* . */ 3, /* . */ 5, 6, 7, /* . */ 12 /* . */}, 7), \
-          std::make_tuple(129, std::vector<TypeParam>{0, /* . */}, 1),                                           \
-          std::make_tuple(129, std::vector<TypeParam>{/* . */ 128}, 1),                                          \
-          std::make_tuple(129, std::vector<TypeParam>{/* . */ 64 /* . */}, 2),                                   \
-          std::make_tuple(129, std::vector<TypeParam>{/* . */ 43, /* . */ 86, /* . */}, 4),                      \
-          std::make_tuple(129, std::vector<TypeParam>{/* . */}, 0),                                              \
-          std::make_tuple(128, std::vector<TypeParam>{0, 1, /* . */ 3, /* . */ 5, 6, 7, /* . */ 12 /* . */}, 7), \
-          std::make_tuple(128, std::vector<TypeParam>{0, /* . */}, 1),                                           \
-          std::make_tuple(128, std::vector<TypeParam>{/* . */ 127}, 1),                                          \
-          std::make_tuple(128, std::vector<TypeParam>{/* . */ 64 /* . */}, 2),                                   \
-          std::make_tuple(129, std::vector<TypeParam>{/* . */ 43, /* . */ 86, /* . */}, 4),                      \
-          std::make_tuple(129, std::vector<TypeParam>{/* . */ 42, /* . */ 84, /* . */}, 4),                      \
-          std::make_tuple(128, std::vector<TypeParam>{/* . */}, 0),                                              \
-          std::make_tuple(4, std::vector<TypeParam>{/* . */}, 0),                                                \
-          std::make_tuple(4, std::vector<TypeParam>{/* . */ 2 /* . */}, 2),                                      \
-          std::make_tuple(1, std::vector<TypeParam>{/* . */}, 0),                                                \
-          std::make_tuple(1, std::vector<TypeParam>{0}, 0),                                                      \
-          std::make_tuple(0, std::vector<TypeParam>{/* . */}, 0)};
+  template <typename ElementType>
+  void T_fails_validation() {
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->outputs_count.emplace_back(0);
 
-T_DEF(DECL_TEST_PARAMS)
+    krylov_m_num_of_alternations_signs_seq::TestTaskSequential<ElementType, CountType> testTaskSequential(taskDataSeq);
+    EXPECT_FALSE(testTaskSequential.validation());
+  }
+};
+
+#define DECL_TYPE_VALUE_PARAMETRIZED_TEST(TypeParam, TestName) \
+  TEST_P(krylov_m_num_of_alternations_signs_seq_test, TestName##__##TypeParam) { TestName<TypeParam>(); }
+#define DECL_TYPE_VALUE_PARAMETRIZED_TEST_ALL(TestName) T_DEF(DECL_TYPE_PARAMETRIZED_TEST, PT_##TestName)
+
+#define DECL_TYPE_PARAMETRIZED_TEST(TypeParam, TestName) \
+  TEST_P(krylov_m_num_of_alternations_signs_seq_test, TestName##__##TypeParam) { TestName<TypeParam>(); }
+#define DECL_TYPE_PARAMETRIZED_TEST_ALL(TestName) T_DEF(DECL_TYPE_PARAMETRIZED_TEST, T_##TestName)
+
+INSTANTIATE_TEST_SUITE_P(
+    krylov_m_num_of_alternations_signs_seq_test, krylov_m_num_of_alternations_signs_seq_test,
+    // clang-format off
+    ::testing::Values(
+      std::make_tuple(129, std::vector<CountType>{0, 1, /* . */ 3, /* . */ 5, 6, 7, /* . */ 12 /* . */}, 7),
+      std::make_tuple(129, std::vector<CountType>{0, /* . */}, 1),
+      std::make_tuple(129, std::vector<CountType>{/* . */ 128}, 1),
+      std::make_tuple(129, std::vector<CountType>{/* . */ 64 /* . */}, 2),
+      std::make_tuple(129, std::vector<CountType>{/* . */ 43, /* . */ 86, /* . */}, 4),
+      std::make_tuple(129, std::vector<CountType>{/* . */}, 0),
+      std::make_tuple(128, std::vector<CountType>{0, 1, /* . */ 3, /* . */ 5, 6, 7, /* . */ 12 /* . */}, 7),
+      std::make_tuple(128, std::vector<CountType>{0, /* . */}, 1),
+      std::make_tuple(128, std::vector<CountType>{/* . */ 127}, 1),
+      std::make_tuple(128, std::vector<CountType>{/* . */ 64 /* . */}, 2),
+      std::make_tuple(129, std::vector<CountType>{/* . */ 43, /* . */ 86, /* . */}, 4),
+      std::make_tuple(129, std::vector<CountType>{/* . */ 42, /* . */ 84, /* . */}, 4),
+      std::make_tuple(128, std::vector<CountType>{/* . */}, 0),
+      std::make_tuple(4,   std::vector<CountType>{/* . */}, 0),
+      std::make_tuple(4,   std::vector<CountType>{/* . */ 2 /* . */}, 2),
+      std::make_tuple(1,   std::vector<CountType>{/* . */}, 0), 
+      std::make_tuple(1,   std::vector<CountType>{0}, 0),
+      std::make_tuple(0,   std::vector<CountType>{/* . */}, 0)
+    )
+    // clang-format on
+);
+
+DECL_TYPE_VALUE_PARAMETRIZED_TEST_ALL(yields_correct_result);
+DECL_TYPE_PARAMETRIZED_TEST_ALL(fails_validation);
