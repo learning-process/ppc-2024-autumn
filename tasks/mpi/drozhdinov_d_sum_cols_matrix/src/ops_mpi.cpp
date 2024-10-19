93d99829224b78b6fded664917528403f0286689
Dmitriy <dima-dimka04@yandex.ru>
2024-10-20 02:00:57 +0300

    clang spaces fix

--- tasks/mpi/drozhdinov_d_sum_cols_matrix/src/ops_mpi.cpp
+++ tasks/mpi/drozhdinov_d_sum_cols_matrix/src/ops_mpi.cpp
@@ -23,7 +23,7 @@ std::vector<int> drozhdinov_d_sum_cols_matrix_mpi::getRandomVector(int sz) {
 int drozhdinov_d_sum_cols_matrix_mpi::makeLinCoords(int x, int y, int xSize) { return y * xSize + x; }
 
 std::vector<int> drozhdinov_d_sum_cols_matrix_mpi::calcMatSumSeq(const std::vector<int>& matrix, int xSize, int ySize, 
-                                int fromX, int toX) {
+                                                                 int fromX, int toX) {
   std::vector<int> result;
   for (int x = fromX; x < toX; x++) {
     int columnSum = 0;
