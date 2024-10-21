c099baa2a1c7bbc04597a884ce9c4a16d1f783fd
Шурыгин Сергей <Shurigin11@mail.ru>
2024-10-21 19:59:42 +0300

    clang format format#2

--- tasks/seq/example/include/ops_seq.hpp
+++ tasks/seq/example/include/ops_seq.hpp
@@ -11,14 +11,13 @@ namespace nesterov_a_test_task_seq {
 class TestTaskSequential : public ppc::core::Task {
  private:
   int input_{}, res{};
+
  public:
   explicit TestTaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
   bool pre_processing() override;
   bool validation() override;
   bool run() override;
   bool post_processing() override;
-
- 
 };
 
 }  // namespace nesterov_a_test_task_seq
