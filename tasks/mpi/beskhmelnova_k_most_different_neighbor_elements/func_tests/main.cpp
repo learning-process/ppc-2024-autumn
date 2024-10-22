7b7982d5b3b6ac165c86e1c7045a3c5b3e1db1f9
KseniyaBeskhmelnova <kuks_2004@mail.ru>
2024-10-22 19:15:13 +0300

    First commit

---
+++ tasks/mpi/beskhmelnova_k_most_different_neighbor_elements/func_tests/main.cpp
@@ -0,0 +1,254 @@
+#include <gtest/gtest.h>
+
+#include "mpi/beskhmelnova_k_most_different_neighbor_elements/src/mpi.cpp"
+
+TEST(beskhmelnova_k_most_different_neighbor_elements_mpi, Test_vector_int_100) {
+  boost::mpi::communicator world;
+  std::vector<int> global_vec;
+  std::vector<int32_t> global_out(2);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    const int count_size_vector = 100;
+    global_vec = beskhmelnova_k_most_different_neighbor_elements_mpi::getRandomVector<int>(count_size_vector);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataPar->inputs_count.emplace_back(global_vec.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_out.data()));
+    taskDataPar->outputs_count.emplace_back(global_out.size());
+  }
+
+  // Create Task
+  beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskParallel<int> testMpiTaskParallel(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+
+    // Create data
+    std::vector<int32_t> reference_out(2);
+
+    // Create TaskData
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataSeq->inputs_count.emplace_back(global_vec.size());
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_out.data()));
+    taskDataSeq->outputs_count.emplace_back(reference_out.size());
+
+    // Create Task
+    beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskSequential<int> testMpiTaskSequential(taskDataSeq);
+    ASSERT_EQ(testMpiTaskSequential.validation(), true);
+    testMpiTaskSequential.pre_processing();
+    testMpiTaskSequential.run();
+    testMpiTaskSequential.post_processing();
+
+    ASSERT_EQ(reference_out[0], global_out[0]);
+    ASSERT_EQ(reference_out[1], global_out[1]);
+  }
+}
+
+TEST(beskhmelnova_k_most_different_neighbor_elements_mpi, Test_vector_int_10000) {
+  boost::mpi::communicator world;
+  std::vector<int> global_vec;
+  std::vector<int32_t> global_out(2);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    const int count_size_vector = 10000;
+    global_vec = beskhmelnova_k_most_different_neighbor_elements_mpi::getRandomVector<int>(count_size_vector);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataPar->inputs_count.emplace_back(global_vec.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_out.data()));
+    taskDataPar->outputs_count.emplace_back(global_out.size());
+  }
+
+  // Create Task
+  beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskParallel<int> testMpiTaskParallel(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+
+    // Create data
+    std::vector<int32_t> reference_out(2);
+
+    // Create TaskData
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataSeq->inputs_count.emplace_back(global_vec.size());
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_out.data()));
+    taskDataSeq->outputs_count.emplace_back(reference_out.size());
+
+    // Create Task
+    beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskSequential<int> testMpiTaskSequential(taskDataSeq);
+    ASSERT_EQ(testMpiTaskSequential.validation(), true);
+    testMpiTaskSequential.pre_processing();
+    testMpiTaskSequential.run();
+    testMpiTaskSequential.post_processing();
+
+    ASSERT_EQ(reference_out[0], global_out[0]);
+    ASSERT_EQ(reference_out[1], global_out[1]);
+  }
+}
+
+TEST(beskhmelnova_k_most_different_neighbor_elements_mpi, Test_world_size_vector_int) {
+  boost::mpi::communicator world;
+  std::vector<int> global_vec;
+  std::vector<int32_t> global_out(2);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    const int count_size_vector = world.size();
+    global_vec = beskhmelnova_k_most_different_neighbor_elements_mpi::getRandomVector<int>(count_size_vector);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataPar->inputs_count.emplace_back(global_vec.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_out.data()));
+    taskDataPar->outputs_count.emplace_back(global_out.size());
+  }
+
+  // Create Task
+  beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskParallel<int> testMpiTaskParallel(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+
+    // Create data
+    std::vector<int32_t> reference_out(2);
+
+    // Create TaskData
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataSeq->inputs_count.emplace_back(global_vec.size());
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_out.data()));
+    taskDataSeq->outputs_count.emplace_back(reference_out.size());
+
+    // Create Task
+    beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskSequential<int> testMpiTaskSequential(taskDataSeq);
+    ASSERT_EQ(testMpiTaskSequential.validation(), true);
+    testMpiTaskSequential.pre_processing();
+    testMpiTaskSequential.run();
+    testMpiTaskSequential.post_processing();
+
+    ASSERT_EQ(reference_out[0], global_out[0]);
+    ASSERT_EQ(reference_out[1], global_out[1]);
+  }
+}
+
+ TEST(beskhmelnova_k_most_different_neighbor_elements_mpi, Test_vector_double_100) {
+   boost::mpi::communicator world;
+   std::vector<double> global_vec;
+   std::vector<double> global_out(2);
+
+   // Create TaskData
+   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+   if (world.rank() == 0) {
+     const int count_size_vector = 100;
+     global_vec = beskhmelnova_k_most_different_neighbor_elements_mpi::getRandomVector<double>(count_size_vector);
+     taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+     taskDataPar->inputs_count.emplace_back(global_vec.size());
+     taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_out.data()));
+     taskDataPar->outputs_count.emplace_back(global_out.size());
+   }
+
+   // Create Task
+   beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskParallel<double> testMpiTaskParallel(taskDataPar);
+   ASSERT_EQ(testMpiTaskParallel.validation(), true);
+   testMpiTaskParallel.pre_processing();
+   testMpiTaskParallel.run();
+   testMpiTaskParallel.post_processing();
+
+   if (world.rank() == 0) {
+
+     // Create data
+     std::vector<double> reference_out(2);
+
+     // Create TaskData
+     std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+     taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+     taskDataSeq->inputs_count.emplace_back(global_vec.size());
+     taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_out.data()));
+     taskDataSeq->outputs_count.emplace_back(reference_out.size());
+
+     // Create Task
+     beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskSequential<double>
+     testMpiTaskSequential(taskDataSeq); ASSERT_EQ(testMpiTaskSequential.validation(), true);
+     testMpiTaskSequential.pre_processing();
+     testMpiTaskSequential.run();
+     testMpiTaskSequential.post_processing();
+
+     ASSERT_EQ(reference_out[0], global_out[0]);
+     ASSERT_EQ(reference_out[1], global_out[1]);
+   }
+ }
+
+ TEST(beskhmelnova_k_most_different_neighbor_elements_mpi, Test_vector_double_10000) {
+   boost::mpi::communicator world;
+   std::vector<double> global_vec;
+   std::vector<double> global_out(2);
+
+   // Create TaskData
+   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+   if (world.rank() == 0) {
+     const int count_size_vector = 100;
+     global_vec = beskhmelnova_k_most_different_neighbor_elements_mpi::getRandomVector<double>(count_size_vector);
+     taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+     taskDataPar->inputs_count.emplace_back(global_vec.size());
+     taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_out.data()));
+     taskDataPar->outputs_count.emplace_back(global_out.size());
+   }
+
+   // Create Task
+   beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskParallel<double> testMpiTaskParallel(taskDataPar);
+   ASSERT_EQ(testMpiTaskParallel.validation(), true);
+   testMpiTaskParallel.pre_processing();
+   testMpiTaskParallel.run();
+   testMpiTaskParallel.post_processing();
+
+   if (world.rank() == 0) {
+
+     // Create data
+     std::vector<double> reference_out(2);
+
+     // Create TaskData
+     std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+     taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+     taskDataSeq->inputs_count.emplace_back(global_vec.size());
+     taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_out.data()));
+     taskDataSeq->outputs_count.emplace_back(reference_out.size());
+
+     // Create Task
+     beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskSequential<double> testMpiTaskSequential(taskDataSeq);
+     ASSERT_EQ(testMpiTaskSequential.validation(), true);
+     testMpiTaskSequential.pre_processing();
+     testMpiTaskSequential.run();
+     testMpiTaskSequential.post_processing();
+
+     ASSERT_EQ(reference_out[0], global_out[0]);
+     ASSERT_EQ(reference_out[1], global_out[1]);
+   }
+ }
+
+int main(int argc, char** argv) {
+  boost::mpi::environment env(argc, argv);
+  boost::mpi::communicator world;
+  ::testing::InitGoogleTest(&argc, argv);
+  ::testing::TestEventListeners& listeners = ::testing::UnitTest::GetInstance()->listeners();
+  if (world.rank() != 0) {
+    delete listeners.Release(listeners.default_result_printer());
+  }
+  return RUN_ALL_TESTS();
+}
---
+++ tasks/mpi/beskhmelnova_k_most_different_neighbor_elements/src/mpi.cpp
@@ -0,0 +1,186 @@
+#include "mpi/beskhmelnova_k_most_different_neighbor_elements/include/mpi.hpp"
+
+
+template <typename DataType>
+std::vector<DataType> beskhmelnova_k_most_different_neighbor_elements_mpi::getRandomVector(int sz) {
+  std::random_device dev;
+  std::mt19937 gen(dev());
+  std::vector<DataType> vec(sz);
+  for (int i = 0; i < sz; i++) {
+    vec[i] = gen() % 100;
+  }
+  return vec;
+}
+
+template <typename DataType>
+int beskhmelnova_k_most_different_neighbor_elements_mpi::position_of_first_neighbour_seq(std::vector<DataType> vector) {
+  int n = vector.size();
+  if (n == 0 || n == 1) return -1;
+  DataType max_dif = abs(vector[0] - vector[1]);
+  DataType dif;
+  int index = 0;
+  for (int i = 1; i < n - 1; i++) {
+    dif = abs(vector[i] - vector[i + 1]);
+    if (dif > max_dif) {
+      max_dif = dif;
+      index = i;
+    }
+  }
+  return index;
+}
+
+template <typename DataType>
+bool beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskSequential<DataType>::pre_processing() {
+  internal_order_test();
+  // Init value for input
+  int n = taskData->inputs_count[0];
+  input_ = std::vector<DataType>(n);
+  void* ptr_r = taskData->inputs[0];
+  void* ptr_d = input_.data();
+  memcpy(ptr_d, ptr_r, sizeof(DataType) * n);
+  return true;
+}
+
+template <typename DataType>
+bool beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskSequential<DataType>::validation() {
+  internal_order_test();
+  // Check count elements of output
+  return taskData->outputs_count[0] == 2;
+}
+
+template <typename DataType>
+bool beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskSequential<DataType>::run() {
+  internal_order_test();
+  int index = position_of_first_neighbour_seq(input_);
+  if (index == -1) {
+    res[0] = -1;
+    res[1] = -1;
+    return true;
+  }
+  res[0] = input_[index];
+  res[1] = input_[index + 1];
+  return true;
+}
+
+template <typename DataType>
+bool beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskSequential<DataType>::post_processing() {
+  internal_order_test();
+  reinterpret_cast<int*>(taskData->outputs[0])[0] = res[0];
+  reinterpret_cast<int*>(taskData->outputs[0])[1] = res[1];
+  return true;
+}
+
+//Struct of 2 most different neighbour elements
+template <typename DataType>
+struct NeighborDifference {
+  DataType first;
+  DataType second;
+  DataType dif;
+
+  //Serialization of the structure for MPI transmission
+  template <class Archive>
+  void serialize(Archive& ar, const unsigned int version) {
+    ar& first;
+    ar& second;
+    ar & dif;
+  }
+};
+
+//Find the most different neighbors in a vector
+template <typename DataType>
+NeighborDifference<DataType> find_max_difference(const std::vector<DataType> vector) {
+  int n = vector.size();
+  if (n == 0 || n == 1)
+      return NeighborDifference<DataType>{1, 1, -1};
+  NeighborDifference<DataType> max_dif = {vector[0], vector[1], std::abs(vector[1] - vector[0])};
+  for (int i = 1; i < vector.size() - 1; i++) {
+    DataType dif = std::abs(vector[i + 1] - vector[i]);
+    if (dif > max_dif.dif) {
+      max_dif = {vector[i], vector[i + 1], dif};
+    }
+  }
+  return max_dif;
+}
+
+//Functor for a custom operation in reduce
+template <typename DataType>
+struct reduce_max_difference {
+  NeighborDifference<DataType> operator()(const NeighborDifference<DataType>& a, const NeighborDifference<DataType>& b) const {
+    return (a.dif > b.dif) ? a : b;
+  }
+};
+
+
+template <typename DataType>
+bool beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskParallel<DataType>::pre_processing() {
+  internal_order_test();
+  int world_rank = world.rank();
+  int world_size = world.size();
+  int n;
+  if (world_rank == 0) {
+    n = taskData->inputs_count[0];
+    input_ = std::vector<DataType>(n);
+    void* ptr_r = taskData->inputs[0];
+    void* ptr_d = input_.data();
+    memcpy(ptr_d, ptr_r, sizeof(DataType) * n);
+  }
+  broadcast(world, n, 0);
+  int send_size = n / world_size;
+  int over_size = n % world_size;
+  std::vector<int> send_counts(world_size, send_size);
+  std::vector<int> offset(world_size, 0);
+  if (world_rank == 0) {
+    for (int i = 0; i < world_size; ++i) {
+      if (i < over_size)
+        send_counts[i]++;
+      if (send_counts[i] % 2 != 0)
+        send_counts[i]++;
+      if (i > 0)
+          offset[i] = offset[i - 1] + send_counts[i - 1];
+    }
+  }
+  int local_vector_size = send_counts[world_rank];
+  local_input_.resize(local_vector_size);
+  if (world.rank() == 0)
+    boost::mpi::scatterv(world, input_, send_counts, offset, local_input_.data(), local_vector_size, 0);
+  else
+    boost::mpi::scatterv(world, local_input_.data(), local_vector_size, 0);
+  local_input_size = local_vector_size;
+  return true;
+}
+
+template <typename DataType>
+bool beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskParallel<DataType>::validation() {
+  internal_order_test();
+  if (world.rank() == 0) {
+    // Check count elements of output
+    return taskData->inputs_count.size() == 1 && taskData->inputs_count[0] >= 0 && taskData->outputs_count[0] == 2;
+  }
+  return true;
+}
+
+template <typename DataType>
+bool beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskParallel<DataType>::run() {
+  internal_order_test();
+  if (local_input_size == 0 || taskData->inputs_count[0] == 0 || taskData->inputs_count[0] == 1) {
+    res[0] = -1;
+    res[1] = -1;
+    return true;
+  }
+  NeighborDifference<DataType> local_result = find_max_difference(local_input_);
+  NeighborDifference<DataType> global_result = {0, 0, 0};
+  reduce(world, local_result, global_result, reduce_max_difference<DataType>(), 0);
+  res[0] = global_result.first;
+  res[1] = global_result.second;
+  return true;
+}
+
+template <typename DataType>
+bool beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskParallel<DataType>::post_processing() {
+  internal_order_test();
+  if (world.rank() == 0) {
+    reinterpret_cast<int*>(taskData->outputs[0])[0] = res[0];
+    reinterpret_cast<int*>(taskData->outputs[0])[1] = res[1];
+  }
+  return true;
+}
