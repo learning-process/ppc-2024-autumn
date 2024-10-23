6c4d0cf4d886fee78d41a073dd98cf8b48b331bb
Dmitriy <dima-dimka04@yandex.ru>
2024-10-23 16:16:26 +0300

    codcov++

--- tasks/mpi/drozhdinov_d_sum_cols_matrix/func_tests/main.cpp
+++ tasks/mpi/drozhdinov_d_sum_cols_matrix/func_tests/main.cpp
@@ -7,6 +7,40 @@
 
 #include "mpi/drozhdinov_d_sum_cols_matrix/include/ops_mpi.hpp"
 
+TEST(drozhdinov_d_sum_cols_matrix_mpi, RandomMatrixTest) {
+  boost::mpi::communicator world;
+
+  int c = 50;
+  int r = 50;
+
+  // Create data
+  std::vector<int> matrix = drozhdinov_d_sum_cols_matrix_mpi::getRandomVector(c * r);
+  std::vector<int> v = drozhdinov_d_sum_cols_matrix_mpi::calculateMatrixSumSequentially(matrix, c, r);
+  std::vector<int> expres_par(c, 0);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrix.data()));
+    taskDataPar->inputs_count.emplace_back(matrix.size());
+    taskDataPar->inputs_count.emplace_back(c);
+    taskDataPar->inputs_count.emplace_back(r);
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(expres_par.data()));
+    taskDataPar->outputs_count.emplace_back(expres_par.size());
+  }
+
+  drozhdinov_d_sum_cols_matrix_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+    ASSERT_EQ(expres_par, v);
+  }
+}
+
 TEST(drozhdinov_d_sum_cols_matrix_mpi, ParallelTest1) {
   boost::mpi::communicator world;
 
