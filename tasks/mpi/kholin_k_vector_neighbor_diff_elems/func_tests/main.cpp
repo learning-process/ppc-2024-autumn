ae97038a41f1ac879f4b3655fd71209d05a8d908
Kirius257 <holin.kirill@yandex.ru>
2024-10-24 21:41:31 +0300

    fix test with double

--- tasks/mpi/kholin_k_vector_neighbor_diff_elems/func_tests/main.cpp
+++ tasks/mpi/kholin_k_vector_neighbor_diff_elems/func_tests/main.cpp
@@ -396,7 +396,7 @@ TEST(kholin_k_vector_neighbor_diff_elems_mpi, check_float) {
     testTaskSequential.run();
     testTaskSequential.post_processing();
     float test2 = reference_elems[0] - reference_elems[1];
-    ASSERT_NEAR(test, test2,1e-5);
+    ASSERT_NEAR(test, test2, 1e-5);
   }
 }
 
@@ -432,7 +432,7 @@ TEST(kholin_k_vector_neighbor_diff_elems_mpi, check_double) {
   testMpiTaskParallel.pre_processing();
   testMpiTaskParallel.run();
   testMpiTaskParallel.post_processing();
-  float test = global_elems[0] - global_elems[1];
+  double test = global_elems[0] - global_elems[1];
 
   if (world.rank() == 0) {
     // Create data
@@ -456,6 +456,6 @@ TEST(kholin_k_vector_neighbor_diff_elems_mpi, check_double) {
     testTaskSequential.run();
     testTaskSequential.post_processing();
     double test2 = reference_elems[0] - reference_elems[1];
-    ASSERT_EQ(test, test2);
+    ASSERT_EQ(test, test2, 1e-5);
   }
 }
