1bc0f88b012df29777fdb916dab8ce3d62581e08
Kirius257 <holin.kirill@yandex.ru>
2024-10-24 18:41:21 +0300

    uncomment  1 mpi test

--- tasks/mpi/kholin_k_vector_neighbor_diff_elems/func_tests/main.cpp
+++ tasks/mpi/kholin_k_vector_neighbor_diff_elems/func_tests/main.cpp
@@ -6,6 +6,54 @@
 
 #include "mpi/kholin_k_vector_neighbor_diff_elems/include/ops_mpi.hpp"
 
+TEST(kholin_k_vector_neighbor_diff_elems_mpi, check_validation) {
+  boost::mpi::communicator world;
+  const int count_size_vector = 500;
+  std::vector<int> global_vec(count_size_vector, 0);
+  std::vector<int> global_elems(2, 0);
+  std::vector<uint64_t> global_indices(2, 0);
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    global_vec[100] = 5000;
+    global_vec[101] = 1;
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataPar->inputs_count.emplace_back(global_vec.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_elems.data()));
+    taskDataPar->outputs_count.emplace_back(global_elems.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_indices.data()));
+    taskDataPar->outputs_count.emplace_back(global_indices.size());
+  }
+
+  kholin_k_vector_neighbor_diff_elems_mpi::TestMPITaskParallel<int, uint64_t> testMpiTaskParallel(taskDataPar,
+                                                                                                  "MAX_DIFFERENCE");
+  bool IsValid = testMpiTaskParallel.validation();
+  ASSERT_EQ(IsValid, true);
+
+  if (world.rank() == 0) {
+    // Create data
+    std::vector<int> reference_elems(2, 0);
+    std::vector<uint64_t> reference_indices(2, 0);
+
+    // Create TaskData
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataSeq->inputs_count.emplace_back(global_vec.size());
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_elems.data()));
+    taskDataSeq->outputs_count.emplace_back(reference_elems.size());
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_indices.data()));
+    taskDataSeq->outputs_count.emplace_back(reference_indices.size());
+
+    // Create Task
+    kholin_k_vector_neighbor_diff_elems_mpi::TestTaskSequential<int, uint64_t> testTaskSequential(taskDataSeq,
+                                                                                                  "MAX_DIFFERENCE");
+    bool IsValid_ = testTaskSequential.validation();
+    ASSERT_EQ(IsValid_, true);
+  }
+}
+
 //TEST(kholin_k_vector_neighbor_diff_elems_mpi, check_int) {
 //  boost::mpi::communicator world;
 //  std::vector<int> global_vec;
