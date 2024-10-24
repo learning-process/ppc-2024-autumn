1e4e50cd8d406043ac9c46d3d09d16eb16a438a4
Alexey-Solovev <149035736+Alexey-Solovev@users.noreply.github.com>
2024-10-24 07:02:28 +0300

    Update ops_seq.cpp

--- tasks/seq/solovev_a_word_count/src/ops_seq.cpp
+++ tasks/seq/solovev_a_word_count/src/ops_seq.cpp
@@ -2,42 +2,51 @@
 
 namespace solovev_a_word_count_seq {
 
-std::string create_text(int quan_words) {
-  std::string res;
+std::vector<char> create_text(int quan_words) {
+  std::vector<char> res;
   std::string word = "word ";
-  for (int i = 0; i < quan_words; i++) {
-    res += word;
+  std::string last = "word.";
+  for (int i = 0; i < quan_words-1; i++)
+    for (unsigned long int symbol = 0; symbol < word.length(); symbol++) {
+      res.push_back(word[symbol]);
+    }
+  for (unsigned long int symbol = 0; symbol < last.length(); symbol++) {
+    res.push_back(last[symbol]);
   }
   return res;
 }
 
-int word_count(const std::string& input) {
-  std::istringstream iss(input);
-  return std::distance(std::istream_iterator<std::string>(iss), std::istream_iterator<std::string>());
-}
-
 bool solovev_a_word_count_seq::TestTaskSequential::pre_processing() {
-  internal_order_test();
-  input_ = std::string(reinterpret_cast<char*>(taskData->inputs[0]), taskData->inputs_count[0]);
-  res = 0;
-  return true;
+internal_order_test();
+input_ = std::vector<char>(taskData->inputs_count[0]);
+auto* tmp_ptr = reinterpret_cast<char*>(taskData->inputs[0]);
+for (unsigned i = 0; i < taskData->inputs_count[0]; i++) {
+  input_[i] = tmp_ptr[i];
+}
+res = 0;
+return true;
 }
 
 bool solovev_a_word_count_seq::TestTaskSequential::validation() {
-  internal_order_test();
-  return taskData->inputs_count[0] > 0 && taskData->outputs_count[0] == 1;
+internal_order_test();
+return taskData->inputs_count[0] > 0 && taskData->outputs_count[0] == 1;
 }
 
 bool solovev_a_word_count_seq::TestTaskSequential::run() {
-  internal_order_test();
-  res = word_count(input_);
+internal_order_test();
+  for (char symbol : input_) {
+  if (symbol != ' ' && symbol != '.') {
+    } else {
+      res++;
+    }
+  }
   return true;
 }
 
 bool solovev_a_word_count_seq::TestTaskSequential::post_processing() {
-  internal_order_test();
-  reinterpret_cast<int*>(taskData->outputs[0])[0] = res;
-  return true;
+internal_order_test();
+reinterpret_cast<int*>(taskData->outputs[0])[0] = res;
+return true;
 }
 
 }  // namespace solovev_a_word_count_seq
