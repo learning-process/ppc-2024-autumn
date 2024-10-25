4d490c0c574d37b7bab20dd8ad3abad6bf95b4ea
KseniyaBeskhmelnova <kuks_2004@mail.ru>
2024-10-25 17:44:07 +0300

    tyazhelo snova

--- tasks/mpi/beskhmelnova_k_most_different_neighbor_elements/func_tests/main.cpp
+++ tasks/mpi/beskhmelnova_k_most_different_neighbor_elements/func_tests/main.cpp
@@ -193,53 +193,6 @@ TEST(beskhmelnova_k_most_different_neighbor_elements_mpi, Test_vector_double_100
   }
 }
 
-TEST(beskhmelnova_k_most_different_neighbor_elements_mpi, Test_vector_int_uneven_size_10001) {
-  boost::mpi::communicator world;
-  std::vector<int> global_vec;
-  std::vector<int32_t> global_out(2);
-
-  // Create TaskData
-  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
-
-  if (world.rank() == 0) {
-    const int count_size_vector = 10001;
-    global_vec = beskhmelnova_k_most_different_neighbor_elements_mpi::getRandomVector<int>(count_size_vector);
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
-    taskDataPar->inputs_count.emplace_back(global_vec.size());
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_out.data()));
-    taskDataPar->outputs_count.emplace_back(global_out.size());
-  }
-
-  // Create Task
-  beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskParallel<int> testMpiTaskParallel(taskDataPar);
-  ASSERT_EQ(testMpiTaskParallel.validation(), true);
-  testMpiTaskParallel.pre_processing();
-  testMpiTaskParallel.run();
-  testMpiTaskParallel.post_processing();
-
-  if (world.rank() == 0) {
-    // Create data
-    std::vector<int32_t> reference_out(2);
-
-    // Create TaskData
-    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
-    taskDataSeq->inputs_count.emplace_back(global_vec.size());
-    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_out.data()));
-    taskDataSeq->outputs_count.emplace_back(reference_out.size());
-
-    // Create Task
-    beskhmelnova_k_most_different_neighbor_elements_mpi::TestMPITaskSequential<int> testMpiTaskSequential(taskDataSeq);
-    ASSERT_EQ(testMpiTaskSequential.validation(), true);
-    testMpiTaskSequential.pre_processing();
-    testMpiTaskSequential.run();
-    testMpiTaskSequential.post_processing();
-
-    ASSERT_EQ(reference_out[0], global_out[0]);
-    ASSERT_EQ(reference_out[1], global_out[1]);
-  }
-}
-
 TEST(beskhmelnova_k_most_different_neighbor_elements_mpi, Test_vector_double_uneven_size_10001) {
   boost::mpi::communicator world;
   std::vector<double> global_vec;
