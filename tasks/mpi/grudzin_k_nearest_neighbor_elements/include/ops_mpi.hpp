416fcbe64e03e148641ca0680c19f32e099348f4
Grudzin Konstantin <113104424+Konstantin-Grudzin@users.noreply.github.com>
2024-10-27 19:28:26 +0300

    sime fixes #2

--- tasks/mpi/grudzin_k_nearest_neighbor_elements/include/ops_mpi.hpp
+++ tasks/mpi/grudzin_k_nearest_neighbor_elements/include/ops_mpi.hpp
@@ -20,7 +20,7 @@ std::vector<int> getRandomVector(int sz);
 
 class TestMPITaskSequential : public ppc::core::Task {
  public:
-  explicit TestMPITaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)){}
+  explicit TestMPITaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
   bool pre_processing() override;
   bool validation() override;
   bool run() override;
