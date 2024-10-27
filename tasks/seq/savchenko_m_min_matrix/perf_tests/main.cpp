86ee5534029a2adc078ac213d4742dc162fa3321
MaxikGuy <S_Maksssim@mail.ru>
2024-10-28 00:08:21 +0300

    update sec/perf_tests

--- tasks/seq/savchenko_m_min_matrix/perf_tests/main.cpp
+++ tasks/seq/savchenko_m_min_matrix/perf_tests/main.cpp
@@ -1,24 +1,39 @@
 // Copyright 2023 Nesterov Alexander
 #include <gtest/gtest.h>
 
+#include <climits>
+#include <random>
 #include <vector>
 
 #include "core/perf/include/perf.hpp"
 #include "seq/savchenko_m_min_matrix/include/ops_seq.hpp"
 
 TEST(savchenko_m_min_matrix_seq, test_pipeline_run) {
-  const int count = 100;
+  std::vector<int> matrix;
+  std::vector<int32_t> min_value(1, INT_MAX);
+  
+  std::random_device dev;
+  std::mt19937 gen(dev());
 
   // Create data
-  std::vector<int> in(1, count);
-  std::vector<int> out(1, 0);
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  int rows = 5000;
+  int columns = 5000;
+  int gen_min = -1000;
+  int gen_max = 1000;
+  int ref = INT_MIN;
+
+  matrix = savchenko_m_min_matrix_seq::getRandomMatrix(rows, columns, gen_min, gen_max);
+  int index = gen() % (rows * columns);
+  matrix[index] = INT_MIN;
 
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
-  taskDataSeq->inputs_count.emplace_back(in.size());
-  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-  taskDataSeq->outputs_count.emplace_back(out.size());
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix.data()));
+  taskDataSeq->inputs_count.emplace_back(matrix.size());
+
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(min_value.data()));
+  taskDataSeq->outputs_count.emplace_back(min_value.size());
 
   // Create Task
   auto testTaskSequential = std::make_shared<savchenko_m_min_matrix_seq::TestTaskSequential>(taskDataSeq);
@@ -40,22 +55,35 @@ TEST(savchenko_m_min_matrix_seq, test_pipeline_run) {
   auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskSequential);
   perfAnalyzer->pipeline_run(perfAttr, perfResults);
   ppc::core::Perf::print_perf_statistic(perfResults);
-  ASSERT_EQ(count, out[0]);
+  ASSERT_EQ(ref, min_value[0]);
 }
 
 TEST(savchenko_m_min_matrix_seq, test_task_run) {
-  const int count = 100;
+  std::vector<int> matrix;
+  std::vector<int32_t> min_value(1, INT_MAX);
+  int ref = INT_MIN;
+
+  std::random_device dev;
+  std::mt19937 gen(dev());
 
   // Create data
-  std::vector<int> in(1, count);
-  std::vector<int> out(1, 0);
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  int rows = 5000;
+  int columns = 5000;
+  int gen_min = -1000;
+  int gen_max = 1000;
+
+  matrix = savchenko_m_min_matrix_seq::getRandomMatrix(rows, columns, gen_min, gen_max);
+  int index = gen() % (rows * columns);
+  matrix[index] = INT_MIN;
 
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
-  taskDataSeq->inputs_count.emplace_back(in.size());
-  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-  taskDataSeq->outputs_count.emplace_back(out.size());
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix.data()));
+  taskDataSeq->inputs_count.emplace_back(matrix.size());
+
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(min_value.data()));
+  taskDataSeq->outputs_count.emplace_back(min_value.size());
 
   // Create Task
   auto testTaskSequential = std::make_shared<savchenko_m_min_matrix_seq::TestTaskSequential>(taskDataSeq);
@@ -77,7 +105,7 @@ TEST(savchenko_m_min_matrix_seq, test_task_run) {
   auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskSequential);
   perfAnalyzer->task_run(perfAttr, perfResults);
   ppc::core::Perf::print_perf_statistic(perfResults);
-  ASSERT_EQ(count, out[0]);
+  ASSERT_EQ(ref, min_value[0]);
 }
 
 int main(int argc, char **argv) {
