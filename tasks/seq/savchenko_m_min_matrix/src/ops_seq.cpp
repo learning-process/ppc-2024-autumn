2f129f61406a17a745a060a5b886cc9962f01095
MaxikGuy <S_Maksssim@mail.ru>
2024-10-27 23:35:29 +0300

    clang fix

--- tasks/seq/savchenko_m_min_matrix/src/ops_seq.cpp
+++ tasks/seq/savchenko_m_min_matrix/src/ops_seq.cpp
@@ -6,10 +6,12 @@
 
 using namespace std::chrono_literals;
 
-std::vector<int> savchenko_m_min_matrix_seq::getRandomMatrix(int rows, int columns, int min, int max) {
+std::vector<int> savchenko_m_min_matrix_seq::getRandomMatrix(int rows,
+                                                             int columns,
+                                                             int min, int max) {
   std::random_device dev;
   std::mt19937 gen(dev());
-  
+
   // Forming a random matrix
   std::vector<int> matrix(rows * columns);
   for (int i = 0; i < rows; i++) {
@@ -24,7 +26,8 @@ std::vector<int> savchenko_m_min_matrix_seq::getRandomMatrix(int rows, int colum
 bool savchenko_m_min_matrix_seq::TestTaskSequential::validation() {
   internal_order_test();
   // Check count elements of output
-  return taskData->inputs_count[0] > 0 && taskData->inputs_count[1] > 0 && taskData->outputs_count[0] == 1;
+  return taskData->inputs_count[0] > 0 && taskData->inputs_count[1] > 0 &&
+         taskData->outputs_count[0] == 1;
 }
 
 bool savchenko_m_min_matrix_seq::TestTaskSequential::pre_processing() {
@@ -35,21 +38,21 @@ bool savchenko_m_min_matrix_seq::TestTaskSequential::pre_processing() {
   matrix = std::vector<int>(rows * columns);
 
   for (int i = 0; i < rows; ++i) {
-    auto* temp = reinterpret_cast<int*>(taskData->inputs[i]);
+    auto *temp = reinterpret_cast<int *>(taskData->inputs[i]);
 
     for (int j = 0; j < columns; ++j) {
       matrix[i * columns + j] = temp[j];
     }
   }
   res = matrix[0];
-  
+
   return true;
 }
 
 bool savchenko_m_min_matrix_seq::TestTaskSequential::run() {
   internal_order_test();
   for (int i = 0; i < matrix.size(); i++) {
-    if (matrix[i] < res){
+    if (matrix[i] < res) {
       res = matrix[i];
     }
   }
@@ -58,6 +61,6 @@ bool savchenko_m_min_matrix_seq::TestTaskSequential::run() {
 
 bool savchenko_m_min_matrix_seq::TestTaskSequential::post_processing() {
   internal_order_test();
-  reinterpret_cast<int*>(taskData->outputs[0])[0] = res;
+  reinterpret_cast<int *>(taskData->outputs[0])[0] = res;
   return true;
 }
