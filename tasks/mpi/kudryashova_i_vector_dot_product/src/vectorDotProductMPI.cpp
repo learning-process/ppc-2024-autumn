37d0763b4fd564fc8736a8e4db719e17c2fe1a1e
Irina2004-tech <irakudr2015@gmail.com>
2024-10-28 21:08:54 +0300

    mpi_15

--- tasks/mpi/kudryashova_i_vector_dot_product/src/vectorDotProductMPI.cpp
+++ tasks/mpi/kudryashova_i_vector_dot_product/src/vectorDotProductMPI.cpp
@@ -58,46 +58,6 @@ bool kudryashova_i_vector_dot_product_mpi::TestMPITaskSequential::post_processin
   return true;
 }
 
-//bool kudryashova_i_vector_dot_product_mpi::TestMPITaskParallel::pre_processing() {
-//  internal_order_test();
-//  unsigned int delta = 0;
-//
-//  if (world.rank() == 0) {
-//    delta = taskData->inputs_count[0] / world.size();
-//  }
-//  broadcast(world, delta, 0);
-//
-//  if (world.rank() == 0) {
-//    input_.resize(taskData->inputs.size());
-//    for (size_t i = 0; i < taskData->inputs.size(); ++i) {
-//      input_[i].resize(taskData->inputs_count[i]);
-//
-//      int* source_ptr = reinterpret_cast<int*>(taskData->inputs[i]);
-//
-//      if (source_ptr != nullptr) {
-//        std::copy(source_ptr, source_ptr + taskData->inputs_count[i], input_[i].begin());
-//      } else
-//        return false;
-//    }
-//    for (int proc = 1; proc < world.size(); ++proc) {
-//      world.send(proc, 0, input_[0].data() + proc * delta, delta);
-//      world.send(proc, 1, input_[1].data() + proc * delta, delta);
-//    }
-//  }
-//  local_input1_.resize(delta);
-//  local_input2_.resize(delta);
-//
-//  if (world.rank() == 0) {
-//    std::copy(input_[0].begin(), input_[0].begin() + delta, local_input1_.begin());
-//    std::copy(input_[1].begin(), input_[1].begin() + delta, local_input2_.begin());
-//  } else {
-//    world.recv(0, 0, local_input1_.data(), delta);
-//    world.recv(0, 1, local_input2_.data(), delta);
-//  }
-//  result = 0;
-//  return true;
-//}
-
 
 bool kudryashova_i_vector_dot_product_mpi::TestMPITaskParallel::pre_processing() {
   internal_order_test();
@@ -105,7 +65,7 @@ bool kudryashova_i_vector_dot_product_mpi::TestMPITaskParallel::pre_processing()
 
   if (world.rank() == 0) {
     if (taskData->inputs_count[0] % world.size() != 0) {
-      return false;  // Обработка ошибки
+      return false;
     }
     delta = taskData->inputs_count[0] / world.size();
   }
@@ -155,8 +115,6 @@ bool kudryashova_i_vector_dot_product_mpi::TestMPITaskParallel::validation() {
   return true;
 }
 
-
-
 bool kudryashova_i_vector_dot_product_mpi::TestMPITaskParallel::run() {
   internal_order_test();
 
@@ -182,71 +140,3 @@ bool kudryashova_i_vector_dot_product_mpi::TestMPITaskParallel::post_processing(
   }
   return true;
 }
-
-
- //bool kudryashova_i_vector_dot_product_mpi::TestMPITaskParallel::post_processing() {
- //  internal_order_test();
- //  if (world.rank() == 0) {
- //    reinterpret_cast<int*>(taskData->outputs[0])[0] = result;
- //  }
- //  return true;
- //}
-
-// bool kudryashova_i_vector_dot_product_mpi::TestMPITaskParallel::pre_processing() {
-//   internal_order_test();
-//
-//   // Вычисляем количество данных для каждого процесса
-//   std::vector<int> counts(world.size());
-//   for (int i = 0; i < world.size(); ++i) {
-//     counts[i] = taskData->inputs_count[i] / world.size();
-//   }
-//
-//   // Вычисляем смещения для ручного распределения данных
-//   std::vector<int> displacements(world.size());
-//   displacements[0] = 0;
-//   for (int i = 1; i < world.size(); ++i) {
-//     displacements[i] = displacements[i - 1] + counts[i - 1];
-//   }
-//
-//   // Проверяем, что количество данных достаточно для распределения
-//   //if (world.rank() == 0) {
-//   //  if (taskData->inputs_count[0] < world.size()) {
-//   //    return false;
-//   //  }
-//   //}
-//
-//   // Заполняем input_ на всех процессах
-//   input_.resize(taskData->inputs.size());
-//   for (size_t i = 0; i < taskData->inputs.size(); ++i) {
-//     input_[i].resize(taskData->inputs_count[i]);
-//     std::copy(taskData->inputs[i], taskData->inputs[i] + taskData->inputs_count[i], input_[i].begin());
-//   }
-//
-//   // Инициализация локальных буферов
-//   local_input1_.resize(counts[world.rank()]);
-//   local_input2_.resize(counts[world.rank()]);
-//
-//   // Ручное распределение данных
-//   if (world.rank() == 0) {
-//     // Процесс 0 распределяет данные
-//     for (int proc = 0; proc < world.size(); ++proc) {
-//       if (proc != 0) {
-//         world.send(proc, 0, input_[0].data() + displacements[proc], counts[proc]);
-//         world.send(proc, 1, input_[1].data() + displacements[proc], counts[proc]);
-//       } else {
-//         // Процесс 0 копирует свою часть данных
-//         std::copy(input_[0].begin() + displacements[proc], input_[0].begin() + displacements[proc] + counts[proc],
-//                   local_input1_.begin());
-//         std::copy(input_[1].begin() + displacements[proc], input_[1].begin() + displacements[proc] + counts[proc],
-//                   local_input2_.begin());
-//       }
-//     }
-//   } else {
-//     // Остальные процессы получают данные
-//     world.recv(0, 0, local_input1_.data(), counts[world.rank()]);
-//     world.recv(0, 1, local_input2_.data(), counts[world.rank()]);
-//   }
-//
-//   result = 0;
-//   return true;
-// }
