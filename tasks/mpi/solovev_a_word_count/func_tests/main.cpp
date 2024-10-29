50be5050b7051c6b7577ca688f9c5f11ea0b25b5
Alexey-Solovev <149035736+Alexey-Solovev@users.noreply.github.com>
2024-10-29 05:03:56 +0300

    add empty string and small string test

--- tasks/mpi/solovev_a_word_count/func_tests/main.cpp
+++ tasks/mpi/solovev_a_word_count/func_tests/main.cpp
@@ -6,6 +6,60 @@
 
 #include "mpi/solovev_a_word_count/include/ops_mpi.hpp"
 
+TEST(solovev_a_word_count_mpi, test_0_word) {
+  std::vector<char> input = {};
+  std::vector<int> global_out(1, 0);
+  boost::mpi::communicator world;
+
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+  if (world.rank() == 0) {
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
+    taskDataPar->inputs_count.emplace_back(input.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(global_out.data()));
+    taskDataPar->outputs_count.emplace_back(global_out.size());
+
+    solovev_a_word_count_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+    ASSERT_EQ(testMpiTaskParallel.validation(), false);
+  }
+}
+
+TEST(solovev_a_word_count_mpi, test_5_word) {
+  std::vector<char> input = solovev_a_word_count_mpi::create_text(5);
+  std::vector<int> global_out(1, 0);
+  boost::mpi::communicator world;
+
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+  if (world.rank() == 0) {
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
+    taskDataPar->inputs_count.emplace_back(input.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(global_out.data()));
+    taskDataPar->outputs_count.emplace_back(global_out.size());
+  }
+
+  solovev_a_word_count_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+  if (world.rank() == 0) {
+    std::vector<int> ref_out(1, 0);
+
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
+    taskDataSeq->inputs_count.emplace_back(input.size());
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(ref_out.data()));
+    taskDataSeq->outputs_count.emplace_back(ref_out.size());
+
+    solovev_a_word_count_mpi::TestMPITaskSequential testMpiTaskSequential(taskDataSeq);
+    ASSERT_EQ(testMpiTaskSequential.validation(), true);
+    testMpiTaskSequential.pre_processing();
+    testMpiTaskSequential.run();
+    testMpiTaskSequential.post_processing();
+
+    ASSERT_EQ(ref_out[0], global_out[0]);
+  }
+}
+
 TEST(solovev_a_word_count_mpi, test_120_word) {
   std::vector<char> input = solovev_a_word_count_mpi::create_text(120);
   std::vector<int> global_out(1, 0);
