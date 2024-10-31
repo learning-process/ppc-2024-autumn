b217ff3fb68622746bcd314820ac0fee54b8b6a3
Svolota <seraphimvolochaev@gmail.com>
2024-10-31 14:20:31 +0300

    fix build 10

--- tasks/mpi/volochaev_s_count_characters_27/src/ops_mpi.cpp
+++ tasks/mpi/volochaev_s_count_characters_27/src/ops_mpi.cpp
@@ -77,7 +77,7 @@ bool volochaev_s_count_characters_27_mpi::Lab1_27_mpi::pre_processing() {
 
   if (world.rank() == 0) {
     // Init vectors
-    input_ = std::vector<std::pair<char, char>>(world.size() * delta);
+    input_ = std::vector<std::pair<char, char>>(world.size() * (delta + 1));
     for (size_t i = 0; i < std::min(tmp1.size(), tmp2.size()); i++) {
       input_[i].first = tmp1[i];
       input_[i].second = tmp2[i];
