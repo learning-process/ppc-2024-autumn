6f3468c576946f21112fce96a8d39115f482e823
ivan.lysov.05@gmail.com <ivan.lysov.05@gmail.com>
2024-10-31 11:04:28 +0300

    add randoom test

--- tasks/mpi/lysov_i_integration_the_trapezoid_method/func_tests/main.cpp
+++ tasks/mpi/lysov_i_integration_the_trapezoid_method/func_tests/main.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <random>
 
 #include <boost/mpi/communicator.hpp>
 #include <boost/mpi/environment.hpp>
@@ -12,7 +13,7 @@ TEST(lysov_i_integration_the_trapezoid_method_mpi, Test_Integration_mpi_1) {
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
   double a = -1.45;
   double b = 0.0;
-  double epsilon = 0.01;
+  double epsilon = 0.000001;
   if (world.rank() == 0) {
     taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&a));
     taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&b));
@@ -47,7 +48,7 @@ TEST(lysov_i_integration_the_trapezoid_method_mpi, Test_Integration_mpi_2) {
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
   double a = 0.0;
   double b = 1.45;
-  double epsilon = 0.01;
+  double epsilon = 0.000001;
   if (world.rank() == 0) {
     taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&a));
     taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&b));
@@ -76,14 +77,14 @@ TEST(lysov_i_integration_the_trapezoid_method_mpi, Test_Integration_mpi_2) {
   }
 }
 
-TEST(lysov_i_integration_the_trapezoid_method_mpi, Test_Integration_mpi_5) {
+TEST(lysov_i_integration_the_trapezoid_method_mpi, Test_Integration_mpi_3) {
   boost::mpi::communicator world;
   std::vector<double> global_result(1, 0.0);
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
 
   double a = -10.0;
   double b = 65.0;
-  double epsilon = 0.00001;
+  double epsilon = 0.000001;
 
   if (world.rank() == 0) {
     taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&a));
@@ -114,45 +115,7 @@ TEST(lysov_i_integration_the_trapezoid_method_mpi, Test_Integration_mpi_5) {
   }
 }
 
-TEST(lysov_i_integration_the_trapezoid_method_mpi, Test_Integration_mpi_6) {
-  boost::mpi::communicator world;
-  std::vector<double> global_result(1, 0.0);
-  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
-
-  double a = 0.0;
-  double b = 100.0;
-  double epsilon = 0.00001;
-
-  if (world.rank() == 0) {
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&a));
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&b));
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&epsilon));
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_result.data()));
-  }
-
-  lysov_i_integration_the_trapezoid_method_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
-  ASSERT_TRUE(testMpiTaskParallel.validation());
-  testMpiTaskParallel.pre_processing();
-  testMpiTaskParallel.run();
-  testMpiTaskParallel.post_processing();
-
-  if (world.rank() == 0) {
-    std::vector<double> reference_result(1, 0.0);
-    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&a));
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&b));
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&epsilon));
-    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_result.data()));
-    lysov_i_integration_the_trapezoid_method_mpi::TestMPITaskSequential testMpiTaskSequential(taskDataSeq);
-    ASSERT_EQ(testMpiTaskSequential.validation(), true);
-    testMpiTaskSequential.pre_processing();
-    testMpiTaskSequential.run();
-    testMpiTaskSequential.post_processing();
-    ASSERT_NEAR(reference_result[0], global_result[0], 1e-1);
-  }
-}
-
-TEST(lysov_i_integration_the_trapezoid_method_mpi, Test_Integration_mpi_7) {
+TEST(lysov_i_integration_the_trapezoid_method_mpi, Test_Integration_mpi_4) {
   boost::mpi::communicator world;
   std::vector<double> global_result(1, 0.0);
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
@@ -189,6 +152,44 @@ TEST(lysov_i_integration_the_trapezoid_method_mpi, Test_Integration_mpi_7) {
     ASSERT_NEAR(reference_result[0], global_result[0], 1e-1);
   }
 }
+
+TEST(lysov_i_integration_the_trapezoid_method_mpi, Test_Integration_mpi_random) {
+  boost::mpi::communicator world;
+  std::vector<double> global_result(1, 0.0);
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+  std::random_device dev;
+  std::mt19937 gen(dev());
+  double a = (gen() % 100) / 100.0;
+  double b = (gen() % 100) / 100.0;
+  double epsilon = 0.0001;
+  if (world.rank() == 0) {
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&a));
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&b));
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&epsilon));
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_result.data()));
+  }
+  lysov_i_integration_the_trapezoid_method_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+    std::vector<double> reference_result(1, 0.0);
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&a));
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&b));
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&epsilon));
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_result.data()));
+    lysov_i_integration_the_trapezoid_method_mpi::TestMPITaskSequential testMpiTaskSequential(taskDataSeq);
+    ASSERT_EQ(testMpiTaskSequential.validation(), true);
+    testMpiTaskSequential.pre_processing();
+    testMpiTaskSequential.run();
+    testMpiTaskSequential.post_processing();
+    ASSERT_NEAR(reference_result[0], global_result[0], 1e-1);
+  }
+}
+
 
 TEST(lysov_i_integration_the_trapezoid_method_mpi, TaskMpi_InputSizeLessThan3) {
   std::shared_ptr<ppc::core::TaskData> taskDataMPIParallel = std::make_shared<ppc::core::TaskData>();
