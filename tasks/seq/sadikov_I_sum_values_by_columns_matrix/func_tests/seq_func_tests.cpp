c79517f5158a793fad9638f4d3ae25c39973cf8b
Иван Садиков <sadikov.04@list.ru>
2024-11-02 00:22:12 +0300

    Matrix task v 1.11

--- tasks/seq/sadikov_I_sum_values_by_columns_matrix/func_tests/seq_func_tests.cpp
+++ tasks/seq/sadikov_I_sum_values_by_columns_matrix/func_tests/seq_func_tests.cpp
@@ -90,22 +90,6 @@ TEST(sum_values_by_columns_matrix, check_square_matrix3) {
 }
 
 TEST(sum_values_by_columns_matrix, check_rect_matrix1) {
-  std::vector<int> in(200, 1);
-  std::vector<int> in_index{10, 20};
-  std::vector<int> out(20, 0);
-  std::shared_ptr<ppc::core::TaskData> taskData =
-      sadikov_I_Sum_values_by_columns_matrix_seq::CreateTaskData(in, in_index, out);
-  sadikov_I_Sum_values_by_columns_matrix_seq::MatrixTask sv(taskData);
-  ASSERT_EQ(sv.validation(), true);
-  sv.pre_processing();
-  sv.run();
-  sv.post_processing();
-  for (int i = 0; i < in_index[1]; ++i) {
-    ASSERT_EQ(out[i], in_index[0]);
-  }
-}
-
-TEST(sum_values_by_columns_matrix, check_rect_matrix2) {
   std::vector<int> in(500, 1);
   std::vector<int> in_index{50, 10};
   std::vector<int> out(10, 0);
@@ -121,7 +105,7 @@ TEST(sum_values_by_columns_matrix, check_rect_matrix2) {
   }
 }
 
-TEST(sum_values_by_columns_matrix, check_rect_matrix3) {
+TEST(sum_values_by_columns_matrix, check_rect_matrix2) {
   std::vector<int> in(10000, 1);
   std::vector<int> in_index{500, 20};
   std::vector<int> out(20, 0);
