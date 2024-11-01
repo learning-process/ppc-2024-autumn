35e48ed9747cb3a81bd79566dd6a980225531a5d
Aleksandra Matyunina <ERTY-00@yandex.ru>
2024-11-01 14:03:25 +0300

    starting

---
+++ tasks/seq/matyunina_a_average_of_vector_elements/src/ops_seq.cpp
@@ -0,0 +1,35 @@
+// Copyright 2024 Nesterov Alexander
+#include "seq/matyunina_a_average_of_vector_elements/include/ops_seq.hpp"
+
+#include <thread>
+
+using namespace std::chrono_literals;
+
+bool matyunina_a_average_of_vector_elements_seq::TestTaskSequential::pre_processing() {
+  internal_order_test();
+  input_ = std::vector<int>(taskData->inputs_count[0]);
+  auto tmp_ptr = reinterpret_cast<int*>(taskData->inputs[0]);
+  for (unsigned i = 0; i < taskData->inputs_count[0]; i++) {
+    input_[i] = tmp_ptr[i];
+  }
+  res_ = 0;
+  return true;
+}
+
+bool matyunina_a_average_of_vector_elements_seq::TestTaskSequential::validation() {
+  internal_order_test();
+  return taskData->inputs_count[0] > 0 && taskData->outputs_count[0] == 1;
+}
+
+bool matyunina_a_average_of_vector_elements_seq::TestTaskSequential::run() {
+  internal_order_test();
+  res_ = std::accumulate(input_.begin(), input_.end(), 0);
+  res_ /= static_cast<int>(input_.size());
+  return true;
+}
+
+bool matyunina_a_average_of_vector_elements_seq::TestTaskSequential::post_processing() {
+  internal_order_test();
+  reinterpret_cast<int*>(taskData->outputs[0])[0] = res_;
+  return true;
+}
