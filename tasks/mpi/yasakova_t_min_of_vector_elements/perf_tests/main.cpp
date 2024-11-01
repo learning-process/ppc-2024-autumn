ca39e841ec28522369108a14bb91b0fedbd31a0d
Tanya-Yasakova <yasakova_04@mail.ru>
2024-11-02 01:55:18 +0300

    clang-format

--- tasks/mpi/yasakova_t_min_of_vector_elements/perf_tests/main.cpp
+++ tasks/mpi/yasakova_t_min_of_vector_elements/perf_tests/main.cpp
@@ -26,7 +26,7 @@ TEST(yasakova_t_min_of_vector_elements_mpi, test_pipeline_run) {
     int gen_maximum = 500;
 
     global_matrix =
-        yasakova_t_min_of_vector_elements_mpi::RandomMatrix(count_rows, count_columns, gen_minimum, gen_maximum);
+       yasakova_t_min_of_vector_elements_seq::RandomMatrix(count_rows, count_columns, gen_minimum, gen_maximum);
     int index = gen() % (count_rows * count_columns);
     global_matrix[index / count_columns][index / count_rows] = ref;
 
@@ -77,7 +77,7 @@ TEST(yasakova_t_min_of_vector_elements_mpi, test_task_run) {
     int gen_maximum = 500;
 
     global_matrix =
-        yasakova_t_min_of_vector_elements_mpi::RandomMatrix(count_rows, count_columns, gen_minimum, gen_maximum);
+      yasakova_t_min_of_vector_elements_seq::RandomMatrix(count_rows, count_columns, gen_minimum, gen_maximum);
     int index = gen() % (count_rows * count_columns);
     global_matrix[index / count_columns][index / count_rows] = ref;
 
