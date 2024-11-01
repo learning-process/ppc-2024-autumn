96ce32fed73cc372a765490e2b20c70a2cd29c92
MaximChizhov <maxim070205@gmail.com>
2024-11-01 14:31:58 +0300

    add mpi test

--- tasks/mpi/chizhov_m_max_values_by_columns_matrix/func_tests/main.cpp
+++ tasks/mpi/chizhov_m_max_values_by_columns_matrix/func_tests/main.cpp
@@ -22,6 +22,36 @@ std::vector<int> getRandomVector(int sz) {
   return vec;
 }
 
+TEST(chizhov_m_max_values_by_columns_matrix_mpi, Test_Zero_Columns) {
+  boost::mpi::communicator world;
+
+  int cols = 0;
+  int rows = 0;
+
+  std::vector<int> matrix;
+  std::vector<int> res_par(cols, 0);
+
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    const int count_size_vector = cols * rows;
+    matrix = getRandomVector(count_size_vector);
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrix.data()));
+    taskDataPar->inputs_count.emplace_back(matrix.size());
+    taskDataPar->inputs_count.emplace_back(cols);
+    taskDataPar->inputs_count.emplace_back(rows);
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(res_par.data()));
+    taskDataPar->outputs_count.emplace_back(res_par.size());
+  }
+
+  chizhov_m_max_values_by_columns_matrix_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+
+  if (world.rank() == 0) {
+    ASSERT_FALSE(testMpiTaskParallel.validation());
+  }
+}
+
 TEST(chizhov_m_max_values_by_columns_matrix_mpi, Test_Empty_Matrix) {
   boost::mpi::communicator world;
 
