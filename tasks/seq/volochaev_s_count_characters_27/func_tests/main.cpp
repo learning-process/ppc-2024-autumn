1c650f229fcf8c5d57552a095790d290ba76afe7
Svolota <seraphimvolochaev@gmail.com>
2024-11-01 11:22:34 +0300

    fix comments 3

--- tasks/seq/volochaev_s_count_characters_27/func_tests/main.cpp
+++ tasks/seq/volochaev_s_count_characters_27/func_tests/main.cpp
@@ -130,3 +130,25 @@ TEST(volochaev_s_count_characters_27_seq, Test_5) {
   testTaskSequential.post_processing();
   ASSERT_EQ(ans, out[0]);
 }
+
+TEST(volochaev_s_count_characters_27_seq, Test_6) {
+  // Create data
+  std::vector<std::string> in = {"aaabbcс", "aaabbcс"};
+  std::vector<int> out(1, 0);
+  int ans = 0;
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  volochaev_s_count_characters_27_seq::Lab1_27 testTaskSequential(taskDataSeq);
+  ASSERT_EQ(testTaskSequential.validation(), true);
+  testTaskSequential.pre_processing();
+  testTaskSequential.run();
+  testTaskSequential.post_processing();
+  ASSERT_EQ(ans, out[0]);
+}
