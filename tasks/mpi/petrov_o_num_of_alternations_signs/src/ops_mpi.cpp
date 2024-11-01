e938cf9cefe9a935e0b28c4558fe6c2aac3c663c
ValeraDanger <oleg.petrov.03@list.ru>
2024-11-01 18:17:39 +0300

    Try to fix clang-tidy error

--- tasks/mpi/petrov_o_num_of_alternations_signs/src/ops_mpi.cpp
+++ tasks/mpi/petrov_o_num_of_alternations_signs/src/ops_mpi.cpp
@@ -54,7 +54,8 @@ bool petrov_o_num_of_alternations_signs_mpi::ParallelTask::pre_processing() {
     int distribution = chunk_size + static_cast<int>(world.rank() < remainder);
 
     chunk.resize(distribution);  // Зарезервируем необходимое место под данные
-    boost::mpi::scatterv(world, chunk.data(), distribution, 0);
+    // boost::mpi::scatterv(world, chunk.data(), distribution, 0);
+    boost::mpi::scatterv(world, (int*)nullptr, chunk.data(), distribution, 0);
   }
 
   this->res = 0;
