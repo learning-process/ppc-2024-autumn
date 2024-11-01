9bdad03e741c09e46a9e6cdbf61bee1a0e1d403c
maximchhek <max.slykov@yandex.ru>
2024-11-02 01:18:39 +0300

    clang again x7

--- tasks/seq/shlyakov_m_min_value_of_row/src/ops_seq.cpp
+++ tasks/seq/shlyakov_m_min_value_of_row/src/ops_seq.cpp
@@ -26,7 +26,7 @@ bool shlyakov_m_min_value_of_row_seq::TestTaskSequential::pre_processing() {
 
 bool shlyakov_m_min_value_of_row_seq::TestTaskSequential::validation() {
   internal_order_test();
-  bool flag = 1;
+  bool flag = true;
 
   if (flag ==
       ((!taskData->inputs.empty() && !taskData->outputs.empty()) &&
