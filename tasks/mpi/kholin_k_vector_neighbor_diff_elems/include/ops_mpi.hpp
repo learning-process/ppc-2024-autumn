dfbbff86cb6ee25cccb297a8d2547244fe5d6fd8
Kirius257 <holin.kirill@yandex.ru>
2024-11-01 19:16:44 +0300

    fix

--- tasks/mpi/kholin_k_vector_neighbor_diff_elems/include/ops_mpi.hpp
+++ tasks/mpi/kholin_k_vector_neighbor_diff_elems/include/ops_mpi.hpp
@@ -31,12 +31,12 @@ std::vector<TypeElem> get_random_vector(int sz) {
   std::vector<TypeElem> vec(sz);
 
   if (std::is_integral<TypeElem>::value) {
-    std::uniform_int_distribution<int> dist(0, 99);
+    std::uniform_int_distribution<int> dist(1, 99);
     for (int i = 0; i < sz; i++) {
       vec[i] = dist(gen);
     }
   } else if (std::is_floating_point<TypeElem>::value) {
-    std::uniform_real_distribution<float> dist(0.0, 99.0);
+    std::uniform_real_distribution<float> dist(1.0, 99.0);
     for (int i = 0; i < sz; i++) {
       vec[i] = dist(gen);
     }
