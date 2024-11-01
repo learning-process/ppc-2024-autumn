3e089441cbbe28d6063feb5206906e9e55103819
Sozonov_Ilushka <113029719+sozozzya@users.noreply.github.com>
2024-11-01 11:51:05 +0300

    change tests for review

--- tasks/mpi/sozonov_i_nearest_neighbor_elements/func_tests/main.cpp
+++ tasks/mpi/sozonov_i_nearest_neighbor_elements/func_tests/main.cpp
@@ -6,7 +6,133 @@
 
 #include "mpi/sozonov_i_nearest_neighbor_elements/include/ops_mpi.hpp"
 
-TEST(sozonov_i_nearest_neighbor_elements_mpi, test_nearest_neighbor_elements_100) {
+TEST(sozonov_i_nearest_neighbor_elements_mpi, test_for_empty_vector) {
+  boost::mpi::communicator world;
+  std::vector<int> global_vec;
+  std::vector<int32_t> global_ans(2, 0);
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataPar->inputs_count.emplace_back(global_vec.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_ans.data()));
+    taskDataPar->outputs_count.emplace_back(global_ans.size());
+  }
+
+  sozonov_i_nearest_neighbor_elements_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+  ASSERT_FALSE(testMpiTaskParallel.validation());
+}
+
+TEST(sozonov_i_nearest_neighbor_elements_mpi, test_on_100_elements) {
+  boost::mpi::communicator world;
+  std::vector<int> global_vec;
+  std::vector<int32_t> global_ans(2, 0);
+  std::vector<int> ans(2, 1);
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    const int count_size_vector = 100;
+    global_vec = std::vector<int>(count_size_vector);
+    std::iota(global_vec.begin(), global_vec.end(), 0);
+    global_vec[0] = 1;
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataPar->inputs_count.emplace_back(global_vec.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_ans.data()));
+    taskDataPar->outputs_count.emplace_back(global_ans.size());
+  }
+
+  sozonov_i_nearest_neighbor_elements_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+  ASSERT_EQ(ans, global_ans);
+}
+
+TEST(sozonov_i_nearest_neighbor_elements_mpi, test_on_200_elements) {
+  boost::mpi::communicator world;
+  std::vector<int> global_vec;
+  std::vector<int32_t> global_ans(2, 0);
+  std::vector<int> ans(2, 1);
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    const int count_size_vector = 200;
+    global_vec = std::vector<int>(count_size_vector);
+    std::iota(global_vec.begin(), global_vec.end(), 0);
+    global_vec[0] = 1;
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataPar->inputs_count.emplace_back(global_vec.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_ans.data()));
+    taskDataPar->outputs_count.emplace_back(global_ans.size());
+  }
+
+  sozonov_i_nearest_neighbor_elements_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+  ASSERT_EQ(ans, global_ans);
+}
+
+TEST(sozonov_i_nearest_neighbor_elements_mpi, test_on_500_elements) {
+  boost::mpi::communicator world;
+  std::vector<int> global_vec;
+  std::vector<int32_t> global_ans(2, 0);
+  std::vector<int> ans(2, 1);
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    const int count_size_vector = 500;
+    global_vec = std::vector<int>(count_size_vector);
+    std::iota(global_vec.begin(), global_vec.end(), 0);
+    global_vec[0] = 1;
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataPar->inputs_count.emplace_back(global_vec.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_ans.data()));
+    taskDataPar->outputs_count.emplace_back(global_ans.size());
+  }
+
+  sozonov_i_nearest_neighbor_elements_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+  ASSERT_EQ(ans, global_ans);
+}
+
+TEST(sozonov_i_nearest_neighbor_elements_mpi, test_on_1000_elements) {
+  boost::mpi::communicator world;
+  std::vector<int> global_vec;
+  std::vector<int32_t> global_ans(2, 0);
+  std::vector<int> ans(2, 1);
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    const int count_size_vector = 1000;
+    global_vec = std::vector<int>(count_size_vector);
+    std::iota(global_vec.begin(), global_vec.end(), 0);
+    global_vec[0] = 1;
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataPar->inputs_count.emplace_back(global_vec.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_ans.data()));
+    taskDataPar->outputs_count.emplace_back(global_ans.size());
+  }
+
+  sozonov_i_nearest_neighbor_elements_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+  ASSERT_EQ(ans, global_ans);
+}
+
+TEST(sozonov_i_nearest_neighbor_elements_mpi, test_random_on_100_elements) {
   boost::mpi::communicator world;
   std::vector<int> global_vec;
   std::vector<int32_t> global_ans(2, 0);
@@ -50,7 +176,7 @@ TEST(sozonov_i_nearest_neighbor_elements_mpi, test_nearest_neighbor_elements_100
   }
 }
 
-TEST(sozonov_i_nearest_neighbor_elements_mpi, test_nearest_neighbor_elements_200) {
+TEST(sozonov_i_nearest_neighbor_elements_mpi, test_random_on_200_elements) {
   boost::mpi::communicator world;
   std::vector<int> global_vec;
   std::vector<int32_t> global_ans(2, 0);
@@ -94,7 +220,7 @@ TEST(sozonov_i_nearest_neighbor_elements_mpi, test_nearest_neighbor_elements_200
   }
 }
 
-TEST(sozonov_i_nearest_neighbor_elements_mpi, test_nearest_neighbor_elements_500) {
+TEST(sozonov_i_nearest_neighbor_elements_mpi, test_random_on_500_elements) {
   boost::mpi::communicator world;
   std::vector<int> global_vec;
   std::vector<int32_t> global_ans(2, 0);
@@ -138,7 +264,7 @@ TEST(sozonov_i_nearest_neighbor_elements_mpi, test_nearest_neighbor_elements_500
   }
 }
 
-TEST(sozonov_i_nearest_neighbor_elements_mpi, test_nearest_neighbor_elements_1000) {
+TEST(sozonov_i_nearest_neighbor_elements_mpi, test_random_on_1000_elements) {
   boost::mpi::communicator world;
   std::vector<int> global_vec;
   std::vector<int32_t> global_ans(2, 0);
