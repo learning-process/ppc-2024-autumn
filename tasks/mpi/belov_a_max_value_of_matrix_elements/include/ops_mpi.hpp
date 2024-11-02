1845d13558ff8644028d09fb34ac7f7938d5399b
belovtut <artem.belov2004@yandex.ru>
2024-11-03 02:01:35 +0300

    task completed

---
+++ tasks/mpi/belov_a_max_value_of_matrix_elements/include/ops_mpi.hpp
@@ -0,0 +1,228 @@
+#ifndef OPS_MPI_HPP
+#define OPS_MPI_HPP
+
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <boost/mpi.hpp>
+#include <boost/mpi/collectives.hpp>
+#include <boost/mpi/communicator.hpp>
+#include <iostream>
+#include <limits>
+#include <memory>
+#include <random>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace belov_a_max_value_of_matrix_elements_mpi {
+
+template <typename T>
+class MaxValueOfMatrixElementsParallel : public ppc::core::Task {
+ public:
+  explicit MaxValueOfMatrixElementsParallel(std::shared_ptr<ppc::core::TaskData> taskData_)
+      : Task(std::move(taskData_)) {}
+
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+  static std::vector<T> generate_random_matrix(int rows, int cols);
+
+ private:
+  boost::mpi::communicator world;
+
+  int rows_ = 0;
+  int cols_ = 0;
+
+  T global_max_{};
+  T local_max_ = std::numeric_limits<T>::lowest();
+  std::vector<T> matrix;
+
+  static T get_max_matrix_element(const std::vector<T>& matrix);
+};
+
+template <typename T>
+T MaxValueOfMatrixElementsParallel<T>::get_max_matrix_element(const std::vector<T>& matrix) {
+  return matrix.empty() ? 0 : *std::max_element(matrix.begin(), matrix.end());
+}
+
+template <typename T>
+std::vector<T> MaxValueOfMatrixElementsParallel<T>::generate_random_matrix(int rows, int cols) {
+  std::vector<T> res(rows * cols);
+
+  std::random_device dev;
+  std::mt19937 gen(dev());
+  for (size_t i = 0; i < res.size(); i++) {
+    res[i] = static_cast<T>(gen() % 1000);
+  }
+  return res;
+}
+
+template <typename T>
+bool MaxValueOfMatrixElementsParallel<T>::pre_processing() {
+  internal_order_test();
+
+  if (world.rank() == 0) {
+    auto* dimensions = reinterpret_cast<int*>(taskData->inputs[0]);
+    rows_ = dimensions[0];
+    cols_ = dimensions[1];
+
+    if (rows_ <= 0 || cols_ <= 0) {
+      // std::cerr << "Error: Matrix dimensions must be positive." << std::endl;
+      return false;
+    }
+
+    auto inputMatrixData = reinterpret_cast<T*>(taskData->inputs[1]);
+    matrix.assign(inputMatrixData, inputMatrixData + rows_ * cols_);
+  }
+
+  return true;
+}
+
+template <typename T>
+bool MaxValueOfMatrixElementsParallel<T>::validation() {
+  internal_order_test();
+
+  if (world.rank() == 0) {
+    if (taskData->inputs.empty() || taskData->outputs.empty()) {
+      // std::cerr << "Validation error: Missing input or output data." << std::endl;
+      return false;
+    }
+
+    auto* dimensions = reinterpret_cast<int*>(taskData->inputs[0]);
+    if (dimensions[0] <= 0 || dimensions[1] <= 0) {
+      // std::cerr << "Validation error: Matrix dimensions must be positive." << std::endl;
+      return false;
+    }
+  }
+
+  return true;
+}
+
+template <typename T>
+bool MaxValueOfMatrixElementsParallel<T>::run() {
+  internal_order_test();
+
+  int rank = world.rank();
+  int size = world.size();
+
+  boost::mpi::broadcast(world, rows_, 0);
+  boost::mpi::broadcast(world, cols_, 0);
+
+  int delta = rows_ * cols_ / size;
+  int remainder = rows_ * cols_ % size;
+
+  std::vector<int> distr(size, delta);
+  std::vector<int> displ(size, 0);
+
+  for (int i = 0; i < remainder; ++distr[i], ++i)
+    ;
+  for (int i = 1; i < size; ++i) {
+    displ[i] = displ[i - 1] + distr[i - 1];
+  }
+
+  std::vector<T> local_matrix(distr[rank]);
+
+  if (rank == 0) {
+    boost::mpi::scatterv(world, matrix, distr, displ, local_matrix.data(), distr[0], 0);
+  } else {
+    boost::mpi::scatterv(world, local_matrix.data(), distr[rank], 0);
+  }
+
+  local_max_ = get_max_matrix_element(local_matrix);
+  boost::mpi::reduce(world, local_max_, global_max_, boost::mpi::maximum<T>(), 0);
+
+  return true;
+}
+
+template <typename T>
+bool MaxValueOfMatrixElementsParallel<T>::post_processing() {
+  internal_order_test();
+
+  if (world.rank() == 0) {
+    reinterpret_cast<T*>(taskData->outputs[0])[0] = global_max_;
+  }
+
+  return true;
+}
+
+//////////
+
+template <typename T>
+class MaxValueOfMatrixElementsSequential : public ppc::core::Task {
+ public:
+  explicit MaxValueOfMatrixElementsSequential(std::shared_ptr<ppc::core::TaskData> taskData_)
+      : Task(std::move(taskData_)) {}
+
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  int rows_ = 0;
+  int cols_ = 0;
+  T res{};
+  std::vector<T> matrix;
+
+  static T get_max_matrix_element(const std::vector<T>& matrix);
+};
+
+template <typename T>
+T MaxValueOfMatrixElementsSequential<T>::get_max_matrix_element(const std::vector<T>& matrix) {
+  return *std::max_element(matrix.begin(), matrix.end());
+}
+
+template <typename T>
+bool MaxValueOfMatrixElementsSequential<T>::pre_processing() {
+  internal_order_test();
+
+  auto* dimensions = reinterpret_cast<int*>(taskData->inputs[0]);
+  rows_ = dimensions[0];
+  cols_ = dimensions[1];
+
+  if (rows_ <= 0 || cols_ <= 0) {
+    // std::cerr << "Error: Matrix dimensions must be positive." << std::endl;
+    return false;
+  }
+
+  auto inputMatrixData = reinterpret_cast<T*>(taskData->inputs[1]);
+  matrix.assign(inputMatrixData, inputMatrixData + rows_ * cols_);
+
+  return true;
+}
+
+template <typename T>
+bool MaxValueOfMatrixElementsSequential<T>::validation() {
+  internal_order_test();
+  if (taskData->inputs.empty() || taskData->outputs.empty()) {
+    // std::cerr << "Validation error: Missing input or output data." << std::endl;
+    return false;
+  }
+  return true;
+}
+
+template <typename T>
+bool MaxValueOfMatrixElementsSequential<T>::run() {
+  internal_order_test();
+
+  res = get_max_matrix_element(matrix);
+
+  auto outputData = reinterpret_cast<T*>(taskData->outputs[0]);
+  outputData[0] = res;
+
+  return true;
+}
+
+template <typename T>
+bool MaxValueOfMatrixElementsSequential<T>::post_processing() {
+  internal_order_test();
+  // std::cout << "Maximum value in the matrix: " << res << std::endl;
+  return true;
+}
+
+}  // namespace belov_a_max_value_of_matrix_elements_mpi
+
+#endif  // OPS_MPI_HPP
---
+++ tasks/seq/belov_a_max_value_of_matrix_elements/include/ops_seq.hpp
@@ -0,0 +1,103 @@
+#ifndef OPS_SEQ_HPP
+#define OPS_SEQ_HPP
+
+#include <algorithm>
+#include <iostream>
+#include <memory>
+#include <random>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace belov_a_max_value_of_matrix_elements_seq {
+
+template <typename T>
+class MaxValueOfMatrixElementsSequential : public ppc::core::Task {
+ public:
+  explicit MaxValueOfMatrixElementsSequential(std::shared_ptr<ppc::core::TaskData> taskData_)
+      : Task(std::move(taskData_)) {}
+
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+  static std::vector<T> generate_random_matrix(int rows, int cols);
+
+ private:
+  int rows_ = 0;
+  int cols_ = 0;
+  T res{};
+  std::vector<T> matrix;
+
+  static T get_max_matrix_element(const std::vector<T>& matrix);
+};
+
+template <typename T>
+T MaxValueOfMatrixElementsSequential<T>::get_max_matrix_element(const std::vector<T>& matrix) {
+  return *std::max_element(matrix.begin(), matrix.end());
+}
+
+template <typename T>
+std::vector<T> MaxValueOfMatrixElementsSequential<T>::generate_random_matrix(int rows, int cols) {
+  std::vector<T> res(rows * cols);
+
+  std::random_device dev;
+  std::mt19937 gen(dev());
+  for (size_t i = 0; i < res.size(); i++) {
+    res[i] = static_cast<T>(gen() % 1000);
+  }
+  return res;
+}
+
+template <typename T>
+bool MaxValueOfMatrixElementsSequential<T>::pre_processing() {
+  internal_order_test();
+
+  auto dimensions = reinterpret_cast<int*>(taskData->inputs[0]);
+  rows_ = dimensions[0];
+  cols_ = dimensions[1];
+
+  if (rows_ <= 0 || cols_ <= 0) {
+    std::cerr << "Error: Matrix dimensions must be positive." << std::endl;
+    return false;
+  }
+
+  auto inputMatrixData = reinterpret_cast<T*>(taskData->inputs[1]);
+  matrix.assign(inputMatrixData, inputMatrixData + rows_ * cols_);
+
+  return true;
+}
+
+template <typename T>
+bool MaxValueOfMatrixElementsSequential<T>::validation() {
+  internal_order_test();
+  if (taskData->inputs.empty() || taskData->outputs.empty()) {
+    std::cerr << "Validation error: Missing input or output data." << std::endl;
+    return false;
+  }
+  return true;
+}
+
+template <typename T>
+bool MaxValueOfMatrixElementsSequential<T>::run() {
+  internal_order_test();
+
+  res = get_max_matrix_element(matrix);
+
+  auto outputData = reinterpret_cast<T*>(taskData->outputs[0]);
+  outputData[0] = res;
+
+  return true;
+}
+
+template <typename T>
+bool MaxValueOfMatrixElementsSequential<T>::post_processing() {
+  internal_order_test();
+  // std::cout << "Maximum value in the matrix: " << res << std::endl;
+  return true;
+}
+
+}  // namespace belov_a_max_value_of_matrix_elements_seq
+
+#endif  // OPS_SEQ_HPP
