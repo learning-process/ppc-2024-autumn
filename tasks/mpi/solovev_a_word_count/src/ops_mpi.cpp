6d597c7803d87d3458bd066fbe98530af93689b3
Alexey-Solovev <149035736+Alexey-Solovev@users.noreply.github.com>
2024-11-02 23:48:18 +0300

    fix ops_mpi.cpp

--- tasks/mpi/solovev_a_word_count/src/ops_mpi.cpp
+++ tasks/mpi/solovev_a_word_count/src/ops_mpi.cpp
@@ -7,27 +7,11 @@
 
 namespace solovev_a_word_count_mpi {
 
-std::vector<char> create_text(int quan_words) {
-  std::vector<char> res;
-  std::string word = "word ";
-  std::string last = "word.";
-  for (int i = 0; i < quan_words - 1; i++)
-    for (unsigned long int symbol = 0; symbol < word.length(); symbol++) {
-      res.push_back(word[symbol]);
-    }
-  for (unsigned long int symbol = 0; symbol < last.length(); symbol++) {
-    res.push_back(last[symbol]);
-  }
-  return res;
-}
-
 bool solovev_a_word_count_mpi::TestMPITaskSequential::pre_processing() {
   internal_order_test();
   input_ = std::vector<char>(taskData->inputs_count[0]);
   auto* tmp_ptr = reinterpret_cast<char*>(taskData->inputs[0]);
-  for (unsigned i = 0; i < taskData->inputs_count[0]; i++) {
-    input_[i] = tmp_ptr[i];
-  }
+  std::copy(tmp_ptr, tmp_ptr + taskData->inputs_count[0], input_.begin());
   res = 0;
   return true;
 }
@@ -40,8 +24,7 @@ bool solovev_a_word_count_mpi::TestMPITaskSequential::validation() {
 bool solovev_a_word_count_mpi::TestMPITaskSequential::run() {
   internal_order_test();
   for (char symbol : input_) {
-    if (symbol != ' ' && symbol != '.') {
-    } else {
+    if (symbol == ' ' || symbol == '.') {
       res++;
     }
   }
@@ -58,9 +41,7 @@ bool solovev_a_word_count_mpi::TestMPITaskParallel::pre_processing() {
   if (world.rank() == 0) {
     input_ = std ::vector<char>(taskData->inputs_count[0]);
     auto* tmp_ptr = reinterpret_cast<char*>(taskData->inputs[0]);
-    for (unsigned long int i = 0; i < taskData->inputs_count[0]; i++) {
-      input_[i] = tmp_ptr[i];
-    }
+    std::copy(tmp_ptr, tmp_ptr + taskData->inputs_count[0], input_.begin());
   }
   res = 0;
   l_res = 0;
@@ -94,8 +75,7 @@ bool solovev_a_word_count_mpi::TestMPITaskParallel::run() {
     world.recv(0, 0, l_input_.data(), delta);
   }
   for (char symbol : input_) {
-    if (symbol != ' ' && symbol != '.') {
-    } else {
+    if (symbol == ' ' || symbol == '.') {
       l_res++;
     }
   }
