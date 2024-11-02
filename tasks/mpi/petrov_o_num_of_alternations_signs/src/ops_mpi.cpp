994133f0e61e95b9b15c7914bb900e5bb52fe6e1
ValeraDanger <oleg.petrov.03@list.ru>
2024-11-02 20:42:54 +0300

    Fixed clang

--- tasks/mpi/petrov_o_num_of_alternations_signs/src/ops_mpi.cpp
+++ tasks/mpi/petrov_o_num_of_alternations_signs/src/ops_mpi.cpp
@@ -20,7 +20,8 @@ bool petrov_o_num_of_alternations_signs_mpi::ParallelTask::validation() {
     input_size = taskData->inputs_count[0];
   }
 
-  boost::mpi::broadcast(world, input_size, 0);  //Без broadcast не получается сделать проверку на избыточное число процессов
+  boost::mpi::broadcast(world, input_size,
+                        0);  // Без broadcast не получается сделать проверку на избыточное число процессов
 
   if (input_size < world.size()) {
     return false;
