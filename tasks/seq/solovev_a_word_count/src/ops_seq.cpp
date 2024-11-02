2483bcad06e0c79d456eb0a8c93d91f253aaad2c
Alexey-Solovev <149035736+Alexey-Solovev@users.noreply.github.com>
2024-11-03 00:13:48 +0300

    fix ops_seq.cpp

--- tasks/seq/solovev_a_word_count/src/ops_seq.cpp
+++ tasks/seq/solovev_a_word_count/src/ops_seq.cpp
@@ -2,27 +2,11 @@
 
 namespace solovev_a_word_count_seq {
 
-std::vector<char> create_text(int quan_words) {
-  std::vector<char> res;
-  std::string word = "word ";
-  std::string last = "word.";
-  for (int i = 0; i < quan_words - 1; i++)
-    for (unsigned long int symbol = 0; symbol < word.length(); symbol++) {
-      res.push_back(word[symbol]);
-    }
-  for (unsigned long int symbol = 0; symbol < last.length(); symbol++) {
-    res.push_back(last[symbol]);
-  }
-  return res;
-}
-
 bool solovev_a_word_count_seq::TestTaskSequential::pre_processing() {
   internal_order_test();
   input_ = std::vector<char>(taskData->inputs_count[0]);
   auto* tmp_ptr = reinterpret_cast<char*>(taskData->inputs[0]);
-  for (unsigned i = 0; i < taskData->inputs_count[0]; i++) {
-    input_[i] = tmp_ptr[i];
-  }
+  std::copy(tmp_ptr, tmp_ptr + taskData->inputs_count[0], input_.begin());
   res = 0;
   return true;
 }
@@ -35,8 +19,7 @@ bool solovev_a_word_count_seq::TestTaskSequential::validation() {
 bool solovev_a_word_count_seq::TestTaskSequential::run() {
   internal_order_test();
   for (char symbol : input_) {
-    if (symbol != ' ' && symbol != '.') {
-    } else {
+    if (symbol == ' ' || symbol == '.') {
       res++;
     }
   }
