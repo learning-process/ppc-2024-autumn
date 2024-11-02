bc3f4d6071d7763232eccef5ef0fbd253d0d20e5
SingularityNN <mihamakhov@gmail.com>
2024-11-02 20:29:10 +0300

    all seq

---
+++ tasks/seq/makhov_m_num_of_diff_elements_in_two_str/func_tests/main.cpp
@@ -0,0 +1,147 @@
+// Copyright 2023 Nesterov Alexander
+#include <gtest/gtest.h>
+
+#include <vector>
+
+#include "seq/makhov_m_num_of_diff_elements_in_two_str/include/ops_seq.hpp"
+
+TEST(makhov_m_num_of_diff_elements_in_two_str_seq, SameEmptySymbolsStrings) {
+  std::string str1, str2;
+
+  // Create data
+  str1 = "   ";
+  str2 = str1;
+  std::vector<int> out(1, 0);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(str2.data()));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(str1.data()));
+  taskDataSeq->inputs_count.emplace_back(str2.size());
+  taskDataSeq->inputs_count.emplace_back(str1.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  int ref = 0;
+  makhov_m_num_of_diff_elements_in_two_str_seq::TestTaskSequential testTaskmakhov_m_num_of_diff_elements_in_two_str_seq(
+      taskDataSeq);
+  ASSERT_EQ(testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.validation(), true);
+  testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.pre_processing();
+  testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.run();
+  testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.post_processing();
+  ASSERT_EQ(ref, out[0]);
+}
+
+TEST(makhov_m_num_of_diff_elements_in_two_str_seq, DiffSizeEmptySymbolsStrings) {
+  std::string str1, str2;
+
+  // Create data
+  str1 = "   ";
+  str2 = "      ";
+  std::vector<int> out(1, 0);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(str2.data()));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(str1.data()));
+  taskDataSeq->inputs_count.emplace_back(str2.size());
+  taskDataSeq->inputs_count.emplace_back(str1.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  int ref = 3;
+  makhov_m_num_of_diff_elements_in_two_str_seq::TestTaskSequential testTaskmakhov_m_num_of_diff_elements_in_two_str_seq(
+      taskDataSeq);
+  ASSERT_EQ(testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.validation(), true);
+  testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.pre_processing();
+  testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.run();
+  testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.post_processing();
+  ASSERT_EQ(ref, out[0]);
+}
+
+TEST(makhov_m_num_of_diff_elements_in_two_str_seq, EqualSizeDiffStrings) {
+  std::string str1, str2;
+
+  // Create data
+  str1 = "Hello, World!!";
+  str2 = "Goodbye, World";
+  std::vector<int> out(1, 0);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(str2.data()));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(str1.data()));
+  taskDataSeq->inputs_count.emplace_back(str2.size());
+  taskDataSeq->inputs_count.emplace_back(str1.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  int ref = str1.size();
+  makhov_m_num_of_diff_elements_in_two_str_seq::TestTaskSequential testTaskmakhov_m_num_of_diff_elements_in_two_str_seq(
+      taskDataSeq);
+  ASSERT_EQ(testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.validation(), true);
+  testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.pre_processing();
+  testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.run();
+  testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.post_processing();
+  ASSERT_EQ(ref, out[0]);
+}
+
+TEST(makhov_m_num_of_diff_elements_in_two_str_seq, DiffSizeDiffStrings) {
+  std::string str1, str2;
+
+  // Create data
+  str1 = "Hello, World!";
+  str2 = "Goodbye";
+  std::vector<int> out(1, 0);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(str2.data()));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(str1.data()));
+  taskDataSeq->inputs_count.emplace_back(str2.size());
+  taskDataSeq->inputs_count.emplace_back(str1.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  int ref = str1.size();
+  makhov_m_num_of_diff_elements_in_two_str_seq::TestTaskSequential testTaskmakhov_m_num_of_diff_elements_in_two_str_seq(
+      taskDataSeq);
+  ASSERT_EQ(testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.validation(), true);
+  testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.pre_processing();
+  testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.run();
+  testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.post_processing();
+  ASSERT_EQ(ref, out[0]);
+}
+
+TEST(makhov_m_num_of_diff_elements_in_two_str_seq, SameStrings) {
+  std::string str1, str2;
+
+  // Create data
+  str1 = "Hello, World!";
+  str2 = str1;
+  std::vector<int> out(1, 0);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(str2.data()));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(str1.data()));
+  taskDataSeq->inputs_count.emplace_back(str2.size());
+  taskDataSeq->inputs_count.emplace_back(str1.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  int ref = 0;
+  makhov_m_num_of_diff_elements_in_two_str_seq::TestTaskSequential testTaskmakhov_m_num_of_diff_elements_in_two_str_seq(
+      taskDataSeq);
+  ASSERT_EQ(testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.validation(), true);
+  testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.pre_processing();
+  testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.run();
+  testTaskmakhov_m_num_of_diff_elements_in_two_str_seq.post_processing();
+  ASSERT_EQ(ref, out[0]);
+}
+
---
+++ tasks/seq/makhov_m_num_of_diff_elements_in_two_str/include/ops_seq.hpp
@@ -0,0 +1,28 @@
+// Copyright 2023 Nesterov Alexander
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace makhov_m_num_of_diff_elements_in_two_str_seq {
+
+int countDiffElem(std::string str1_, std::string str2_);
+std::string getRandStr(size_t size_, char min, char max);
+
+class TestTaskSequential : public ppc::core::Task {
+ public:
+  explicit TestTaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool validation() override;
+  bool pre_processing() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  std::string str1{};
+  std::string str2{};
+  int res{};
+};
+
+}  // namespace makhov_m_num_of_diff_elements_in_two_str_seq
