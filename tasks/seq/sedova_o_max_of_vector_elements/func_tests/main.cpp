f47aeec7d65e6f37f249f1528f567010c8dee326
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2024-11-02 16:28:56 +0300

    fix8

--- tasks/seq/sedova_o_max_of_vector_elements/func_tests/main.cpp
+++ tasks/seq/sedova_o_max_of_vector_elements/func_tests/main.cpp
@@ -41,22 +41,6 @@ TEST(sedova_o_max_of_vector_elements_seq, Test_Sum_Empty) {
   ASSERT_EQ(testTaskSequential.validation(), false);
 }
 
-TEST(sedova_o_max_of_vector_elements_seq, Test_Sum_Output_Incorrect) {
-  int count = 10;
-  // Create data
-  std::vector<int> in(count, 0);
-  std::vector<int> out(2, 0);  // Изменен размер выходного вектора
-  // Create TaskData
-  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
-  taskDataSeq->inputs_count.emplace_back(in.size());
-  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-  taskDataSeq->outputs_count.emplace_back(out.size());
-  // Create Task
-  sedova_o_max_of_vector_elements_seq::TestTaskSequential testTaskSequential(taskDataSeq);
-  ASSERT_EQ(testTaskSequential.validation(), false);
-}
-
 TEST(sedova_o_max_of_vector_elements_seq, Test_Sum_Input_Incorrect) {
   int count = 10;
   // Create data
