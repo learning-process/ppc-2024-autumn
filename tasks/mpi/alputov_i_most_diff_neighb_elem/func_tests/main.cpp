a11e5ef29d7fd8fdd89c6b874057d19823a03667
Ivan Alputov <i.alputov@yadro.com>
2024-11-02 17:34:55 +0300

    help8

--- tasks/mpi/alputov_i_most_diff_neighb_elem/func_tests/main.cpp
+++ tasks/mpi/alputov_i_most_diff_neighb_elem/func_tests/main.cpp
@@ -112,6 +112,63 @@ TEST(alputov_i_most_diff_neighb_elem_mpi, Test_MaxDiff_LargeVector_MPI_with_1000
     ASSERT_EQ(inputVector[index + 1], outputPair[1]);
   }
 }
+TEST(alputov_i_most_diff_neighb_elem_mpi, Test_MaxDiff_Vector_MPI_with_777_elem) {
+  boost::mpi::communicator world;
+  std::vector<int> inputVector(777);
+  int outputPair[2] = {};
+
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    inputVector = alputov_i_most_diff_neighb_elem_mpi::RandomVector(777);
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(inputVector.data()));
+    taskDataPar->inputs_count.emplace_back(inputVector.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(outputPair));
+    taskDataPar->outputs_count.emplace_back(2);
+  }
+
+  alputov_i_most_diff_neighb_elem_mpi::MPIParallelTask testMpiTaskParallel(taskDataPar);
+  ASSERT_TRUE(testMpiTaskParallel.validation());
+  ASSERT_TRUE(testMpiTaskParallel.pre_processing());
+  ASSERT_TRUE(testMpiTaskParallel.run());
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+    int index = alputov_i_most_diff_neighb_elem_mpi::Max_Neighbour_Seq_Pos(inputVector);
+    ASSERT_EQ(inputVector[index], outputPair[0]);
+    ASSERT_EQ(inputVector[index + 1], outputPair[1]);
+  }
+}
+TEST(alputov_i_most_diff_neighb_elem_mpi, Test_MaxDiff_RandomVector_MPI_100k_elem) {
+  boost::mpi::communicator world;
+  std::vector<int> inputVector(100000);
+  int outputPair[2] = {};
+
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    inputVector = alputov_i_most_diff_neighb_elem_mpi::RandomVector(100000);
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(inputVector.data()));
+    taskDataPar->inputs_count.emplace_back(inputVector.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(outputPair));
+    taskDataPar->outputs_count.emplace_back(2);
+  }
+
+  alputov_i_most_diff_neighb_elem_mpi::MPIParallelTask testMpiTaskParallel(taskDataPar);
+  ASSERT_TRUE(testMpiTaskParallel.validation());
+  ASSERT_TRUE(testMpiTaskParallel.pre_processing());
+  ASSERT_TRUE(testMpiTaskParallel.run());
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+    int index = alputov_i_most_diff_neighb_elem_mpi::Max_Neighbour_Seq_Pos(inputVector);
+    ASSERT_EQ(inputVector[index], outputPair[0]);
+    ASSERT_EQ(inputVector[index + 1], outputPair[1]);
+  }
+}
+/*
 /* TEST(alputov_i_most_diff_neighb_elem_mpi, Test_MaxDiff_RandomLargeVector_MPI_1_million_elem) {
   boost::mpi::communicator world;
   std::vector<int> inputVector(1000000);
