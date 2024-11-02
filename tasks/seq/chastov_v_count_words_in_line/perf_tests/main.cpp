fa19d9e09afc724c40f03b9c4d3262c3b314b020
Z ECNFK <crychs142@gmail.com>
2024-11-03 01:41:43 +0300

    add my task

---
+++ tasks/seq/chastov_v_count_words_in_line/perf_tests/main.cpp
@@ -0,0 +1,80 @@
+#include <gtest/gtest.h>
+
+#include "core/perf/include/perf.hpp"
+#include "seq/chastov_v_count_words_in_line/include/ops_seq.hpp"
+
+std::vector<char> wordCountInput = chastov_v_count_words_in_line_seq::createString(1000);
+
+TEST(word_count_seq, test_pipeline_run) {
+
+  // Create data
+  std::vector<char> input = wordCountInput;
+  std::vector<int> word_count(1, 0);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskData = std::make_shared<ppc::core::TaskData>();
+  taskData->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
+  taskData->inputs_count.emplace_back(input.size());
+  taskData->outputs.emplace_back(reinterpret_cast<uint8_t *>(word_count.data()));
+  taskData->outputs_count.emplace_back(word_count.size());
+
+  // Create Task
+  auto testTask = std::make_shared<chastov_v_count_words_in_line_seq::TestTaskSequential>(taskData);
+
+  // Create Perf attributes
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 1000;
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perfAttr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  // Create and init perf results
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
+  // Create Perf analyzer
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTask);
+  perfAnalyzer->pipeline_run(perfAttr, perfResults);
+  ppc::core::Perf::print_perf_statistic(perfResults);
+
+  ASSERT_EQ(word_count[0], 15000);
+}
+
+TEST(word_count_seq, test_task_run) {
+
+  // Create data
+  std::vector<char> input = wordCountInput;
+  std::vector<int> word_count(1, 0);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskData = std::make_shared<ppc::core::TaskData>();
+  taskData->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
+  taskData->inputs_count.emplace_back(input.size());
+  taskData->outputs.emplace_back(reinterpret_cast<uint8_t *>(word_count.data()));
+  taskData->outputs_count.emplace_back(word_count.size());
+
+  // Create Task
+  auto testTask = std::make_shared<chastov_v_count_words_in_line_seq::TestTaskSequential>(taskData);
+
+  // Create Perf attributes
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 1000;
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perfAttr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  // Create and init perf results
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
+  // Create Perf analyzer
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTask);
+  perfAnalyzer->task_run(perfAttr, perfResults);
+  ppc::core::Perf::print_perf_statistic(perfResults);
+
+  ASSERT_EQ(word_count[0], 15000);
+}
---
+++ tasks/seq/chastov_v_count_words_in_line/src/ops_seq.cpp
@@ -0,0 +1,67 @@
+#include "seq/chastov_v_count_words_in_line/include/ops_seq.hpp"
+
+namespace chastov_v_count_words_in_line_seq {
+
+std::vector<char> createString(int n) {
+  std::vector<char> wordCountInput;
+  std::string firstSentence = "Hello my name is Slava. Now I am a third year student at Lobachevsky University. ";
+  for (int i = 0; i < n - 1; i++) {
+    for (unsigned long int j = 0; j < firstSentence.length(); j++) {
+      wordCountInput.push_back(firstSentence[j]);
+    }
+  }
+  std::string lastSentence = "This is a proposal to evaluate the performance of a word counting algorithm via MPI.";
+  for (unsigned long int j = 0; j < lastSentence.length(); j++) {
+    wordCountInput.push_back(lastSentence[j]);
+  }
+  return wordCountInput;
+}
+
+bool chastov_v_count_words_in_line_seq::TestTaskSequential::pre_processing() {
+  internal_order_test();
+  inputString = std::vector<char>(taskData->inputs_count[0]);
+  auto* tmp = reinterpret_cast<char*>(taskData->inputs[0]);
+  for (unsigned i = 0; i < taskData->inputs_count[0]; i++) {
+    inputString[i] = tmp[i];
+  }
+  return true;
+}
+
+bool chastov_v_count_words_in_line_seq::TestTaskSequential::validation() {
+  internal_order_test();
+  return taskData->inputs_count[0] > 0 && taskData->outputs_count[0] == 1;
+}
+
+
+bool chastov_v_count_words_in_line_seq::TestTaskSequential::run() {
+  internal_order_test();
+  spacesFound = 0;
+  wordsFound = 0;
+
+  bool inWord = false;
+
+  for (char c : inputString) {
+    if (std::isspace(c)) {
+      if (inWord) {
+        inWord = false;
+        spacesFound++;
+      }
+    }
+    else {
+      if (!inWord) {
+        inWord = true;
+        wordsFound++;
+      }
+    }
+  }
+
+  return true;
+}
+
+bool chastov_v_count_words_in_line_seq::TestTaskSequential::post_processing() {
+  internal_order_test();
+  reinterpret_cast<int*>(taskData->outputs[0])[0] = wordsFound;
+  return true;
+}
+
+}  // namespace chastov_v_count_words_in_line_seq
