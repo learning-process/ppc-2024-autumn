85dc519384c68c79a199b2305225c643002676d4
emilien-gus <emiguseynov@gmail.com>
2024-11-02 11:30:14 +0300

    clang-format fix2

--- tasks/seq/guseynov_e_check_lex_order_of_two_string/src/ops_seq.cpp
+++ tasks/seq/guseynov_e_check_lex_order_of_two_string/src/ops_seq.cpp
@@ -8,7 +8,7 @@ bool guseynov_e_check_lex_order_of_two_string_seq::TestTaskSequential::pre_proce
   internal_order_test();
   input_ = std::vector<std::vector<char>>(taskData->inputs_count[0]);
   for (unsigned i = 0; i < taskData->inputs_count[0]; i++) {
-    auto *tmp_ptr = reinterpret_cast<char*>(taskData->inputs[i]);
+    auto *tmp_ptr = reinterpret_cast<char *>(taskData->inputs[i]);
     input_[i] = std::vector<char>(taskData->inputs_count[i + 1]);
     for (unsigned j = 0; j < taskData->inputs_count[i + 1]; j++) {
       input_[i][j] = tmp_ptr[j];
@@ -48,6 +48,6 @@ bool guseynov_e_check_lex_order_of_two_string_seq::TestTaskSequential::run() {
 
 bool guseynov_e_check_lex_order_of_two_string_seq::TestTaskSequential::post_processing() {
   internal_order_test();
-  reinterpret_cast<int*>(taskData->outputs[0])[0] = res_;
+  reinterpret_cast<int *>(taskData->outputs[0])[0] = res_;
   return true;
 }
