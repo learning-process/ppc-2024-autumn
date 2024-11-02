dd7afa99b8b780bc8f9a78230e6d578f37523829
MaxikGuy <S_Maksssim@mail.ru>
2024-11-02 19:50:18 +0300

    tests with zero rows or columns

--- tasks/seq/savchenko_m_min_matrix/func_tests/main_savchenko.cpp
+++ tasks/seq/savchenko_m_min_matrix/func_tests/main_savchenko.cpp
@@ -15,14 +15,14 @@ TEST(savchenko_m_min_matrix_seq, test_min_10x10) {
   std::mt19937 gen(dev());
 
   // Create data
-  const size_t rows = 10;
-  const size_t columns = 10;
+  const int rows = 10;
+  const int columns = 10;
   const int gen_min = -1000;
   const int gen_max = 1000;
   const int ref = INT_MIN;
 
   matrix = savchenko_m_min_matrix_seq::getRandomMatrix(rows, columns, gen_min, gen_max);
-  const size_t index = gen() % (rows * columns);
+  const int index = gen() % (rows * columns);
   matrix[index] = INT_MIN;
 
   // Create TaskData
@@ -51,14 +51,14 @@ TEST(savchenko_m_min_matrix_seq, test_min_100x10) {
   std::mt19937 gen(dev());
 
   // Create data
-  const size_t rows = 100;
-  const size_t columns = 10;
+  const int rows = 100;
+  const int columns = 10;
   const int gen_min = -1000;
   const int gen_max = 1000;
   const int ref = INT_MIN;
 
   matrix = savchenko_m_min_matrix_seq::getRandomMatrix(rows, columns, gen_min, gen_max);
-  const size_t index = gen() % (rows * columns);
+  const int index = gen() % (rows * columns);
   matrix[index] = INT_MIN;
 
   // Create TaskData
@@ -87,14 +87,14 @@ TEST(savchenko_m_min_matrix_seq, test_min_10x100) {
   std::mt19937 gen(dev());
 
   // Create data
-  const size_t rows = 10;
-  const size_t columns = 100;
+  const int rows = 10;
+  const int columns = 100;
   const int gen_min = -1000;
   const int gen_max = 1000;
   const int ref = INT_MIN;
 
   matrix = savchenko_m_min_matrix_seq::getRandomMatrix(rows, columns, gen_min, gen_max);
-  const size_t index = gen() % (rows * columns);
+  const int index = gen() % (rows * columns);
   matrix[index] = INT_MIN;
 
   // Create TaskData
@@ -123,14 +123,14 @@ TEST(savchenko_m_min_matrix_seq, test_min_100x100) {
   std::mt19937 gen(dev());
 
   // Create data
-  const size_t rows = 100;
-  const size_t columns = 100;
+  const int rows = 100;
+  const int columns = 100;
   const int gen_min = -1000;
   const int gen_max = 1000;
   const int ref = INT_MIN;
 
   matrix = savchenko_m_min_matrix_seq::getRandomMatrix(rows, columns, gen_min, gen_max);
-  const size_t index = gen() % (rows * columns);
+  const int index = gen() % (rows * columns);
   matrix[index] = INT_MIN;
 
   // Create TaskData
@@ -159,14 +159,14 @@ TEST(savchenko_m_min_matrix_seq, test_min_100x1) {
   std::mt19937 gen(dev());
 
   // Create data
-  const size_t rows = 100;
-  const size_t columns = 1;
+  const int rows = 100;
+  const int columns = 1;
   const int gen_min = -1000;
   const int gen_max = 1000;
   const int ref = INT_MIN;
 
   matrix = savchenko_m_min_matrix_seq::getRandomMatrix(rows, columns, gen_min, gen_max);
-  const size_t index = gen() % (rows * columns);
+  const int index = gen() % (rows * columns);
   matrix[index] = INT_MIN;
 
   // Create TaskData
@@ -195,14 +195,14 @@ TEST(savchenko_m_min_matrix_seq, test_min_1000x1) {
   std::mt19937 gen(dev());
 
   // Create data
-  const size_t rows = 1000;
-  const size_t columns = 1;
+  const int rows = 1000;
+  const int columns = 1;
   const int gen_min = -1000;
   const int gen_max = 1000;
   const int ref = INT_MIN;
 
   matrix = savchenko_m_min_matrix_seq::getRandomMatrix(rows, columns, gen_min, gen_max);
-  const size_t index = gen() % (rows * columns);
+  const int index = gen() % (rows * columns);
   matrix[index] = INT_MIN;
 
   // Create TaskData
@@ -231,14 +231,14 @@ TEST(savchenko_m_min_matrix_seq, test_min_1x100) {
   std::mt19937 gen(dev());
 
   // Create data
-  const size_t rows = 1;
-  const size_t columns = 100;
+  const int rows = 1;
+  const int columns = 100;
   const int gen_min = -1000;
   const int gen_max = 1000;
   const int ref = INT_MIN;
 
   matrix = savchenko_m_min_matrix_seq::getRandomMatrix(rows, columns, gen_min, gen_max);
-  const size_t index = gen() % (rows * columns);
+  const int index = gen() % (rows * columns);
   matrix[index] = INT_MIN;
 
   // Create TaskData
@@ -267,14 +267,14 @@ TEST(savchenko_m_min_matrix_seq, test_min_1x1000) {
   std::mt19937 gen(dev());
 
   // Create data
-  const size_t rows = 10;
-  const size_t columns = 10;
+  const int rows = 1;
+  const int columns = 1000;
   const int gen_min = -1000;
   const int gen_max = 1000;
   const int ref = INT_MIN;
 
   matrix = savchenko_m_min_matrix_seq::getRandomMatrix(rows, columns, gen_min, gen_max);
-  const size_t index = gen() % (rows * columns);
+  const int index = gen() % (rows * columns);
   matrix[index] = INT_MIN;
 
   // Create TaskData
@@ -293,4 +293,62 @@ TEST(savchenko_m_min_matrix_seq, test_min_1x1000) {
   testTaskSequential.run();
   testTaskSequential.post_processing();
   ASSERT_EQ(ref, min_value[0]);
+}
+
+TEST(savchenko_m_min_matrix_seq, test_min_0x10) {
+  std::vector<int> matrix;
+  std::vector<int32_t> min_value(1, INT_MAX);
+
+  std::random_device dev;
+  std::mt19937 gen(dev());
+
+  // Create data
+  const int rows = 0;
+  const int columns = 10;
+  const int gen_min = -1000;
+  const int gen_max = 1000;
+
+  matrix = savchenko_m_min_matrix_seq::getRandomMatrix(rows, columns, gen_min, gen_max);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix.data()));
+  taskDataSeq->inputs_count.emplace_back(rows);
+  taskDataSeq->inputs_count.emplace_back(columns);
+
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(min_value.data()));
+  taskDataSeq->outputs_count.emplace_back(min_value.size());
+
+  // Create Task
+  savchenko_m_min_matrix_seq::TestTaskSequential testTaskSequential(taskDataSeq);
+  ASSERT_EQ(testTaskSequential.validation(), false);
+}
+
+TEST(savchenko_m_min_matrix_seq, test_min_10x0) {
+  std::vector<int> matrix;
+  std::vector<int32_t> min_value(1, INT_MAX);
+
+  std::random_device dev;
+  std::mt19937 gen(dev());
+
+  // Create data
+  const int rows = 10;
+  const int columns = 0;
+  const int gen_min = -1000;
+  const int gen_max = 1000;
+
+  matrix = savchenko_m_min_matrix_seq::getRandomMatrix(rows, columns, gen_min, gen_max);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(matrix.data()));
+  taskDataSeq->inputs_count.emplace_back(rows);
+  taskDataSeq->inputs_count.emplace_back(columns);
+
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(min_value.data()));
+  taskDataSeq->outputs_count.emplace_back(min_value.size());
+
+  // Create Task
+  savchenko_m_min_matrix_seq::TestTaskSequential testTaskSequential(taskDataSeq);
+  ASSERT_EQ(testTaskSequential.validation(), false);
 }
