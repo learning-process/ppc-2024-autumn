f6503d6eaca8e942b43976d3747e9dfc594047a4
Alexander Nesterov <nesterov.alexander@outlook.com>
2024-11-02 02:29:41 +0100

    test

--- scripts/run_perf_collector.sh
+++ scripts/run_perf_collector.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+if [[ -z "$ASAN_RUN" ]]; then
+  if [[ $OSTYPE == "linux-gnu" ]]; then
+    mpirun --oversubscribe -np 4 ./build/bin/sample_mpi
+    mpirun --oversubscribe -np 4 ./build/bin/sample_mpi_boost
+  elif [[ $OSTYPE == "darwin"* ]]; then
+    mpirun -np 2 ./build/bin/sample_mpi
+    mpirun -np 2 ./build/bin/sample_mpi_boost
+  fi
+fi
+./build/bin/sample_omp
+./build/bin/sample_stl
+./build/bin/sample_tbb
+
 # separate tests for debug
 for test_item in $(./build/bin/mpi_perf_tests --gtest_list_tests | awk '/\./{ SUITE=$1 }  /  / { print SUITE $1 }')
 do
