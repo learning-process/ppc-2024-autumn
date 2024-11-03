0c7cebbdf8d3b4ec95aac8b893a6067a909b70c5
Sarafanov <k1pasta@yandex.ru>
2024-11-03 22:39:40 +0300

    Add tests

--- tasks/seq/sarafanov_m_num_of_mismatch_characters_of_two_strings/func_tests/main.cpp
+++ tasks/seq/sarafanov_m_num_of_mismatch_characters_of_two_strings/func_tests/main.cpp
@@ -114,3 +114,37 @@ TEST(sarafanov_m_num_of_mismatch_characters_of_two_strings_seq, all_different_ch
   task.post_processing();
   ASSERT_EQ(expected, output[0]);
 }
+
+TEST(sarafanov_m_num_of_mismatch_characters_of_two_strings_seq, error_when_input_lengths_are_different) {
+  auto input_a = std::string("abcdefg");
+  auto input_b = std::string("abc");
+  auto output = std::vector<int>(1);
+
+  auto task_data = std::make_shared<ppc::core::TaskData>();
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t *>(input_a.data()));
+  task_data->inputs_count.emplace_back(input_a.size());
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t *>(input_b.data()));
+  task_data->inputs_count.emplace_back(input_b.size());
+  task_data->outputs.emplace_back(reinterpret_cast<uint8_t *>(output.data()));
+  task_data->outputs_count.emplace_back(output.size());
+
+  auto task = sarafanov_m_num_of_mismatch_characters_of_two_strings_seq::SequentialTask(task_data);
+  ASSERT_FALSE(task.validation());
+}
+
+TEST(sarafanov_m_num_of_mismatch_characters_of_two_strings_seq, error_when_output_size_is_not_equal_to_one) {
+  auto input_a = std::string("abcdefg");
+  auto input_b = std::string("abcxyzg");
+  auto output = std::vector<int>(0);
+
+  auto task_data = std::make_shared<ppc::core::TaskData>();
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t *>(input_a.data()));
+  task_data->inputs_count.emplace_back(input_a.size());
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t *>(input_b.data()));
+  task_data->inputs_count.emplace_back(input_b.size());
+  task_data->outputs.emplace_back(reinterpret_cast<uint8_t *>(output.data()));
+  task_data->outputs_count.emplace_back(output.size());
+
+  auto task = sarafanov_m_num_of_mismatch_characters_of_two_strings_seq::SequentialTask(task_data);
+  ASSERT_FALSE(task.validation());
+}
