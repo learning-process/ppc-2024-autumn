192be84ce65e7a4289ce31837b486d2cc9d0f2c8
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2024-11-03 17:43:44 +0300

    Update main.cpp

--- tasks/seq/vavilov_v_min_elements_in_columns_of_matrix/func_tests/main.cpp
+++ tasks/seq/vavilov_v_min_elements_in_columns_of_matrix/func_tests/main.cpp
@@ -78,30 +78,6 @@ TEST(vavilov_v_min_elements_in_columns_of_matrix_seq, validation_output_empty_10
   ASSERT_EQ(testTaskSequential.validation(), false);
 }
 
-TEST(vavilov_v_min_elements_in_columns_of_matrix_seq, validation_find_min_elem_in_col_10x0_matr) {
-  const int rows = 10;
-  const int cols = 0;
-
-  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-
-  vavilov_v_min_elements_in_columns_of_matrix_seq::TestTaskSequential testTaskSequential(taskDataSeq);
-  std::vector<std::vector<int>> matr =
-      vavilov_v_min_elements_in_columns_of_matrix_seq::TestTaskSequential::generate_rand_matr(rows, cols);
-
-  for (auto& row : matr) {
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(row.data()));
-  }
-
-  taskDataSeq->inputs_count.emplace_back(rows);
-  taskDataSeq->inputs_count.emplace_back(cols);
-
-  std::vector<int> vec_res(cols, 0);
-  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(vec_res.data()));
-  taskDataSeq->outputs_count.emplace_back(vec_res.size());
-
-  ASSERT_EQ(testTaskSequential.validation(), false);
-}
-
 TEST(vavilov_v_min_elements_in_columns_of_matrix_seq, validation_fails_on_invalid_output_of_size) {
   const int rows = 10;
   const int cols = 10;
