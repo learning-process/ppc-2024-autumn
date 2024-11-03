bf096e0f7392b77431250ffbb5c188f43aefba86
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2024-11-03 13:17:07 +0300

    Update main.cpp

--- tasks/mpi/vavilov_v_min_elements_in_columns_of_matrix/perf_tests/main.cpp
+++ tasks/mpi/vavilov_v_min_elements_in_columns_of_matrix/perf_tests/main.cpp
@@ -19,7 +19,7 @@ TEST(vavilov_v_min_elements_in_columns_of_matrix_mpi, test_pipeline_run_min) {
     count_row = 5000;
     count_col = 5000;
     global_matr = vavilov_v_min_elements_in_columns_of_matrix_mpi::TestMPITaskSequential::generate_rand_matr(count_row,
-                                                                                                            count_col);
+                                                                                                             count_col);
     min_col.resize(count_col, INT_MAX);
 
     for (auto& row : global_matr) {
@@ -59,7 +59,7 @@ TEST(vavilov_v_min_elements_in_columns_of_matrix_mpi, test_task_run) {
     count_row = 5000;
     count_col = 5000;
     global_matr = vavilov_v_min_elements_in_columns_of_matrix_mpi::TestMPITaskSequential::generate_rand_matr(count_row,
-                                                                                                            count_col);
+                                                                                                             count_col);
     min_col.resize(count_col, INT_MAX);
 
     for (auto& row : global_matr) {
