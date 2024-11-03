0f02214e7d54ed5f8acc94b36d07759601e1dcf7
Egor1dzeN-learn <egormorozov123450@gmail.com>
2024-11-03 16:38:05 +0300

    minor fix

--- tasks/mpi/morozov_e_min_val_in_rows_matrix/func_tests/main.cpp
+++ tasks/mpi/morozov_e_min_val_in_rows_matrix/func_tests/main.cpp
@@ -61,6 +61,24 @@ TEST(morozov_e_min_val_in_rows_matrix_MPI, Test_Validation_isFalse2) {
     ASSERT_FALSE(testMpiTaskSequential.validation());
   }
 }
+TEST(morozov_e_min_val_in_rows_matrix_MPI, Test_Validation_isFalse3) {
+  boost::mpi::communicator world;
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+  std::vector<std::vector<int>> matrix = {{1, 1}, {2, 2}};
+  std::vector<int> res(2, 0);
+  if (world.rank() == 0) {
+    for (size_t i = 0; i < matrix.size(); ++i) {
+      taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrix[i].data()));
+      taskDataPar->inputs_count.emplace_back(2);
+    }
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(res.data()));
+    taskDataPar->outputs_count.emplace_back(3);
+    morozov_e_min_val_in_rows_matrix::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+    morozov_e_min_val_in_rows_matrix::TestMPITaskSequential testMpiTaskSequential(taskDataPar);
+    ASSERT_FALSE(testMpiTaskParallel.validation());
+    ASSERT_FALSE(testMpiTaskSequential.validation());
+  }
+}
 
 TEST(morozov_e_min_val_in_rows_matrix_MPI, Test_Validation_isTrue) {
   boost::mpi::communicator world;
@@ -81,54 +99,7 @@ TEST(morozov_e_min_val_in_rows_matrix_MPI, Test_Validation_isTrue) {
     ASSERT_TRUE(testMpiTaskSequential.validation());
   }
 }
-TEST(morozov_e_min_val_in_rows_matrix_MPI, Test_Main0) {
-  std::vector<std::vector<int>> matrixPar;
-  std::vector<std::vector<int>> matrixSeq;
-  const int n = 2;
-  const int m = 2;
-  std::vector<int32_t> resPar(n);
-  std::vector<int32_t> resSeq(n);
-  boost::mpi::communicator world;
-  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
-  matrixSeq = {{1, 2}, {3, 4}};
-  for (size_t i = 0; i < matrixSeq.size(); ++i) {
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixSeq[i].data()));
-  }
 
-  taskDataSeq->inputs_count.emplace_back(n);
-  taskDataSeq->inputs_count.emplace_back(m);
-  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(resSeq.data()));
-  taskDataSeq->outputs_count.emplace_back(resSeq.size());
-  morozov_e_min_val_in_rows_matrix::TestMPITaskSequential testMpiTaskSequential(taskDataSeq);
-  ASSERT_EQ(testMpiTaskSequential.validation(), true);
-  testMpiTaskSequential.pre_processing();
-  testMpiTaskSequential.run();
-  testMpiTaskSequential.post_processing();
-  if (world.rank() == 0) {
-    matrixPar = {{1, 2}, {3, 4}};
-    for (size_t i = 0; i < matrixPar.size(); ++i) {
-      taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixPar[i].data()));
-    }
-
-    taskDataPar->inputs_count.emplace_back(n);
-    taskDataPar->inputs_count.emplace_back(m);
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(resPar.data()));
-    taskDataPar->outputs_count.emplace_back(resPar.size());
-  }
-  morozov_e_min_val_in_rows_matrix::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
-
-  ASSERT_EQ(testMpiTaskParallel.validation(), true);
-  testMpiTaskParallel.pre_processing();
-  testMpiTaskParallel.run();
-  testMpiTaskParallel.post_processing();
-  if (world.rank() == 0) {
-    ASSERT_EQ(resPar[0], 1);
-    ASSERT_EQ(resPar[1], 3);
-    ASSERT_EQ(resSeq[0], 1);
-    ASSERT_EQ(resSeq[1], 3);
-  }
-}
 TEST(morozov_e_min_val_in_rows_matrix_MPI, Test_Main1) {
   std::vector<std::vector<int>> matrix;
   const int n = 10;
