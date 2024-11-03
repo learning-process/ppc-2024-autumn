030ed25df861ea06664d23086eec975e9ca861fa
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2024-11-03 17:42:24 +0300

    final

--- tasks/mpi/sedova_o_max_of_vector_elements/include/ops_mpi.hpp
+++ tasks/mpi/sedova_o_max_of_vector_elements/include/ops_mpi.hpp
@@ -15,7 +15,7 @@
 namespace sedova_o_max_of_vector_elements_mpi {
 class TestMPITaskSequential : public ppc::core::Task {
  public:
-  explicit TestMPITaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)){};
+  explicit TestMPITaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {};
   bool pre_processing() override;
   bool validation() override;
   bool run() override;
@@ -29,7 +29,7 @@ class TestMPITaskSequential : public ppc::core::Task {
 
 class TestMPITaskParallel : public ppc::core::Task {
  public:
-  explicit TestMPITaskParallel(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)){};
+  explicit TestMPITaskParallel(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {};
   bool pre_processing() override;
   bool validation() override;
   bool run() override;
