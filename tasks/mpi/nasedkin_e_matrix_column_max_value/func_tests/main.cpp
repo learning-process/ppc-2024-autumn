97a0e9dda3dee536ae005386a15c6166dda098f1
myffii <132840123+myffii@users.noreply.github.com>
2024-11-03 03:58:47 +0300

    test

--- tasks/mpi/nasedkin_e_matrix_column_max_value/func_tests/main.cpp
+++ tasks/mpi/nasedkin_e_matrix_column_max_value/func_tests/main.cpp
@@ -3,17 +3,12 @@
 
 namespace nasedkin_e_matrix_column_max_value_mpi {
 
-    TEST(nasedkin_e_matrix_column_max_value_mpi, test_findMaxInColumns) {
-        std::vector<std::vector<double>> matrix = {
-            {1.0, 2.0, 3.0},
-            {4.0, 5.0, 6.0},
-            {7.0, 8.0, 9.0}
+TEST(nasedkin_e_matrix_column_max_value_mpi, test_findMaxInColumns) {
+  std::vector<std::vector<double>> matrix = {{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}};
         };
 
-        std::vector<double> expected = {7.0, 8.0, 9.0};
-        auto result = findMaxInColumns(matrix);
+std::vector<double> expected = {7.0, 8.0, 9.0};
+auto result = findMaxInColumns(matrix);
 
-        EXPECT_EQ(result, expected);
-    }
-
-}  // namespace nasedkin_e_matrix_column_max_value_mpi
+EXPECT_EQ(result, expected);
+}
