a929451c52ae18f533895339c3eb773d4220571c
Svolota <seraphimvolochaev@gmail.com>
2024-11-03 08:08:51 +0300

    fix test with wrong data 3

--- tasks/mpi/volochaev_s_count_characters_27/func_tests/main.cpp
+++ tasks/mpi/volochaev_s_count_characters_27/func_tests/main.cpp
@@ -9,22 +9,10 @@
 
 TEST(volochaev_s_count_characters_27_MPI, Test_0) {
   boost::mpi::communicator world;
-  std::vector<std::string> global_vec;
+  std::vector<std::string> global_vec(1, volochaev_s_count_characters_27_mpi::get_random_string(20));
   std::vector<int32_t> global_diff(1, 0);
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
-
-  if (world.rank() == 0) {
-    const int size_str1 = 240;
-    global_vec = {volochaev_s_count_characters_27_mpi::get_random_string(size_str1)};
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
-    taskDataPar->inputs_count.emplace_back(global_vec.size());
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_diff.data()));
-    taskDataPar->outputs_count.emplace_back(global_diff.size());
-  }
-
-  volochaev_s_count_characters_27_mpi::Lab1_27_mpi testMpiTaskParallel(taskDataPar);
-  ASSERT_EQ(testMpiTaskParallel.validation(), false);
 
   if (world.rank() == 0) {
     // Create data
