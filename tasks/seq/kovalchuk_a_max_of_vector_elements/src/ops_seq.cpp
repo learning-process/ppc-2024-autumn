910a5f98bc53531c41065b010325ea3c460655b8
Alexandr Kovalchuk <sasha-kovalchuk-2004@mail.ru>
2024-11-04 02:57:43 +0300

    Fix&

--- tasks/seq/kovalchuk_a_max_of_vector_elements/src/ops_seq.cpp
+++ tasks/seq/kovalchuk_a_max_of_vector_elements/src/ops_seq.cpp
@@ -1,13 +1,12 @@
 // Copyright 2023 Nesterov Alexander
-#include "seq/kovalchuk_a_max_of_vector_elements/include/ops_seq.hpp"
-
+#include "mpi/kovalchuk_a_max_of_vector_elements/include/ops_mpi.hpp"
 #include <algorithm>
 #include <functional>
 #include <random>
 #include <string>
 #include <vector>
 
-std::vector<int> kovalchuk_a_max_of_vector_elements_seq::getRandomVector(int sz, int min, int max) {
+std::vector<int> kovalchuk_a_max_of_vector_elements::getRandomVector(int sz, int min, int max) {
   std::random_device dev;
   std::mt19937 gen(dev());
   std::vector<int> vec(sz);
@@ -17,16 +16,16 @@ std::vector<int> kovalchuk_a_max_of_vector_elements_seq::getRandomVector(int sz,
   return vec;
 }
 
-std::vector<std::vector<int>> kovalchuk_a_max_of_vector_elements_seq::getRandomMatrix(int rows, int columns, int min,
-                                                                                      int max) {
+std::vector<std::vector<int>> kovalchuk_a_max_of_vector_elements::getRandomMatrix(int rows, int columns, int min,
+                                                                                  int max) {
   std::vector<std::vector<int>> vec(rows);
   for (int i = 0; i < rows; i++) {
-    vec[i] = kovalchuk_a_max_of_vector_elements_seq::getRandomVector(columns, min, max);
+    vec[i] = kovalchuk_a_max_of_vector_elements::getRandomVector(columns, min, max);
   }
   return vec;
 }
 
-bool kovalchuk_a_max_of_vector_elements_seq::TestSequentialTask::pre_processing() {
+bool kovalchuk_a_max_of_vector_elements::TestMPITaskSequential::pre_processing() {
   internal_order_test();
   // Init vectors
   input_ = std::vector<std::vector<int>>(taskData->inputs_count[0], std::vector<int>(taskData->inputs_count[1]));
@@ -39,13 +38,13 @@ bool kovalchuk_a_max_of_vector_elements_seq::TestSequentialTask::pre_processing(
   return true;
 }
 
-bool kovalchuk_a_max_of_vector_elements_seq::TestSequentialTask::validation() {
+bool kovalchuk_a_max_of_vector_elements::TestMPITaskSequential::validation() {
   internal_order_test();
   // Check count elements of output
   return taskData->outputs_count[0] == 1 && taskData->inputs_count[0] > 0 && taskData->inputs_count[1] > 0;
 }
 
-bool kovalchuk_a_max_of_vector_elements_seq::TestSequentialTask::run() {
+bool kovalchuk_a_max_of_vector_elements::TestMPITaskSequential::run() {
   internal_order_test();
   std::vector<int> local_res(input_.size());
   for (unsigned int i = 0; i < input_.size(); i++) {
@@ -55,8 +54,65 @@ bool kovalchuk_a_max_of_vector_elements_seq::TestSequentialTask::run() {
   return true;
 }
 
-bool kovalchuk_a_max_of_vector_elements_seq::TestSequentialTask::post_processing() {
+bool kovalchuk_a_max_of_vector_elements::TestMPITaskSequential::post_processing() {
   internal_order_test();
   reinterpret_cast<int*>(taskData->outputs[0])[0] = res_;
+  return true;
+}
+
+bool kovalchuk_a_max_of_vector_elements::TestMPITaskParallel::pre_processing() {
+  internal_order_test();
+  unsigned int delta = 0;
+  if (world.rank() == 0) {
+    delta = taskData->inputs_count[0] * taskData->inputs_count[1] / world.size();
+  }
+  broadcast(world, delta, 0);
+  if (world.rank() == 0) {
+    // Init vectors
+    unsigned int rows = taskData->inputs_count[0];
+    unsigned int columns = taskData->inputs_count[1];
+    input_ = std::vector<int>(rows * columns);
+    for (unsigned int i = 0; i < rows; i++) {
+      auto* tmp_ptr = reinterpret_cast<int*>(taskData->inputs[i]);
+      for (unsigned int j = 0; j < columns; j++) {
+        input_[i * columns + j] = tmp_ptr[j];
+      }
+    }
+    for (int proc = 1; proc < world.size(); proc++) {
+      world.send(proc, 0, input_.data() + delta * proc, delta);
+    }
+  }
+  local_input_ = std::vector<int>(delta);
+  if (world.rank() == 0) {
+    local_input_ = std::vector<int>(input_.begin(), input_.begin() + delta);
+  } else {
+    world.recv(0, 0, local_input_.data(), delta);
+  }
+  // Init value for output
+  res_ = INT_MIN;
+  return true;
+}
+
+bool kovalchuk_a_max_of_vector_elements::TestMPITaskParallel::validation() {
+  internal_order_test();
+  if (world.rank() == 0) {
+    // Check count elements of output
+    return taskData->outputs_count[0] == 1 && !taskData->inputs.empty();
+  }
+  return true;
+}
+
+bool kovalchuk_a_max_of_vector_elements::TestMPITaskParallel::run() {
+  internal_order_test();
+  int local_res = *std::max_element(local_input_.begin(), local_input_.end());
+  reduce(world, local_res, res_, boost::mpi::maximum<int>(), 0);
+  return true;
+}
+
+bool kovalchuk_a_max_of_vector_elements::TestMPITaskParallel::post_processing() {
+  internal_order_test();
+  if (world.rank() == 0) {
+    reinterpret_cast<int*>(taskData->outputs[0])[0] = res_;
+  }
   return true;
 }
