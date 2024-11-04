e986f7a04948b42683625b7b95c9c9d6dded0f00
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2024-11-04 17:44:59 +0300

    Update main.cpp

--- tasks/mpi/vavilov_v_min_elements_in_columns_of_matrix/perf_tests/main.cpp
+++ tasks/mpi/vavilov_v_min_elements_in_columns_of_matrix/perf_tests/main.cpp
@@ -6,6 +6,26 @@
 #include "core/perf/include/perf.hpp"
 #include "mpi/vavilov_v_min_elements_in_columns_of_matrix/include/ops_mpi.hpp"
 
+std::vector<int> generate_rand_vec(int size, int lower_bound, int upper_bound) {
+  std::vector<int> vec(size);
+  for (auto& n : vec) {
+    n = lower_bound + std::rand() % (upper_bound - lower_bound + 1);
+  }
+  return vec;
+}
+
+std::vector<std::vector<int>> generate_rand_matr(int rows, int cols) {
+  std::vector<std::vector<int>> matr(rows, std::vector<int>(cols));
+  for (int i = 0; i < rows; i++) {
+    matr[i] = generate_rand_vec(cols, -1000, 1000);
+  }
+  for (int j = 0; j < cols; j++) {
+    int r_row = std::rand() % rows;
+    matr[r_row][j] = INT_MIN;
+  }
+  return matr;
+}
+
 TEST(vavilov_v_min_elements_in_columns_of_matrix_mpi, test_pipeline_run_min) {
   boost::mpi::communicator world;
   std::vector<std::vector<int>> global_matr;
@@ -18,8 +38,7 @@ TEST(vavilov_v_min_elements_in_columns_of_matrix_mpi, test_pipeline_run_min) {
   if (world.rank() == 0) {
     count_row = 5000;
     count_col = 5000;
-    global_matr = vavilov_v_min_elements_in_columns_of_matrix_mpi::TestMPITaskSequential::generate_rand_matr(count_row,
-                                                                                                             count_col);
+    global_matr = generate_rand_matr(count_row, count_col);
     min_col.resize(count_col, INT_MAX);
 
     for (auto& row : global_matr) {
@@ -58,8 +77,7 @@ TEST(vavilov_v_min_elements_in_columns_of_matrix_mpi, test_task_run) {
   if (world.rank() == 0) {
     count_row = 5000;
     count_col = 5000;
-    global_matr = vavilov_v_min_elements_in_columns_of_matrix_mpi::TestMPITaskSequential::generate_rand_matr(count_row,
-                                                                                                             count_col);
+    global_matr = generate_rand_matr(count_row, count_col);
     min_col.resize(count_col, INT_MAX);
 
     for (auto& row : global_matr) {
