40679ea703cc83982d24f01b3941caed317fd6ea
Maksim Anikin <zengaming1122@mail.ru>
2024-11-04 05:50:26 +0300

    clang...

--- tasks/mpi/anikin_m_summ_of_different_symbols/src/ops_mpi.cpp
+++ tasks/mpi/anikin_m_summ_of_different_symbols/src/ops_mpi.cpp
@@ -89,11 +89,9 @@ bool anikin_m_summ_of_different_symbols_mpi::SumDifSymMPIParallel::validation() 
 
 bool anikin_m_summ_of_different_symbols_mpi::SumDifSymMPIParallel::run() {
   internal_order_test();
-  size_t loc_size = 0;
-  if (com.rank() == 0) {
-    size_t temp = (strlen(input[0]) + com.size() - 1);
-    loc_size = (temp / com.size()); 
-  }
+  size_t loc_size;
+  size_t temp = (strlen(input[0]) + com.size() - 1);
+  loc_size = ((com.rank() == 0) ? (temp / com.size()) : 0);
   broadcast(com, loc_size, 0);
   if (com.rank() == 0) {
     for (int pr = 1; pr < com.size(); pr++) {
