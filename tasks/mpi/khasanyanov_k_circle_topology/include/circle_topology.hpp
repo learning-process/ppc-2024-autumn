e10322628eec795cc233475835c5e88e615fea37
Kirill Khasanyanov <hasanyanovk2@mail.ru>
2024-11-04 18:29:31 +0000

    initial commit for task 2

---
+++ tasks/mpi/khasanyanov_k_circle_topology/include/circle_topology.hpp
@@ -0,0 +1,36 @@
+#ifndef _CIRCLE_TOPOLOGY_HPP_
+#define _CIRCLE_TOPOLOGY_HPP_
+
+#include <random>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace khasanyanov_k_circle_topology_mpi {
+
+// not included 'right' with integers, not included left always
+template <typename T = int>
+std::vector<T> generate_random_vector(size_t size = 100, const T& left = T{-1000}, const T& right = T{1000}) {
+  std::vector<T> res(size);
+  std::random_device dev;
+  std::mt19937 gen(dev());
+  double frac = (gen() % 100) / 100.0;
+  for (size_t i = 0; i < size; i++) {
+    res[i] = left + frac + static_cast<T>(gen() % static_cast<int>(right - left));
+  }
+  return res;
+}
+
+class CircleTopology : ppc::core::Task {
+ public:
+  explicit CircleTopology(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+}
+
+}  // namespace khasanyanov_k_circle_topology_mpi
+
+#endif
