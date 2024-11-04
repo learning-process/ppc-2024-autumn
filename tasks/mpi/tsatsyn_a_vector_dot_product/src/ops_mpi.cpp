5489ca52efd4fbef2ca2eb9a09a71fd819a73aae
KAPUSHA228 <shura>
2024-11-04 18:36:39 +0300

    validation 2

--- tasks/mpi/tsatsyn_a_vector_dot_product/src/ops_mpi.cpp
+++ tasks/mpi/tsatsyn_a_vector_dot_product/src/ops_mpi.cpp
@@ -73,10 +73,18 @@ bool tsatsyn_a_vector_dot_product_mpi::TestMPITaskParallel::pre_processing() {
 bool tsatsyn_a_vector_dot_product_mpi::TestMPITaskParallel::validation() {
   internal_order_test();
   if (world.rank() == 0) {
-    return (taskData->inputs_count[0] == taskData->inputs_count[1]) &&
-           (taskData->inputs.size() == taskData->inputs_count.size() && taskData->inputs.size() == 2) &&
-           taskData->outputs_count[0] == 1 && (taskData->outputs.size() == taskData->outputs_count.size()) &&
-           taskData->outputs.size() == 1 && taskData->inputs_count[0] != 0 && taskData->inputs_count[1] != 0;
+    if (taskData->inputs.empty() || taskData->outputs.empty()) {
+      return false;
+    }
+    if (taskData->inputs_count[0] != taskData->inputs_count[1]) {
+      return false;
+    }
+    if (taskData->inputs_count[0] == 0 || taskData->inputs_count[1] == 0) {
+      return false;
+    }
+    if (taskData->outputs_count[0] == 1) {
+      return false;
+    }
   }
   return true;
 }
