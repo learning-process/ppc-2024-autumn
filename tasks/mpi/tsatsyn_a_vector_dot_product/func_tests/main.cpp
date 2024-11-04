86d7886c89b3f0d2e3250af2660b324af9818f48
KAPUSHA228 <shura>
2024-11-04 19:18:45 +0300

    pam

--- tasks/mpi/tsatsyn_a_vector_dot_product/func_tests/main.cpp
+++ tasks/mpi/tsatsyn_a_vector_dot_product/func_tests/main.cpp
@@ -53,7 +53,7 @@ std::vector<int> toGetRandomVector(int size) {
 //    ASSERT_FALSE(testMpiTaskSequential.validation());
 //  }
 //}
-//TEST(tsatsyn_a_vector_dot_product_mpi, Test_Scalar_Positive_Value) {
+// TEST(tsatsyn_a_vector_dot_product_mpi, Test_Scalar_Positive_Value) {
 //  boost::mpi::communicator world;
 //  std::vector<int> v1 = {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 //                         21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
