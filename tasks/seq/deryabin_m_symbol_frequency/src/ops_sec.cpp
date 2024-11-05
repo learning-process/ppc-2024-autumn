ce952fe9a71a9a0c5e1a2b7a455849b649be5a39
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-11-05 09:26:52 +0300

    Update ops_sec.cpp

--- tasks/seq/deryabin_m_symbol_frequency/src/ops_sec.cpp
+++ tasks/seq/deryabin_m_symbol_frequency/src/ops_sec.cpp
@@ -6,7 +6,7 @@ using namespace std::chrono_literals;
 
 #include <string>
 
-bool deryabin_m_symbol_frequency_seq::SymbolFrequencyTaskSequential::pre_processing() {
+bool deryabin_m_symbol_frequency_sec::SymbolFrequencyTaskSequential::pre_processing() {
 	internal_order_test();
 	// Init value for input and output
 	input_str_ = reinterpret_cast<std::string*>(taskData->inputs[0])[0];
@@ -15,13 +15,13 @@ bool deryabin_m_symbol_frequency_seq::SymbolFrequencyTaskSequential::pre_process
 	return true;
 }
 
-bool deryabin_m_symbol_frequency_seq::SymbolFrequencyTaskSequential::validation() {
+bool deryabin_m_symbol_frequency_sec::SymbolFrequencyTaskSequential::validation() {
 	internal_order_test();
 	// Check count elements of output
 	return taskData->inputs_count[0] == 1 && taskData->outputs_count[0] == 1 && taskData->inputs_count[1] == 1;
 }
 
-bool deryabin_m_symbol_frequency_seq::SymbolFrequencyTaskSequential::run() {
+bool deryabin_m_symbol_frequency_sec::SymbolFrequencyTaskSequential::run() {
 	internal_order_test();
 	double found = 0;
 	for (char i : input_str_)
@@ -31,7 +31,7 @@ bool deryabin_m_symbol_frequency_seq::SymbolFrequencyTaskSequential::run() {
 	return true;
 }
 
-bool deryabin_m_symbol_frequency_seq::SymbolFrequencyTaskSequential::post_processing() {
+bool deryabin_m_symbol_frequency_sec::SymbolFrequencyTaskSequential::post_processing() {
 	internal_order_test();
 	reinterpret_cast<double*>(taskData->outputs[0])[0] = frequency_;
 	return true;
