14324cf7575e1bc8b2eb9cdb85207bbc280fa936
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-11-05 07:21:19 +0300

    Update main.cpp

--- tasks/seq/deryabin_m_symbol_frequency/func_tests/main.cpp
+++ tasks/seq/deryabin_m_symbol_frequency/func_tests/main.cpp
@@ -84,32 +84,6 @@ TEST(deryabin_m_symbol_frequency_seq, test_large_string) {
 	ASSERT_EQ(TEST_frequency, out[0]);
 }
 
-TEST(deryabin_m_symbol_frequency_seq, test_empty_string) {
-	const double TEST_frequency = 0;
-
-	// Create data
-	std::vector<std::string> in(1, std::string());
-	std::vector<char>in_ch(1, '@');
-	std::vector<double> out(1, 0);
-
-	// Create TaskData
-	std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-	taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
-	taskDataSeq->inputs_count.emplace_back(in.size());
-	taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(in_ch.data()));
-	taskDataSeq->inputs_count.emplace_back(in_ch.size());
-	taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
-	taskDataSeq->outputs_count.emplace_back(out.size());
-
-	// Create Task
-	deryabin_m_symbol_frequency_seq::Symbol_frequency_TaskSequential symbol_frequency_TaskSequential(taskDataSeq);
-	ASSERT_EQ(symbol_frequency_TaskSequential.validation(), true);
-	symbol_frequency_TaskSequential.pre_processing();
-	symbol_frequency_TaskSequential.run();
-	symbol_frequency_TaskSequential.post_processing();
-	ASSERT_EQ(TEST_frequency, out[0]);
-}
-
 TEST(deryabin_m_symbol_frequency_seq, test_arithmetic_string) {
 	const double TEST_frequency = 0.1;
 
