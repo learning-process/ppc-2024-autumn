d54b7d71705f2f2dff06ad53a40b3ddcc75e9e1e
0xG00SE <61384845+DSolo03@users.noreply.github.com>
2024-11-05 11:45:54 +0300

    remove debug output

--- tasks/mpi/solovyev_d_vector_max/func_tests/main.cpp
+++ tasks/mpi/solovyev_d_vector_max/func_tests/main.cpp
@@ -2,7 +2,6 @@
 
 #include <boost/mpi/communicator.hpp>
 #include <boost/mpi/environment.hpp>
-#include <iostream>
 #include <random>
 #include <vector>
 
@@ -24,7 +23,6 @@ TEST(solovyev_d_vector_max_mpi, Test_Max) {
   std::vector<int32_t> global_max(1, 0);
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
-  std::cerr << "1 " << world.rank() << std::endl;
   if (world.rank() == 0) {
     const int count_size_vector = 240;
     global_vec = solovyev_d_vector_max_mpi::getRandomVector(count_size_vector);
@@ -33,13 +31,11 @@ TEST(solovyev_d_vector_max_mpi, Test_Max) {
     taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_max.data()));
     taskDataPar->outputs_count.emplace_back(global_max.size());
   }
-  std::cerr << "2 " << world.rank() << std::endl;
   solovyev_d_vector_max_mpi::VectorMaxMPIParallel VectorMaxMPIParallel(taskDataPar);
   ASSERT_EQ(VectorMaxMPIParallel.validation(), true);
   VectorMaxMPIParallel.pre_processing();
   VectorMaxMPIParallel.run();
   VectorMaxMPIParallel.post_processing();
-  std::cerr << "3 " << world.rank() << std::endl;
   if (world.rank() == 0) {
     // Create data
     std::vector<int32_t> reference_max(1, 0);
