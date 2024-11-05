b835502d454d10fed33526ee48ebeceab6a4b11b
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-11-05 07:43:29 +0300

    Update ops_mpi.cpp

--- tasks/mpi/deryabin_m_symbol_frequency/src/ops_mpi.cpp
+++ tasks/mpi/deryabin_m_symbol_frequency/src/ops_mpi.cpp
@@ -9,7 +9,7 @@ using namespace std::chrono_literals;
 bool deryabin_m_symbol_frequency_mpi::SymbolFrequencyMPITaskSequential::pre_processing() {
     internal_order_test();
     // Init value for input and output
-    input_str_ = std::string(taskData->inputs_count[0], char());
+    input_str_ = std::vector<char>(taskData->inputs_count[0]);
     auto* tmp_ptr = reinterpret_cast<char*>(taskData->inputs[0]);
     for (unsigned i = 0; i < taskData->inputs_count[0]; i++) {
         input_str_[i] = tmp_ptr[i];
@@ -54,20 +54,20 @@ bool deryabin_m_symbol_frequency_mpi::SymbolFrequencyMPITaskParallel::pre_proces
     boost::mpi::broadcast(world, delta, 0);
     boost::mpi::broadcast(world, ostatock, 0);
     boost::mpi::broadcast(world, input_symbol_, 0);
-    local_input_str_ = std::string();
+    local_input_str_ = std::vector<char>(delta);
     if (world.rank() == 0) {
         // Init value for input
-        input_str_ = std::string(taskData->inputs_count[0], char());
+        input_str_ = std::vector<char>(taskData->inputs_count[0]);
         auto* tmp_ptr = reinterpret_cast<char*>(taskData->inputs[0]);
         for (unsigned i = 0; i < taskData->inputs_count[0]; i++) {
             input_str_[i] = tmp_ptr[i];
         }
-        local_input_str_ = std::string(input_str_, 0, delta + 1);
         for (int proc = 1; proc < world.size(); proc++) {
             if (proc == world.size() - 1)
-                world.send(proc, 0, input_str_.data() + proc * delta + 1, delta + ostatock);
+                local_input_str_.resize(delta + ostatock);
+                world.send(proc, 0, input_str_.data() + proc * delta, delta + ostatock);
             else
-                world.send(proc, 0, input_str_.data() + proc * delta + 1, delta);
+                world.send(proc, 0, input_str_.data() + proc * delta, delta);
         }
     }
     if (world.rank() == world.size() - 1) {
