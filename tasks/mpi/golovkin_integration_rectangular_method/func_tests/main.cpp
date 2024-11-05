55c4e75402e33cbabbe79e6b902505cab4062fa0
Maksim Golovkin <Mxaqq>
2024-11-05 03:46:00 +0300

    fix4test

--- tasks/mpi/golovkin_integration_rectangular_method/func_tests/main.cpp
+++ tasks/mpi/golovkin_integration_rectangular_method/func_tests/main.cpp
@@ -1,4 +1,4 @@
-﻿// Golovkin Maksims
+﻿// Golovkin Maksim
 #define _USE_MATH_DEFINES
 #include <gtest/gtest.h>
 
@@ -10,6 +10,68 @@
 
 #include "mpi/golovkin_integration_rectangular_method/include/ops_mpi.hpp"
 
+TEST(golovkin_integration_rectangular_method, test_constant_function) {
+  boost::mpi::communicator world;
+  std::vector<double> global_result(1, 0);
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  double a = 0.0;
+  double b = 5.0;
+  double epsilon = 0.1;
+
+  if (world.rank() == 0) {
+    // Инициализация данных на нулевом процессе
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&a));
+    taskDataPar->inputs_count.emplace_back(1);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&b));
+    taskDataPar->inputs_count.emplace_back(1);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&epsilon));
+    taskDataPar->inputs_count.emplace_back(1);
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_result.data()));
+    taskDataPar->outputs_count.emplace_back(global_result.size());
+  }
+
+  // Синхронизация перед запуском параллельной задачи
+  world.barrier();
+
+  golovkin_integration_rectangular_method::MPIIntegralCalculator parallelTask(taskDataPar);
+
+  // Выполнение параллельных задач на всех процессах
+  ASSERT_EQ(parallelTask.validation(), true);
+  parallelTask.pre_processing();
+  parallelTask.run();
+  parallelTask.post_processing();
+
+  // Синхронизация после завершения параллельных задач
+  world.barrier();
+
+  if (world.rank() == 0) {
+    // Создание и инициализация данных для последовательной задачи на нулевом процессе
+    std::vector<double> reference_result(1, 0);
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&a));
+    taskDataSeq->inputs_count.emplace_back(1);
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&b));
+    taskDataSeq->inputs_count.emplace_back(1);
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&epsilon));
+    taskDataSeq->inputs_count.emplace_back(1);
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_result.data()));
+    taskDataSeq->outputs_count.emplace_back(reference_result.size());
+
+    // Выполнение последовательной задачи
+    golovkin_integration_rectangular_method::MPIIntegralCalculator sequentialTask(taskDataSeq);
+    ASSERT_EQ(sequentialTask.validation(), true);
+    sequentialTask.pre_processing();
+    sequentialTask.run();
+    sequentialTask.post_processing();
+
+    // Сравнение результатов параллельной и последовательной интеграции
+    ASSERT_NEAR(reference_result[0], global_result[0], 1e-2);
+  }
+  world.barrier();
+}
+
 TEST(golovkin_integration_rectangular_method, test_square_function) {
   boost::mpi::communicator world;
   std::vector<double> global_result(1, 0);
@@ -17,6 +79,7 @@ TEST(golovkin_integration_rectangular_method, test_square_function) {
 
   double a = 0.0;
   double b = 2.5;
+
   double epsilon = 0.1;
 
   if (world.rank() == 0) {
@@ -30,15 +93,12 @@ TEST(golovkin_integration_rectangular_method, test_square_function) {
     taskDataPar->outputs_count.emplace_back(global_result.size());
   }
 
-  world.barrier();  // Синхронизация перед параллельной задачей
-
   golovkin_integration_rectangular_method::MPIIntegralCalculator parallelTask(taskDataPar);
+
   ASSERT_EQ(parallelTask.validation(), true);
   parallelTask.pre_processing();
   parallelTask.run();
   parallelTask.post_processing();
-
-  world.barrier();  // Синхронизация после параллельной задачи
 
   if (world.rank() == 0) {
     std::vector<double> reference_result(1, 0);
@@ -54,6 +114,7 @@ TEST(golovkin_integration_rectangular_method, test_square_function) {
     taskDataSeq->outputs_count.emplace_back(reference_result.size());
 
     golovkin_integration_rectangular_method::MPIIntegralCalculator sequentialTask(taskDataSeq);
+
     ASSERT_EQ(sequentialTask.validation(), true);
     sequentialTask.pre_processing();
     sequentialTask.run();
@@ -61,16 +122,14 @@ TEST(golovkin_integration_rectangular_method, test_square_function) {
 
     ASSERT_NEAR(reference_result[0], global_result[0], 1e-2);
   }
-
-  world.barrier();  // Финальная синхронизация
 }
-
 TEST(golovkin_integration_rectangular_method, test_sine_function) {
   boost::mpi::communicator world;
   std::vector<double> global_result(1, 0);
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
 
   double a = 0.0;
+
   double b = M_PI;
   double epsilon = 0.1;
 
@@ -85,15 +144,12 @@ TEST(golovkin_integration_rectangular_method, test_sine_function) {
     taskDataPar->outputs_count.emplace_back(global_result.size());
   }
 
-  world.barrier();
-
   golovkin_integration_rectangular_method::MPIIntegralCalculator parallelTask(taskDataPar);
+
   ASSERT_EQ(parallelTask.validation(), true);
   parallelTask.pre_processing();
   parallelTask.run();
   parallelTask.post_processing();
-
-  world.barrier();
 
   if (world.rank() == 0) {
     std::vector<double> reference_result(1, 0);
@@ -109,6 +165,7 @@ TEST(golovkin_integration_rectangular_method, test_sine_function) {
     taskDataSeq->outputs_count.emplace_back(reference_result.size());
 
     golovkin_integration_rectangular_method::MPIIntegralCalculator sequentialTask(taskDataSeq);
+
     ASSERT_EQ(sequentialTask.validation(), true);
     sequentialTask.pre_processing();
     sequentialTask.run();
@@ -116,8 +173,6 @@ TEST(golovkin_integration_rectangular_method, test_sine_function) {
 
     ASSERT_NEAR(reference_result[0], global_result[0], 1e-2);
   }
-
-  world.barrier();
 }
 
 TEST(golovkin_integration_rectangular_method, test_exponential_function) {
@@ -126,7 +181,7 @@ TEST(golovkin_integration_rectangular_method, test_exponential_function) {
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
 
   double a = 0.0;
-  double b = 2.5;
+  double b = 2.5;  // Integrating e^x from 0 to 1
   double epsilon = 0.1;
 
   if (world.rank() == 0) {
@@ -140,15 +195,12 @@ TEST(golovkin_integration_rectangular_method, test_exponential_function) {
     taskDataPar->outputs_count.emplace_back(global_result.size());
   }
 
-  world.barrier();
-
   golovkin_integration_rectangular_method::MPIIntegralCalculator parallelTask(taskDataPar);
+
   ASSERT_EQ(parallelTask.validation(), true);
   parallelTask.pre_processing();
   parallelTask.run();
   parallelTask.post_processing();
-
-  world.barrier();
 
   if (world.rank() == 0) {
     std::vector<double> reference_result(1, 0);
@@ -164,6 +216,7 @@ TEST(golovkin_integration_rectangular_method, test_exponential_function) {
     taskDataSeq->outputs_count.emplace_back(reference_result.size());
 
     golovkin_integration_rectangular_method::MPIIntegralCalculator sequentialTask(taskDataSeq);
+
     ASSERT_EQ(sequentialTask.validation(), true);
     sequentialTask.pre_processing();
     sequentialTask.run();
@@ -171,8 +224,6 @@ TEST(golovkin_integration_rectangular_method, test_exponential_function) {
 
     ASSERT_NEAR(reference_result[0], global_result[0], 1e-2);
   }
-
-  world.barrier();
 }
 
 TEST(golovkin_integration_rectangular_method, test_polynomial_function) {
@@ -181,7 +232,7 @@ TEST(golovkin_integration_rectangular_method, test_polynomial_function) {
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
 
   double a = 1.0;
-  double b = 2.5;
+  double b = 2.5;  // Integrating f(x) = x^3 from 1 to 3
   double epsilon = 0.1;
 
   if (world.rank() == 0) {
@@ -195,15 +246,12 @@ TEST(golovkin_integration_rectangular_method, test_polynomial_function) {
     taskDataPar->outputs_count.emplace_back(global_result.size());
   }
 
-  world.barrier();
-
   golovkin_integration_rectangular_method::MPIIntegralCalculator parallelTask(taskDataPar);
+
   ASSERT_EQ(parallelTask.validation(), true);
   parallelTask.pre_processing();
   parallelTask.run();
   parallelTask.post_processing();
-
-  world.barrier();
 
   if (world.rank() == 0) {
     std::vector<double> reference_result(1, 0);
@@ -219,6 +267,7 @@ TEST(golovkin_integration_rectangular_method, test_polynomial_function) {
     taskDataSeq->outputs_count.emplace_back(reference_result.size());
 
     golovkin_integration_rectangular_method::MPIIntegralCalculator sequentialTask(taskDataSeq);
+
     ASSERT_EQ(sequentialTask.validation(), true);
     sequentialTask.pre_processing();
     sequentialTask.run();
@@ -226,6 +275,4 @@ TEST(golovkin_integration_rectangular_method, test_polynomial_function) {
 
     ASSERT_NEAR(reference_result[0], global_result[0], 1e-2);
   }
-
-  world.barrier();
 }
