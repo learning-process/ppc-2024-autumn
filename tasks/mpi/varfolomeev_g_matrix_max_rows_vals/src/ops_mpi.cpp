0f6e75d3d7ab01aa10d90ae6ae9ef89c2325ed31
thunderbird <greg.varfolomeev@yandex.ru>
2024-11-06 14:54:51 +0300

    transform is my enemy?

--- tasks/mpi/varfolomeev_g_matrix_max_rows_vals/src/ops_mpi.cpp
+++ tasks/mpi/varfolomeev_g_matrix_max_rows_vals/src/ops_mpi.cpp
@@ -115,9 +115,12 @@ bool varfolomeev_g_matrix_max_rows_vals_mpi::MaxInRowsParallel::run() {
 
   if (world.size() == 1) {
     std::vector<int> local_maxes(size_m);
-    std::transform(mtr.begin(), mtr.end(), local_maxes.begin(), [this](int val) {
-      return *std::max_element(mtr.begin() + val * size_m, mtr.begin() + (val + 1) * size_m);
-    });
+    // std::transform(mtr.begin(), mtr.end(), local_maxes.begin(), [this](int val) {
+    //   return *std::max_element(mtr.begin() + val * size_m, mtr.begin() + (val + 1) * size_m);
+    // });
+    for (int i = 0; i < size_m; ++i) {
+      local_maxes[i] = *std::max_element(mtr.begin() + i * size_n, mtr.begin() + (i + 1) * size_n);
+    }
     res_vec = local_maxes;
     return true;
   }
@@ -141,7 +144,7 @@ bool varfolomeev_g_matrix_max_rows_vals_mpi::MaxInRowsParallel::run() {
         world.send(i + 1, TERMINATE, &continue_flag, 1);
         world.send(i + 1, SEND_DATA, distr_vec.data() + (row + i) * size_n, size_n);
       }
-
+      // MPI_Barrier(MPI_COMM_WORLD);  // Вход в КС: барьерная синхронизация
       for (int i = 0; i < std::min(world.size() - 1, size_m - row); i++) {
         world.recv(i + 1, SEND_RESULT, max_vec.data() + row + i, 1);
       }
@@ -163,6 +166,7 @@ bool varfolomeev_g_matrix_max_rows_vals_mpi::MaxInRowsParallel::run() {
       world.recv(0, SEND_DATA, distr_vec.data(), size_n);
       out = *std::max_element(distr_vec.begin(), distr_vec.end());
       world.send(0, SEND_RESULT, &out, 1);
+      // MPI_Barrier(MPI_COMM_WORLD);  // Выход из КС: барьерная синхронизация
     }
   }
   return true;
