46a1961c7c767e6cb6c1994b81b451c83c8f77e6
Matthewsoul124 <132999776+Matthewsoul124@users.noreply.github.com>
2024-11-07 00:19:38 +0300

    fix3

--- tasks/mpi/fyodorov_m_num_of_orderly_violations/func_tests/main.cpp
+++ tasks/mpi/fyodorov_m_num_of_orderly_violations/func_tests/main.cpp
@@ -111,6 +111,46 @@ TEST(fyodorov_m_num_of_orderly_violations_mpi, Test_Count_Violations_Random_1000
   }
 }
 
+TEST(fyodorov_m_num_of_orderly_violations_mpi, Test_Count_Violations_Random_3000) {
+  boost::mpi::communicator world;
+  std::vector<int> global_vec;
+  const int count = 3000;
+  std::vector<int32_t> global_violations(1, 0);
+
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    global_vec = fyodorov_m_num_of_orderly_violations_mpi::getRandomVector(count);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataPar->inputs_count.emplace_back(global_vec.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_violations.data()));
+    taskDataPar->outputs_count.emplace_back(global_violations.size());
+  }
+
+  fyodorov_m_num_of_orderly_violations_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+    std::vector<int32_t> reference_violations(1, 0);
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataSeq->inputs_count.emplace_back(global_vec.size());
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_violations.data()));
+    taskDataSeq->outputs_count.emplace_back(reference_violations.size());
+
+    fyodorov_m_num_of_orderly_violations_mpi::TestMPITaskSequential testMpiTaskSequential(taskDataSeq);
+    ASSERT_EQ(testMpiTaskSequential.validation(), true);
+    testMpiTaskSequential.pre_processing();
+    testMpiTaskSequential.run();
+    testMpiTaskSequential.post_processing();
+
+    ASSERT_EQ(reference_violations[0], global_violations[0]);
+  }
+}
+
 TEST(fyodorov_m_num_of_orderly_violations_mpi, Test_Count_Violations_Random_10) {
   boost::mpi::communicator world;
   std::vector<int> global_vec;
