57b3cabe14bcda3d25db46e0c042b6e94318b5a5
thunderbird <greg.varfolomeev@yandex.ru>
2024-11-06 20:17:33 +0300

    emptiness & generators tests

--- tasks/mpi/varfolomeev_g_matrix_max_rows_vals/func_tests/main.cpp
+++ tasks/mpi/varfolomeev_g_matrix_max_rows_vals/func_tests/main.cpp
@@ -7,6 +7,90 @@
 
 #include "mpi/varfolomeev_g_matrix_max_rows_vals/include/ops_mpi.hpp"
 
+TEST(varfolomeev_g_matrix_max_rows_mpi, Test_Empty_Matrix) {
+  int size_m = 0;
+  int size_n = 0;
+
+  boost::mpi::communicator world;
+
+  std::vector<int> global_mat;
+  std::vector<int32_t> global_max(size_m, 0);
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  taskDataPar->inputs_count.emplace_back(size_m);
+  taskDataPar->inputs_count.emplace_back(size_n);
+  if (world.rank() == 0) {
+    global_mat = varfolomeev_g_matrix_max_rows_vals_mpi::getRandomVector(size_n * size_m);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(global_mat.data()));
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(global_max.data()));
+    taskDataPar->outputs_count.emplace_back(global_max.size());
+  }
+
+  varfolomeev_g_matrix_max_rows_vals_mpi::MaxInRowsParallel maxInRowsParallel(taskDataPar);
+  ASSERT_EQ(maxInRowsParallel.validation(), true);
+  maxInRowsParallel.pre_processing();
+  maxInRowsParallel.run();
+  maxInRowsParallel.post_processing();
+  if (world.rank() == 0) {
+    // Create data
+    std::vector<int32_t> reference_max(size_m, 0);
+    // Create TaskData
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(global_mat.data()));
+    taskDataSeq->inputs_count.emplace_back(size_m);
+    taskDataSeq->inputs_count.emplace_back(size_n);
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(reference_max.data()));
+    taskDataSeq->outputs_count.emplace_back(reference_max.size());
+
+    // Create Task
+    varfolomeev_g_matrix_max_rows_vals_mpi::MaxInRowsSequential maxInRowsSequential(taskDataSeq);
+    ASSERT_EQ(maxInRowsSequential.validation(), true);
+    maxInRowsSequential.pre_processing();
+    maxInRowsSequential.run();
+    maxInRowsSequential.post_processing();
+
+    ASSERT_EQ((int)global_max.size(), 0);
+  }
+}
+
+TEST(varfolomeev_g_matrix_max_rows_mpi, Test_getRandomVector_generator) {
+  int sz = 100;
+  std::vector<int> vec = varfolomeev_g_matrix_max_rows_vals_mpi::getRandomVector(sz);
+
+  // Проверка размера вектора
+  ASSERT_EQ(vec.size(), sz);
+
+  // Проверка, что все элементы находятся в диапазоне от -100 до 99
+  for (int i = 0; i < sz; ++i) {
+    ASSERT_GE(vec[i], -100);
+    ASSERT_LT(vec[i], 100);
+  }
+}
+
+TEST(varfolomeev_g_matrix_max_rows_mpi, Test_generateMatrix) {
+  int rows = 5;
+  int cols = 10;
+  int a = -50;
+  int b = 50;
+
+  std::vector<std::vector<int>> matrix = varfolomeev_g_matrix_max_rows_vals_mpi::generateMatrix(rows, cols, a, b);
+
+  // Проверка размера матрицы
+  ASSERT_EQ(matrix.size(), rows);
+  for (int i = 0; i < rows; ++i) {
+    ASSERT_EQ(matrix[i].size(), cols);
+  }
+
+  // Проверка, что все элементы находятся в диапазоне от a до b
+  for (int i = 0; i < rows; ++i) {
+    for (int j = 0; j < cols; ++j) {
+      ASSERT_GE(matrix[i][j], a);
+      ASSERT_LE(matrix[i][j], b);
+    }
+  }
+}
+
 TEST(varfolomeev_g_matrix_max_rows_mpi, Test_gen_5x5_Matrix) {
   int size_m = 5;
   int size_n = 5;
