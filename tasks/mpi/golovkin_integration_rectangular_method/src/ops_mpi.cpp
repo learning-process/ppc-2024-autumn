3f77777f49631e5cf0b35b73d91b9176df90ad55
Maksim Golovkin <Mxaqq>
2024-11-06 13:57:54 +0300

    fix32test

--- tasks/mpi/golovkin_integration_rectangular_method/src/ops_mpi.cpp
+++ tasks/mpi/golovkin_integration_rectangular_method/src/ops_mpi.cpp
@@ -25,7 +25,6 @@ bool MPIIntegralCalculator::validation() {
 }
 
 bool MPIIntegralCalculator::pre_processing() {
-
   if (world.rank() == 0) {
     a = *reinterpret_cast<double*>(taskData->inputs[0]);
     b = *reinterpret_cast<double*>(taskData->inputs[1]);
@@ -41,26 +40,24 @@ bool MPIIntegralCalculator::pre_processing() {
   if (cnt_of_splits <= 0) return false;
 
   h = (b - a) / cnt_of_splits;  // Вычисление ширины подынтервала
-  // std::cout << "Process " << world.rank() << " - a: " << a << ", b: " << b << ", cnt_of_splits: " << cnt_of_splits
-  // << ", h: " << h << std::endl;
+    std::cout << "Process " << world.rank() << " - a: " << a << ", b: " << b << ", cnt_of_splits: " << cnt_of_splits<< ", h: " << h << std::endl;
 
   return true;
 }
 
 bool MPIIntegralCalculator::run() {
-
   // Проверка, что cnt_of_splits, a, и h инициализированы и имеют корректные значения
   if (cnt_of_splits <= 0 || h <= 0.0 || a >= b) {
     std::cerr << "Process " << world.rank() << ": Invalid configuration (cnt_of_splits, h, or range a-b)" << std::endl;
     return false;
   }
-
+  std::cout << "Process ";
   // Делим работу между процессами
   int splits_per_proc = cnt_of_splits / world.size();
   int remaining_splits = cnt_of_splits % world.size();
   int start = world.rank() * splits_per_proc + std::min(world.rank(), remaining_splits);
   int end = start + splits_per_proc + (world.rank() < remaining_splits ? 1 : 0);
-
+  std::cout << "Process1 ";
   // Проверка диапазона
   if (start >= end) {
     std::cerr << "Process " << world.rank() << " has no work to do (start >= end)." << std::endl;
@@ -74,21 +71,20 @@ bool MPIIntegralCalculator::run() {
     }
     local_res = local_result * h;  // Умножаем на ширину подынтервала
   }
-
+  std::cout << "Process2 ";
   // Сбор результатов, проверка глобальной синхронизации
   double local_global_res = 0.0;
   MPI_Reduce(&local_res, &local_global_res, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
+  std::cout << "Process3 ";
   if (world.rank() == 0) {
     global_res = local_global_res;
     std::cout << "Root process has global result after reduction: " << global_res << std::endl;
   }
-
+  std::cout << "Process4 ";
   return true;
 }
 
 bool MPIIntegralCalculator::post_processing() {
-
   if (world.rank() == 0) {
     if (taskData->outputs.empty()) return false;
     *reinterpret_cast<double*>(taskData->outputs[0]) = global_res;
