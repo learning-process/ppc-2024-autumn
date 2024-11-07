0fc38ab272723e2d0ae2396faa297f205266d263
DZaytsev4 <deniszaytsev79524555@gmail.com>
2024-11-07 18:37:52 +0300

    fix_preproc

--- tasks/mpi/zaytsev_d_num_of_alternations_signs/src/ops_mpi.cpp
+++ tasks/mpi/zaytsev_d_num_of_alternations_signs/src/ops_mpi.cpp
@@ -55,35 +55,10 @@ bool zaytsev_d_num_of_alternations_signs_mpi::TestMPITaskSequential::post_proces
 bool zaytsev_d_num_of_alternations_signs_mpi::TestMPITaskParallel::pre_processing() {
   internal_order_test();
 
-  unsigned int delta = 0;
-  unsigned int remainder = 0;
-
-  if (world.rank() == 0) {
-    delta = taskData->inputs_count[0] / world.size();
-    remainder = taskData->inputs_count[0] % world.size();
-  }
-
-  broadcast(world, delta, 0);
-  broadcast(world, remainder, 0);
-
   if (world.rank() == 0) {
     input_ = std::vector<int>(taskData->inputs_count[0]);
     auto* tmp_ptr = reinterpret_cast<int*>(taskData->inputs[0]);
     std::copy(tmp_ptr, tmp_ptr + taskData->inputs_count[0], input_.begin());
-
-    for (int proc = 1; proc < world.size(); proc++) {
-      int send_count = delta + (proc == world.size() - 1 ? remainder : 0);
-      world.send(proc, 0, input_.data() + proc * (int)delta, send_count);
-    }
-  }
-
-  int local_size = (int)delta + (world.rank() == world.size() - 1 ? remainder : 0);
-  local_input_ = std::vector<int>(local_size);
-
-  if (world.rank() == 0) {
-    local_input_ = std::vector<int>(input_.begin(), input_.begin() + local_size);
-  } else {
-    world.recv(0, 0, local_input_.data(), local_size);
   }
 
   res = 0;
@@ -100,6 +75,33 @@ bool zaytsev_d_num_of_alternations_signs_mpi::TestMPITaskParallel::validation() 
 
 bool zaytsev_d_num_of_alternations_signs_mpi::TestMPITaskParallel::run() {
   internal_order_test();
+
+  unsigned int delta = 0;
+  unsigned int remainder = 0;
+
+  if (world.rank() == 0) {
+    delta = taskData->inputs_count[0] / world.size();
+    remainder = taskData->inputs_count[0] % world.size();
+  }
+
+  broadcast(world, delta, 0);
+  broadcast(world, remainder, 0);
+
+  if (world.rank() == 0) {
+    for (int proc = 1; proc < world.size(); proc++) {
+      int send_count = delta + (proc == world.size() - 1 ? remainder : 0);
+      world.send(proc, 0, input_.data() + proc * (int)delta, send_count);
+    }
+  }
+
+  int local_size = (int)delta + (world.rank() == world.size() - 1 ? remainder : 0);
+  local_input_ = std::vector<int>(local_size);
+
+  if (world.rank() == 0) {
+    local_input_ = std::vector<int>(input_.begin(), input_.begin() + local_size);
+  } else {
+    world.recv(0, 0, local_input_.data(), local_size);
+  }
 
   int local_count = 0;
 
