71260fdb191c9f00334625f5da76d3b8194e4770
Ofuuse <Ofuuse@gmail.com>
2024-11-07 15:56:45 +0300

    the end??

--- tasks/mpi/durynichev_d_most_different_neighbor_elements/func_tests/main.cpp
+++ tasks/mpi/durynichev_d_most_different_neighbor_elements/func_tests/main.cpp
@@ -12,7 +12,7 @@ TEST(durynichev_d_most_different_neighbor_elements_mpi, default_vector) {
   std::vector<int> outputPar{0, 0};
   auto taskDataPar = std::make_shared<ppc::core::TaskData>();
   if (world.rank() == 0) {
-    input = durynichev_d_most_different_neighbor_elements_mpi::getRandomVector(10'000);
+    input = durynichev_d_most_different_neighbor_elements_mpi::getRandomVector(20'000);
     taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
     taskDataPar->inputs_count.emplace_back(input.size());
     taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(outputPar.data()));
@@ -50,44 +50,7 @@ TEST(durynichev_d_most_different_neighbor_elements_mpi, huge_vector) {
   std::vector<int> outputPar{0, 0};
   auto taskDataPar = std::make_shared<ppc::core::TaskData>();
   if (world.rank() == 0) {
-    input = durynichev_d_most_different_neighbor_elements_mpi::getRandomVector(20'000);
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
-    taskDataPar->inputs_count.emplace_back(input.size());
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(outputPar.data()));
-    taskDataPar->outputs_count.emplace_back(outputPar.size());
-  }
-
-  durynichev_d_most_different_neighbor_elements_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
-  ASSERT_TRUE(testMpiTaskParallel.validation());
-  testMpiTaskParallel.pre_processing();
-  testMpiTaskParallel.run();
-  testMpiTaskParallel.post_processing();
-
-  if (world.rank() == 0) {
-    std::vector<int> outputSeq{0, 0};
-    auto taskDataSeq = std::make_shared<ppc::core::TaskData>();
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
-    taskDataSeq->inputs_count.emplace_back(input.size());
-    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(outputSeq.data()));
-    taskDataSeq->outputs_count.emplace_back(outputSeq.size());
-
-    durynichev_d_most_different_neighbor_elements_mpi::TestMPITaskSequential testMpiTaskSequential(taskDataSeq);
-    ASSERT_TRUE(testMpiTaskSequential.validation());
-    testMpiTaskSequential.pre_processing();
-    testMpiTaskSequential.run();
-    testMpiTaskSequential.post_processing();
-
-    ASSERT_EQ(outputSeq, outputPar);
-  }
-}
-TEST(durynichev_d_most_different_neighbor_elements_mpi, small_vector) {
-  boost::mpi::communicator world;
-  std::vector<int> input;
-
-  std::vector<int> outputPar{0, 0};
-  auto taskDataPar = std::make_shared<ppc::core::TaskData>();
-  if (world.rank() == 0) {
-    input = durynichev_d_most_different_neighbor_elements_mpi::getRandomVector(10);
+    input = durynichev_d_most_different_neighbor_elements_mpi::getRandomVector(100'000);
     taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
     taskDataPar->inputs_count.emplace_back(input.size());
     taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(outputPar.data()));
