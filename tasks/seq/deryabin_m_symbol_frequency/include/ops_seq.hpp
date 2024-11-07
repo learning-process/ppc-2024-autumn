1f8d1454518551064c7844da9b71aa32d1414c60
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-11-07 06:18:20 +0300

    Update ops_seq.hpp

--- tasks/seq/deryabin_m_symbol_frequency/include/ops_seq.hpp
+++ tasks/seq/deryabin_m_symbol_frequency/include/ops_seq.hpp
@@ -6,16 +6,18 @@
 
 namespace deryabin_m_symbol_frequency_seq {
 
-	class SymbolFrequencyTaskSequential : public ppc::core::Task {
-	public:
-		explicit SymbolFrequencyTaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
-		bool pre_processing() override;
-		bool validation() override;
-		bool run() override;
-		bool post_processing() override;
+class SymbolFrequencyTaskSequential : public ppc::core::Task {
+ public:
+  explicit SymbolFrequencyTaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
 
-	private:
-		std::string input_str_{}; int frequency_{}; char input_symbol_{};
-	};
+ private:
+  std::string input_str_{};
+  int frequency_{};
+  char input_symbol_{};
+};
 
 }  // namespace deryabin_m_symbol_frequency_seq
