8a68ffed3f33602461de5d633531c79d65a3dd58
Elena <laganinaelena@gmail.com>
2024-11-07 13:44:53 +0300

    Test-7

--- tasks/mpi/laganina_e_sum_values_by_columns_matrix/include/ops_mpi.hpp
+++ tasks/mpi/laganina_e_sum_values_by_columns_matrix/include/ops_mpi.hpp
@@ -4,9 +4,6 @@
 
 #include <boost/mpi/collectives.hpp>
 #include <boost/mpi/communicator.hpp>
-#include <memory>
-#include <numeric>
-#include <utility>
 #include <vector>
 
 #include "core/task/include/task.hpp"
