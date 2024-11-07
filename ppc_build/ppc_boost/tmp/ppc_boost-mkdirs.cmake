342e8bec53154502806d9f6a1053771fa49d8e2e
Будажапова Екатерина <ka10nnn@yandex.ru>
2024-11-07 20:45:22 +0300

    cmake

---
+++ ppc_build/ppc_boost/tmp/ppc_boost-mkdirs.cmake
@@ -0,0 +1,27 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+
+# If CMAKE_DISABLE_SOURCE_CHANGES is set to true and the source directory is an
+# existing directory in our source tree, calling file(MAKE_DIRECTORY) on it
+# would cause a fatal error, even though it would be a no-op.
+if(NOT EXISTS "D:/Laba_ppc/ppc-2024-autumn/3rdparty/boost")
+  file(MAKE_DIRECTORY "D:/Laba_ppc/ppc-2024-autumn/3rdparty/boost")
+endif()
+file(MAKE_DIRECTORY
+  "D:/Laba_ppc/ppc_build/ppc_boost/build"
+  "D:/Laba_ppc/ppc_build/ppc_boost/install"
+  "D:/Laba_ppc/ppc_build/ppc_boost/tmp"
+  "D:/Laba_ppc/ppc_build/ppc_boost/src/ppc_boost-stamp"
+  "D:/Laba_ppc/ppc_build/ppc_boost/src"
+  "D:/Laba_ppc/ppc_build/ppc_boost/src/ppc_boost-stamp"
+)
+
+set(configSubDirs Debug;Release;MinSizeRel;RelWithDebInfo)
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "D:/Laba_ppc/ppc_build/ppc_boost/src/ppc_boost-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "D:/Laba_ppc/ppc_build/ppc_boost/src/ppc_boost-stamp${cfgdir}") # cfgdir has leading slash
+endif()
