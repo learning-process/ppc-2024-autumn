ccd4b27165b6d3f3968ce3ebdcc51fc203b70c7e
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-11-07 07:56:16 +0300

    Update main.cpp

--- tasks/mpi/deryabin_m_symbol_frequency/func_tests/main.cpp
+++ tasks/mpi/deryabin_m_symbol_frequency/func_tests/main.cpp
@@ -87,7 +87,7 @@ TEST(deryabin_m_symbol_frequency_mpi, test_empty) {
   ASSERT_EQ(0, global_frequency[0]);
 }
 
-TEST(deryabin_m_symbol_frequency_mpi, test_povtor) {
+TEST(deryabin_m_symbol_frequency_mpi, test_every_secondary) {
   boost::mpi::communicator world;
   std::vector<char> global_str;
   std::vector<char> input_symbol(1, 'A');
@@ -99,9 +99,9 @@ TEST(deryabin_m_symbol_frequency_mpi, test_povtor) {
     global_str = {'1', '2', '3', '4', '5', '6', '7', '8', '9', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
                   'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
                   'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    // std::random_device rd;
-    // std::mt19937 generator(rd());
-    // std::shuffle(global_str.begin(), global_str.end(), generator);
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::shuffle(global_str.begin(), global_str.end(), generator);
     for (size_t i = 0; i < global_str.size(); i++) {
       if (i % 2 == 0) {
         global_str[i] = input_symbol[0];
@@ -120,7 +120,28 @@ TEST(deryabin_m_symbol_frequency_mpi, test_povtor) {
   testMpiTaskParallel.pre_processing();
   testMpiTaskParallel.run();
   testMpiTaskParallel.post_processing();
-  ASSERT_EQ(30, global_frequency[0]);
+  
+  if (world.rank() == 0) {
+    // Create data
+    std::vector<int32_t> reference_frequency(1, 0);
+
+    // Create TaskData
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_str.data()));
+    taskDataSeq->inputs_count.emplace_back(global_str.size());
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_symbol.data()));
+    taskDataSeq->inputs_count.emplace_back(input_symbol.size());
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_frequency.data()));
+    taskDataSeq->outputs_count.emplace_back(reference_frequency.size());
+
+    // Create Task
+    deryabin_m_symbol_frequency_mpi::SymbolFrequencyMPITaskSequential testMpiTaskSequential(taskDataSeq);
+    ASSERT_EQ(testMpiTaskSequential.validation(), true);
+    testMpiTaskSequential.pre_processing();
+    testMpiTaskSequential.run();
+    testMpiTaskSequential.post_processing();
+
+    ASSERT_EQ(reference_frequency[0], global_frequency[0]);
 }
 
 TEST(deryabin_m_symbol_frequency_mpi, test_const) {
