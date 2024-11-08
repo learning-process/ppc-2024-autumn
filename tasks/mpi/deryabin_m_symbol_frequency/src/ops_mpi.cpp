cb6842c815959f1bf0d8f4031b666df74993e72b
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-11-09 02:31:40 +0300

    Update ops_mpi.cpp

--- tasks/mpi/deryabin_m_symbol_frequency/src/ops_mpi.cpp
+++ tasks/mpi/deryabin_m_symbol_frequency/src/ops_mpi.cpp
@@ -1,7 +1,6 @@
 #include "mpi/deryabin_m_symbol_frequency/include/ops_mpi.hpp"
 
 #include <thread>
-#include <vector>
 
 bool deryabin_m_symbol_frequency_mpi::SymbolFrequencyMPITaskSequential::pre_processing() {
   internal_order_test();
