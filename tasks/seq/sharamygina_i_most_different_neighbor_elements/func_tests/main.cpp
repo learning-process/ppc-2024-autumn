f84d871386433e725fdd5d5005154afa7f37f629
isharamygina <121282432+isharamygina@users.noreply.github.com>
2024-11-08 14:11:13 +0300

    Add files via upload

--- tasks/seq/sharamygina_i_most_different_neighbor_elements/func_tests/main.cpp
+++ tasks/seq/sharamygina_i_most_different_neighbor_elements/func_tests/main.cpp
@@ -1,4 +1,3 @@
-// Copyright 2024 Sharamygina Irina
 #include <gtest/gtest.h>
 
 #include <random>
@@ -7,122 +6,128 @@
 #include "seq/sharamygina_i_most_different_neighbor_elements/include/ops_seq.hpp"
 
 namespace sharamygina_i_most_different_neighbor_elements_seq {
-	void getRandomVector(std::vector<int>& v) {
-		std::random_device dev;
-		std::mt19937 gen(dev());
+void generator(std::vector<int> &v) {
+  std::random_device dev;
+  std::mt19937 gen(dev());
 
-		for (int i = 0; i < v.size(); ++i) {
-			v[i] = -1000 + gen() % 1000;
-		}
-	}
+  for (int i = 0; i < v.size(); ++i) {
+    v[i] = -1000 + gen() % 1000;
+  }
+}
 }  // namespace sharamygina_i_most_different_neighbor_elements_seq
 
 TEST(sharamygina_i_most_different_neighbor_elements_seq, wrong_data) {
-	std::vector<int> in = {};
-	std::vector<std::vector<int>> out(1);
+  std::vector<int> in = {};
+  std::vector<std::vector<int>> out(1);
 
-	std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-	taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
-	taskDataSeq->inputs_count.emplace_back(in.size());
-	taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
-	taskDataSeq->outputs_count.emplace_back(out.size());
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
 
-	sharamygina_i_most_different_neighbor_elements_seq::most_different_neighbor_elements_seq testTaskSequential(taskDataSeq);
-	ASSERT_EQ(testTaskSequential.validation(), false);
+  sharamygina_i_most_different_neighbor_elements_seq::most_different_neighbor_elements_seq testTaskSequential(
+      taskDataSeq);
+  ASSERT_EQ(testTaskSequential.validation(), false);
 }
 
 TEST(sharamygina_i_most_different_neighbor_elements_seq, minimum_size_test) {
-	std::vector<int> in(2);
-	sharamygina_i_most_different_neighbor_elements_seq::generator(in);
-	std::vector<int> out(1);
-	int ans = abs(in[0] - in[1]);
+  std::vector<int> in(2);
+  sharamygina_i_most_different_neighbor_elements_seq::generator(in);
+  std::vector<int> out(1);
+  int ans = abs(in[0] - in[1]);
 
-	std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-	taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
-	taskDataSeq->inputs_count.emplace_back(in.size());
-	taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
-	taskDataSeq->outputs_count.emplace_back(out.size());
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
 
-	sharamygina_i_most_different_neighbor_elements_seq::most_different_neighbor_elements_seq testTaskSequential(taskDataSeq);
-	ASSERT_EQ(testTaskSequential.validation(), true);
-	testTaskSequential.pre_processing();
-	testTaskSequential.run();
-	testTaskSequential.post_processing();
-	ASSERT_EQ(ans, out[0]);
+  sharamygina_i_most_different_neighbor_elements_seq::most_different_neighbor_elements_seq testTaskSequential(
+      taskDataSeq);
+  ASSERT_EQ(testTaskSequential.validation(), true);
+  testTaskSequential.pre_processing();
+  testTaskSequential.run();
+  testTaskSequential.post_processing();
+  ASSERT_EQ(ans, out[0]);
 }
 
 TEST(sharamygina_i_most_different_neighbor_elements_seq, random_test) {
-	std::vector<int> in = { 12, 23, 46, 84, 50, 10000, 12 };
-	std::vector<int> out(1);
-	int ans = 9988;
+  std::vector<int> in = {12, 23, 46, 84, 50, 10000, 12};
+  std::vector<int> out(1);
+  int ans = 9988;
 
-	std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-	taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
-	taskDataSeq->inputs_count.emplace_back(in.size());
-	taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
-	taskDataSeq->outputs_count.emplace_back(out.size());
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
 
-	sharamygina_i_most_different_neighbor_elements_seq::most_different_neighbor_elements_seq testTaskSequential(taskDataSeq);
-	ASSERT_EQ(testTaskSequential.validation(), true);
-	testTaskSequential.pre_processing();
-	testTaskSequential.run();
-	testTaskSequential.post_processing();
-	ASSERT_EQ(ans, out[0]);
+  sharamygina_i_most_different_neighbor_elements_seq::most_different_neighbor_elements_seq testTaskSequential(
+      taskDataSeq);
+  ASSERT_EQ(testTaskSequential.validation(), true);
+  testTaskSequential.pre_processing();
+  testTaskSequential.run();
+  testTaskSequential.post_processing();
+  ASSERT_EQ(ans, out[0]);
 }
 
 TEST(sharamygina_i_most_different_neighbor_elements_seq, ne_no_null_number) {
-	std::vector<int> in = { 0, 0, 0, 0, 0, 0, 0, 1000000, 0, 0 };
-	std::vector<int> out(1);
-	int ans = 1000000;
+  std::vector<int> in = {0, 0, 0, 0, 0, 0, 0, 1000000, 0, 0};
+  std::vector<int> out(1);
+  int ans = 1000000;
 
-	std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-	taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
-	taskDataSeq->inputs_count.emplace_back(in.size());
-	taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
-	taskDataSeq->outputs_count.emplace_back(out.size());
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
 
-	sharamygina_i_most_different_neighbor_elements_seq::most_different_neighbor_elements_seq testTaskSequential(taskDataSeq);
-	ASSERT_EQ(testTaskSequential.validation(), true);
-	testTaskSequential.pre_processing();
-	testTaskSequential.run();
-	testTaskSequential.post_processing();
-	ASSERT_EQ(ans, out[0]);
+  sharamygina_i_most_different_neighbor_elements_seq::most_different_neighbor_elements_seq testTaskSequential(
+      taskDataSeq);
+  ASSERT_EQ(testTaskSequential.validation(), true);
+  testTaskSequential.pre_processing();
+  testTaskSequential.run();
+  testTaskSequential.post_processing();
+  ASSERT_EQ(ans, out[0]);
 }
 
 TEST(sharamygina_i_most_different_neighbor_elements_seq, all_eqal_numbers) {
-	std::vector<int> in(12, 10);
-	std::vector<int> out(1);
-	int ans = 0;
+  std::vector<int> in(12, 10);
+  std::vector<int> out(1);
+  int ans = 0;
 
-	std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-	taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
-	taskDataSeq->inputs_count.emplace_back(in.size());
-	taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
-	taskDataSeq->outputs_count.emplace_back(out.size());
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
 
-	sharamygina_i_most_different_neighbor_elements_seq::most_different_neighbor_elements_seq testTaskSequential(taskDataSeq);
-	ASSERT_EQ(testTaskSequential.validation(), true);
-	testTaskSequential.pre_processing();
-	testTaskSequential.run();
-	testTaskSequential.post_processing();
-	ASSERT_EQ(ans, out[0]);
+  sharamygina_i_most_different_neighbor_elements_seq::most_different_neighbor_elements_seq testTaskSequential(
+      taskDataSeq);
+  ASSERT_EQ(testTaskSequential.validation(), true);
+  testTaskSequential.pre_processing();
+  testTaskSequential.run();
+  testTaskSequential.post_processing();
+  ASSERT_EQ(ans, out[0]);
 }
 
 TEST(sharamygina_i_most_different_neighbor_elements_seq, test_with_neg_numbers) {
-	std::vector<int> in = { -1000, -100000, -12 };
-	std::vector<int> out(1);
-	int ans = 99988;
+  std::vector<int> in = {-1000, -100000, -12};
+  std::vector<int> out(1);
+  int ans = 99988;
 
-	std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-	taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
-	taskDataSeq->inputs_count.emplace_back(in.size());
-	taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
-	taskDataSeq->outputs_count.emplace_back(out.size());
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
 
-	sharamygina_i_most_different_neighbor_elements_seq::most_different_neighbor_elements_seq testTaskSequential(taskDataSeq);
-	ASSERT_EQ(testTaskSequential.validation(), true);
-	testTaskSequential.pre_processing();
-	testTaskSequential.run();
-	testTaskSequential.post_processing();
-	ASSERT_EQ(ans, out[0]);
+  sharamygina_i_most_different_neighbor_elements_seq::most_different_neighbor_elements_seq testTaskSequential(
+      taskDataSeq);
+  ASSERT_EQ(testTaskSequential.validation(), true);
+  testTaskSequential.pre_processing();
+  testTaskSequential.run();
+  testTaskSequential.post_processing();
+  ASSERT_EQ(ans, out[0]);
 }
--- tasks/seq/sharamygina_i_most_different_neighbor_elements/include/ops_seq.hpp
+++ tasks/seq/sharamygina_i_most_different_neighbor_elements/include/ops_seq.hpp
@@ -1,4 +1,3 @@
-// Copyright 2024 Sharamygina Irina
 #pragma once
 
 #include <string>
@@ -8,18 +7,18 @@
 
 namespace sharamygina_i_most_different_neighbor_elements_seq {
 
-	class most_different_neighbor_elements_seq : public ppc::core::Task {
-	public:
-		explicit most_different_neighbor_elements_seq(std::shared_ptr<ppc::core::TaskData> taskData_)
-			: Task(std::move(taskData_)) {}
-		bool pre_processing() override;
-		bool validation() override;
-		bool run() override;
-		bool post_processing() override;
+class most_different_neighbor_elements_seq : public ppc::core::Task {
+ public:
+  explicit most_different_neighbor_elements_seq(std::shared_ptr<ppc::core::TaskData> taskData_)
+      : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
 
-	private:
-		std::vector<int> input_;
-		int res{};
-	};
+ private:
+  std::vector<int> input_;
+  int res{};
+};
 
-}  // namespace alputov_i_most_different_neighbor_elements_seq
+}  // namespace sharamygina_i_most_different_neighbor_elements_seq
