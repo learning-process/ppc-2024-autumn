d79412e3b8ae8422b712273935d252bab0298f95
andrewmeow <91527374+andrewmeow@users.noreply.github.com>
2024-11-08 18:02:06 +0300

    add getRandomString

--- tasks/mpi/zolotareva_a_count_of_words/perf_tests/main.cpp
+++ tasks/mpi/zolotareva_a_count_of_words/perf_tests/main.cpp
@@ -6,26 +6,33 @@
 #include "core/perf/include/perf.hpp"
 #include "mpi/zolotareva_a_count_of_words/include/ops_mpi.hpp"
 
+std::string getRandomString(int size, size_t *count_words) {
+  const std::string ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  std::string res = "";
+  *count_words = 1;
+  for (int i = 0; i < size; i++) {
+    int x = rand() % (ABC.size() + 1);
+    res += ABC[x];
+    if (rand() % 2 == 1 && i < size - 1) {
+      res += " ";
+      (*count_words) += 1;
+    }
+  }
+  return res;
+}
+
 TEST(mpi_zolotareva_a_count_of_words_perf_test, test_pipeline_run) {
   boost::mpi::communicator world;
   std::string global_string;
+  size_t answer = 0;
   size_t global_count = 0;
-  size_t answer = 162;
+
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
   if (world.rank() == 0) {
-    global_string =
-        "I was too young to be other than awed and puzzled by Doc Marlowe when I knew him. I was only sixteen when he "
-        "died. He was sixty-seven. There was that vast difference in our ages and there was a vaster difference in our "
-        "backgrounds. Doc Marlowe was a medicine-show man. He had been a lot of other things, too: a circus man, the "
-        "proprietor of a concession at Coney Island, a saloon-keeper; but in his fifties he had travelled around with "
-        "a tent-show troupe made up of a Mexican named Chickalilli, who threw knives, and a man called Professor "
-        "Jones, who played the banjo. Doc Marlowe would come out after the entertainment and harangue the crowd and "
-        "sell bottles of medicine for all kinds of ailments. I found out all this about him gradually, toward the "
-        "last, and after he died. When I first knew him, he represented the Wild West to me, and there was nobody I "
-        "admired so much.";
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_string.data()));
+    global_string = getRandomString(1000, &answer);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(global_string.data()));
     taskDataPar->inputs_count.emplace_back(global_string.size());
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(&global_count));
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(&global_count));
     taskDataPar->outputs_count.emplace_back(1);
   }
 
@@ -53,24 +60,18 @@ TEST(mpi_zolotareva_a_count_of_words_perf_test, test_pipeline_run) {
 TEST(mpi_zolotareva_a_count_of_words_perf_test, test_task_run) {
   boost::mpi::communicator world;
   std::string global_string;
+  size_t answer = 0;
   size_t global_count = 0;
-  size_t answer = 162;
+
+  global_string = getRandomString(1000, &answer);
+
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
 
   if (world.rank() == 0) {
-    global_string =
-        "I was too young to be other than awed and puzzled by Doc Marlowe when I knew him. I was only sixteen when he "
-        "died. He was sixty-seven. There was that vast difference in our ages and there was a vaster difference in our "
-        "backgrounds. Doc Marlowe was a medicine-show man. He had been a lot of other things, too: a circus man, the "
-        "proprietor of a concession at Coney Island, a saloon-keeper; but in his fifties he had travelled around with "
-        "a tent-show troupe made up of a Mexican named Chickalilli, who threw knives, and a man called Professor "
-        "Jones, who played the banjo. Doc Marlowe would come out after the entertainment and harangue the crowd and "
-        "sell bottles of medicine for all kinds of ailments. I found out all this about him gradually, toward the "
-        "last, and after he died. When I first knew him, he represented the Wild West to me, and there was nobody I "
-        "admired so much.";
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_string.data()));
+    global_string = getRandomString(1000, &answer);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(global_string.data()));
     taskDataPar->inputs_count.emplace_back(global_string.size());
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(&global_count));
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(&global_count));
     taskDataPar->outputs_count.emplace_back(1);
   }
 
