0677a0b95667a5072a69d1ae924acac012e5151a
myffii <132840123+myffii@users.noreply.github.com>
2024-11-08 18:49:31 +0300

    fix

--- tasks/mpi/nasedkin_e_matrix_column_max_value/src/ops_mpi.cpp
+++ tasks/mpi/nasedkin_e_matrix_column_max_value/src/ops_mpi.cpp
@@ -61,87 +61,70 @@ bool nasedkin_e_matrix_column_max_value_mpi::TestMPITaskParallel::pre_processing
   internal_order_test();
 
   if (world.rank() == 0) {
-    numCols = taskData->inputs_count[1];
-    numRows = taskData->inputs_count[2];
-    delta = numCols / world.size();
-    extra = numCols % world.size();
+      numCols = taskData->inputs_count[1];
+      numRows = taskData->inputs_count[2];
+      delta = numCols / world.size();
+      extra = numCols % world.size();
+
+      inputMatrix_ = std::vector<int>(taskData->inputs_count[0]);
+      auto* tmpPtr = reinterpret_cast<int*>(taskData->inputs[0]);
+      for (unsigned int i = 0; i < taskData->inputs_count[0]; i++) {
+          inputMatrix_[i] = tmpPtr[i];
+      }
   }
-
-  if (world.rank() == 0) {
-    // Init vectors
-    inputMatrix_ = std::vector<int>(taskData->inputs_count[0]);
-    auto* tmpPtr = reinterpret_cast<int*>(taskData->inputs[0]);
-    for (unsigned int i = 0; i < taskData->inputs_count[0]; i++) {
-      inputMatrix_[i] = tmpPtr[i];
-    }
-  } else {
-    inputMatrix_ = std::vector<int>(numCols * numRows, 0);
-  }
-
-  result_ = std::vector<int>(numCols, 0);
-
-  return true;
-}
-
-bool nasedkin_e_matrix_column_max_value_mpi::TestMPITaskParallel::validation() {
-  internal_order_test();
-  if (world.rank() == 0) {
-    if (taskData->inputs_count[1] == 0 || taskData->inputs_count[2] == 0) {
-      return false;
-    }
-    if (taskData->inputs.empty() || taskData->inputs_count[0] <= 0) {
-      return false;
-    }
-    if (taskData->inputs_count[1] != taskData->outputs_count[0]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-bool nasedkin_e_matrix_column_max_value_mpi::TestMPITaskParallel::run() {
-  internal_order_test();
 
   broadcast(world, numCols, 0);
   broadcast(world, numRows, 0);
   broadcast(world, delta, 0);
   broadcast(world, extra, 0);
 
+  if (world.rank() != 0) {
+      inputMatrix_.resize(numCols * numRows);
+  }
   broadcast(world, inputMatrix_.data(), numCols * numRows, 0);
+
+  result_ = std::vector<int>(numCols, 0);
+  return true;
+}
+
+bool nasedkin_e_matrix_column_max_value_mpi::TestMPITaskParallel::run() {
+  internal_order_test();
 
   int startCol = delta * world.rank();
   int lastCol = startCol + delta;
   if (world.rank() == world.size() - 1) {
-    lastCol += extra;
+      lastCol += extra;
   }
 
   std::vector<int> localMax;
   for (int j = startCol; j < lastCol; j++) {
-    auto maxElem = *std::max_element(inputMatrix_.begin() + j * numRows, inputMatrix_.begin() + (j + 1) * numRows);
-    localMax.push_back(maxElem);
+      auto maxElem = *std::max_element(inputMatrix_.begin() + j * numRows, inputMatrix_.begin() + (j + 1) * numRows);
+      localMax.push_back(maxElem);
   }
+
   localMax.resize(delta + (world.rank() == world.size() - 1 ? extra : 0));
 
   if (world.rank() == 0) {
-    std::vector<int> globalRes(numCols);
-    std::vector<int> sizes(world.size());
-    for (int i = 0; i < world.size(); i++) {
-      sizes[i] = delta + (i == world.size() - 1 ? extra : 0);
-    }
-    boost::mpi::gatherv(world, localMax.data(), localMax.size(), globalRes.data(), sizes, 0);
-    result_ = globalRes;
+      std::vector<int> globalRes(numCols);
+      std::vector<int> sizes(world.size());
+      for (int i = 0; i < world.size(); i++) {
+          sizes[i] = delta + (i == world.size() - 1 ? extra : 0);
+      }
+      gatherv(world, localMax.data(), localMax.size(), globalRes.data(), sizes, 0);
+      result_ = globalRes;
   } else {
-    boost::mpi::gatherv(world, localMax.data(), localMax.size(), 0);
+      gatherv(world, localMax.data(), localMax.size(), 0);
   }
   return true;
 }
 
 bool nasedkin_e_matrix_column_max_value_mpi::TestMPITaskParallel::post_processing() {
   internal_order_test();
+
   if (world.rank() == 0) {
-    for (int i = 0; i < numCols; i++) {
-      reinterpret_cast<int*>(taskData->outputs[0])[i] = result_[i];
-    }
+      for (int i = 0; i < numCols; i++) {
+          reinterpret_cast<int*>(taskData->outputs[0])[i] = result_[i];
+      }
   }
   return true;
 }
