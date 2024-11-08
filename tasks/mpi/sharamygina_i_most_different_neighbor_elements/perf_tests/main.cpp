ce7f767167ecaeb19b75f559c5bedc532bac5870
isharamygina <121282432+isharamygina@users.noreply.github.com>
2024-11-08 14:09:43 +0300

    Add files via upload

--- tasks/mpi/sharamygina_i_most_different_neighbor_elements/perf_tests/main.cpp
+++ tasks/mpi/sharamygina_i_most_different_neighbor_elements/perf_tests/main.cpp
@@ -1,4 +1,3 @@
-// Copyright 2024 Sharamygina Irina
 #include <gtest/gtest.h>
 
 #include <boost/mpi/timer.hpp>
@@ -9,77 +8,79 @@
 #include "mpi/sharamygina_i_most_different_neighbor_elements/include/ops_mpi.hpp"
 
 TEST(sharamygina_i_most_different_neighbor_elements_mpi, test_pipeline_run) {
-	boost::mpi::communicator world;
-	std::vector<int> global_vec(1000000, 0);
-	std::vector<int32_t> global_sum(1, 0);
-	// Create TaskData
-	std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
-	if (world.rank() == 0) {
-		taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
-		taskDataPar->inputs_count.emplace_back(global_vec.size());
-		taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_sum.data()));
-		taskDataPar->outputs_count.emplace_back(global_sum.size());
-	}
+  boost::mpi::communicator world;
+  std::vector<int> global_vec(1000000, 0);
+  std::vector<int32_t> global_sum(1, 0);
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+  if (world.rank() == 0) {
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataPar->inputs_count.emplace_back(global_vec.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_sum.data()));
+    taskDataPar->outputs_count.emplace_back(global_sum.size());
+  }
 
-	auto testMpiTaskParallel = std::make_shared<sharamygina_i_most_different_neighbor_elements_mpi::most_different_neighbor_elements_mpi>(taskDataPar);
-	ASSERT_EQ(testMpiTaskParallel->validation(), true);
-	testMpiTaskParallel->pre_processing();
-	testMpiTaskParallel->run();
-	testMpiTaskParallel->post_processing();
+  auto testMpiTaskParallel =
+      std::make_shared<sharamygina_i_most_different_neighbor_elements_mpi::most_different_neighbor_elements_mpi>(
+          taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel->validation(), true);
+  testMpiTaskParallel->pre_processing();
+  testMpiTaskParallel->run();
+  testMpiTaskParallel->post_processing();
 
-	// Create Perf attributes
-	auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
-	perfAttr->num_running = 10;
-	const boost::mpi::timer current_timer;
-	perfAttr->current_timer = [&] { return current_timer.elapsed(); };
+  // Create Perf attributes
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+  const boost::mpi::timer current_timer;
+  perfAttr->current_timer = [&] { return current_timer.elapsed(); };
 
-	// Create and init perf results
-	auto perfResults = std::make_shared<ppc::core::PerfResults>();
+  // Create and init perf results
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
 
-	// Create Perf analyzer
-	auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testMpiTaskParallel);
-	perfAnalyzer->pipeline_run(perfAttr, perfResults);
-	if (world.rank() == 0) {
-		ppc::core::Perf::print_perf_statistic(perfResults);
-		ASSERT_EQ(2, global_sum[0]);
-	}
+  // Create Perf analyzer
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testMpiTaskParallel);
+  perfAnalyzer->pipeline_run(perfAttr, perfResults);
+  if (world.rank() == 0) {
+    ppc::core::Perf::print_perf_statistic(perfResults);
+    ASSERT_EQ(0, global_sum[0]);
+  }
 }
 
 TEST(sharamygina_i_most_different_neighbor_elements_mpi, test_task_run) {
-	boost::mpi::communicator world;
-	std::vector<int> global_vec(1000000, 0);
-	std::vector<int32_t> global_sum(1, 0);
-	// Create TaskData
-	std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
-	if (world.rank() == 0) {
-		taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
-		taskDataPar->inputs_count.emplace_back(global_vec.size());
-		taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_sum.data()));
-		taskDataPar->outputs_count.emplace_back(global_sum.size());
-	}
+  boost::mpi::communicator world;
+  std::vector<int> global_vec(1000000, 0);
+  std::vector<int32_t> global_sum(1, 0);
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+  if (world.rank() == 0) {
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataPar->inputs_count.emplace_back(global_vec.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_sum.data()));
+    taskDataPar->outputs_count.emplace_back(global_sum.size());
+  }
 
-	auto testMpiTaskParallel =
-		std::make_shared<sharamygina_i_most_different_neighbor_elements_mpi::most_different_neighbor_elements_mpi>(
-			taskDataPar);
-	ASSERT_EQ(testMpiTaskParallel->validation(), true);
-	testMpiTaskParallel->pre_processing();
-	testMpiTaskParallel->run();
-	testMpiTaskParallel->post_processing();
+  auto testMpiTaskParallel =
+      std::make_shared<sharamygina_i_most_different_neighbor_elements_mpi::most_different_neighbor_elements_mpi>(
+          taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel->validation(), true);
+  testMpiTaskParallel->pre_processing();
+  testMpiTaskParallel->run();
+  testMpiTaskParallel->post_processing();
 
-	// Create Perf attributes
-	auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
-	perfAttr->num_running = 10;
-	const boost::mpi::timer current_timer;
-	perfAttr->current_timer = [&] { return current_timer.elapsed(); };
+  // Create Perf attributes
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+  const boost::mpi::timer current_timer;
+  perfAttr->current_timer = [&] { return current_timer.elapsed(); };
 
-	// Create and init perf results
-	auto perfResults = std::make_shared<ppc::core::PerfResults>();
+  // Create and init perf results
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
 
-	// Create Perf analyzer
-	auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testMpiTaskParallel);
-	perfAnalyzer->task_run(perfAttr, perfResults);
-	if (world.rank() == 0) {
-		ppc::core::Perf::print_perf_statistic(perfResults);
-		ASSERT_EQ(0, global_sum[0]);
-	}
+  // Create Perf analyzer
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testMpiTaskParallel);
+  perfAnalyzer->task_run(perfAttr, perfResults);
+  if (world.rank() == 0) {
+    ppc::core::Perf::print_perf_statistic(perfResults);
+    ASSERT_EQ(0, global_sum[0]);
+  }
 }
