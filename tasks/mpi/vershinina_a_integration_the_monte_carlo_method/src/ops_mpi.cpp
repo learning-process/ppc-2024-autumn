1615f6ac881823b9f2e044722264ec13d1b38bc6
Александра Вершинина <xx.vershinina.xx@yandex.ru>
2024-11-08 23:17:10 +0300

    com 17 fixed clang

--- tasks/mpi/vershinina_a_integration_the_monte_carlo_method/src/ops_mpi.cpp
+++ tasks/mpi/vershinina_a_integration_the_monte_carlo_method/src/ops_mpi.cpp
@@ -72,7 +72,7 @@ bool vershinina_a_integration_the_monte_carlo_method::TestMPITaskParallel::pre_p
   if (world.rank() == 0) {
     auto* pr = reinterpret_cast<double*>(taskData->inputs[0]);
     input_.resize(taskData->inputs_count[0]);
-    std::copy(pr, pr + input_.size(), input_.begin()); 
+    std::copy(pr, pr + input_.size(), input_.begin());
   }
   return true;
 }
