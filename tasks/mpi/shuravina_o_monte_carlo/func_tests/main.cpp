6dfcfc94d23a5fda85774213d4e2cf7191d4939c
ОКСАНА ШУРАВИНА <shyravina19102004@gmail.com>
2024-11-08 23:52:46 +0300

    10

--- tasks/mpi/shuravina_o_monte_carlo/func_tests/main.cpp
+++ tasks/mpi/shuravina_o_monte_carlo/func_tests/main.cpp
@@ -29,3 +29,26 @@ TEST(MonteCarloIntegrationTaskParallel, Test_Integration) {
     ASSERT_NEAR(expected_integral, out[0], 0.01);
   }
 }
+TEST(MonteCarloIntegrationTaskParallel, Test_Boundary_Conditions) {
+  boost::mpi::communicator world;
+  std::vector<double> out(1, 0.0);
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    taskDataPar->inputs.emplace_back(nullptr);
+    taskDataPar->inputs_count.emplace_back(0);
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
+    taskDataPar->outputs_count.emplace_back(out.size());
+  }
+
+  auto testMpiTaskParallel = std::make_shared<shuravina_o_monte_carlo::MonteCarloIntegrationTaskParallel>(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel->validation(), true);
+  testMpiTaskParallel->pre_processing();
+  testMpiTaskParallel->run();
+  testMpiTaskParallel->post_processing();
+
+  if (world.rank() == 0) {
+    double expected_integral = 1.0 / 3.0;
+    ASSERT_NEAR(expected_integral, out[0], 0.01);
+  }
+}
--- tasks/seq/shuravina_o_monte_carlo/func_tests/main.cpp
+++ tasks/seq/shuravina_o_monte_carlo/func_tests/main.cpp
@@ -21,3 +21,21 @@ TEST(MonteCarloIntegrationTaskSequential, Test_Integration) {
   double expected_integral = 1.0 / 3.0;
   ASSERT_NEAR(expected_integral, out[0], 0.01);
 }
+TEST(MonteCarloIntegrationTaskSequential, Test_Boundary_Conditions) {
+  std::vector<double> out(1, 0.0);
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+
+  taskDataSeq->inputs.emplace_back(nullptr);
+  taskDataSeq->inputs_count.emplace_back(0);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  auto testTaskSequential = std::make_shared<shuravina_o_monte_carlo::MonteCarloIntegrationTaskSequential>(taskDataSeq);
+  ASSERT_EQ(testTaskSequential->validation(), true);
+  testTaskSequential->pre_processing();
+  testTaskSequential->run();
+  testTaskSequential->post_processing();
+
+  double expected_integral = 1.0 / 3.0;
+  ASSERT_NEAR(expected_integral, out[0], 0.01);
+}
