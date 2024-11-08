3184d4b15a16ff0e3b71c9b945f79dd1e901bd25
Александра Вершинина <xx.vershinina.xx@yandex.ru>
2024-11-08 18:34:47 +0300

    com 14

--- tasks/mpi/vershinina_a_integration_the_monte_carlo_method/src/ops_mpi.cpp
+++ tasks/mpi/vershinina_a_integration_the_monte_carlo_method/src/ops_mpi.cpp
@@ -12,7 +12,7 @@ using namespace std::chrono_literals;
 std::vector<double> vershinina_a_integration_the_monte_carlo_method::getRandomVector(double sz) {
   std::random_device dev;
   std::mt19937 gen(dev());
-  std::uniform_int_distribution<> distr(0, 50);
+  std::uniform_int_distribution<> distr(10, 60);
   std::vector<double> vec(sz);
   vec[0] = distr(gen);
   vec[1] = vec[0] + distr(gen);
