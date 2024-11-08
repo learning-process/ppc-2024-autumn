80c59861367090d6c5232bb9b1bfcd84173057de
SingularityNN <mihamakhov@gmail.com>
2024-11-08 22:32:57 +0300

    deleted EmptyStrTest

--- tasks/mpi/makhov_m_num_of_diff_elements_in_two_str/func_tests/main.cpp
+++ tasks/mpi/makhov_m_num_of_diff_elements_in_two_str/func_tests/main.cpp
@@ -19,30 +19,6 @@ std::string getRandStr(size_t size_, char min = '0', char max = '9') {
   return str;
 }
 
-TEST(makhov_m_num_of_diff_elements_in_two_str_mpi, EmptyStrings) {
-  boost::mpi::communicator world;
-  std::string str1;
-  std::string str2;
-  std::vector<int32_t> global_sum(1, 0);
-  str1 = "";
-  str2 = "";
-  // Create TaskData
-  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
-
-  if (world.rank() == 0) {
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(str1.data()));
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(str2.data()));
-    taskDataPar->inputs_count.emplace_back(str1.size());
-    taskDataPar->inputs_count.emplace_back(str2.size());
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(global_sum.data()));
-    taskDataPar->outputs_count.emplace_back(global_sum.size());
-  }
-
-  // Create Task
-  makhov_m_num_of_diff_elements_in_two_str_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
-  ASSERT_FALSE(testMpiTaskParallel.validation());
-}
-
 TEST(makhov_m_num_of_diff_elements_in_two_str_mpi, SameSizeRandomStrings) {
   boost::mpi::communicator world;
   std::string str1;
