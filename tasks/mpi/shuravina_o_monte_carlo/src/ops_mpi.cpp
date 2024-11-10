7cd5bc2a7df986648047d3821319c5b57553cd10
ОКСАНА ШУРАВИНА <shyravina19102004@gmail.com>
2024-11-11 00:04:46 +0300

    27

--- tasks/mpi/shuravina_o_monte_carlo/src/ops_mpi.cpp
+++ tasks/mpi/shuravina_o_monte_carlo/src/ops_mpi.cpp
@@ -23,10 +23,34 @@ bool shuravina_o_monte_carlo::MonteCarloIntegrationTaskParallel::validation() {
   return true;
 }
 
+#include <boost/mpi/collectives.hpp>
+#include <random>
+
+#include "mpi/shuravina_o_monte_carlo/include/ops_mpi.hpp"
+
+bool shuravina_o_monte_carlo::MonteCarloIntegrationTaskParallel::pre_processing() {
+  internal_order_test();
+  integral_value_ = 0.0;
+  return true;
+}
+
+bool shuravina_o_monte_carlo::MonteCarloIntegrationTaskParallel::validation() {
+  internal_order_test();
+  if (taskData->inputs_count.size() != 1 || taskData->outputs_count.size() != 1) {
+    return false;
+  }
+  if (taskData->inputs_count[0] != 0 || taskData->outputs_count[0] != 1) {
+    return false;
+  }
+  if (taskData->inputs[0] != nullptr || taskData->outputs[0] == nullptr) {
+    return false;
+  }
+  return true;
+}
+
 bool shuravina_o_monte_carlo::MonteCarloIntegrationTaskParallel::run() {
   internal_order_test();
   int num_processes = world.size();
-  int rank = world.rank();
 
   int local_num_points = num_points_ / num_processes;
 
@@ -44,6 +68,13 @@ bool shuravina_o_monte_carlo::MonteCarloIntegrationTaskParallel::run() {
   return true;
 }
 
+bool shuravina_o_monte_carlo::MonteCarloIntegrationTaskParallel::post_processing() {
+  internal_order_test();
+  if (world.rank() == 0) {
+    reinterpret_cast<double*>(taskData->outputs[0])[0] = integral_value_;
+  }
+  return true;
+}
 bool shuravina_o_monte_carlo::MonteCarloIntegrationTaskParallel::post_processing() {
   internal_order_test();
   if (world.rank() == 0) {
