32f8c5d1c88c2113b7153d37d20b62656c6899d1
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2024-11-17 23:17:32 +0300

    Update main.cpp

--- tasks/mpi/vavilov_v_contrast_enhancement/func_tests/main.cpp
+++ tasks/mpi/vavilov_v_contrast_enhancement/func_tests/main.cpp
@@ -68,23 +68,23 @@ TEST(vavilov_v_contrast_enhancement_mpi, IncorrectOutputSize) {
 TEST(vavilov_v_contrast_enhancement_mpi, NormalContrastEnhancement) {
   mpi::environment env;
   mpi::communicator world;
+
+  auto taskDataPar = std::make_shared<ppc::core::TaskData>();
+  std::vector<int> input = {10, 20, 30, 40, 50};
+  taskDataPar->inputs_count.emplace_back(input.size());
+  taskDataPar->outputs_count.emplace_back(input.size());
+  std::vector<int> output(input.size());
+  taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
+  taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(output.data()));
+
+  vavilov_v_contrast_enhancement_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+  ASSERT_TRUE(testMpiTaskParallel.validation());
+  ASSERT_TRUE(testMpiTaskParallel.pre_processing());
+  ASSERT_TRUE(testMpiTaskParallel.run());
+  ASSERT_TRUE(testMpiTaskParallel.post_processing());
+
   if (world.rank() == 0) {
-    auto taskDataPar = std::make_shared<ppc::core::TaskData>();
-    std::vector<int> input = {10, 20, 30, 40, 50};
-    std::vector<int> output(input.size());
     std::vector<int> expected_output = {0, 63, 127, 191, 255};
-
-    taskDataPar->inputs_count.emplace_back(input.size());
-    taskDataPar->outputs_count.emplace_back(input.size());
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(output.data()));
-
-    vavilov_v_contrast_enhancement_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
-    ASSERT_TRUE(testMpiTaskParallel.validation());
-    ASSERT_TRUE(testMpiTaskParallel.pre_processing());
-    ASSERT_TRUE(testMpiTaskParallel.run());
-    ASSERT_TRUE(testMpiTaskParallel.post_processing());
-
-    ASSERT_EQ(output, expected_output);
+    EXPECT_EQ(output, expected_output);
   }
 }
