4c53b6d0490d095073152609873fb9e18d8b5c3b
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2024-11-17 18:34:27 +0300

    Update main.cpp

--- tasks/mpi/vavilov_v_contrast_enhancement/func_tests/main.cpp
+++ tasks/mpi/vavilov_v_contrast_enhancement/func_tests/main.cpp
@@ -70,21 +70,38 @@ TEST(vavilov_v_contrast_enhancement_mpi, NormalContrastEnhancement) {
   mpi::communicator world;
 
   auto taskDataPar = std::make_shared<ppc::core::TaskData>();
-  std::vector<int> input = {10, 20, 30, 40, 50};
-  taskDataPar->inputs_count.emplace_back(input.size());
-  taskDataPar->outputs_count.emplace_back(input.size());
-  std::vector<int> output(input.size());
-  taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
-  taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(output.data()));
-
+  if (world.rank() == 0) {
+    std::vector<int> input = {10, 20, 30, 40, 50};
+    std::vector<int> expected_output = {0, 63, 127, 191, 255};
+    taskDataPar->inputs_count.emplace_back(input.size());
+    taskDataPar->outputs_count.emplace_back(input.size());
+    std::vector<int> output(input.size());
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(output.data()));
+  }
+  
   vavilov_v_contrast_enhancement_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
-  ASSERT_TRUE(testMpiTaskParallel.validation());
-  ASSERT_TRUE(testMpiTaskParallel.pre_processing());
-  ASSERT_TRUE(testMpiTaskParallel.run());
-  ASSERT_TRUE(testMpiTaskParallel.post_processing());
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
 
   if (world.rank() == 0) {
     std::vector<int> expected_output = {0, 63, 127, 191, 255};
-    EXPECT_EQ(output, expected_output);
+    std::vector<int> input_2 = {10, 20, 30, 40, 50};
+    auto taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs_count.emplace_back(input_2.size());
+    taskDataPar->outputs_count.emplace_back(input_2.size());
+    std::vector<int> output_2(input_2.size());
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(input_2.data()));
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(output_2.data()));
+    vavilov_v_contrast_enhancement_mpi::TestMPITaskSequential testMpiTaskSequential(taskDataSeq);
+    ASSERT_EQ(testMpiTaskSequential.validation(), true);
+    testMpiTaskSequential.pre_processing();
+    testMpiTaskSequential.run();
+    testMpiTaskSequential.post_processing();
+    
+    ASSERT_EQ(output, expected_output);
+    ASSERT_EQ(output_2, expected_output);
   }
 }
