167e902febdaa8e9df703de1a073d8b5ddf9d350
Egor1dzeN-learn <egormorozov123450@gmail.com>
2024-11-18 22:40:05 +0300

    add: tests

--- tasks/mpi/morozov_e_writers_readers/func_tests/main.cpp
+++ tasks/mpi/morozov_e_writers_readers/func_tests/main.cpp
@@ -80,7 +80,6 @@ TEST(morozov_e_writers_readers, Test_Main0) {
   std::vector<int> vec{0};
   std::vector<int> ans{1};
   int countIteration = 100;
-  morozov_e_writers_readers::TestMPITaskParallel obj(data);
   if (world.rank() == 0) {
     data->inputs.emplace_back(reinterpret_cast<uint8_t*>(vec.data()));
     data->inputs.emplace_back(reinterpret_cast<uint8_t*>(new int{countIteration}));
@@ -88,6 +87,7 @@ TEST(morozov_e_writers_readers, Test_Main0) {
     data->outputs.emplace_back(reinterpret_cast<uint8_t*>(ans.data()));
     data->outputs_count.emplace_back(ans.size());
   }
+  morozov_e_writers_readers::TestMPITaskParallel obj(data);
   ASSERT_TRUE(obj.validation());
   obj.pre_processing();
   obj.run();
@@ -96,49 +96,48 @@ TEST(morozov_e_writers_readers, Test_Main0) {
     ASSERT_EQ(ans[0], 0);
   }
 }
-// TEST(morozov_e_writers_readers, Test_Main1) {
-//   boost::mpi::communicator world;
-//   std::shared_ptr<ppc::core::TaskData> data = std::make_shared<ppc::core::TaskData>();
-//   std::vector<int> vec{0};
-//   std::vector<int> ans{100};
-//   int countIteration = 100;
-//   if (world.rank() == 0) {
-//     data->inputs.emplace_back(reinterpret_cast<uint8_t*>(vec.data()));
-//     data->inputs.emplace_back(reinterpret_cast<uint8_t*>(new int{countIteration}));
-//     data->inputs_count.emplace_back(vec.size());
-//     data->outputs.emplace_back(reinterpret_cast<uint8_t*>(ans.data()));
-//     data->outputs_count.emplace_back(ans.size());
-//     morozov_e_writers_readers::TestMPITaskParallel obj(data);
-//     ASSERT_TRUE(obj.validation());
-//     obj.pre_processing();
-//     obj.run();
-//     obj.post_processing();
-//   }
-//
-//   if (world.rank() == 0) {
-//     ASSERT_EQ(ans[0], 0);
-//   }
-// }
-// TEST(morozov_e_writers_readers, Test_Main2) {
-//   boost::mpi::communicator world;
-//   std::shared_ptr<ppc::core::TaskData> data = std::make_shared<ppc::core::TaskData>();
-//   std::vector<int> vec{0};
-//   std::vector<int> ans{INT_MAX};
-//   int countIteration = 100;
-//   if (world.rank() == 0) {
-//     data->inputs.emplace_back(reinterpret_cast<uint8_t*>(vec.data()));
-//     data->inputs.emplace_back(reinterpret_cast<uint8_t*>(new int{countIteration}));
-//     data->inputs_count.emplace_back(vec.size());
-//     data->outputs.emplace_back(reinterpret_cast<uint8_t*>(ans.data()));
-//     data->outputs_count.emplace_back(ans.size());
-//     morozov_e_writers_readers::TestMPITaskParallel obj(data);
-//     ASSERT_TRUE(obj.validation());
-//     obj.pre_processing();
-//     obj.run();
-//     obj.post_processing();
-//   }
-//
-//   if (world.rank() == 0) {
-//     ASSERT_EQ(ans[0], 0);
-//   }
-// }
+TEST(morozov_e_writers_readers, Test_Main1) {
+  boost::mpi::communicator world;
+  std::shared_ptr<ppc::core::TaskData> data = std::make_shared<ppc::core::TaskData>();
+  std::vector<int> vec{0};
+  std::vector<int> ans{100};
+  int countIteration = 100;
+
+  if (world.rank() == 0) {
+    data->inputs.emplace_back(reinterpret_cast<uint8_t*>(vec.data()));
+    data->inputs.emplace_back(reinterpret_cast<uint8_t*>(new int{countIteration}));
+    data->inputs_count.emplace_back(vec.size());
+    data->outputs.emplace_back(reinterpret_cast<uint8_t*>(ans.data()));
+    data->outputs_count.emplace_back(ans.size());
+  }
+  morozov_e_writers_readers::TestMPITaskParallel obj(data);
+  ASSERT_TRUE(obj.validation());
+  obj.pre_processing();
+  obj.run();
+  obj.post_processing();
+  if (world.rank() == 0) {
+    ASSERT_EQ(ans[0], 0);
+  }
+}
+TEST(morozov_e_writers_readers, Test_Main2) {
+  boost::mpi::communicator world;
+  std::shared_ptr<ppc::core::TaskData> data = std::make_shared<ppc::core::TaskData>();
+  std::vector<int> vec{0};
+  std::vector<int> ans{INT_MAX};
+  int countIteration = 100;
+  if (world.rank() == 0) {
+    data->inputs.emplace_back(reinterpret_cast<uint8_t*>(vec.data()));
+    data->inputs.emplace_back(reinterpret_cast<uint8_t*>(new int{countIteration}));
+    data->inputs_count.emplace_back(vec.size());
+    data->outputs.emplace_back(reinterpret_cast<uint8_t*>(ans.data()));
+    data->outputs_count.emplace_back(ans.size());
+  }
+  morozov_e_writers_readers::TestMPITaskParallel obj(data);
+  ASSERT_TRUE(obj.validation());
+  obj.pre_processing();
+  obj.run();
+  obj.post_processing();
+  if (world.rank() == 0) {
+    ASSERT_EQ(ans[0], 0);
+  }
+}
