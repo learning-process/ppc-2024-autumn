7147e170d1437b82c126e8f535fe1cedea9e87c6
0xG00SE <61384845+DSolo03@users.noreply.github.com>
2024-11-19 23:35:44 +0300

    remove main from test

--- tasks/mpi/solovyev_d_topology_hypercube/func_tests/main.cpp
+++ tasks/mpi/solovyev_d_topology_hypercube/func_tests/main.cpp
@@ -167,14 +167,3 @@ TEST(solovyev_d_topology_hypercube_mpi, Test_Transfer_Not_Exist) {
     }
   }
 }
-
-int main(int argc, char** argv) {
-  boost::mpi::environment env(argc, argv);
-  boost::mpi::communicator world;
-  ::testing::InitGoogleTest(&argc, argv);
-  ::testing::TestEventListeners& listeners = ::testing::UnitTest::GetInstance()->listeners();
-  if (world.rank() != 0) {
-    delete listeners.Release(listeners.default_result_printer());
-  }
-  return RUN_ALL_TESTS();
-}
