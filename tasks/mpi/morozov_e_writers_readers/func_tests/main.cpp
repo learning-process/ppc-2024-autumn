266e948b509a7153617c6eeb63910334916971ae
Egor1dzeN-learn <egormorozov123450@gmail.com>
2024-11-20 20:32:32 +0300

    add: func test

--- tasks/mpi/morozov_e_writers_readers/func_tests/main.cpp
+++ tasks/mpi/morozov_e_writers_readers/func_tests/main.cpp
@@ -141,3 +141,25 @@ TEST(morozov_e_writers_readers, Test_Main2) {
     ASSERT_EQ(ans[0], 0);
   }
 }
+TEST(morozov_e_writers_readers, Test_Main3) {
+  boost::mpi::communicator world;
+  std::shared_ptr<ppc::core::TaskData> data = std::make_shared<ppc::core::TaskData>();
+  std::vector<int> vec{0};
+  std::vector<int> ans{-1};
+  int countIteration = 200;
+  if (world.rank() == 0) {
+    data->inputs.emplace_back(reinterpret_cast<uint8_t*>(vec.data()));
+    data->inputs.emplace_back(reinterpret_cast<uint8_t*>(new int{countIteration}));
+    data->inputs_count.emplace_back(vec.size());
+    data->outputs.emplace_back(reinterpret_cast<uint8_t*>(ans.data()));
+    data->outputs_count.emplace_back(ans.size());
+  }
+  morozov_e_writers_readers::TestMPITaskParallel obj(data);
+  ASSERT_TRUE(obj.validation());
+  obj.pre_processing();
+  obj.run();
+  obj.post_processing();
+  if (world.rank() == 0) {
+    ASSERT_EQ(ans[0], 0);
+  }
+}
