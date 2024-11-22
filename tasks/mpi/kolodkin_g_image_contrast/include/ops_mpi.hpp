bb0480361e90aa9faf3277aaddb79653b2d33964
KolodkinGrigorii <113025092+KolodkinGrigorii@users.noreply.github.com>
2024-11-22 19:50:28 +0300

    God, help me with tidy, please!

--- tasks/mpi/kolodkin_g_image_contrast/include/ops_mpi.hpp
+++ tasks/mpi/kolodkin_g_image_contrast/include/ops_mpi.hpp
@@ -17,8 +17,7 @@ namespace kolodkin_g_image_contrast_mpi {
 
 class TestMPITaskSequential : public ppc::core::Task {
  public:
-  explicit TestMPITaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_)
-      : Task(std::move(taskData_)), av_br(0) {}
+  explicit TestMPITaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)), {}
   bool pre_processing() override;
   bool validation() override;
   bool run() override;
