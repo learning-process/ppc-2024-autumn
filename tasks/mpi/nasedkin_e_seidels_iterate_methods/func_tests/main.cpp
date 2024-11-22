0e98d17598877a1a1a20af6b6c1096382fc8f821
myffii <132840123+myffii@users.noreply.github.com>
2024-11-23 00:24:56 +0300

    1

--- tasks/mpi/nasedkin_e_seidels_iterate_methods/func_tests/main.cpp
+++ tasks/mpi/nasedkin_e_seidels_iterate_methods/func_tests/main.cpp
@@ -26,9 +26,9 @@ TEST(nasedkin_e_seidels_iterate_methods_mpi, test_with_invalid_input) {
 
 TEST(nasedkin_e_seidels_iterate_methods_mpi, test_with_negative_input) {
     auto taskData = std::make_shared<ppc::core::TaskData>();
-    taskData->inputs_count.push_back(static_cast<int>(-3));
+    taskData->inputs_count.push_back(static_cast<unsigned>(-3));
 
     nasedkin_e_seidels_iterate_methods_mpi::SeidelIterateMethodsMPI seidel_task(taskData);
 
-    ASSERT_FALSE(seidel_task.validation());
+    ASSERT_FALSE(seidel_task.validation()) << "Validation passed for negative input, expected failure";
 }
