194b8a5c2bdb6ca3be591ab0bff34b8571e362da
Alexey-Solovev <149035736+Alexey-Solovev@users.noreply.github.com>
2024-11-24 20:06:11 +0300

    fix ops_mpi.cpp for review

--- tasks/mpi/solovev_a_star_topology/src/ops_mpi.cpp
+++ tasks/mpi/solovev_a_star_topology/src/ops_mpi.cpp
@@ -2,10 +2,22 @@
 
 #include <algorithm>
 #include <functional>
+#include <random>
 #include <string>
 #include <vector>
 
 namespace solovev_a_star_topology_mpi {
+
+std::vector<int> generate_random_vector(size_t size) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(-1000, 1000);
+  std::vector<int> random_vector(size);
+  for (size_t i = 0; i < size; ++i) {
+    random_vector[i] = dis(gen);
+  }
+  return random_vector;
+}
 
 bool solovev_a_star_topology_mpi::TestMPITaskParallel::pre_processing() {
   internal_order_test();
@@ -15,6 +27,8 @@ bool solovev_a_star_topology_mpi::TestMPITaskParallel::pre_processing() {
     std::copy(tmp_ptr, tmp_ptr + taskData->inputs_count[0], input_.begin());
   }
   res = std::vector<int>(input_.size(), 0);
+  order = std::vector<int>(world.size() + 1, -1);
+  l_rank = -1;
   return true;
 }
 
@@ -30,17 +44,26 @@ bool solovev_a_star_topology_mpi::TestMPITaskParallel::validation() {
 bool solovev_a_star_topology_mpi::TestMPITaskParallel::run() {
   internal_order_test();
   if (world.rank() == 0) {
+    order.clear();
+    order.push_back(0);
     for (int i = 1; i < world.size(); ++i) {
       world.send(i, 0, input_);
+      world.send(i, 0, l_rank);
     }
   } else {
     world.recv(0, 0, input_);
+    world.recv(0, 0, l_rank);
+    l_rank = world.rank();
     world.send(0, 0, input_);
+    world.send(0, 0, l_rank);
   }
   if (world.rank() == 0) {
     for (int i = 1; i < world.size(); ++i) {
       world.recv(i, 0, res);
+      world.recv(i, 0, l_rank);
+      order.push_back(l_rank);
     }
+    order.push_back(world.size());
   }
   return true;
 }
@@ -50,6 +73,7 @@ bool solovev_a_star_topology_mpi::TestMPITaskParallel::post_processing() {
   world.barrier();
   if (world.rank() == 0) {
     std::copy(res.begin(), res.end(), reinterpret_cast<int*>(taskData->outputs[0]));
+    std::copy(order.begin(), order.end(), reinterpret_cast<int*>(taskData->outputs[1]));
   }
   return true;
 }
