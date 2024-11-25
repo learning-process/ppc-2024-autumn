3add12f69d1cf69cc91f5f29806386e6e616d910
Semyon1104 <sema_titov_04@mail.ru>
2024-11-25 22:22:52 +0300

    test Pre_proc

--- tasks/mpi/titov_s_simple_iteration/func_tests/main.cpp
+++ tasks/mpi/titov_s_simple_iteration/func_tests/main.cpp
@@ -34,3 +34,32 @@ TEST(titov_s_simple_iteration_mpi, Test_Simple_Iteration_Not_A_Diagonally_Domina
     ASSERT_FALSE(taskPar.validation());
   }
 }
+
+TEST(titov_s_simple_iteration_mpi, Test_Simple_Iteration_Parallel_1_1) {
+  boost::mpi::communicator world;
+
+  size_t matrix_size = 1;
+  std::vector<double> global_result(matrix_size, 0.0);
+  std::vector<double> Matrix = {10.0};
+  std::vector<double> Values = {1.0};
+  double epsilon = 0.001;
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(Matrix.data()));
+    taskDataPar->inputs_count.emplace_back(Matrix.size());
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(Values.data()));
+    taskDataPar->inputs_count.emplace_back(Values.size());
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&matrix_size));
+    taskDataPar->inputs_count.emplace_back(1);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&epsilon));
+    taskDataPar->inputs_count.emplace_back(1);
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_result.data()));
+    taskDataPar->outputs_count.emplace_back(global_result.size());
+  }
+
+  titov_s_simple_iteration_mpi::MPISimpleIterationParallel taskPar(taskDataPar);
+  ASSERT_TRUE(taskPar.validation());
+  ASSERT_TRUE(taskPar.pre_processing());
+
+}
