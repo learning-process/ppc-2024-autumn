70a1ea02b38a0a2352fc36c4d94ce10723eb4fdd
Sedova-Olga <vyaz-sedovaolga@mail.ru>
2024-11-25 20:47:20 +0300

    macos2

--- tasks/mpi/sedova_o_vertical_ribbon_scheme/src/ops_mpi.cpp
+++ tasks/mpi/sedova_o_vertical_ribbon_scheme/src/ops_mpi.cpp
@@ -1,8 +1,6 @@
 ﻿#include "mpi/sedova_o_vertical_ribbon_scheme/include/ops_mpi.hpp"
 
 #include <algorithm>
-#include <boost/serialization/array.hpp>
-#include <boost/serialization/vector.hpp>
 #include <functional>
 #include <iostream>
 #include <limits>
