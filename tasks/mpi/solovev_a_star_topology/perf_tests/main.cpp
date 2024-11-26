cc8ba5ce8146a1a6eafbdc7e6567e142b2bf632b
Alexey-Solovev <149035736+Alexey-Solovev@users.noreply.github.com>
2024-11-26 19:56:08 +0300

    fix main.cpp for review

--- tasks/mpi/solovev_a_star_topology/perf_tests/main.cpp
+++ tasks/mpi/solovev_a_star_topology/perf_tests/main.cpp
@@ -22,22 +22,26 @@ TEST(solovev_a_star_topology_mpi_perf_test, test_pipeline_run) {
     taskDataPar->outputs_count.emplace_back(order.size());
   }
   auto testMpiTaskParallel = std::make_shared<solovev_a_star_topology_mpi::TestMPITaskParallel>(taskDataPar);
-  ASSERT_EQ(testMpiTaskParallel->validation(), true);
-  testMpiTaskParallel->pre_processing();
-  testMpiTaskParallel->run();
-  testMpiTaskParallel->post_processing();
-  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
-  perfAttr->num_running = 10;
-  const boost::mpi::timer current_timer;
-  perfAttr->current_timer = [&] { return current_timer.elapsed(); };
+  if (world.size() == 1) {
+    ASSERT_EQ(testMpiTaskParallel->validation(), false);
+  } else {
+    ASSERT_EQ(testMpiTaskParallel->validation(), true);
+    testMpiTaskParallel->pre_processing();
+    testMpiTaskParallel->run();
+    testMpiTaskParallel->post_processing();
+    auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+    perfAttr->num_running = 10;
+    const boost::mpi::timer current_timer;
+    perfAttr->current_timer = [&] { return current_timer.elapsed(); };
 
-  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+    auto perfResults = std::make_shared<ppc::core::PerfResults>();
 
-  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testMpiTaskParallel);
-  perfAnalyzer->pipeline_run(perfAttr, perfResults);
-  if (world.rank() == 0) {
-    ppc::core::Perf::print_perf_statistic(perfResults);
-    ASSERT_EQ(output, input);
+    auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testMpiTaskParallel);
+    perfAnalyzer->pipeline_run(perfAttr, perfResults);
+    if (world.rank() == 0) {
+      ppc::core::Perf::print_perf_statistic(perfResults);
+      ASSERT_EQ(output, input);
+    }
   }
 }
 
@@ -56,21 +60,25 @@ TEST(solovev_a_star_topology_mpi_perf_test, test_task_run) {
     taskDataPar->outputs_count.emplace_back(order.size());
   }
   auto testMpiTaskParallel = std::make_shared<solovev_a_star_topology_mpi::TestMPITaskParallel>(taskDataPar);
-  ASSERT_EQ(testMpiTaskParallel->validation(), true);
-  testMpiTaskParallel->pre_processing();
-  testMpiTaskParallel->run();
-  testMpiTaskParallel->post_processing();
-  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
-  perfAttr->num_running = 10;
-  const boost::mpi::timer current_timer;
-  perfAttr->current_timer = [&] { return current_timer.elapsed(); };
+  if (world.size() == 1) {
+    ASSERT_EQ(testMpiTaskParallel->validation(), false);
+  } else {
+    ASSERT_EQ(testMpiTaskParallel->validation(), true);
+    testMpiTaskParallel->pre_processing();
+    testMpiTaskParallel->run();
+    testMpiTaskParallel->post_processing();
+    auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+    perfAttr->num_running = 10;
+    const boost::mpi::timer current_timer;
+    perfAttr->current_timer = [&] { return current_timer.elapsed(); };
 
-  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+    auto perfResults = std::make_shared<ppc::core::PerfResults>();
 
-  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testMpiTaskParallel);
-  perfAnalyzer->task_run(perfAttr, perfResults);
-  if (world.rank() == 0) {
-    ppc::core::Perf::print_perf_statistic(perfResults);
-    ASSERT_EQ(output, input);
+    auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testMpiTaskParallel);
+    perfAnalyzer->task_run(perfAttr, perfResults);
+    if (world.rank() == 0) {
+      ppc::core::Perf::print_perf_statistic(perfResults);
+      ASSERT_EQ(output, input);
+    }
   }
 }
