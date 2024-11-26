ea80843e8721900ab98b4d5a4ef09f2864730d32
keshaproger <125856206+keshaproger@users.noreply.github.com>
2024-11-26 03:00:54 +0300

    add test and fix clang

--- tasks/mpi/konkov_i_task_dining_philosophers/perf_tests/main.cpp
+++ tasks/mpi/konkov_i_task_dining_philosophers/perf_tests/main.cpp
@@ -3,8 +3,8 @@
 #include <boost/mpi/timer.hpp>
 #include <vector>
 
-#include "mpi/konkov_i_task_dining_philosophers/include/ops_mpi.hpp"
 #include "core/perf/include/perf.hpp"
+#include "mpi/konkov_i_task_dining_philosophers/include/ops_mpi.hpp"
 
 TEST(konkov_i_mpi_dining_philosophers_perf_test, test_pipeline_run) {
   boost::mpi::communicator world;
@@ -40,6 +40,45 @@ TEST(konkov_i_mpi_dining_philosophers_perf_test, test_pipeline_run) {
   // Create Perf analyzer
   auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testMpiTaskParallel);
   perfAnalyzer->pipeline_run(perfAttr, perfResults);
+  if (world.rank() == 0) {
+    ppc::core::Perf::print_perf_statistic(perfResults);
+    ASSERT_EQ(count_size_vector, global_result[0]);
+  }
+}
+TEST(konkov_i_mpi_dining_philosophers_perf_test, test_task_run) {
+  boost::mpi::communicator world;
+  std::vector<int> global_vec;
+  std::vector<int32_t> global_result(1, 0);
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+  int count_size_vector;
+  if (world.rank() == 0) {
+    count_size_vector = 120;
+    global_vec = std::vector<int>(count_size_vector, 1);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataPar->inputs_count.emplace_back(global_vec.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_result.data()));
+    taskDataPar->outputs_count.emplace_back(global_result.size());
+  }
+
+  auto testMpiTaskParallel = std::make_shared<konkov_i_task_dining_philosophers::TestMPITaskParallel>(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel->validation(), true);
+  testMpiTaskParallel->pre_processing();
+  testMpiTaskParallel->run();
+  testMpiTaskParallel->post_processing();
+
+  // Create Perf attributes
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+  const boost::mpi::timer current_timer;
+  perfAttr->current_timer = [&] { return current_timer.elapsed(); };
+
+  // Create and init perf results
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
+  // Create Perf analyzer
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testMpiTaskParallel);
+  perfAnalyzer->task_run(perfAttr, perfResults);
   if (world.rank() == 0) {
     ppc::core::Perf::print_perf_statistic(perfResults);
     ASSERT_EQ(count_size_vector, global_result[0]);
