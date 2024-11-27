bdf6ce415e008bfa7504e5e7941ca7ea6fe5a661
Temik The Witness <artemis.zaytsev@gmail.com>
2024-11-27 18:09:14 +0300

    Templated version based on handwritten scatter

---
+++ tasks/mpi/zaitsev_a_scatter/func_tests/main.cpp
@@ -0,0 +1,98 @@
+// Copyright 2023 Liolya Seledkina
+#include <gtest/gtest.h>
+
+#include <boost/mpi/communicator.hpp>
+#include <boost/mpi/environment.hpp>
+#include <random>
+
+#include "mpi/zaitsev_a_scatter/include/ops_mpi.hpp"
+
+namespace zaitsev_a_scatter {
+std::vector<int> get_random_int_vector(int sz, int min, int max, int extrema = -100) {
+  std::random_device dev;
+  std::mt19937 gen(dev());
+  std::vector<int> vec(sz);
+  for (int i = 0; i < sz; i++) vec[i] = min + gen() % (max - min + 1);
+  vec[0] = extrema;
+  return vec;
+}
+
+std::vector<double> get_random_double_vector(int sz, double min, double max, double extrema) {
+  std::uniform_real_distribution<double> unif(min, max);
+  std::default_random_engine re;
+  std::vector<double> vec(sz);
+  for (int i = 0; i < sz; i++) vec[i] = unif(re);
+  vec[0] = extrema;
+  return vec;
+}
+}  // namespace zaitsev_a_scatter
+
+void test_int(int sz, int min = -1000, int max = 1000, int extrema = -5000) {
+  std::vector<int> inp;
+  std::vector<int> ref(1, 15);
+
+  boost::mpi::communicator world;
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    inp = zaitsev_a_scatter::get_random_int_vector(sz, min, max, extrema);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(inp.data()));
+    taskDataPar->inputs_count.emplace_back(sz);
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(ref.data()));
+    taskDataPar->outputs_count.emplace_back(1);
+  }
+
+  zaitsev_a_scatter::TestMPITaskParallel<int> testMpiTaskParallel(taskDataPar, 0, MPI_INT);
+  if (!testMpiTaskParallel.validation()) {
+    GTEST_SKIP();
+    return;
+  }
+
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+    ASSERT_EQ(extrema, reinterpret_cast<int*>(taskDataPar->outputs[0])[0]);
+  }
+}
+
+void test_double(int sz, double min = -10e3, double max = 10e3, double extrema = -10e4) {
+  std::vector<double> inp;
+  std::vector<double> ref(1, 0);
+
+  boost::mpi::communicator world;
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    inp = zaitsev_a_scatter::get_random_double_vector(sz, min, max, extrema);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(inp.data()));
+    taskDataPar->inputs_count.emplace_back(sz);
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(ref.data()));
+    taskDataPar->outputs_count.emplace_back(1);
+  }
+
+  zaitsev_a_scatter::TestMPITaskParallel<double> testMpiTaskParallel(taskDataPar, 0, MPI_DOUBLE);
+  if (!testMpiTaskParallel.validation()) {
+    GTEST_SKIP();
+    return;
+  }
+
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+    ASSERT_DOUBLE_EQ(extrema, reinterpret_cast<double*>(taskDataPar->outputs[0])[0]);
+  }
+}
+
+TEST(zaitsev_a_scatter, test__dtype_int__size_1) { test_int(1); }
+TEST(zaitsev_a_scatter, test__dtype_int__size_1e2) { test_int(1e2); }
+TEST(zaitsev_a_scatter, test__dtype_int__size_1e5) { test_int(1e5); }
+TEST(zaitsev_a_scatter, test__dtype_int__size_1e7) { test_int(1e7); }
+
+TEST(zaitsev_a_scatter, test__dtype_double__size_1) { test_double(1); }
+TEST(zaitsev_a_scatter, test__dtype_double__size_1e2) { test_double(1e2); }
+TEST(zaitsev_a_scatter, test__dtype_double__size_1e3) { test_double(1e3); }
+TEST(zaitsev_a_scatter, test__dtype_double__size_1e5) { test_double(1e5); }
---
+++ tasks/mpi/zaitsev_a_scatter/include/ops_mpi.hpp
@@ -0,0 +1,87 @@
+// Copyright 2023 Liolya Seledkina
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include <boost/mpi/collectives.hpp>
+#include <boost/mpi/communicator.hpp>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace zaitsev_a_scatter {
+
+int scatter(void* sendbuf, int sendcount, MPI_Datatype sendtype, void* recvbuf, int recvcount, MPI_Datatype recvtype,
+            int root, MPI_Comm comm);
+
+template <typename T>
+class TestMPITaskParallel : public ppc::core::Task {
+ public:
+  explicit TestMPITaskParallel(std::shared_ptr<ppc::core::TaskData> taskData_, int root_, MPI_Datatype dtype_)
+      : Task(std::move(taskData_)), root(root_), dtype(dtype_) {}
+
+ private:
+  std::vector<T> input;
+  std::vector<T> local_input;
+  int root;
+  MPI_Datatype dtype;
+  T res;
+  boost::mpi::communicator world;
+
+ public:
+  bool pre_processing() override {
+    internal_order_test();
+
+    if (world.rank() == 0) {
+      input = std::vector<T>(taskData->inputs_count[0]);
+      auto* tmp_ptr = reinterpret_cast<T*>(taskData->inputs[0]);
+      std::copy(tmp_ptr, tmp_ptr + taskData->inputs_count[0], input.begin());
+    }
+    res = 0;
+    return true;
+  };
+
+  bool validation() override {
+    internal_order_test();
+    return world.size() > 1 && (world.rank() != 0 || taskData->inputs_count[0] > 0);
+  };
+
+  bool run() override {
+    internal_order_test();
+
+    unsigned int shift = 0;
+
+    if (world.rank() == root) {
+      shift = taskData->inputs_count[0] / world.size();
+      local_input = std::vector<T>(shift + taskData->inputs_count[0] % world.size());
+    }
+
+    MPI_Bcast(&shift, 1, MPI_INT, root, world);
+
+    if (world.rank() != root) local_input = std::vector<T>(shift);
+
+    zaitsev_a_scatter::scatter(input.data(), shift, dtype, local_input.data(), shift, dtype, root, world);
+
+    if (world.rank() == root) {
+      std::copy(input.begin() + shift * world.size(), input.end(), local_input.begin() + shift);
+    }
+
+    T local_res = 0;
+    if (!local_input.empty()) {
+      local_res = *std::min_element(local_input.begin(), local_input.end());
+    }
+
+    reduce(world, local_res, res, boost::mpi::minimum<T>(), root);
+
+    return true;
+  };
+  bool post_processing() override {
+    internal_order_test();
+
+    if (world.rank() == 0) reinterpret_cast<T*>(taskData->outputs[0])[0] = res;
+
+    return true;
+  };
+};
+
+}  // namespace zaitsev_a_scatter
