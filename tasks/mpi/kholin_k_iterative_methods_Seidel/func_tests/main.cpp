a306f8fdf99969686f18ce771cb4967365364344
Kirius257 <holin.kirill@yandex.ru>
2024-11-27 20:42:18 +0300

    DONE!

--- tasks/mpi/kholin_k_iterative_methods_Seidel/func_tests/main.cpp
+++ tasks/mpi/kholin_k_iterative_methods_Seidel/func_tests/main.cpp
@@ -231,66 +231,6 @@ TEST(kholin_k_iterative_methods_Seidel_mpi, test_post_processing) {
   }
 }
 
-TEST(kholin_k_iterative_methods_Seidel_mpi, validation_false_when_matrix_no_diag) {
-  int ProcRank = 0;
-  MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-  const size_t count_rows = 25;
-  const size_t count_colls = 25;
-  float epsilon = 0.001f;
-  list_ops::ops_ op = list_ops::METHOD_SEIDEL;
-  float *in = nullptr;
-  float *out = nullptr;
-  // Create TaskData
-  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
-  if (ProcRank == 0) {
-    in = new float[count_colls * count_rows];
-    out = new float[count_rows];
-    for (size_t i = 0; i < count_rows; i++) {
-      for (size_t j = 0; j < count_colls; j++) {
-        in[count_colls * i + j] = i + j;
-      }
-    }
-    kholin_k_iterative_methods_Seidel_mpi::setA_(in, count_rows, count_colls);
-    kholin_k_iterative_methods_Seidel_mpi::copyA_(in, count_rows, count_colls);
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(in));
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(&epsilon));
-    taskDataPar->inputs_count.emplace_back(count_rows);
-    taskDataPar->inputs_count.emplace_back(count_colls);
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(out));
-    taskDataPar->outputs_count.emplace_back(count_rows);
-  }
-
-  kholin_k_iterative_methods_Seidel_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar, op);
-  bool IsValid1 = testMpiTaskParallel.validation();
-  ASSERT_EQ(IsValid1, false);
-
-  float *out_ref = nullptr;
-  if (ProcRank == 0) {
-    out_ref = new float[count_rows];
-    // Create TaskData
-    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in));
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(&epsilon));
-    taskDataSeq->inputs_count.emplace_back(count_rows);
-    taskDataSeq->inputs_count.emplace_back(count_colls);
-    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_ref));
-    taskDataSeq->outputs_count.emplace_back(count_rows);
-
-    // Create Task
-    kholin_k_iterative_methods_Seidel_mpi::TestMPITaskSequential testMpiTaskSequential(taskDataSeq, op);
-    bool IsValid2 = testMpiTaskSequential.validation();
-    ASSERT_EQ(IsValid2, false);
-  }
-  if (ProcRank == 0) {
-    delete[] in;
-    delete[] out;
-    delete[] out_ref;
-  }
-  if (ProcRank == 0) {
-    kholin_k_iterative_methods_Seidel_mpi::freeA_();
-  }
-}
-
 // int main(int argc, char **argv) {
 //   boost::mpi::environment env(argc, argv);
 //   boost::mpi::communicator world;
