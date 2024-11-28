52d73e848fef3177c1f5f79e29b3136808dc745d
dssss12 <125500790+dssss12@users.noreply.github.com>
2024-11-28 20:55:49 +0300

    fix

--- tasks/mpi/sorochkin_d_ring_topology/func_tests/main.cpp
+++ tasks/mpi/sorochkin_d_ring_topology/func_tests/main.cpp
@@ -102,3 +102,31 @@ TEST(sorochkin_d_ring_topology_mpi_test, test_size_400) {
     ASSERT_EQ(input, output);
   }
 }
+
+TEST(sorochkin_d_ring_topology_mpi_test, test_size_0) {
+  boost::mpi::communicator world;
+  std::vector<int> input;
+  std::vector<int> output;
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    const int count_size_vector = 0;
+    input = sorochkin_d_ring_topology_mpi_test::getRandomVector(count_size_vector);
+    output.resize(count_size_vector);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(input.data()));
+    taskDataPar->inputs_count.emplace_back(input.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
+    taskDataPar->outputs_count.emplace_back(output.size());
+  }
+
+  sorochkin_d_test_task_mpi::TestMPITaskParallel<int> testMpiTaskParallel(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+    ASSERT_EQ(input, output);
+  }
+}
