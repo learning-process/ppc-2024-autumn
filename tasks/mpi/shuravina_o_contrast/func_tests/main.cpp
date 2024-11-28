e1ec23bf7b2f3cc3a5fe2541a358608b44d40522
ОКСАНА ШУРАВИНА <shyravina19102004@gmail.com>
2024-11-28 17:10:07 +0300

    k12

--- tasks/mpi/shuravina_o_contrast/func_tests/main.cpp
+++ tasks/mpi/shuravina_o_contrast/func_tests/main.cpp
@@ -2,7 +2,6 @@
 
 #include <boost/mpi/communicator.hpp>
 #include <boost/mpi/environment.hpp>
-#include <chrono>
 #include <iostream>
 #include <vector>
 
@@ -18,9 +17,9 @@ TEST(shuravina_o_contrast, Test_Contrast_10) {
     const int count_size_vector = 10;
     global_vec = std::vector<uint8_t>(count_size_vector, 128);
     global_out = std::vector<uint8_t>(count_size_vector, 0);
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(global_vec.data()));
     taskDataPar->inputs_count.emplace_back(global_vec.size());
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_out.data()));
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(global_out.data()));
     taskDataPar->outputs_count.emplace_back(global_out.size());
   }
 
@@ -32,6 +31,7 @@ TEST(shuravina_o_contrast, Test_Contrast_10) {
 
   if (world.rank() == 0) {
     for (std::size_t i = 0; i < global_out.size(); ++i) {
+      std::cout << "Output[" << i << "]: " << static_cast<int>(global_out[i]) << std::endl;
       ASSERT_EQ(global_out[i], 255);
     }
   }
@@ -47,9 +47,9 @@ TEST(shuravina_o_contrast, Test_Contrast_20) {
     const int count_size_vector = 20;
     global_vec = std::vector<uint8_t>(count_size_vector, 64);
     global_out = std::vector<uint8_t>(count_size_vector, 0);
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(global_vec.data()));
     taskDataPar->inputs_count.emplace_back(global_vec.size());
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_out.data()));
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(global_out.data()));
     taskDataPar->outputs_count.emplace_back(global_out.size());
   }
 
@@ -61,6 +61,7 @@ TEST(shuravina_o_contrast, Test_Contrast_20) {
 
   if (world.rank() == 0) {
     for (std::size_t i = 0; i < global_out.size(); ++i) {
+      std::cout << "Output[" << i << "]: " << static_cast<int>(global_out[i]) << std::endl;
       ASSERT_EQ(global_out[i], 255);
     }
   }
@@ -76,9 +77,9 @@ TEST(shuravina_o_contrast, Test_Contrast_30) {
     const int count_size_vector = 30;
     global_vec = std::vector<uint8_t>(count_size_vector, 32);
     global_out = std::vector<uint8_t>(count_size_vector, 0);
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(global_vec.data()));
     taskDataPar->inputs_count.emplace_back(global_vec.size());
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_out.data()));
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(global_out.data()));
     taskDataPar->outputs_count.emplace_back(global_out.size());
   }
 
@@ -90,6 +91,7 @@ TEST(shuravina_o_contrast, Test_Contrast_30) {
 
   if (world.rank() == 0) {
     for (std::size_t i = 0; i < global_out.size(); ++i) {
+      std::cout << "Output[" << i << "]: " << static_cast<int>(global_out[i]) << std::endl;
       ASSERT_EQ(global_out[i], 255);
     }
   }
@@ -105,9 +107,9 @@ TEST(shuravina_o_contrast, Test_Contrast_40) {
     const int count_size_vector = 40;
     global_vec = std::vector<uint8_t>(count_size_vector, 16);
     global_out = std::vector<uint8_t>(count_size_vector, 0);
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(global_vec.data()));
     taskDataPar->inputs_count.emplace_back(global_vec.size());
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_out.data()));
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(global_out.data()));
     taskDataPar->outputs_count.emplace_back(global_out.size());
   }
 
@@ -119,6 +121,7 @@ TEST(shuravina_o_contrast, Test_Contrast_40) {
 
   if (world.rank() == 0) {
     for (std::size_t i = 0; i < global_out.size(); ++i) {
+      std::cout << "Output[" << i << "]: " << static_cast<int>(global_out[i]) << std::endl;
       ASSERT_EQ(global_out[i], 255);
     }
   }
