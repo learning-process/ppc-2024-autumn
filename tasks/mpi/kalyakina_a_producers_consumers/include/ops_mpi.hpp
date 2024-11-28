f1b1bce5ccd78655169b6287b4baf0e935bd51aa
kalyakinaa <nastyakalyakinann@gmail.com>
2024-11-28 16:57:54 +0300

    Producers-concumers solution

---
+++ tasks/mpi/kalyakina_a_producers_consumers/include/ops_mpi.hpp
@@ -0,0 +1,38 @@
+// Copyright 2023 Nesterov Alexander
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include <boost/mpi/collectives.hpp>
+#include <boost/mpi/communicator.hpp>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace kalyakina_a_producers_consumers_mpi {
+
+class ProducersConsumersTaskParallel : public ppc::core::Task {
+  int ProducersFunction();
+  void ConsumersFunction(int data);
+
+ public:
+  explicit ProducersConsumersTaskParallel(std::shared_ptr<ppc::core::TaskData> taskData_)
+      : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  int producers_count{};
+  int buffer_size{};
+  int sources{};
+  boost::mpi::communicator world;
+};
+
+}  // namespace kalyakina_a_producers_consumers_mpi
