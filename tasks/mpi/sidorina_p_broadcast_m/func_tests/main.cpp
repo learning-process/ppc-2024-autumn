160ba180252a8e91221c0cafe427acb0232b2011
Polina Sidorina <polishas206@gmail.com>
2024-11-28 15:12:42 +0300

    1.0

--- tasks/mpi/sidorina_p_broadcast_m/func_tests/main.cpp
+++ tasks/mpi/sidorina_p_broadcast_m/func_tests/main.cpp
@@ -238,6 +238,7 @@ TEST(sidorina_p_broadcast_m_mpi, Test_negative_m) {
   std::shared_ptr<ppc::core::TaskData> taskDataGlob = std::make_shared<ppc::core::TaskData>();
   std::shared_ptr<ppc::core::TaskData> taskDataRef = std::make_shared<ppc::core::TaskData>();
 
+  //neg
   if (world.rank() == 0) {
     array = std::vector<int>({1, 2, 3});
     terms = std::vector<int>({-1, 2, -3});
@@ -285,6 +286,7 @@ TEST(sidorina_p_broadcast_m_mpi, Test_negative_m) {
   }
 }
 
+//random !!!
 std::vector<int> randomVector(size_t size) {
   std::vector<int> v(size);
   std::random_device r;
