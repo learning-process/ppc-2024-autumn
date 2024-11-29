5f1228a256f85865c0b0b8a992c0c54fc9734ee4
myffii <132840123+myffii@users.noreply.github.com>
2024-11-29 15:40:43 +0300

    1

--- tasks/mpi/nasedkin_e_seidels_iterate_methods/func_tests/main.cpp
+++ tasks/mpi/nasedkin_e_seidels_iterate_methods/func_tests/main.cpp
@@ -52,7 +52,7 @@ TEST(nasedkin_e_seidels_iterate_methods_mpi, test_random_diag_dominant_matrix) {
 
   std::vector<std::vector<double>> matrix;
   std::vector<double> vector;
-  seidel_task.generate_random_diag_dominant_matrix(5, matrix, vector);
+  nasedkin_e_seidels_iterate_methods_mpi::SeidelIterateMethodsMPI::generate_random_diag_dominant_matrix(5, matrix, vector);
   seidel_task.set_matrix(matrix, vector);
 
   ASSERT_TRUE(seidel_task.validation()) << "Validation failed for random diagonally dominant matrix";
