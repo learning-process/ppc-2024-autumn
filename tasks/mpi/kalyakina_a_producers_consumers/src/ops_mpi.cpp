dfdaece2ad4f2e68a0c00c9c8d474b0fe6ab699b
kalyakinaa <nastyakalyakinann@gmail.com>
2024-11-29 19:31:24 +0300

    Code changes 0.5

--- tasks/mpi/kalyakina_a_producers_consumers/src/ops_mpi.cpp
+++ tasks/mpi/kalyakina_a_producers_consumers/src/ops_mpi.cpp
@@ -16,7 +16,9 @@ int kalyakina_a_producers_consumers_mpi::ProducersConsumersTaskParallel::Produce
   return data;
 }
 void kalyakina_a_producers_consumers_mpi::ProducersConsumersTaskParallel::ConsumersFunction(int data) {
-  std::this_thread::sleep_for(std::chrono::milliseconds(data));
+  if (data > 0) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(data));
+  }
 }
 
 bool kalyakina_a_producers_consumers_mpi::ProducersConsumersTaskParallel::pre_processing() {
