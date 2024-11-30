59bc8636706311752755ba6e2e9a399917ff5aff
Maksim Anikin <maksim.anikin@radiogigabit.net>
2024-12-01 01:48:01 +0300

    Seq part complete

---
+++ tasks/seq/anikin_m_contrastscale/include/ops_seq.hpp
@@ -0,0 +1,35 @@
+// Copyright 2023 Anikin Maksim
+#pragma once
+
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <random>
+#include "core/task/include/task.hpp"
+
+namespace anikin_m_contrastscale_seq {
+
+struct RGB {
+  uint8_t R;
+  uint8_t G;
+  uint8_t B;
+};
+
+RGB getrandomRGB();
+double getcontrast(std::vector<RGB>& in);
+
+
+class ContrastScaleSeq : public ppc::core::Task {
+ public:
+  explicit ContrastScaleSeq(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  float correction;
+  std::vector<RGB> input_, output_;
+};
+
+} // namespace anikin_m_contrastscale_seq
