da989c6ea6328a0d3fef1f52eaa59d1f788d1f65
ОКСАНА ШУРАВИНА <shyravina19102004@gmail.com>
2024-12-01 02:08:54 +0300

    k43

--- tasks/mpi/shuravina_o_contrast/src/ops_mpi.cpp
+++ tasks/mpi/shuravina_o_contrast/src/ops_mpi.cpp
@@ -48,6 +48,13 @@ bool shuravina_o_contrast::ContrastTaskParallel::run() {
   reduce(world, local_min_val, global_min_val, boost::mpi::minimum<uint8_t>(), 0);
   reduce(world, local_max_val, global_max_val, boost::mpi::maximum<uint8_t>(), 0);
 
+  if (world.rank() == 0) {
+    std::cout << "Local min: " << static_cast<int>(local_min_val) << ", Local max: " << static_cast<int>(local_max_val)
+              << std::endl;
+    std::cout << "Global min: " << static_cast<int>(global_min_val)
+              << ", Global max: " << static_cast<int>(global_max_val) << std::endl;
+  }
+
   for (size_t i = 0; i < local_input_.size(); ++i) {
     output_[i] = static_cast<uint8_t>((local_input_[i] - global_min_val) * 255.0 / (global_max_val - global_min_val));
   }
