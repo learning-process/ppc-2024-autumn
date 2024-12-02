c149c0f62fba11cc1c513c528a987312507154f6
laganina-cod <laganinaelena@gmail.com>
2024-12-02 23:53:10 +0300

    try

--- tasks/mpi/laganina_e_readers_writers/include/ops_mpi.hpp
+++ tasks/mpi/laganina_e_readers_writers/include/ops_mpi.hpp
@@ -14,20 +14,20 @@
 
 namespace laganina_e_readers_writers_mpi {
 
-std::vector<int> getRandomVector(int sz);
+	std::vector<int> getRandomVector(int sz);
 
-class TestMPITaskParallel : public ppc::core::Task {
- public:
-  explicit TestMPITaskParallel(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
-  bool pre_processing() override;
-  bool validation() override;
-  bool run() override;
-  bool post_processing() override;
+	class TestMPITaskParallel : public ppc::core::Task {
+	public:
+		explicit TestMPITaskParallel(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+		bool pre_processing() override;
+		bool validation() override;
+		bool run() override;
+		bool post_processing() override;
 
- private:
-  std::vector<int> shared_data{};
-  std::vector<int> res_{};
-  boost::mpi::communicator world;
-};
+	private:
+		std::vector<int> shared_data{};
+		std::vector<int> res_{};
+		boost::mpi::communicator world;
+	};
 
-} 
+}
