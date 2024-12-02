9f3e5a5cf53bd4c435eb6cc22ccba7100752febd
IljaSmirnov <ilsmirnov693n@gmail.com>
2024-12-02 08:20:03 +0300

    fixes

--- tasks/mpi/smirnov_i_tape_splitting_A/func_tests/main.cpp
+++ tasks/mpi/smirnov_i_tape_splitting_A/func_tests/main.cpp
@@ -22,7 +22,78 @@ void get_random_matrix(double* matr, int size) {
 }
 
 }  // namespace smirnov_i_tape_splitting_A
+TEST(smirnov_i_tape_splitting_A_mpi, mult_matrix_and_matrix_size_of_ones) {
+  boost::mpi::communicator world;
 
+  double* A = nullptr;
+  double* B = nullptr;
+  double* res = nullptr;
+  int m_a = 1;
+  int n_a = 1;
+  int m_b = 1;
+  int n_b = 1;
+
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    A = new double[m_a * n_a];
+    B = new double[m_b * n_b];
+    smirnov_i_tape_splitting_A::get_random_matrix(A, m_a * n_a);
+    smirnov_i_tape_splitting_A::get_random_matrix(B, m_b * n_b);
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(A));
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(B));
+
+    taskDataPar->inputs_count.emplace_back(m_a);
+    taskDataPar->inputs_count.emplace_back(n_a);
+    taskDataPar->inputs_count.emplace_back(m_b);
+    taskDataPar->inputs_count.emplace_back(n_b);
+
+    res = new double[m_a * n_b];
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(res));
+    taskDataPar->outputs_count.emplace_back(m_a);
+    taskDataPar->outputs_count.emplace_back(n_b);
+  }
+
+  smirnov_i_tape_splitting_A::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(A));
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(B));
+
+    taskDataSeq->inputs_count.emplace_back(m_a);
+    taskDataSeq->inputs_count.emplace_back(n_a);
+    taskDataSeq->inputs_count.emplace_back(m_b);
+    taskDataSeq->inputs_count.emplace_back(n_b);
+
+    auto* res_seq = new double[m_a * n_b];
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(res_seq));
+    taskDataSeq->outputs_count.emplace_back(m_a);
+    taskDataSeq->outputs_count.emplace_back(n_b);
+
+    auto TestTaskSequential = std::make_shared<smirnov_i_tape_splitting_A::TestMPITaskSequential>(taskDataSeq);
+
+    ASSERT_EQ(TestTaskSequential->validation(), true);
+    TestTaskSequential->pre_processing();
+    TestTaskSequential->run();
+    TestTaskSequential->post_processing();
+
+    for (int i = 0; i < m_a * n_b; i++) {
+      ASSERT_NEAR(res_seq[i], res[i], 1e-6);
+    }
+    delete[] res_seq;
+  }
+  if (world.rank() == 0) {
+    delete[] A;
+    delete[] B;
+    delete[] res;
+  }
+}
 TEST(smirnov_i_tape_splitting_A_mpi, matrix_negative_sizes) {
   boost::mpi::communicator world;
   int m_a = -3;
