5d5415fa4845cc8a5581b3904cb5fbeffe325e7d
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2024-12-04 17:53:40 +0300

    Update ops_seq.cpp

--- tasks/seq/vavilov_v_bellman_ford/src/ops_seq.cpp
+++ tasks/seq/vavilov_v_bellman_ford/src/ops_seq.cpp
@@ -9,7 +9,7 @@ bool vavilov_v_bellman_ford_seq::TestTaskSequential::pre_processing() {
 
   int* edges_data = reinterpret_cast<int*>(taskData->inputs[3]);
   for (int i = 0; i < edges_count_; ++i) {
-      edges_.push_back({ edges_data[i * 3], edges_data[i * 3 + 1], edges_data[i * 3 + 2] });
+    edges_.push_back({ edges_data[i * 3], edges_data[i * 3 + 1], edges_data[i * 3 + 2] });
   }
 
   distances_.resize(vertices_, INF);
@@ -22,23 +22,23 @@ bool vavilov_v_bellman_ford_seq::TestTaskSequential::validation() {
   internal_order_test();
 
   return (!taskData->inputs.empty() && taskData->inputs_count.size() >= 4 &&
-      taskData->outputs.size() == 1 && taskData->outputs_count[0] == vertices_);
+          taskData->outputs.size() == 1 && taskData->outputs_count[0] == vertices_);
 }
 
 bool vavilov_v_bellman_ford_seq::TestTaskSequential::run() {
   internal_order_test();
 
   for (int i = 1; i < vertices_; ++i) {
-      for (const auto& edge : edges_) {
-          if (distances_[edge.src] != INF && distances_[edge.src] + edge.weight < distances_[edge.dest]) {
-              distances_[edge.dest] = distances_[edge.src] + edge.weight;
-          }
+    for (const auto& edge : edges_) {
+      if (distances_[edge.src] != INF && distances_[edge.src] + edge.weight < distances_[edge.dest]) {
+        distances_[edge.dest] = distances_[edge.src] + edge.weight;
       }
+    }
   }
 
   for (const auto& edge : edges_) {
-      if (distances_[edge.src] != INF && distances_[edge.src] + edge.weight < distances_[edge.dest]) {
-          return false;  // Negative weight cycle detected
+    if (distances_[edge.src] != INF && distances_[edge.src] + edge.weight < distances_[edge.dest]) {
+      return false;  // Negative weight cycle detected
       }
   }
 
