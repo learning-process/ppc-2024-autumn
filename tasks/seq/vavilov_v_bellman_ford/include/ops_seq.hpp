a1b8d077d0889d327c9055e7382ae8ce0a95fd02
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2024-12-04 17:45:42 +0300

    Update ops_seq.hpp

--- tasks/seq/vavilov_v_bellman_ford/include/ops_seq.hpp
+++ tasks/seq/vavilov_v_bellman_ford/include/ops_seq.hpp
@@ -26,7 +26,7 @@ class TestTaskSequential : public ppc::core::Task {
 
   std::vector<Edge> edges_;
   std::vector<int> distances_;
-  int vertices_{ 0 }, edges_count_{ 0 }, source_{ 0 };
+  int vertices_{0}, edges_count_{0}, source_{0};
 
   static constexpr int INF = std::numeric_limits<int>::max();
 };
