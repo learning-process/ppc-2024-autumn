1c282602c3b29dbb37f0ead3a9fb14ecde857ad9
ivan.lysov.05@gmail.com <ivan.lysov.05@gmail.com>
2024-12-04 03:19:05 +0300

    bugfix3

--- tasks/mpi/lysov_i_simple_iteration_method/src/ops_mpi.cpp
+++ tasks/mpi/lysov_i_simple_iteration_method/src/ops_mpi.cpp
@@ -92,8 +92,7 @@ bool lysov_i_simple_iteration_method_mpi::SlaeIterationTask::run() {
   if (!isDiagonallyDominant()) return false;
   std::vector<double> x_new(input_size_, 0.0);
   double max_diff = 0.0;
-  int iter = 0;
-
+  
   do {
     max_diff = 0.0;
 
@@ -113,7 +112,6 @@ bool lysov_i_simple_iteration_method_mpi::SlaeIterationTask::run() {
     }
 
     x_ = x_new;
-    ++iter;
 
   } while (max_diff > tolerance_);
 
