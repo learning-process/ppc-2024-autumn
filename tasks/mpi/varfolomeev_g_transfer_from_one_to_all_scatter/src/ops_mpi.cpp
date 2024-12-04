61c24cf76c020b584fc15beca620f5d77f379110
thunderbird <greg.varfolomeev@yandex.ru>
2024-12-05 00:45:49 +0300

    ops_mpi fix & better generator

--- tasks/mpi/varfolomeev_g_transfer_from_one_to_all_scatter/src/ops_mpi.cpp
+++ tasks/mpi/varfolomeev_g_transfer_from_one_to_all_scatter/src/ops_mpi.cpp
@@ -11,12 +11,12 @@
 
 using namespace std::chrono_literals;
 
-std::vector<int> varfolomeev_g_transfer_from_one_to_all_scatter_mpi::getRandomVector(int sz) {
+std::vector<int> varfolomeev_g_transfer_from_one_to_all_scatter_mpi::getRandomVector(int sz, int a, int b) {  // [a, b]
   std::random_device dev;
   std::mt19937 gen(dev());
   std::vector<int> vec(sz);
   for (int i = 0; i < sz; i++) {
-    vec[i] = gen() % 100;
+    vec[i] = gen() % (b - a + 1) + a;
   }
   return vec;
 }
@@ -57,9 +57,6 @@ bool varfolomeev_g_transfer_from_one_to_all_scatter_mpi::TestMPITaskSequential::
   reinterpret_cast<int*>(taskData->outputs[0])[0] = res;
   return true;
 }
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// bc - все данные  scatter - часть данных
 
 bool varfolomeev_g_transfer_from_one_to_all_scatter_mpi::TestMPITaskParallel::pre_processing() {
   internal_order_test();
@@ -68,31 +65,69 @@ bool varfolomeev_g_transfer_from_one_to_all_scatter_mpi::TestMPITaskParallel::pr
     auto* tempPtr = reinterpret_cast<int*>(taskData->inputs[0]);
     std::copy(tempPtr, tempPtr + taskData->inputs_count[0], input_values.begin());
   }
+  res = 0;
   return true;
 }
 
 bool varfolomeev_g_transfer_from_one_to_all_scatter_mpi::TestMPITaskParallel::validation() {
   internal_order_test();
-  if (world.size() < 0 || world.rank() >= world.size()) {
+  if (world.size() < 0 || world.rank() >= world.size() || (ops != "+" && ops != "-" && ops != "max")) {
     return false;
   }
+
   return true;
 }
 
 bool varfolomeev_g_transfer_from_one_to_all_scatter_mpi::TestMPITaskParallel::run() {
   internal_order_test();
-  if (world.rank() == 0) {
-    for (int proc_num = 1; proc_num < world.size(); proc_num++) {
-      std::vector<int> local_input_data;
+  int rank = world.rank();
+  int world_size = world.size();
+
+  // Spread the data
+  if (rank == 0) {
+    for (int proc_num = 1; proc_num < world_size; proc_num++) {
+      std::vector<int> local_data;
       for (int i = proc_num - 1; i < (int)input_values.size(); i += (world.size() - 1)) {
-        local_input_data.push_back(input_values[i]);
+        local_data.push_back(input_values[i]);
       }
-      world.send(proc_num, 0, local_input_data);
+      world.send(proc_num, 0, local_data);
     }
   } else {
     world.recv(0, 0, local_input_values);
-    // std::cout << local_input_values.size() << std::endl;
   }
+
+  // Выполнение операции
+  int local_result = 0;
+  if (ops == "+") {
+    local_result = std::accumulate(local_input_values.begin(), local_input_values.end(), 0);
+  } else if (ops == "-") {
+    local_result = -std::accumulate(local_input_values.begin(), local_input_values.end(), 0);
+  } else if (ops == "max") {
+    if (!local_input_values.empty()) {
+      local_result = local_input_values[0];
+      for (int value : local_input_values) {
+        if (value > local_result) {
+          local_result = value;
+        }
+      }
+    }
+  }
+
+  // Gathering results (root)
+  if (rank == 0) {
+    std::vector<int> results(world_size - 1);
+    for (int proc_num = 1; proc_num < world_size; proc_num++) {
+      world.recv(proc_num, 0, results[proc_num - 1]);
+    }
+    if (ops == "max") {
+      res = *std::max_element(results.begin(), results.end());
+    } else {
+      res = std::accumulate(results.begin(), results.end(), 0);
+    }
+  } else {  // Sending results (non-root)
+    world.send(0, 0, local_result);
+  }
+
   return true;
 }
 
