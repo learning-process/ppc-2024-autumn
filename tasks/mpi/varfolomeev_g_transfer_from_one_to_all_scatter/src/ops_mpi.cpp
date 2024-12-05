36395c8c11316d074726abf8aab0bf7adb372d1f
thunderbird <greg.varfolomeev@yandex.ru>
2024-12-05 15:19:40 +0300

    src major fix

--- tasks/mpi/varfolomeev_g_transfer_from_one_to_all_scatter/src/ops_mpi.cpp
+++ tasks/mpi/varfolomeev_g_transfer_from_one_to_all_scatter/src/ops_mpi.cpp
@@ -1,8 +1,8 @@
-// Copyright 2023 Nesterov Alexander
 #include "mpi/varfolomeev_g_transfer_from_one_to_all_scatter/include/ops_mpi.hpp"
 
 #include <algorithm>
 #include <cstddef>
+#include <cstdint>
 #include <functional>
 #include <random>
 #include <string>
@@ -61,14 +61,9 @@ bool varfolomeev_g_transfer_from_one_to_all_scatter_mpi::TestMPITaskSequential::
 bool varfolomeev_g_transfer_from_one_to_all_scatter_mpi::TestMPITaskParallel::pre_processing() {
   internal_order_test();
   if (world.rank() == 0) {
-    if (taskData->inputs[0] == nullptr || taskData->outputs[0] == nullptr) {
-      return false;
-    }
-    // input_values.resize(taskData->inputs_count[0]);
-    // auto* tempPtr = reinterpret_cast<int*>(taskData->inputs[0]);
-    // std::copy(tempPtr, tempPtr + taskData->inputs_count[0], input_values.begin());
-    input_values.assign(reinterpret_cast<int*>(taskData->inputs[0]),
-                        reinterpret_cast<int*>(taskData->inputs[0]) + taskData->inputs_count[0]);
+    input_values.resize(taskData->inputs_count[0]);
+    auto* tempPtr = reinterpret_cast<int*>(taskData->inputs[0]);
+    std::copy(tempPtr, tempPtr + taskData->inputs_count[0], input_values.begin());
   }
   res = 0;
   return true;
@@ -76,20 +71,21 @@ bool varfolomeev_g_transfer_from_one_to_all_scatter_mpi::TestMPITaskParallel::pr
 
 bool varfolomeev_g_transfer_from_one_to_all_scatter_mpi::TestMPITaskParallel::validation() {
   internal_order_test();
+  if (world.rank() == 0 && taskData->outputs_count[0] != 1) {
+    return false;
+  }
   if (world.size() < 0 || world.rank() >= world.size() || (ops != "+" && ops != "-" && ops != "max")) {
     return false;
   }
-
   return true;
 }
 
 bool varfolomeev_g_transfer_from_one_to_all_scatter_mpi::TestMPITaskParallel::run() {
   internal_order_test();
-  int rank = world.rank();
   int world_size = world.size();
 
   // Spread the data
-  if (rank == 0) {
+  if (world.rank() == 0) {
     for (int proc_num = 1; proc_num < world_size; proc_num++) {
       std::vector<int> local_data;
       for (int i = proc_num - 1; i < (int)input_values.size(); i += (world.size() - 1)) {
@@ -101,36 +97,50 @@ bool varfolomeev_g_transfer_from_one_to_all_scatter_mpi::TestMPITaskParallel::ru
     world.recv(0, 0, local_input_values);
   }
 
-  // Выполнение операции
-  int local_result = 0;
-  if (ops == "+") {
-    local_result = std::accumulate(local_input_values.begin(), local_input_values.end(), 0);
-  } else if (ops == "-") {
-    local_result = -std::accumulate(local_input_values.begin(), local_input_values.end(), 0);
-  } else if (ops == "max") {
-    if (!local_input_values.empty()) {
-      local_result = local_input_values[0];
-      for (int value : local_input_values) {
-        if (value > local_result) {
-          local_result = value;
+  int local_res = INT32_MAX;
+
+  if (world.rank() != 0) {
+    // Operation execution
+    if (ops == "+") {
+      local_res = std::accumulate(local_input_values.begin(), local_input_values.end(), 0);
+
+    } else if (ops == "-") {
+      local_res = -std::accumulate(local_input_values.begin(), local_input_values.end(), 0);
+    } else if (ops == "max") {
+      if (!local_input_values.empty()) {
+        local_res = local_input_values[0];
+        for (int value : local_input_values) {
+          if (value > local_res) {
+            local_res = value;
+          }
         }
+      } else {
+        return false;
       }
     }
   }
 
-  // Gathering results (root)
-  if (rank == 0) {
-    std::vector<int> results(world_size - 1);
+  if (world.rank() == 0) {  // catching results (root)
+    std::vector<int> results(world_size - 1, -1);
     for (int proc_num = 1; proc_num < world_size; proc_num++) {
-      world.recv(proc_num, 0, results[proc_num - 1]);
+      int tmp;
+      world.recv(proc_num, 0, tmp);
+      results[proc_num - 1] = tmp;
     }
     if (ops == "max") {
-      res = *std::max_element(results.begin(), results.end());
+      res = results[0];
+      for (int value : results) {
+        if (value > res) {
+          res = value;
+        }
+      }
     } else {
       res = std::accumulate(results.begin(), results.end(), 0);
     }
   } else {  // Sending results (non-root)
-    world.send(0, 0, local_result);
+    if (local_res != INT32_MAX) {
+      world.send(0, 0, local_res);
+    }
   }
 
   return true;
@@ -141,7 +151,5 @@ bool varfolomeev_g_transfer_from_one_to_all_scatter_mpi::TestMPITaskParallel::po
   if (world.rank() == 0) {
     reinterpret_cast<int*>(taskData->outputs[0])[0] = res;
   }
-  input_values.clear();
-  local_input_values.clear();
   return true;
 }
