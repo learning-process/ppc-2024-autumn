045dc3185a029d6b2d2e0fa65518b9c50aefd287
ОКСАНА ШУРАВИНА <shyravina19102004@gmail.com>
2024-12-05 18:16:38 +0300

    k59

--- tasks/mpi/shuravina_o_contrast/func_tests/main.cpp
+++ tasks/mpi/shuravina_o_contrast/func_tests/main.cpp
@@ -2,32 +2,9 @@
 
 #include <boost/mpi/communicator.hpp>
 #include <boost/mpi/environment.hpp>
-#include <boost/mpi/timer.hpp>
-#include <random>
 #include <vector>
 
-#include "core/perf/include/perf.hpp"
 #include "mpi/shuravina_o_contrast/include/ops_mpi.hpp"
-
-TEST(shuravina_o_contrast, Test_Contrast_Enhancement_Correct_Output_Size) {
-  boost::mpi::environment env;
-  boost::mpi::communicator world;
-
-  if (world.rank() == 0) {
-    auto taskDataPar = std::make_shared<ppc::core::TaskData>();
-
-    std::vector<uint8_t> input = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(input.data()));
-    taskDataPar->inputs_count.emplace_back(input.size());
-
-    std::vector<uint8_t> output(input.size());
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
-    taskDataPar->outputs_count.emplace_back(output.size());
-
-    shuravina_o_contrast::ContrastTaskParallel contrastTaskParallel(taskDataPar);
-    ASSERT_TRUE(contrastTaskParallel.validation());
-  }
-}
 
 TEST(shuravina_o_contrast, Test_Contrast_Enhancement_Empty_Input) {
   boost::mpi::environment env;
