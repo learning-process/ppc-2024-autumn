dd44dc6fd8dc232582a363d238f8c155f71be2bd
Иван Садиков <sadikov.04@list.ru>
2024-12-05 17:52:45 +0300

    New gather restart test

--- tasks/mpi/sadikov_I_gather/include/ops_mpi.h
+++ tasks/mpi/sadikov_I_gather/include/ops_mpi.h
@@ -12,6 +12,7 @@
 
 #include "core/task/include/task.hpp"
 
+
 namespace sadikov_I_gather_mpi {
 int DataLocation(int rank, int delta, int maxDelta);
 void Gather(const boost::mpi::communicator& world, std::map<int, int> id, std::vector<int>& inValues,
