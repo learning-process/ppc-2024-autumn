ef8ffa16383585b6c3c499f204ce52e521814a1c
MikeMuradov <mike.muradov.2005@mail.ru>
2024-12-06 15:56:17 +0300

    kavtorevd review

--- tasks/mpi/muradov_m_broadcast/func_tests/main.cpp
+++ tasks/mpi/muradov_m_broadcast/func_tests/main.cpp
@@ -7,6 +7,7 @@
 #include <boost/serialization/vector.hpp>
 #include <random>
 
+#include "boost/mpi/collectives/broadcast.hpp"
 #include "mpi/muradov_m_broadcast/include/ops_mpi.hpp"
 
 namespace muradov_m_broadcast_mpi {
@@ -25,7 +26,7 @@ std::vector<int> gen_rundom_vector(int n) {
 void test_template_rundom(int n, int source = 0) {
   boost::mpi::communicator world;
 
-  if (world.size() < source) {
+  if (world.size() <= source) {
     GTEST_SKIP();
   }
 
@@ -102,3 +103,55 @@ TEST(muradov_m_broadcast_mpi, data_size_50) { muradov_m_broadcast_mpi::test_temp
 TEST(muradov_m_broadcast_mpi, source_1_data_size_50) { muradov_m_broadcast_mpi::test_template_rundom(50, 1); }
 
 TEST(muradov_m_broadcast_mpi, source_2_data_size_10) { muradov_m_broadcast_mpi::test_template_rundom(50, 2); }
+
+TEST(muradov_m_broadcast_mpi, rundom_source_between_0_and_world_size_minus_1) {
+  boost::mpi::communicator world;
+
+  if (world.size() < 2) {
+    GTEST_SKIP();
+  }
+
+  int n = 50;
+  int source;
+  if (world.rank() == 0) {
+    std::random_device dev;
+    std::mt19937 gen(dev());
+    std::uniform_int_distribution<int> dist(0, world.size() - 1);
+    std::vector<int> vector(n);
+    source = dist(gen);
+  }
+  boost::mpi::broadcast(world, source, 0);
+
+  std::vector<int> A;
+  std::vector<int> A_res(n);
+  std::vector<int> B_res(n);
+
+  std::shared_ptr<ppc::core::TaskData> taskDataBroadcast = std::make_shared<ppc::core::TaskData>();
+
+  taskDataBroadcast->inputs.emplace_back(reinterpret_cast<uint8_t*>(&source));
+  taskDataBroadcast->inputs_count.emplace_back(1);
+
+  if (world.rank() == source) {
+    A = muradov_m_broadcast_mpi::gen_rundom_vector(n);
+
+    taskDataBroadcast->inputs.emplace_back(reinterpret_cast<uint8_t*>(A.data()));
+    taskDataBroadcast->inputs_count.emplace_back(n);
+
+    taskDataBroadcast->inputs.emplace_back(reinterpret_cast<uint8_t*>(A.data()));
+    taskDataBroadcast->inputs_count.emplace_back(n);
+  }
+
+  taskDataBroadcast->outputs.emplace_back(reinterpret_cast<uint8_t*>(A_res.data()));
+  taskDataBroadcast->outputs_count.emplace_back(n);
+
+  taskDataBroadcast->outputs.emplace_back(reinterpret_cast<uint8_t*>(B_res.data()));
+  taskDataBroadcast->outputs_count.emplace_back(n);
+
+  auto taskBroadcast = std::make_shared<muradov_m_broadcast_mpi::BroadcastParallelMPI>(taskDataBroadcast);
+  taskBroadcast->validation();
+  taskBroadcast->pre_processing();
+  taskBroadcast->run();
+  taskBroadcast->post_processing();
+
+  EXPECT_EQ(A_res, B_res);
+}
