53c82891215b1012f8608e63b572d68cf480eb0d
GrayPirat <s.agafeev@mail.ru>
2024-12-06 23:46:39 +0300

    Maybe it will build properly

--- tasks/mpi/agafeev_s_max_of_vector_elements/include/ops_mpi.hpp
+++ tasks/mpi/agafeev_s_max_of_vector_elements/include/ops_mpi.hpp
@@ -22,7 +22,13 @@ std::vector<T> create_RandomMatrix(int row_size, int column_size) {
 }
 
 template <typename T>
-T get_MaxValue(std::vector<T> matrix);
+T get_MaxValue(std::vector<T> matrix) {
+  T max_result = std::numeric_limits<T>::min();
+  for (unsigned int i = 0; i < matrix.size(); i++)
+    if (max_result < matrix[i]) max_result = matrix[i];
+
+  return max_result;
+}
 
 /*
 class MaxMatrixSeq : public ppc::core::Task {
