89af70e507626aca01ef5e0926a77ce1bbd9d2ef
KolodkinGrigorii <113025092+KolodkinGrigorii@users.noreply.github.com>
2024-12-07 16:50:25 +0300

    fix

--- tasks/mpi/kolodkin_g_image_contrast/func_tests/main.cpp
+++ tasks/mpi/kolodkin_g_image_contrast/func_tests/main.cpp
@@ -114,6 +114,58 @@ TEST(kolodkin_g_image_contrast_MPI, Test_image_center_line) {
   }
 }
 
+TEST(kolodkin_g_image_contrast_MPI, Test_image_10_x_3) {
+  boost::mpi::communicator world;
+  // Create data
+  std::vector<int> image;
+  std::vector<int> global_out(900, 0);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataMpi = std::make_shared<ppc::core::TaskData>();
+  if (world.rank() == 0) {
+    for (unsigned long i = 0; i < 900; i++) {
+      image.push_back(0 + rand() % 255);
+    }
+    taskDataMpi->inputs.emplace_back(reinterpret_cast<uint8_t *>(image.data()));
+    taskDataMpi->inputs_count.emplace_back(image.size());
+    taskDataMpi->outputs.emplace_back(reinterpret_cast<uint8_t *>(new std::vector<int>(global_out)));
+  } else {
+    image.resize(300, 0);
+    taskDataMpi->inputs.emplace_back(reinterpret_cast<uint8_t *>(image.data()));
+    taskDataMpi->inputs_count.emplace_back(image.size());
+    taskDataMpi->outputs.emplace_back(reinterpret_cast<uint8_t *>(new std::vector<int>(global_out)));
+  }
+  // Create Task
+  kolodkin_g_image_contrast_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataMpi);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+    // Create data
+    std::vector<int> reference_out(900, 0);
+
+    // Create TaskData
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(image.data()));
+    taskDataSeq->inputs_count.emplace_back(image.size());
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(new std::vector<int>(reference_out)));
+
+    // Create Task
+    kolodkin_g_image_contrast_mpi::TestMPITaskSequential testTaskSequential(taskDataSeq);
+    ASSERT_EQ(testTaskSequential.validation(), true);
+    testTaskSequential.pre_processing();
+    testTaskSequential.run();
+    testTaskSequential.post_processing();
+    global_out = *reinterpret_cast<std::vector<int> *>(taskDataMpi->outputs[0]);
+    reference_out = *reinterpret_cast<std::vector<int> *>(taskDataSeq->outputs[0]);
+    for (unsigned long i = 0; i < global_out.size(); i++) {
+      ASSERT_EQ(global_out[i], reference_out[i]);
+    }
+  }
+}
+
 TEST(kolodkin_g_image_contrast_MPI, Test_incorrect_image) {
   boost::mpi::communicator world;
   std::vector<int> image;
