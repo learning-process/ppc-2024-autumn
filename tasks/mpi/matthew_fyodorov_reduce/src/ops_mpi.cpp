73d8c3492d66d3893afbcd63358cb17ed0198f83
Matthewsoul124 <132999776+Matthewsoul124@users.noreply.github.com>
2024-12-07 23:15:04 +0300

    prius

---
+++ tasks/mpi/matthew_fyodorov_reduce/src/ops_mpi.cpp
@@ -0,0 +1,122 @@
+// Copyright 2023 Nesterov Alexander
+#include "mpi/matthew_fyodorov_reduce/include/ops_mpi.hpp"
+
+#include <algorithm>
+#include <boost/mpi.hpp>  // Для boost::mpi::communicator и boost::mpi::reduce
+#include <functional>
+#include <numeric>
+#include <random>
+#include <string>
+#include <thread>
+#include <vector>
+
+using namespace std::chrono_literals;
+
+std::vector<int> matthew_fyodorov_reduce_mpi::getRandomVector(int sz) {
+  std::random_device dev;
+  std::mt19937 gen(dev());
+  std::vector<int> vec(sz);
+  for (int i = 0; i < sz; i++) {
+    vec[i] = gen() % 100;
+  }
+  return vec;
+}
+
+bool matthew_fyodorov_reduce_mpi::TestMPITaskSequential::pre_processing() {
+  internal_order_test();
+  // Init vectors
+  input_ = std::vector<int>(taskData->inputs_count[0]);
+  auto* tmp_ptr = reinterpret_cast<int*>(taskData->inputs[0]);
+  for (unsigned i = 0; i < taskData->inputs_count[0]; i++) {
+    input_[i] = tmp_ptr[i];
+  }
+  // Init value for output
+  res = 0;
+  return true;
+}
+
+bool matthew_fyodorov_reduce_mpi::TestMPITaskSequential::validation() {
+  internal_order_test();
+  // Check count elements of output
+  return taskData->outputs_count[0] == 1;
+}
+
+bool matthew_fyodorov_reduce_mpi::TestMPITaskSequential::run() {
+  internal_order_test();
+  if (ops == "+") {
+    res = std::accumulate(input_.begin(), input_.end(), 0);
+  } else if (ops == "-") {
+    res = -std::accumulate(input_.begin(), input_.end(), 0);
+  } else if (ops == "max") {
+    res = *std::max_element(input_.begin(), input_.end());
+  }
+  return true;
+}
+
+bool matthew_fyodorov_reduce_mpi::TestMPITaskSequential::post_processing() {
+  internal_order_test();
+  reinterpret_cast<int*>(taskData->outputs[0])[0] = res;
+  return true;
+}
+
+bool matthew_fyodorov_reduce_mpi::TestMPITaskParallel::pre_processing() {
+  internal_order_test();
+  if (world.rank() == 0) {
+    input_.resize(taskData->inputs_count[0]);
+    auto* tempPtr = reinterpret_cast<int*>(taskData->inputs[0]);
+    std::copy(tempPtr, tempPtr + taskData->inputs_count[0], input_.begin());
+  }
+  res = 0;
+
+  return true;
+}
+
+bool matthew_fyodorov_reduce_mpi::TestMPITaskParallel::validation() {
+  internal_order_test();
+  if (world.rank() == 0) {
+    // Check count elements of output
+    return taskData->outputs_count[0] == 1;
+  }
+  return true;
+}
+
+bool matthew_fyodorov_reduce_mpi::TestMPITaskParallel::run() {
+  internal_order_test();
+  int rank = world.rank();
+
+  if (rank == 0) {
+    // int offset = 0;
+    for (int i = 0; i < (int)input_.size(); i += world.size()) {
+      local_input_.push_back(input_[i]);
+    }
+    for (int proc_num = 1; proc_num < world.size(); proc_num++) {
+      std::vector<int> local_data;
+      for (int i = proc_num; i < (int)input_.size(); i += world.size()) {
+        local_data.push_back(input_[i]);
+      }
+      world.send(proc_num, 0, local_data);
+    }
+  } else {
+    world.recv(0, 0, local_input_);
+  }
+
+  // for (int i = 0; i < local_input_.size(); i++) {
+  //   std::cout << local_input_[i] << " ";
+  // }
+  // std::cout << "\n";
+  int local_sum = std::accumulate(local_input_.begin(), local_input_.end(), 0);
+  int global_sum;
+
+  boost::mpi::reduce(world, local_sum, global_sum, std::plus<int>(), 0);
+  if (rank == 0) res = global_sum;
+
+  return true;
+}
+
+bool matthew_fyodorov_reduce_mpi::TestMPITaskParallel::post_processing() {
+  internal_order_test();
+  if (world.rank() == 0) {
+    reinterpret_cast<int*>(taskData->outputs[0])[0] = res;
+  }
+  return true;
+}
---
+++ tasks/mpi/matthew_fyodorov_reduce_custom/func_tests/main.cpp
@@ -0,0 +1,185 @@
+// Copyright 2023 Nesterov Alexander
+#include <gtest/gtest.h>
+
+#include <boost/mpi/communicator.hpp>
+#include <boost/mpi/environment.hpp>
+#include <vector>
+
+#include "mpi/matthew_fyodorov_reduce_custom/include/ops_mpi.hpp"
+
+TEST(matthew_fyodorov_reduce_custom_mpi, TestMPITaskParallel_Sum_PositiveNumbers) {
+  boost::mpi::environment env;
+  boost::mpi::communicator world;
+  int rank = world.rank();
+  int size = world.size();
+
+  if (size > 1) {
+    std::vector<int> input = {1, 2, 3, 4, 5};
+    std::vector<int> output(1);
+    std::shared_ptr<ppc::core::TaskData> taskData = std::make_shared<ppc::core::TaskData>();
+
+    taskData->inputs_count.emplace_back(input.size());
+    taskData->inputs.emplace_back(reinterpret_cast<uint8_t*>(input.data()));
+
+    taskData->outputs_count.emplace_back(1);
+    taskData->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
+
+    matthew_fyodorov_reduce_custom_mpi ::TestMPITaskParallel task(taskData);
+    ASSERT_TRUE(task.validation());
+    ASSERT_TRUE(task.pre_processing());
+    ASSERT_TRUE(task.run());
+    ASSERT_TRUE(task.post_processing());
+
+    if (rank == 0) {
+      ASSERT_EQ(output[0], 15);
+    }
+  }
+}
+
+TEST(matthew_fyodorov_reduce_custom_mpi, TestMPITaskParallel_Sum_PositiveNumbers_2) {
+  boost::mpi::environment env;
+  boost::mpi::communicator world;
+  int rank = world.rank();
+  int size = world.size();
+
+  if (size > 1) {
+    std::vector<int> input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::vector<int> output(1);
+    std::shared_ptr<ppc::core::TaskData> taskData = std::make_shared<ppc::core::TaskData>();
+
+    taskData->inputs_count.emplace_back(input.size());
+    taskData->inputs.emplace_back(reinterpret_cast<uint8_t*>(input.data()));
+
+    taskData->outputs_count.emplace_back(1);
+    taskData->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
+
+    matthew_fyodorov_reduce_custom_mpi ::TestMPITaskParallel task(taskData);
+    ASSERT_TRUE(task.validation());
+    ASSERT_TRUE(task.pre_processing());
+    ASSERT_TRUE(task.run());
+    ASSERT_TRUE(task.post_processing());
+
+    if (rank == 0) {
+      ASSERT_EQ(output[0], 55);
+    }
+  }
+}
+
+TEST(matthew_fyodorov_reduce_custom_mpi, TestMPITaskParallel_Sum_Positive_and_NegativeNumbers) {
+  boost::mpi::environment env;
+  boost::mpi::communicator world;
+  int rank = world.rank();
+  int size = world.size();
+
+  if (size > 1) {
+    std::vector<int> input = {1, -2, 3, -4, 5};
+    std::vector<int> output(1);
+    std::shared_ptr<ppc::core::TaskData> taskData = std::make_shared<ppc::core::TaskData>();
+
+    taskData->inputs_count.emplace_back(input.size());
+    taskData->inputs.emplace_back(reinterpret_cast<uint8_t*>(input.data()));
+
+    taskData->outputs_count.emplace_back(1);
+    taskData->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
+
+    matthew_fyodorov_reduce_custom_mpi ::TestMPITaskParallel task(taskData);
+    ASSERT_TRUE(task.validation());
+    ASSERT_TRUE(task.pre_processing());
+    ASSERT_TRUE(task.run());
+    ASSERT_TRUE(task.post_processing());
+
+    if (rank == 0) {
+      ASSERT_EQ(output[0], 3);
+    }
+  }
+}
+
+TEST(matthew_fyodorov_reduce_custom_mpi, TestMPITaskParallel_Sum_Zero_Numbers) {
+  boost::mpi::environment env;
+  boost::mpi::communicator world;
+  int rank = world.rank();
+  int size = world.size();
+
+  if (size > 1) {
+    std::vector<int> input = {0};
+    std::vector<int> output(1);
+    std::shared_ptr<ppc::core::TaskData> taskData = std::make_shared<ppc::core::TaskData>();
+
+    taskData->inputs_count.emplace_back(input.size());
+    taskData->inputs.emplace_back(reinterpret_cast<uint8_t*>(input.data()));
+
+    taskData->outputs_count.emplace_back(1);
+    taskData->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
+
+    matthew_fyodorov_reduce_custom_mpi ::TestMPITaskParallel task(taskData);
+    ASSERT_TRUE(task.validation());
+    ASSERT_TRUE(task.pre_processing());
+    ASSERT_TRUE(task.run());
+    ASSERT_TRUE(task.post_processing());
+
+    if (rank == 0) {
+      ASSERT_EQ(output[0], 0);
+    }
+  }
+}
+
+TEST(matthew_fyodorov_reduce_custom_mpi, TestMPITaskParallel_Sum_Random_Numbers) {
+  boost::mpi::environment env;
+  boost::mpi::communicator world;
+  int rank = world.rank();
+  int size = world.size();
+
+  if (size > 1) {
+    std::vector<int> input = matthew_fyodorov_reduce_custom_mpi ::getRandomVector(5);
+    std::vector<int> output(1);
+    std::shared_ptr<ppc::core::TaskData> taskData = std::make_shared<ppc::core::TaskData>();
+
+    taskData->inputs_count.emplace_back(input.size());
+    taskData->inputs.emplace_back(reinterpret_cast<uint8_t*>(input.data()));
+
+    taskData->outputs_count.emplace_back(1);
+    taskData->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
+
+    matthew_fyodorov_reduce_custom_mpi ::TestMPITaskParallel task(taskData);
+    ASSERT_TRUE(task.validation());
+    ASSERT_TRUE(task.pre_processing());
+    ASSERT_TRUE(task.run());
+    ASSERT_TRUE(task.post_processing());
+
+    if (rank == 0) {
+      int expected_sum = std::accumulate(input.begin(), input.end(), 0);
+      ASSERT_EQ(output[0], expected_sum);
+    }
+  }
+}
+
+/*
+TEST(matthew_fyodorov_reduce_custom_mpi , TestMPITaskParallel_Sum_PositiveNumbers_2) {
+   boost::mpi::environment env;
+   boost::mpi::communicator world;
+   int rank = world.rank();
+   int size = world.size();
+
+   if (size > 1) { // Тест выполняется только если больше одного процесса
+          std::vector<int> input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+          std::vector<int> output(1, 0);
+          std::shared_ptr<ppc::core::TaskData> taskData = std::make_shared<ppc::core::TaskData>();
+          taskData->inputs.emplace_back(reinterpret_cast<uint8_t*>(input.data()));
+          taskData->inputs_count.emplace_back(input.size());
+
+          taskData->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
+          taskData->outputs_count.emplace_back(1);
+
+  matthew_fyodorov_reduce_custom_mpi ::TestMPITaskParallel task(taskData);
+          ASSERT_TRUE(task.validation());
+          ASSERT_TRUE(task.pre_processing());
+          ASSERT_TRUE(task.run());
+          ASSERT_TRUE(task.post_processing());
+
+
+  if (rank == 0) {
+              ASSERT_EQ(output[0], 55);
+          }
+      }
+  }
+  */
