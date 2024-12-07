d989d0b8405a113c61ef8d4e115cd6cf1c728950
Bogdan__ <bogerbloger@yandex.ru>
2024-12-07 20:03:13 +0300

    fix_test

--- tasks/mpi/naumov_b_bubble_sort/func_tests/main.cpp
+++ tasks/mpi/naumov_b_bubble_sort/func_tests/main.cpp
@@ -43,52 +43,56 @@ TEST(naumov_b_bubble_sort_mpi, Test_10_int) {
   tmpTaskPar.run();
   tmpTaskPar.post_processing();
 
-  std::vector<int> reference = in;
-  std::sort(reference.begin(), reference.end());
-  ASSERT_EQ(out, reference);
-}
+  if (world.rank() == 0) {
+    std::vector<int> reference = in;
+    std::sort(reference.begin(), reference.end());
+    ASSERT_EQ(out, reference);
+  }
+  }
 
-// TEST(naumov_b_bubble_sort_mpi, Test_100_int) {
-//   const size_t length = 100;
-//   std::vector<int> in(length);
-//   std::vector<int> out(length);
+TEST(naumov_b_bubble_sort_mpi, Test_100_int) {
+  const size_t length = 100;
+  std::vector<int> in(length);
+  std::vector<int> out(length);
 
-//   std::mt19937 generator(std::random_device{}());
-//   std::uniform_int_distribution<int> distribution(-1000, 1000);
-//   for (size_t i = 0; i < length; ++i) {
-//     in[i] = distribution(generator);
-//   }
+  std::mt19937 generator(std::random_device{}());
+  std::uniform_int_distribution<int> distribution(-1000, 1000);
+  for (size_t i = 0; i < length; ++i) {
+    in[i] = distribution(generator);
+  }
 
-//   boost::mpi::communicator world;
-//   std::shared_ptr<ppc::core::TaskData> tmpPar = std::make_shared<ppc::core::TaskData>();
+  boost::mpi::communicator world;
+  std::shared_ptr<ppc::core::TaskData> tmpPar = std::make_shared<ppc::core::TaskData>();
 
-//   if (in.empty() || out.empty()) {
-//     std::cerr << "Error: Input or output vectors are empty!" << std::endl;
-//   }
+  if (in.empty() || out.empty()) {
+    std::cerr << "Error: Input or output vectors are empty!" << std::endl;
+  }
 
-//   if (world.rank() == 0) {
-//     try {
-//       tmpPar->inputs_count.emplace_back(in.size());
-//       tmpPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
-//       tmpPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
-//       tmpPar->outputs_count.emplace_back(out.size());
-//     } catch (const std::exception &e) {
-//       std::cerr << "Error while initializing tmpPar: " << e.what() << std::endl;
-//     }
-//   }
+  if (world.rank() == 0) {
+    try {
+      tmpPar->inputs_count.emplace_back(in.size());
+      tmpPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+      tmpPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+      tmpPar->outputs_count.emplace_back(out.size());
+    } catch (const std::exception &e) {
+      std::cerr << "Error while initializing tmpPar: " << e.what() << std::endl;
+    }
+  }
 
-//   naumov_b_bubble_sort_mpi::TestMPITaskParallel tmpTaskPar(tmpPar);
+  naumov_b_bubble_sort_mpi::TestMPITaskParallel tmpTaskPar(tmpPar);
 
-//   ASSERT_TRUE(tmpTaskPar.validation());
+  ASSERT_TRUE(tmpTaskPar.validation());
 
-//   tmpTaskPar.pre_processing();
-//   tmpTaskPar.run();
-//   tmpTaskPar.post_processing();
+  tmpTaskPar.pre_processing();
+  tmpTaskPar.run();
+  tmpTaskPar.post_processing();
 
-//   std::vector<int> reference = in;
-//   std::sort(reference.begin(), reference.end());
-//   ASSERT_EQ(out, reference);
-// }
+  if (world.rank() == 0) {
+    std::vector<int> reference = in;
+    std::sort(reference.begin(), reference.end());
+    ASSERT_EQ(out, reference);
+  }
+  }
 
 TEST(naumov_b_bubble_sort_mpi, Test_empty_array) {
   const size_t length = 0;
