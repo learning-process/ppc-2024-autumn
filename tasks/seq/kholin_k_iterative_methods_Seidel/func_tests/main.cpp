b60c3a8939364af2aefaf242e7d9fbcaf816c5c3
Kirius257 <holin.kirill@yandex.ru>
2024-12-08 19:38:12 +0300

    fix

--- tasks/seq/kholin_k_iterative_methods_Seidel/func_tests/main.cpp
+++ tasks/seq/kholin_k_iterative_methods_Seidel/func_tests/main.cpp
@@ -4,7 +4,7 @@
 
 #include "seq/kholin_k_iterative_methods_Seidel/include/ops_seq.hpp"
 
-TEST(kholin_k_iterative_methods_Seidel_seq, validation_true_when_matrix_with_diag_pred) {
+TEST(kholin_k_iterative_methods_Seidel_seq, validation_true_when_matrix_with_diag_pred_ver1) {
   const size_t count_rows = 3;
   const size_t count_colls = 3;
   float epsilon = 0.001f;
@@ -126,4 +126,38 @@ TEST(kholin_k_iterative_methods_Seidel_seq, validation_false_when_matrix_no_quad
 
   kholin_k_iterative_methods_Seidel_seq::TestTaskSequential testTaskSequential(taskDataSeq);
   ASSERT_EQ(testTaskSequential.validation(), false);
+}
+
+TEST(kholin_k_iterative_methods_Seidel_seq, validation_false_when_matrix_no_diag_pred) {
+  const size_t count_rows = 3;
+  const size_t count_colls = 3;
+  float epsilon = 0.001f;
+
+  std::vector<float> in(count_rows * count_colls);
+
+  for (size_t i = 0; i < count_rows; i++) {
+    for (size_t j = 0; j < count_colls; j++) {
+      if (i == j) {
+        in[count_colls * i + j] = 1.0f;
+      }
+      in[count_colls * i + j] = kholin_k_iterative_methods_Seidel_seq::gen_float_value();
+    }
+
+    std::vector<float> out(count_rows);
+    std::vector<float> X0(count_rows, 0.0f);
+    std::vector<float> B = kholin_k_iterative_methods_Seidel_seq::gen_vector(count_rows);
+
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(&epsilon));
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(X0.data()));
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(B.data()));
+    taskDataSeq->inputs_count.emplace_back(count_rows);
+    taskDataSeq->inputs_count.emplace_back(count_colls);
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+    taskDataSeq->outputs_count.emplace_back(count_rows);
+
+    kholin_k_iterative_methods_Seidel_seq::TestTaskSequential testTaskSequential(taskDataSeq);
+    ASSERT_EQ(testTaskSequential.validation(), false);
+  }
 }
