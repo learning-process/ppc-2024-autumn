b13ea171c2bf19dcea7c4e09fa049367b93e0cbd
Шурыгин Сергей <Shurigin11@mail.ru>
2024-12-08 19:47:25 +0300

    add mpi

---
+++ tasks/mpi/shurigin_lin_filtr_razbien_bloch_gaus_3x3/func_tests/main.cpp
@@ -0,0 +1,302 @@
+#include <gtest/gtest.h>
+
+#include <boost/mpi/communicator.hpp>
+#include <boost/mpi/environment.hpp>
+#include <random>
+#include <vector>
+
+#include "mpi/shurigin_lin_filtr_razbien_bloch_gaus_3x3/include/ops_mpi.hpp"
+
+TEST(shurigin_lin_filtr_razbien_bloch_gaus_3x3_mpi, validation_zero_zero) {
+  boost::mpi::communicator world;
+  std::vector<int> global_matrix;
+  std::vector<int> global_result;
+  int num_rows = 0, num_cols = 0;
+
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    global_matrix.resize(num_rows * num_cols);
+    global_result.resize(num_rows * num_cols);
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_matrix.data()));
+    taskDataPar->inputs_count.emplace_back(global_matrix.size());
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_rows));
+    taskDataPar->inputs_count.emplace_back(1);
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_cols));
+    taskDataPar->inputs_count.emplace_back(1);
+
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_result.data()));
+    taskDataPar->outputs_count.emplace_back(global_result.size());
+  }
+
+  auto taskParallel = std::make_shared<shurigin_lin_filtr_razbien_bloch_gaus_3x3_mpi::TaskMpi>(taskDataPar);
+  if (world.rank() == 0) {
+    EXPECT_FALSE(taskParallel->validation());
+  } else {
+    EXPECT_TRUE(taskParallel->validation());
+  }
+}
+
+TEST(shurigin_lin_filtr_razbien_bloch_gaus_3x3_mpi, validation_one_one) {
+  boost::mpi::communicator world;
+  std::vector<int> global_matrix;
+  std::vector<int> global_result;
+  int num_rows = 1, num_cols = 1;
+
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    global_matrix.resize(num_rows * num_cols);
+    global_result.resize(num_rows * num_cols);
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_matrix.data()));
+    taskDataPar->inputs_count.emplace_back(global_matrix.size());
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_rows));
+    taskDataPar->inputs_count.emplace_back(1);
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_cols));
+    taskDataPar->inputs_count.emplace_back(1);
+
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_result.data()));
+    taskDataPar->outputs_count.emplace_back(global_result.size());
+  }
+
+  auto taskParallel = std::make_shared<shurigin_lin_filtr_razbien_bloch_gaus_3x3_mpi::TaskMpi>(taskDataPar);
+  if (world.rank() == 0) {
+    EXPECT_FALSE(taskParallel->validation());
+  } else {
+    EXPECT_TRUE(taskParallel->validation());
+  }
+}
+
+TEST(shurigin_lin_filtr_razbien_bloch_gaus_3x3_mpi, validation_three_two) {
+  boost::mpi::communicator world;
+  std::vector<int> global_matrix;
+  std::vector<int> global_result;
+  int num_rows = 3, num_cols = 2;
+
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    global_matrix.resize(num_rows * num_cols);
+    global_result.resize(num_rows * num_cols);
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_matrix.data()));
+    taskDataPar->inputs_count.emplace_back(global_matrix.size());
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_rows));
+    taskDataPar->inputs_count.emplace_back(1);
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_cols));
+    taskDataPar->inputs_count.emplace_back(1);
+
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_result.data()));
+    taskDataPar->outputs_count.emplace_back(global_result.size());
+  }
+
+  auto taskParallel = std::make_shared<shurigin_lin_filtr_razbien_bloch_gaus_3x3_mpi::TaskMpi>(taskDataPar);
+  if (world.rank() == 0) {
+    EXPECT_FALSE(taskParallel->validation());
+  } else {
+    EXPECT_TRUE(taskParallel->validation());
+  }
+}
+
+TEST(shurigin_lin_filtr_razbien_bloch_gaus_3x3_mpi, three_three) {
+  boost::mpi::communicator world;
+  std::vector<int> global_matrix;
+  std::vector<int> global_result;
+  int num_rows = 3, num_cols = 3;
+
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    std::random_device dev;
+    std::mt19937 gen(dev());
+    std::uniform_int_distribution<int> dist(0, 255);
+
+    global_matrix.resize(num_rows * num_cols);
+    for (int i = 0; i < num_rows * num_cols; i++) {
+      global_matrix[i] = dist(gen);
+    }
+    global_result.resize(num_rows * num_cols);
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_matrix.data()));
+    taskDataPar->inputs_count.emplace_back(global_matrix.size());
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_rows));
+    taskDataPar->inputs_count.emplace_back(1);
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_cols));
+    taskDataPar->inputs_count.emplace_back(1);
+
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_result.data()));
+    taskDataPar->outputs_count.emplace_back(global_result.size());
+  }
+
+  auto taskParallel = std::make_shared<shurigin_lin_filtr_razbien_bloch_gaus_3x3_mpi::TaskMpi>(taskDataPar);
+  ASSERT_TRUE(taskParallel->validation());
+  taskParallel->pre_processing();
+  taskParallel->run();
+  taskParallel->post_processing();
+
+  if (world.rank() == 0) {
+    std::vector<int> seq_result(global_result.size(), 0);
+
+    auto taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_matrix.data()));
+    taskDataSeq->inputs_count.emplace_back(global_matrix.size());
+
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_rows));
+    taskDataSeq->inputs_count.emplace_back(1);
+
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_cols));
+    taskDataSeq->inputs_count.emplace_back(1);
+
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(seq_result.data()));
+    taskDataSeq->outputs_count.emplace_back(seq_result.size());
+
+    auto taskSequential = std::make_shared<shurigin_lin_filtr_razbien_bloch_gaus_3x3_mpi::TaskSeq>(taskDataSeq);
+    ASSERT_TRUE(taskSequential->validation());
+    taskSequential->pre_processing();
+    taskSequential->run();
+    taskSequential->post_processing();
+
+    ASSERT_EQ(global_result.size(), seq_result.size());
+    EXPECT_EQ(global_result, seq_result);
+  }
+}
+
+TEST(shurigin_lin_filtr_razbien_bloch_gaus_3x3_mpi, five_five) {
+  boost::mpi::communicator world;
+  std::vector<int> global_matrix;
+  std::vector<int> global_result;
+  int num_rows = 5, num_cols = 5;
+
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    std::random_device dev;
+    std::mt19937 gen(dev());
+    std::uniform_int_distribution<int> dist(0, 255);
+
+    global_matrix.resize(num_rows * num_cols);
+    for (int i = 0; i < num_rows * num_cols; i++) {
+      global_matrix[i] = dist(gen);
+    }
+    global_result.resize(num_rows * num_cols);
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_matrix.data()));
+    taskDataPar->inputs_count.emplace_back(global_matrix.size());
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_rows));
+    taskDataPar->inputs_count.emplace_back(1);
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_cols));
+    taskDataPar->inputs_count.emplace_back(1);
+
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_result.data()));
+    taskDataPar->outputs_count.emplace_back(global_result.size());
+  }
+
+  auto taskParallel = std::make_shared<shurigin_lin_filtr_razbien_bloch_gaus_3x3_mpi::TaskMpi>(taskDataPar);
+  ASSERT_TRUE(taskParallel->validation());
+  taskParallel->pre_processing();
+  taskParallel->run();
+  taskParallel->post_processing();
+
+  if (world.rank() == 0) {
+    std::vector<int> seq_result(global_result.size(), 0);
+
+    auto taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_matrix.data()));
+    taskDataSeq->inputs_count.emplace_back(global_matrix.size());
+
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_rows));
+    taskDataSeq->inputs_count.emplace_back(1);
+
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_cols));
+    taskDataSeq->inputs_count.emplace_back(1);
+
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(seq_result.data()));
+    taskDataSeq->outputs_count.emplace_back(seq_result.size());
+
+    auto taskSequential = std::make_shared<shurigin_lin_filtr_razbien_bloch_gaus_3x3_mpi::TaskSeq>(taskDataSeq);
+    ASSERT_TRUE(taskSequential->validation());
+    taskSequential->pre_processing();
+    taskSequential->run();
+    taskSequential->post_processing();
+
+    ASSERT_EQ(global_result.size(), seq_result.size());
+    EXPECT_EQ(global_result, seq_result);
+  }
+}
+
+TEST(shurigin_lin_filtr_razbien_bloch_gaus_3x3_mpi, three_six) {
+  boost::mpi::communicator world;
+  std::vector<int> global_matrix;
+  std::vector<int> global_result;
+  int num_rows = 3, num_cols = 6;
+
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    std::random_device dev;
+    std::mt19937 gen(dev());
+    std::uniform_int_distribution<int> dist(0, 255);
+
+    global_matrix.resize(num_rows * num_cols);
+    for (int i = 0; i < num_rows * num_cols; i++) {
+      global_matrix[i] = dist(gen);
+    }
+    global_result.resize(num_rows * num_cols);
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_matrix.data()));
+    taskDataPar->inputs_count.emplace_back(global_matrix.size());
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_rows));
+    taskDataPar->inputs_count.emplace_back(1);
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_cols));
+    taskDataPar->inputs_count.emplace_back(1);
+
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_result.data()));
+    taskDataPar->outputs_count.emplace_back(global_result.size());
+  }
+
+  auto taskParallel = std::make_shared<shurigin_lin_filtr_razbien_bloch_gaus_3x3_mpi::TaskMpi>(taskDataPar);
+  ASSERT_TRUE(taskParallel->validation());
+  taskParallel->pre_processing();
+  taskParallel->run();
+  taskParallel->post_processing();
+
+  if (world.rank() == 0) {
+    std::vector<int> seq_result(global_result.size(), 0);
+
+    auto taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_matrix.data()));
+    taskDataSeq->inputs_count.emplace_back(global_matrix.size());
+
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_rows));
+    taskDataSeq->inputs_count.emplace_back(1);
+
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_cols));
+    taskDataSeq->inputs_count.emplace_back(1);
+
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(seq_result.data()));
+    taskDataSeq->outputs_count.emplace_back(seq_result.size());
+
+    auto taskSequential = std::make_shared<shurigin_lin_filtr_razbien_bloch_gaus_3x3_mpi::TaskSeq>(taskDataSeq);
+    ASSERT_TRUE(taskSequential->validation());
+    taskSequential->pre_processing();
+    taskSequential->run();
+    taskSequential->post_processing();
+
+    ASSERT_EQ(global_result.size(), seq_result.size());
+    EXPECT_EQ(global_result, seq_result);
+  }
+}
