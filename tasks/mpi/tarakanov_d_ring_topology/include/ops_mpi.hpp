ffc337f317c8cf4bcec42f4306f3d1dbf37b149d
Denis Tarakanov <denis.tarakanov@radiogigabit.net>
2024-12-08 16:37:14 +0300

    fixes for PR

--- tasks/mpi/tarakanov_d_ring_topology/include/ops_mpi.hpp
+++ tasks/mpi/tarakanov_d_ring_topology/include/ops_mpi.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <gtest/gtest.h>
+
 #include <boost/mpi/collectives.hpp>
 #include <boost/mpi/communicator.hpp>
 #include <memory>
@@ -12,24 +13,20 @@
 namespace tarakanov_d_test_task_mpi {
 
 class TestMPITaskParallel : public ppc::core::Task {
-public:
-    explicit TestMPITaskParallel(std::shared_ptr<ppc::core::TaskData> taskData)
-        : Task(std::move(taskData)),
-          world_comm_(boost::mpi::communicator()),
-          buffer_size_(0),
-          received_size_(0) {}
+ public:
+  explicit TestMPITaskParallel(std::shared_ptr<ppc::core::TaskData> taskData) : Task(std::move(taskData)) {}
 
-    bool pre_processing() override;
-    bool validation() override;
-    bool run() override;
-    bool post_processing() override;
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
 
-private:
-    std::vector<int> data_buffer_;
-    boost::mpi::communicator world_comm_;
-    
-    int buffer_size_;
-    int received_size_;
+ private:
+  std::vector<int> data_buffer_;
+  boost::mpi::communicator world_comm_;
+
+  int buffer_size_{0};
+  int received_size_{0};
 };
 
-} // namespace tarakanov_d_test_task_mpi
+}  // namespace tarakanov_d_test_task_mpi
