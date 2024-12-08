fbff8b5516936eed19976ab61ceaf9f1875a2f77
Budazhapova <ka10nnn@yandex.ru>
2024-12-08 21:54:26 +0300

    A to A_matrix

--- tasks/seq/budazhapova_e_matrix_multiplication/func_tests/matrix_mult_func_tests.cpp
+++ tasks/seq/budazhapova_e_matrix_multiplication/func_tests/matrix_mult_func_tests.cpp
@@ -29,8 +29,8 @@ TEST(budazhapova_e_matrix_mult_seq, ordinary_test) {
 }
 
 TEST(budazhapova_e_matrix_mult_seq, ordinary_test_2) {
-  std::vector<int> A = {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17, 18,
-                        19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36};
+  std::vector<int> A_matrix = {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17, 18,
+                               19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36};
   std::vector<int> b_vector = {1, 2, 3, 4, 5, 6};
   std::vector<int> out(6, 0);
   std::vector<int> ans = {91, 217, 343, 469, 595, 721};
