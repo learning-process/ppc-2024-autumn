3c001af3ced310bfb0714e51a51f32bc090d1768
MishaRysev <r0botbob@yandex.ru>
2024-12-09 15:49:20 +0300

    Рысев Михаил. Задача 2. Вариант 14. Топология Гиперкуб

--- tasks/mpi/rysev_m_hypercube/include/ops_mpi.hpp
+++ tasks/mpi/rysev_m_hypercube/include/ops_mpi.hpp
@@ -2,13 +2,13 @@
 #pragma once
 
 #include <gtest/gtest.h>
-#include <cmath>
 
 #include <algorithm>
 #include <boost/mpi/collectives.hpp>
 #include <boost/mpi/communicator.hpp>
 #include <boost/serialization/serialization.hpp>
 #include <boost/serialization/vector.hpp>
+#include <cmath>
 #include <memory>
 #include <numeric>
 #include <string>
