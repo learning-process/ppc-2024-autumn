a2050e189a4cf33cd12e2f83cce6de6c3817c266
ARBUZNIJ <kovkonst@gmail.com>
2024-12-09 23:47:10 +0300

    +realisation +tests

---
+++ tasks/seq/kovalev_k_multidimensional_integrals_rectangle_method/include/header.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <stack>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace kovalev_k_multidimensional_integrals_rectangle_method_seq {
+
+class MultidimensionalIntegralsRectangleMethod : public ppc::core::Task {
+ private:
+  std::vector<std::pair<double, double>> limits;
+  size_t n;
+  std::function<double(std::vector<double>& args)> func;
+  double h;
+  double res;
+
+ public:
+  explicit MultidimensionalIntegralsRectangleMethod(std::shared_ptr<ppc::core::TaskData> taskData_,
+                                                    std::function<double(std::vector<double>& args)> func_)
+      : Task(taskData_), n(taskData_->inputs_count[0]), func(func_) {}
+  bool count_multidimensional_integrals_rectangle_method_seq();
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+};
+}  // namespace kovalev_k_multidimensional_integrals_rectangle_method_seq
