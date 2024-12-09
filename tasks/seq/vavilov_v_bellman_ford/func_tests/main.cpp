58487f23717bc526dc4d68e7eafc6f2ef1ba5bc6
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2024-12-09 03:05:52 +0300

    Update main.cpp

--- tasks/seq/vavilov_v_bellman_ford/func_tests/main.cpp
+++ tasks/seq/vavilov_v_bellman_ford/func_tests/main.cpp
@@ -47,7 +47,30 @@ TEST(vavilov_v_bellman_ford_seq, ValidInputWithMultiplePaths_2) {
   ASSERT_TRUE(testTaskSequential.run());
   ASSERT_TRUE(testTaskSequential.post_processing());
 
-  std::vector<int> expected_output = {0, -1, 2, 1, INT_MAX};
+  std::vector<int> expected_output = {0, -1, 2, 1, -2};
+  EXPECT_EQ(output, expected_output);
+}
+
+TEST(vavilov_v_bellman_ford_seq, DisconnectedGraph) {
+  std::vector<int> edges = {0, 1, 4, 0, 2, 1, 1, 3, 2};
+  std::vector<int> output(5);
+  int vertices = 5, edges_count = 3, source = 0;
+
+  auto taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs_count.emplace_back(vertices);
+  taskDataSeq->inputs_count.emplace_back(edges_count);
+  taskDataSeq->inputs_count.emplace_back(source);
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(edges.data()));
+  taskDataSeq->outputs_count.emplace_back(output.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
+
+  vavilov_v_bellman_ford_seq::TestTaskSequential testTaskSequential(taskDataSeq);
+  ASSERT_TRUE(testTaskSequential.validation());
+  ASSERT_TRUE(testTaskSequential.pre_processing());
+  ASSERT_TRUE(testTaskSequential.run());
+  ASSERT_TRUE(testTaskSequential.post_processing());
+
+  std::vector<int> expected_output = {0, 4, 1, 6, INT_MAX};
   EXPECT_EQ(output, expected_output);
 }
 
