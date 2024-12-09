fcb3e5005f8fccac29e44ed58529b76cf2c7caa0
TayaGordeeva <tauona.g2004@gmail.com>
2024-12-09 23:58:39 +0300

    oh1

--- tasks/mpi/gordeeva_t_sleeping_barber/src/ops_mpi.cpp
+++ tasks/mpi/gordeeva_t_sleeping_barber/src/ops_mpi.cpp
@@ -52,25 +52,29 @@ bool gordeeva_t_sleeping_barber_mpi::TestMPITaskParallel::post_processing() {
 
   world.barrier();
 
-  if (world.rank() == 0) {
-    if (!taskData->outputs.empty() && taskData->outputs_count[0] == sizeof(int)) {
+  if (!taskData->outputs.empty() && taskData->outputs_count[0] == sizeof(int)) {
+    if (world.rank() == 0) {
       *reinterpret_cast<int*>(taskData->outputs[0]) = result;
     } else {
-      return false;
+      *reinterpret_cast<int*>(taskData->outputs[0]) = 0;
     }
+  } else {
+    return false;
   }
 
   return true;
 }
 
 void gordeeva_t_sleeping_barber_mpi::TestMPITaskParallel::barber_logic() {
-  boost::mpi::request reqs;
   while (true) {
     int client_id = -1;
+    {
+      boost::mpi::request req = world.irecv(1, 0, client_id);
+      req.wait();
+    }
 
-    reqs = world.irecv(1, 0, client_id);
-    reqs.wait();
     if (client_id == -1) {
+      // end
       result = 0;
       return;
     }
@@ -80,7 +84,6 @@ void gordeeva_t_sleeping_barber_mpi::TestMPITaskParallel::barber_logic() {
 }
 
 void gordeeva_t_sleeping_barber_mpi::TestMPITaskParallel::dispatcher_logic() {
-  //boost::mpi::request reqs;
   std::queue<int> waiting_clients;
   max_waiting_chairs = taskData->inputs_count[0];
   int remaining_clients = world.size() - 2;
@@ -90,78 +93,105 @@ void gordeeva_t_sleeping_barber_mpi::TestMPITaskParallel::dispatcher_logic() {
     int client_id = -1;
 
     if (world.iprobe(boost::mpi::any_source, 0)) {
-      world.irecv(boost::mpi::any_source, 0, client_id);
-      //reqs.wait();
+      {
+        boost::mpi::request req = world.irecv(boost::mpi::any_source, 0, client_id);
+        req.wait();
+      }
 
       if (static_cast<int>(waiting_clients.size()) < max_waiting_chairs) {
         waiting_clients.push(client_id);
-        world.isend(client_id, 1, true);
-        //reqs.wait();
+        {
+          boost::mpi::request req = world.isend(client_id, 1, true);
+          req.wait();
+        }
       } else {
-        world.isend(client_id, 1, false);
-        //reqs.wait();
+        {
+          boost::mpi::request req = world.isend(client_id, 1, false);
+          req.wait();
+        }
       }
     }
 
     if (!barber_busy && !waiting_clients.empty()) {
       int next_client = waiting_clients.front();
       waiting_clients.pop();
-      world.isend(0, 0, next_client);
-      //reqs.wait();
+      {
+        boost::mpi::request req = world.isend(0, 0, next_client);
+        req.wait();
+      }
       barber_busy = true;
     }
 
     if (world.iprobe(0, 4)) {
       int barber_signal;
-      world.irecv(0, 4, barber_signal);
-      //reqs.wait();
+      {
+        boost::mpi::request req = world.irecv(0, 4, barber_signal);
+        req.wait();
+      }
       barber_busy = false;
     }
 
     if (waiting_clients.empty() && remaining_clients == 0 && !barber_busy) {
-      world.isend(0, 0, -1);
-      //reqs.wait();
+      {
+        boost::mpi::request req = world.isend(0, 0, -1);
+        req.wait();
+      }
       break;
     }
 
     if (world.iprobe(boost::mpi::any_source, 3)) {
       int done_signal;
-      world.irecv(boost::mpi::any_source, 3, done_signal);
-      //reqs.wait();
+      {
+        boost::mpi::request req = world.irecv(boost::mpi::any_source, 3, done_signal);
+        req.wait();
+      }
       remaining_clients--;
     }
   }
 }
 
 void gordeeva_t_sleeping_barber_mpi::TestMPITaskParallel::client_logic() {
-  //boost::mpi::request reqs;
   int client_id = world.rank();
   bool accepted = false;
 
-  world.isend(1, 0, client_id);
-  //reqs.wait();
+  {
+    boost::mpi::request req = world.isend(1, 0, client_id);
+    req.wait();
+  }
 
-  world.irecv(1, 1, accepted);
-  //reqs.wait();
+  {
+    boost::mpi::request req = world.irecv(1, 1, accepted);
+    req.wait();
+  }
 
   if (accepted) {
-    world.irecv(0, 2, client_id);
-    //reqs.wait();
+    {
+      boost::mpi::request req = world.irecv(0, 2, client_id);
+      req.wait();
+    }
 
-    world.isend(1, 3, client_id);
-    //reqs.wait();
+    {
+      boost::mpi::request req = world.isend(1, 3, client_id);
+      req.wait();
+    }
   } else {
-    world.isend(1, 3, client_id);
-    //reqs.wait();
+    {
+      boost::mpi::request req = world.isend(1, 3, client_id);
+      req.wait();
+    }
   }
 }
 
 void gordeeva_t_sleeping_barber_mpi::TestMPITaskParallel::serve_next_client(int client_id) {
-  //boost::mpi::request reqs;
-  std::this_thread::sleep_for(20ms);
-  world.isend(client_id, 2, client_id);
-  //reqs.wait();
+  std::this_thread::sleep_for(std::chrono::milliseconds(20));
 
-  world.isend(1, 4, client_id);
-  //reqs.wait();
+  {
+    boost::mpi::request req = world.isend(client_id, 2, client_id);
+    req.wait();
+  }
+
+  {
+    boost::mpi::request req = world.isend(1, 4, client_id);
+    req.wait();
+  }
 }
