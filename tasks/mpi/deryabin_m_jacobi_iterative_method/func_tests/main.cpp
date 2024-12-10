b923624cac841841271c39ad14cba8f4a35c8baf
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-12-10 22:10:25 +0300

    Update main.cpp

--- tasks/mpi/deryabin_m_jacobi_iterative_method/func_tests/main.cpp
+++ tasks/mpi/deryabin_m_jacobi_iterative_method/func_tests/main.cpp
@@ -5,12 +5,18 @@
 
 #include "mpi/deryabin_m_jacobi_iterative_method/include/ops_mpi.hpp"
 
-TEST(deryabin_m_jacobi_iterative_method_mpi, test_lower_triangular_matrix) {
+TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_valid_matrix) {
   boost::mpi::communicator world;
-  std::vector<double> input_matrix_{1, 0, 0, 0,  0, 0, 2, 3,  0,  0,  0,  0, 4,  5,  10, 0,  0,  0,
-                                    6, 7, 8, 22, 0, 0, 9, 10, 11, 12, 43, 0, 13, 14, 15, 16, 17, 76};
-  std::vector<double> input_right_vector_(6, 1);
-  std::vector<double> output_x_vector_ = std::vector<double>(6, 0);
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> distrib(1, 10);
+  std::uniform_real_distribution<> distribut(91, 100);
+  std::vector<double> input_matrix_(100, distrib(gen));
+  std::vector<double> input_right_vector_(10, distrib(gen));
+  for (unsigned short i = 0; i < 10; i++) {
+    input_matrix_[i * 11] = distribut(gen);
+  }
+  std::vector<double> output_x_vector_ = std::vector<double>(10, 0);
   std::vector<std::vector<double>> out_x_vec(1, output_x_vector_);
 
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
@@ -49,17 +55,21 @@ TEST(deryabin_m_jacobi_iterative_method_mpi, test_lower_triangular_matrix) {
   }
 }
 
-TEST(deryabin_m_jacobi_iterative_method_mpi, test_3X_diagonal_matrix) {
+TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_3X_diagonal_matrix) {
   boost::mpi::communicator world;
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> distrib(1, 10);
+  std::uniform_real_distribution<> distribut(91, 100);
   std::vector<double> input_matrix_(100, 0);
-  std::vector<double> input_right_vector_(10, 1);
+  std::vector<double> input_right_vector_(10, distrib(gen));
   for (unsigned short i = 0; i < 10; i++) {
-    input_matrix_[i * 11] = 3;
+    input_matrix_[i * 11] = distribut(gen);
     if (i != 0) {
-      input_matrix_[i * 11 - 1] = 1;
+      input_matrix_[i * 11 - 1] = distrib(gen);
     }
     if (i != 9) {
-      input_matrix_[i * 11 + 1] = 1;
+      input_matrix_[i * 11 + 1] = distrib(gen);
     }
   }
   std::vector<double> output_x_vector_ = std::vector<double>(10, 0);
@@ -101,12 +111,15 @@ TEST(deryabin_m_jacobi_iterative_method_mpi, test_3X_diagonal_matrix) {
   }
 }
 
-TEST(deryabin_m_jacobi_iterative_method_mpi, test_1X_diagonal_matrix) {
+TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_1X_diagonal_matrix) {
   boost::mpi::communicator world;
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> distribut(1, 100);
   std::vector<double> input_matrix_(100, 0);
-  std::vector<double> input_right_vector_(10, 1);
+  std::vector<double> input_right_vector_(10, distribut(gen));
   for (unsigned short i = 0; i < 10; i++) {
-    input_matrix_[i * 11] = 1;
+    input_matrix_[i * 11] = distribut(gen);
   }
   std::vector<double> output_x_vector_ = std::vector<double>(10, 0);
   std::vector<std::vector<double>> out_x_vec(1, output_x_vector_);
@@ -147,12 +160,15 @@ TEST(deryabin_m_jacobi_iterative_method_mpi, test_1X_diagonal_matrix) {
   }
 }
 
-TEST(deryabin_m_jacobi_iterative_method_mpi, test_diagonal_elements_are_much_larger_than_non_diagonal) {
+TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_diagonal_elements_are_much_larger_than_non_diagonal) {
   boost::mpi::communicator world;
-  std::vector<double> input_matrix_(100, 1);
-  std::vector<double> input_right_vector_(10, 1);
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> distrib(1, 10);
+  std::vector<double> input_matrix_(100, distrib(gen));
+  std::vector<double> input_right_vector_(10, distrib(gen));
   for (unsigned short i = 0; i < 10; i++) {
-    input_matrix_[i * 11] = 100;
+    input_matrix_[i * 11] = 100 * distrib(gen);
   }
   std::vector<double> output_x_vector_ = std::vector<double>(10, 0);
   std::vector<std::vector<double>> out_x_vec(1, output_x_vector_);
@@ -193,10 +209,13 @@ TEST(deryabin_m_jacobi_iterative_method_mpi, test_diagonal_elements_are_much_lar
   }
 }
 
-TEST(deryabin_m_jacobi_iterative_method_mpi, test_invalid_matrix_zeros_on_diagonal) {
+TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_invalid_matrix_zeros_on_diagonal) {
   boost::mpi::communicator world;
-  std::vector<double> input_matrix_(100, 1);
-  std::vector<double> input_right_vector_(10, 1);
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> distrib(1, 10);
+  std::vector<double> input_matrix_(100, distrib(gen));
+  std::vector<double> input_right_vector_(10, distrib(gen));
   for (unsigned short i = 0; i < 10; i++) {
     input_matrix_[i * 11] = 0;
   }
