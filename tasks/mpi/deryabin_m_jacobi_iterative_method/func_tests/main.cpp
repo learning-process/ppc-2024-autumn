308c73255e2c2893830eb5b70fade762ab3a9a3e
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-12-10 15:43:44 +0300

    Update main.cpp

--- tasks/mpi/deryabin_m_jacobi_iterative_method/func_tests/main.cpp
+++ tasks/mpi/deryabin_m_jacobi_iterative_method/func_tests/main.cpp
@@ -8,7 +8,8 @@
 TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_valid_matrix) {
   boost::mpi::communicator world;
   std::random_device rd;
-  std::mt19937 gen(rd());
+  std::default_random_engine gen(rd());
+  //std::mt19937 gen(rd());
   std::uniform_real_distribution<> distrib(1, 10);
   std::uniform_real_distribution<> distribut(91, 100);
   std::vector<double> input_matrix_(100, distrib(gen));
@@ -58,7 +59,8 @@ TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_valid_matrix) {
 TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_3X_diagonal_matrix) {
   boost::mpi::communicator world;
   std::random_device rd;
-  std::mt19937 gen(rd());
+  std::default_random_engine gen(rd());
+  //std::mt19937 gen(rd());
   std::uniform_real_distribution<> distrib(1, 10);
   std::uniform_real_distribution<> distribut(91, 100);
   std::vector<double> input_matrix_(100, 0);
@@ -114,7 +116,8 @@ TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_3X_diagonal_matrix) {
 TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_1X_diagonal_matrix) {
   boost::mpi::communicator world;
   std::random_device rd;
-  std::mt19937 gen(rd());
+  std::default_random_engine gen(rd());
+  //std::mt19937 gen(rd());
   std::uniform_real_distribution<> distribut(1, 100);
   std::vector<double> input_matrix_(100, 0);
   std::vector<double> input_right_vector_(10, distribut(gen));
@@ -163,7 +166,8 @@ TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_1X_diagonal_matrix) {
 TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_diagonal_elements_are_much_larger_than_non_diagonal) {
   boost::mpi::communicator world;
   std::random_device rd;
-  std::mt19937 gen(rd());
+  std::default_random_engine gen(rd());
+  //std::mt19937 gen(rd());
   std::uniform_real_distribution<> distrib(1, 10);
   std::vector<double> input_matrix_(100, distrib(gen));
   std::vector<double> input_right_vector_(10, distrib(gen));
@@ -212,7 +216,8 @@ TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_diagonal_elements_are_m
 TEST(deryabin_m_jacobi_iterative_method_mpi, random_invalid_matrix_zeros_on_diagonal) {
   boost::mpi::communicator world;
   std::random_device rd;
-  std::mt19937 gen(rd());
+  std::default_random_engine gen(rd());
+  //std::mt19937 gen(rd());
   std::uniform_real_distribution<> distrib(1, 10);
   std::vector<double> input_matrix_(100, distrib(gen));
   std::vector<double> input_right_vector_(10, distrib(gen));
