39941810b2d7c801d6fd482f01a2d0c4fc34b463
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2024-12-11 14:55:18 +0300

    Update main.cpp

--- tasks/mpi/vavilov_v_bellman_ford/func_tests/main.cpp
+++ tasks/mpi/vavilov_v_bellman_ford/func_tests/main.cpp
@@ -60,78 +60,78 @@ TEST(vavilov_v_bellman_ford_mpi, ValidInputWithMultiplePaths_2) {
 }
 
 TEST(vavilov_v_bellman_ford_seq, DisconnectedGraph) {
-    mpi::communicator world;
+  mpi::communicator world;
 
-    if (world.rank() == 0) {
-      auto taskDataPar = std::make_shared<ppc::core::TaskData>();
-      std::vector<int> edges = {0, 1, 4, 0, 2, 1, 1, 3, 2};
-      std::vector<int> output(5);
-      int vertices = 5, edges_count = 3, source = 0;
+  if (world.rank() == 0) {
+    auto taskDataPar = std::make_shared<ppc::core::TaskData>();
+    std::vector<int> edges = {0, 1, 4, 0, 2, 1, 1, 3, 2};
+    std::vector<int> output(5);
+    int vertices = 5, edges_count = 3, source = 0;
 
-      taskDataPar->inputs_count.emplace_back(vertices);
-      taskDataPar->inputs_count.emplace_back(edges_count);
-      taskDataPar->inputs_count.emplace_back(source);
-      taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(edges.data()));
-      taskDataPar->outputs_count.emplace_back(output.size());
-      taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
+    taskDataPar->inputs_count.emplace_back(vertices);
+    taskDataPar->inputs_count.emplace_back(edges_count);
+    taskDataPar->inputs_count.emplace_back(source);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(edges.data()));
+    taskDataPar->outputs_count.emplace_back(output.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
 
-      vavilov_v_bellman_ford_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
-      ASSERT_TRUE(testTaskParallel.validation());
-      ASSERT_TRUE(testTaskParallel.pre_processing());
-      ASSERT_TRUE(testTaskParallel.run());
-      ASSERT_TRUE(testTaskParallel.post_processing());
+    vavilov_v_bellman_ford_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+    ASSERT_TRUE(testTaskParallel.validation());
+    ASSERT_TRUE(testTaskParallel.pre_processing());
+    ASSERT_TRUE(testTaskParallel.run());
+    ASSERT_TRUE(testTaskParallel.post_processing());
 
-      std::vector<int> expected_output = {0, 4, 1, 6, INT_MAX};
-      EXPECT_EQ(output, expected_output);
-    }
+    std::vector<int> expected_output = {0, 4, 1, 6, INT_MAX};
+    EXPECT_EQ(output, expected_output);
+  }
 }
 
 TEST(vavilov_v_bellman_ford_seq, NegativeCycle) {
-    mpi::communicator world;
+  mpi::communicator world;
 
-    if (world.rank() == 0) {
-      auto taskDataPar = std::make_shared<ppc::core::TaskData>();
-      std::vector<int> edges = {0, 1, 1, 1, 2, -1, 2, 0, -1};
-      std::vector<int> output(3);
-      int vertices = 3, edges_count = 3, source = 0;
+  if (world.rank() == 0) {
+    auto taskDataPar = std::make_shared<ppc::core::TaskData>();
+    std::vector<int> edges = {0, 1, 1, 1, 2, -1, 2, 0, -1};
+    std::vector<int> output(3);
+    int vertices = 3, edges_count = 3, source = 0;
 
-      taskDataPar->inputs_count.emplace_back(vertices);
-      taskDataPar->inputs_count.emplace_back(edges_count);
-      taskDataPar->inputs_count.emplace_back(source);
-      taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(edges.data()));
-      taskDataPar->outputs_count.emplace_back(output.size());
-      taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
+    taskDataPar->inputs_count.emplace_back(vertices);
+    taskDataPar->inputs_count.emplace_back(edges_count);
+    taskDataPar->inputs_count.emplace_back(source);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(edges.data()));
+    taskDataPar->outputs_count.emplace_back(output.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
 
-      vavilov_v_bellman_ford_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
-      ASSERT_TRUE(testTaskParallel.validation());
-      ASSERT_TRUE(testTaskParallel.pre_processing());
-      ASSERT_FALSE(testTaskParallel.run());
-    }
+    vavilov_v_bellman_ford_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+    ASSERT_TRUE(testTaskParallel.validation());
+    ASSERT_TRUE(testTaskParallel.pre_processing());
+    ASSERT_FALSE(testTaskParallel.run());
+  }
 }
 
 TEST(vavilov_v_bellman_ford_seq, SingleVertexGraph) {
-    mpi::communicator world;
+  mpi::communicator world;
 
-    if (world.rank() == 0) {
-      auto taskDataPar = std::make_shared<ppc::core::TaskData>();
-      std::vector<int> edges = {};
-      std::vector<int> output(1, 0);
-      int vertices = 1, edges_count = 0, source = 0;
+  if (world.rank() == 0) {
+    auto taskDataPar = std::make_shared<ppc::core::TaskData>();
+    std::vector<int> edges = {};
+    std::vector<int> output(1, 0);
+    int vertices = 1, edges_count = 0, source = 0;
 
-      taskDataPar->inputs_count.emplace_back(vertices);
-      taskDataPar->inputs_count.emplace_back(edges_count);
-      taskDataPar->inputs_count.emplace_back(source);
-      taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(edges.data()));
-      taskDataPar->outputs_count.emplace_back(output.size());
-      taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
+    taskDataPar->inputs_count.emplace_back(vertices);
+    taskDataPar->inputs_count.emplace_back(edges_count);
+    taskDataPar->inputs_count.emplace_back(source);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(edges.data()));
+    taskDataPar->outputs_count.emplace_back(output.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
 
-      vavilov_v_bellman_ford_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
-      ASSERT_TRUE(testTaskSequential.validation());
-      ASSERT_TRUE(testTaskSequential.pre_processing());
-      ASSERT_TRUE(testTaskSequential.run());
-      ASSERT_TRUE(testTaskSequential.post_processing());
+    vavilov_v_bellman_ford_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+    ASSERT_TRUE(testTaskSequential.validation());
+    ASSERT_TRUE(testTaskSequential.pre_processing());
+    ASSERT_TRUE(testTaskSequential.run());
+    ASSERT_TRUE(testTaskSequential.post_processing());
 
-      std::vector<int> expected_output = {0};
-      EXPECT_EQ(output, expected_output);
-    }
+    std::vector<int> expected_output = {0};
+    EXPECT_EQ(output, expected_output);
+  }
 }
