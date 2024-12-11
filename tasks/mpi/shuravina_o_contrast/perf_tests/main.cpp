0c4bb0aca0b1599e448f8ed624b805099f07d9c9
shyravina19102004 <shyravina19102004@gmail.com>
2024-12-12 00:50:22 +0300

    c17

--- tasks/mpi/shuravina_o_contrast/perf_tests/main.cpp
+++ tasks/mpi/shuravina_o_contrast/perf_tests/main.cpp
@@ -7,7 +7,6 @@
 #include "core/perf/include/perf.hpp"
 #include "mpi/shuravina_o_contrast/include/ops_mpi.hpp"
 
-namespace shuravina_o_contrast {
 std::vector<uint8_t> genRandomData(uint32_t size) {
   std::vector<uint8_t> buff(size);
   std::random_device dev;
@@ -17,72 +16,101 @@ std::vector<uint8_t> genRandomData(uint32_t size) {
   }
   return buff;
 }
-}  // namespace shuravina_o_contrast
 
-TEST(shuravina_o_contrast_perf, test_pipeline_run_small_input) {
+TEST(shuravina_o_contrast, Test_Contrast_Enhancement_Functional) {
   boost::mpi::communicator world;
-  std::vector<uint8_t> input;
-  std::vector<uint8_t> res;
-
-  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
 
   if (world.rank() == 0) {
-    int size = 1000;
-    input = shuravina_o_contrast::genRandomData(size);
-    res.resize(input.size());
+    {
+      auto taskDataPar = std::make_shared<ppc::core::TaskData>();
 
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
-    taskDataPar->inputs_count.emplace_back(input.size());
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(res.data()));
-    taskDataPar->outputs_count.emplace_back(res.size());
-  }
+      std::vector<uint8_t> input = {};
+      taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(input.data()));
+      taskDataPar->inputs_count.emplace_back(input.size());
 
-  auto contrastTaskParallel = std::make_shared<shuravina_o_contrast::ContrastTaskParallel>(taskDataPar);
+      std::vector<uint8_t> output(input.size());
+      taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
+      taskDataPar->outputs_count.emplace_back(output.size());
 
-  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
-  perfAttr->num_running = 10;
-  const boost::mpi::timer current_timer;
-  perfAttr->current_timer = [&] { return current_timer.elapsed(); };
+      shuravina_o_contrast::ContrastTaskParallel contrastTaskParallel(taskDataPar);
+      ASSERT_TRUE(contrastTaskParallel.validation());
+    }
 
-  auto perfResults = std::make_shared<ppc::core::PerfResults>();
-  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(contrastTaskParallel);
-  perfAnalyzer->pipeline_run(perfAttr, perfResults);
+    {
+      auto taskDataPar = std::make_shared<ppc::core::TaskData>();
 
-  if (world.rank() == 0) {
-    ppc::core::Perf::print_perf_statistic(perfResults);
+      std::vector<uint8_t> input = {50};
+      taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(input.data()));
+      taskDataPar->inputs_count.emplace_back(input.size());
+
+      std::vector<uint8_t> output(input.size());
+      taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
+      taskDataPar->outputs_count.emplace_back(output.size());
+
+      shuravina_o_contrast::ContrastTaskParallel contrastTaskParallel(taskDataPar);
+      ASSERT_TRUE(contrastTaskParallel.validation());
+    }
+
+    {
+      auto taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+      std::vector<uint8_t> input = {255, 255, 255, 255, 255};
+      taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(input.data()));
+      taskDataPar->inputs_count.emplace_back(input.size());
+
+      std::vector<uint8_t> output(input.size());
+      taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
+      taskDataPar->outputs_count.emplace_back(output.size());
+
+      shuravina_o_contrast::ContrastTaskParallel contrastTaskParallel(taskDataPar);
+      ASSERT_TRUE(contrastTaskParallel.validation());
+    }
+
+    {
+      auto taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+      std::vector<uint8_t> input = {0, 0, 0, 0, 0};
+      taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(input.data()));
+      taskDataPar->inputs_count.emplace_back(input.size());
+
+      std::vector<uint8_t> output(input.size());
+      taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
+      taskDataPar->outputs_count.emplace_back(output.size());
+
+      shuravina_o_contrast::ContrastTaskParallel contrastTaskParallel(taskDataPar);
+      ASSERT_TRUE(contrastTaskParallel.validation());
+    }
   }
 }
 
-TEST(shuravina_o_contrast_perf, test_pipeline_run_large_input) {
+TEST(shuravina_o_contrast_perf, Test_Contrast_Enhancement_Performance) {
   boost::mpi::communicator world;
-  std::vector<uint8_t> input;
-  std::vector<uint8_t> res;
-
-  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
 
   if (world.rank() == 0) {
-    int size = 1000000;
-    input = shuravina_o_contrast::genRandomData(size);
-    res.resize(input.size());
+    auto taskDataPar = std::make_shared<ppc::core::TaskData>();
 
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(input.data()));
+    int size = 800000;
+    std::vector<uint8_t> input = genRandomData(size);
+    std::vector<uint8_t> output(input.size());
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(input.data()));
     taskDataPar->inputs_count.emplace_back(input.size());
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(res.data()));
-    taskDataPar->outputs_count.emplace_back(res.size());
-  }
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(output.data()));
+    taskDataPar->outputs_count.emplace_back(output.size());
 
-  auto contrastTaskParallel = std::make_shared<shuravina_o_contrast::ContrastTaskParallel>(taskDataPar);
+    auto contrastTaskParallel = std::make_shared<shuravina_o_contrast::ContrastTaskParallel>(taskDataPar);
 
-  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
-  perfAttr->num_running = 10;
-  const boost::mpi::timer current_timer;
-  perfAttr->current_timer = [&] { return current_timer.elapsed(); };
+    auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+    perfAttr->num_running = 10;
+    const boost::mpi::timer current_timer;
+    perfAttr->current_timer = [&] { return current_timer.elapsed(); };
 
-  auto perfResults = std::make_shared<ppc::core::PerfResults>();
-  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(contrastTaskParallel);
-  perfAnalyzer->pipeline_run(perfAttr, perfResults);
+    auto perfResults = std::make_shared<ppc::core::PerfResults>();
+    auto perfAnalyzer = std::make_shared<ppc::core::Perf>(contrastTaskParallel);
+    perfAnalyzer->pipeline_run(perfAttr, perfResults);
 
-  if (world.rank() == 0) {
-    ppc::core::Perf::print_perf_statistic(perfResults);
+    if (world.rank() == 0) {
+      ppc::core::Perf::print_perf_statistic(perfResults);
+    }
   }
 }
