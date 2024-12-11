522bd7eb1349892a84d57b2873aa738a6610bf80
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2024-12-11 14:47:07 +0300

    Update ops_mpi.hpp

--- tasks/mpi/vavilov_v_bellman_ford/include/ops_mpi.hpp
+++ tasks/mpi/vavilov_v_bellman_ford/include/ops_mpi.hpp
@@ -12,44 +12,44 @@
 
 #include "core/task/include/task.hpp"
 
-namespace vavilov_v_bellman_ford_seq {
+namespace vavilov_v_bellman_ford_mpi {
 
-    class TestMPITaskSequential : public ppc::core::Task {
-    public:
-        explicit TestMPITaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+class TestMPITaskSequential : public ppc::core::Task {
+  public:
+    explicit TestMPITaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
 
-        bool pre_processing() override;
-        bool validation() override;
-        bool run() override;
-        bool post_processing() override;
+    bool pre_processing() override;
+    bool validation() override;
+    bool run() override;
+    bool post_processing() override;
 
-    private:
-        struct Edge {
-            int src, dest, weight;
-        };
-
-        std::vector<Edge> edges_;
-        std::vector<int> distances_;
-        int vertices_{0}, edges_count_{0}, source_{0};
+  private:
+    struct Edge {
+      int src, dest, weight;
     };
 
-    class TestMPITaskParallel : public ppc::core::Task {
-    public:
-        explicit TestMPITaskParallel(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+    std::vector<Edge> edges_;
+    std::vector<int> distances_;
+    int vertices_{0}, edges_count_{0}, source_{0};
+};
 
-        bool pre_processing() override;
-        bool validation() override;
-        bool run() override;
-        bool post_processing() override;
+class TestMPITaskParallel : public ppc::core::Task {
+  public:
+    explicit TestMPITaskParallel(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
 
-    private:
-        struct Edge {
-            int src, dest, weight;
-        };
+    bool pre_processing() override;
+    bool validation() override;
+    bool run() override;
+    bool post_processing() override;
 
-        std::vector<Edge> edges_;
-        std::vector<int> distances_;
-        int vertices_{0}, edges_count_{0}, source_{0};
-        boost::mpi::communicator world;
+  private:
+    struct Edge {
+      int src, dest, weight;
     };
-}  // namespace vavilov_v_bellman_ford_seq
+
+    std::vector<Edge> edges_;
+    std::vector<int> distances_;
+    int vertices_{0}, edges_count_{0}, source_{0};
+    boost::mpi::communicator world;
+    };
+}  // namespace vavilov_v_bellman_ford_mpi
