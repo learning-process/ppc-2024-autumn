79c60cb32b47260b4cdf31bcab09039764a988c6
ElizavetaFrolova <liza1827frol@gmail.com>
2024-12-11 20:45:29 +0300

    commitreview

--- tasks/mpi/frolova_e_matrix_multiplication/func_tests/main_frolova_matrix.cpp
+++ tasks/mpi/frolova_e_matrix_multiplication/func_tests/main_frolova_matrix.cpp
@@ -76,6 +76,144 @@ TEST(frolova_e_matrix_multiplication_mpi, multiplication_of_square_matrices) {
   }
 }
 
+TEST(frolova_e_matrix_multiplication_mpi, values_are_prime_numbers) {
+  boost::mpi::communicator world;
+  std::vector<int> values_1 = {61, 61};
+  std::vector<int> values_2 = {61, 61};
+  std::vector<int> matrixA_;
+  std::vector<int> matrixB_;
+  std::vector<int32_t> res(3721);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    frolova_e_matrix_multiplication_mpi::randomNumVec(3721, matrixA_);
+    frolova_e_matrix_multiplication_mpi::randomNumVec(3721, matrixB_);
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(values_1.data()));
+    taskDataPar->inputs_count.emplace_back(values_1.size());
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(values_2.data()));
+    taskDataPar->inputs_count.emplace_back(values_2.size());
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixA_.data()));
+    taskDataPar->inputs_count.emplace_back(matrixA_.size());
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixB_.data()));
+    taskDataPar->inputs_count.emplace_back(matrixB_.size());
+
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(res.data()));
+    taskDataPar->outputs_count.emplace_back(res.size());
+  }
+
+  frolova_e_matrix_multiplication_mpi::matrixMultiplicationParallel testMpiTaskParallel(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+    std::vector<int32_t> reference_matrix(3721);
+
+    // Create TaskData
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(values_1.data()));
+    taskDataSeq->inputs_count.emplace_back(values_1.size());
+
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(values_2.data()));
+    taskDataSeq->inputs_count.emplace_back(values_2.size());
+
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixA_.data()));
+    taskDataSeq->inputs_count.emplace_back(matrixA_.size());
+
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixB_.data()));
+    taskDataSeq->inputs_count.emplace_back(matrixB_.size());
+
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_matrix.data()));
+    taskDataSeq->outputs_count.emplace_back(reference_matrix.size());
+
+    // Create Task
+    frolova_e_matrix_multiplication_mpi::matrixMultiplicationSequential testMpiTaskSequential(taskDataSeq);
+    ASSERT_EQ(testMpiTaskSequential.validation(), true);
+    testMpiTaskSequential.pre_processing();
+    testMpiTaskSequential.run();
+    testMpiTaskSequential.post_processing();
+
+    ASSERT_EQ(reference_matrix, res);
+  }
+}
+
+TEST(frolova_e_matrix_multiplication_mpi, matrix_with_single_element) {
+  boost::mpi::communicator world;
+  std::vector<int> values_1 = {1, 1};
+  std::vector<int> values_2 = {1, 1};
+  std::vector<int> matrixA_;
+  std::vector<int> matrixB_;
+  std::vector<int32_t> res(1);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    frolova_e_matrix_multiplication_mpi::randomNumVec(1, matrixA_);
+    frolova_e_matrix_multiplication_mpi::randomNumVec(1, matrixB_);
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(values_1.data()));
+    taskDataPar->inputs_count.emplace_back(values_1.size());
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(values_2.data()));
+    taskDataPar->inputs_count.emplace_back(values_2.size());
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixA_.data()));
+    taskDataPar->inputs_count.emplace_back(matrixA_.size());
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixB_.data()));
+    taskDataPar->inputs_count.emplace_back(matrixB_.size());
+
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(res.data()));
+    taskDataPar->outputs_count.emplace_back(res.size());
+  }
+
+  frolova_e_matrix_multiplication_mpi::matrixMultiplicationParallel testMpiTaskParallel(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+    std::vector<int32_t> reference_matrix(1);
+
+    // Create TaskData
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(values_1.data()));
+    taskDataSeq->inputs_count.emplace_back(values_1.size());
+
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(values_2.data()));
+    taskDataSeq->inputs_count.emplace_back(values_2.size());
+
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixA_.data()));
+    taskDataSeq->inputs_count.emplace_back(matrixA_.size());
+
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixB_.data()));
+    taskDataSeq->inputs_count.emplace_back(matrixB_.size());
+
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_matrix.data()));
+    taskDataSeq->outputs_count.emplace_back(reference_matrix.size());
+
+    // Create Task
+    frolova_e_matrix_multiplication_mpi::matrixMultiplicationSequential testMpiTaskSequential(taskDataSeq);
+    ASSERT_EQ(testMpiTaskSequential.validation(), true);
+    testMpiTaskSequential.pre_processing();
+    testMpiTaskSequential.run();
+    testMpiTaskSequential.post_processing();
+
+    ASSERT_EQ(reference_matrix, res);
+  }
+}
+
 TEST(frolova_e_matrix_multiplication_mpi, multiplication_of_large_matrices) {
   // creare data
 
@@ -120,6 +258,81 @@ TEST(frolova_e_matrix_multiplication_mpi, multiplication_of_large_matrices) {
   if (world.rank() == 0) {
     // Create data
     std::vector<int32_t> reference_matrix(400);
+
+    // Create TaskData
+
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(values_1.data()));
+    taskDataSeq->inputs_count.emplace_back(values_1.size());
+
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(values_2.data()));
+    taskDataSeq->inputs_count.emplace_back(values_2.size());
+
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixA_.data()));
+    taskDataSeq->inputs_count.emplace_back(matrixA_.size());
+
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixB_.data()));
+    taskDataSeq->inputs_count.emplace_back(matrixB_.size());
+
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_matrix.data()));
+    taskDataSeq->outputs_count.emplace_back(reference_matrix.size());
+
+    // Create Task
+    frolova_e_matrix_multiplication_mpi::matrixMultiplicationSequential testMpiTaskSequential(taskDataSeq);
+    ASSERT_EQ(testMpiTaskSequential.validation(), true);
+    testMpiTaskSequential.pre_processing();
+    testMpiTaskSequential.run();
+    testMpiTaskSequential.post_processing();
+
+    ASSERT_EQ(reference_matrix, res);
+  }
+}
+
+TEST(frolova_e_matrix_multiplication_mpi, matrix_of_size_of_power_of_two) {
+  // creare data
+
+  boost::mpi::communicator world;
+  std::vector<int> values_1 = {64, 64};
+  std::vector<int> values_2 = {64, 64};
+  std::vector<int> matrixA_;
+
+  std::vector<int> matrixB_;
+
+  std::vector<int32_t> res(4096);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    frolova_e_matrix_multiplication_mpi::randomNumVec(4096, matrixA_);
+    frolova_e_matrix_multiplication_mpi::randomNumVec(4096, matrixB_);
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(values_1.data()));
+    taskDataPar->inputs_count.emplace_back(values_1.size());
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(values_2.data()));
+    taskDataPar->inputs_count.emplace_back(values_2.size());
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixA_.data()));
+    taskDataPar->inputs_count.emplace_back(matrixA_.size());
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(matrixB_.data()));
+    taskDataPar->inputs_count.emplace_back(matrixB_.size());
+
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(res.data()));
+    taskDataPar->outputs_count.emplace_back(res.size());
+  }
+
+  frolova_e_matrix_multiplication_mpi::matrixMultiplicationParallel testMpiTaskParallel(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+    // Create data
+    std::vector<int32_t> reference_matrix(4096);
 
     // Create TaskData
 
