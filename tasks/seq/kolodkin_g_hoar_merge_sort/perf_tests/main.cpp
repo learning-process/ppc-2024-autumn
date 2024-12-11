71d6cb5dcd0c68aac7e7f7bede2f7fcdd30fba88
KolodkinGrigorii <113025092+KolodkinGrigorii@users.noreply.github.com>
2024-12-11 11:38:12 +0300

    God, help me, please!

--- tasks/seq/kolodkin_g_hoar_merge_sort/perf_tests/main.cpp
+++ tasks/seq/kolodkin_g_hoar_merge_sort/perf_tests/main.cpp
@@ -1,84 +1,90 @@
 // Copyright 2023 Nesterov Alexander
 #include <gtest/gtest.h>
 
-#include <boost/mpi/timer.hpp>
+#include <algorithm>
 #include <vector>
 
 #include "core/perf/include/perf.hpp"
-#include "mpi/kolodkin_g_hoar_merge_sort/include/ops_mpi.hpp"
+#include "seq/kolodkin_g_hoar_merge_sort/include/ops_seq.hpp"
 
-TEST(mpi_kolodkin_g_image_contrast_test, test_pipeline_run) {
-  boost::mpi::communicator world;
-  std::vector<int> global_vec;
-  std::vector<int32_t> global_out(999, 0);
-  // Create TaskData
-  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
-  if (world.rank() == 0) {
-    for (unsigned i = 0; i < 999; i++) {
-      global_vec.push_back(-10000 + rand() % 10000);
-    }
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(global_vec.data()));
-    taskDataPar->inputs_count.emplace_back(global_vec.size());
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(new std::vector<int>(global_out)));
+TEST(seq_kolodkin_g_hoar_merge_sort_test, test_pipeline_run) {
+  // Create data
+  std::vector<int> vec;
+  std::vector<int> out(10000);
+  for (unsigned i = 0; i < 10000; i++) {
+    vec.push_back(-1000 + rand() % 5000);
   }
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(vec.data()));
+  taskDataSeq->inputs_count.emplace_back(vec.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
 
-  auto testMpiTaskParallel = std::make_shared<kolodkin_g_hoar_merge_sort_mpi::TestMPITaskParallel>(taskDataPar);
-  ASSERT_EQ(testMpiTaskParallel->validation(), true);
-  testMpiTaskParallel->pre_processing();
-  testMpiTaskParallel->run();
-  testMpiTaskParallel->post_processing();
+  // Create Task
+  auto testTaskSequential = std::make_shared<kolodkin_g_hoar_merge_sort_seq::TestTaskSequential>(taskDataSeq);
 
   // Create Perf attributes
   auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
   perfAttr->num_running = 10;
-  const boost::mpi::timer current_timer;
-  perfAttr->current_timer = [&] { return current_timer.elapsed(); };
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perfAttr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
 
   // Create and init perf results
   auto perfResults = std::make_shared<ppc::core::PerfResults>();
 
   // Create Perf analyzer
-  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testMpiTaskParallel);
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskSequential);
   perfAnalyzer->pipeline_run(perfAttr, perfResults);
-  if (world.rank() == 0) {
-    ppc::core::Perf::print_perf_statistic(perfResults);
+  ppc::core::Perf::print_perf_statistic(perfResults);
+  std::sort(vec.begin(), vec.end());
+  out = *reinterpret_cast<std::vector<int> *>(taskDataSeq->outputs[0]);
+  for (unsigned i = 0; i < vec.size(); i++) {
+    ASSERT_EQ(vec[i], out[i]);
   }
 }
 
-TEST(mpi_kolodkin_g_image_contrast_test, test_task_run) {
-  boost::mpi::communicator world;
-  std::vector<int> global_vec;
-  std::vector<int32_t> global_out(9999, 0);
-  // Create TaskData
-  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
-  if (world.rank() == 0) {
-    for (unsigned long int i = 0; i < 9999; i++) {
-      global_vec.push_back(-100000 + rand() % 100000);
-    }
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(global_vec.data()));
-    taskDataPar->inputs_count.emplace_back(global_vec.size());
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(new std::vector<int>(global_out)));
+TEST(seq_kolodkin_g_image_contrast_test, test_task_run) {
+  // Create data
+  std::vector<int> vec;
+  std::vector<int> out(10000);
+  for (unsigned i = 0; i < 10000; i++) {
+    vec.push_back(-1000 + rand() % 5000);
   }
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(vec.data()));
+  taskDataSeq->inputs_count.emplace_back(vec.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
 
-  auto testMpiTaskParallel = std::make_shared<kolodkin_g_hoar_merge_sort_mpi::TestMPITaskParallel>(taskDataPar);
-  ASSERT_EQ(testMpiTaskParallel->validation(), true);
-  testMpiTaskParallel->pre_processing();
-  testMpiTaskParallel->run();
-  testMpiTaskParallel->post_processing();
+  // Create Task
+  auto testTaskSequential = std::make_shared<kolodkin_g_hoar_merge_sort_seq::TestTaskSequential>(taskDataSeq);
 
   // Create Perf attributes
   auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
   perfAttr->num_running = 10;
-  const boost::mpi::timer current_timer;
-  perfAttr->current_timer = [&] { return current_timer.elapsed(); };
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perfAttr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
 
   // Create and init perf results
   auto perfResults = std::make_shared<ppc::core::PerfResults>();
 
   // Create Perf analyzer
-  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testMpiTaskParallel);
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskSequential);
   perfAnalyzer->task_run(perfAttr, perfResults);
-  if (world.rank() == 0) {
-    ppc::core::Perf::print_perf_statistic(perfResults);
+  ppc::core::Perf::print_perf_statistic(perfResults);
+  std::sort(vec.begin(), vec.end());
+  out = *reinterpret_cast<std::vector<int> *>(taskDataSeq->outputs[0]);
+  for (unsigned i = 0; i < vec.size(); i++) {
+    ASSERT_EQ(vec[i], out[i]);
   }
 }
