62e3fe8af3703ca4256258ff8cdb6f6b2ef143bc
Vav1L0N <121250360+Vav1L0N@users.noreply.github.com>
2024-12-13 00:17:33 +0300

    Update ops_mpi.cpp

--- tasks/mpi/vavilov_v_bellman_ford/src/ops_mpi.cpp
+++ tasks/mpi/vavilov_v_bellman_ford/src/ops_mpi.cpp
@@ -85,50 +85,40 @@ bool vavilov_v_bellman_ford_mpi::TestMPITaskParallel::validation() {
 bool vavilov_v_bellman_ford_mpi::TestMPITaskParallel::run() {
   internal_order_test();
 
-  bool changed = true;
-
   int local_start = world.rank() * (edges_count_ / world.size()) + std::min(world.rank(), edges_count_ % world.size());
   int local_end = local_start + (edges_count_ / world.size()) + (world.rank() < edges_count_ % world.size() ? 1 : 0);
 
-  for (int i = 1; i < vertices_; ++i) {
-    if (!changed && i != vertices_) {
-      break;
-    }
-
-    changed = false;
+  for (int i = 0; i < vertices_ - 1; ++i) {
     std::vector<int> local_distances = distances_;
+    bool local_changed = false;
 
     for (int j = local_start; j < local_end; ++j) {
       const auto& edge = edges_[j];
       if (distances_[edge.src] != INT_MAX && distances_[edge.src] + edge.weight < local_distances[edge.dest]) {
         local_distances[edge.dest] = distances_[edge.src] + edge.weight;
-        changed = true;
+        local_changed = true;
       }
     }
 
     boost::mpi::all_reduce(world, local_distances.data(), vertices_, distances_.data(), [](int a, int b) {
-      if (a == INT_MAX) return b;
-      if (b == INT_MAX) return a;
       return std::min(a, b);
     });
-    boost::mpi::all_reduce(world, changed, std::logical_or<bool>());
 
-    if (i == vertices_) {  // Проверка на отрицательные циклы.
-      bool has_negative_cycle = false;
-      for (int j = local_start; j < local_end; ++j) {
-        const auto& edge = edges_[j];
-        if (distances_[edge.src] != INT_MAX && distances_[edge.src] + edge.weight < distances_[edge.dest]) {
-          has_negative_cycle = true;
-          break;
-        }
-      }
-      has_negative_cycle = boost::mpi::all_reduce(world, has_negative_cycle, std::logical_or<bool>());
-      if (has_negative_cycle) {
-        return !has_negative_cycle;
-      }
+    bool global_changed = boost::mpi::all_reduce(world, local_changed, std::logical_or<bool>());
+    if (!global_changed) break;
+  }
+
+  bool has_negative_cycle = false;
+  for (int j = local_start; j < local_end; ++j) {
+    const auto& edge = edges_[j];
+    if (distances_[edge.src] != INT_MAX && distances_[edge.src] + edge.weight < distances_[edge.dest]) {
+      has_negative_cycle = true;
+      break;
     }
   }
-  return true;
+
+  has_negative_cycle = boost::mpi::all_reduce(world, has_negative_cycle, std::logical_or<bool>());
+  return !has_negative_cycle;
 }
 
 bool vavilov_v_bellman_ford_mpi::TestMPITaskParallel::post_processing() {
