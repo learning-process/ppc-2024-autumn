0bf1e5117efd7fd2fb863c2532828f4b029f1fb7
1alron <roma.niko393@gmail.com>
2024-12-12 15:44:16 +0300

    seq first version

---
+++ tasks/seq/nikolaev_r_strassen_matrix_multiplication_method/perf_tests/main.cpp
@@ -0,0 +1,106 @@
+#include <gtest/gtest.h>
+
+#include <random>
+
+#include "core/perf/include/perf.hpp"
+#include "seq/nikolaev_r_strassen_matrix_multiplication_method/include/ops_seq.hpp"
+
+std::vector<double> generate_random_square_matrix(int n, double minValue, double maxValue) {
+  std::vector<double> matrix(n * n);
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<double> dis(minValue, maxValue);
+
+  for (int i = 0; i < n * n; ++i) {
+    matrix[i] = dis(gen);
+  }
+  return matrix;
+}
+
+TEST(nikolaev_r_strassen_matrix_multiplication_method_seq, test_pipeline_run) {
+  const size_t N = 50;
+
+  std::vector<double> A = generate_random_square_matrix(N, -15.0, 15.0);
+  std::vector<double> B = generate_random_square_matrix(N, -15.0, 15.0);
+
+  auto taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(A.data()));
+  taskDataSeq->inputs_count.emplace_back(A.size());
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(B.data()));
+  taskDataSeq->inputs_count.emplace_back(B.size());
+
+  std::vector<double> out(N * N, 0.0);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  auto testTaskSequential =
+      std::make_shared<nikolaev_r_strassen_matrix_multiplication_method_seq::StrassenMatrixMultiplicationSequential>(
+          taskDataSeq);
+  ASSERT_TRUE(testTaskSequential->validation());
+  ASSERT_TRUE(testTaskSequential->pre_processing());
+  ASSERT_TRUE(testTaskSequential->run());
+  ASSERT_TRUE(testTaskSequential->post_processing());
+
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perfAttr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskSequential);
+
+  perfAnalyzer->pipeline_run(perfAttr, perfResults);
+  ppc::core::Perf::print_perf_statistic(perfResults);
+
+  ASSERT_EQ(A.size(), out.size());
+}
+
+TEST(nikolaev_r_strassen_matrix_multiplication_method_seq, test_task_run) {
+  const size_t N = 50;
+
+  std::vector<double> A = generate_random_square_matrix(N, -15.0, 15.0);
+  std::vector<double> B = generate_random_square_matrix(N, -15.0, 15.0);
+
+  auto taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(A.data()));
+  taskDataSeq->inputs_count.emplace_back(A.size());
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(B.data()));
+  taskDataSeq->inputs_count.emplace_back(B.size());
+
+  std::vector<double> out(N * N, 0.0);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  auto testTaskSequential =
+      std::make_shared<nikolaev_r_strassen_matrix_multiplication_method_seq::StrassenMatrixMultiplicationSequential>(
+          taskDataSeq);
+
+  ASSERT_TRUE(testTaskSequential->validation());
+  ASSERT_TRUE(testTaskSequential->pre_processing());
+  ASSERT_TRUE(testTaskSequential->run());
+  ASSERT_TRUE(testTaskSequential->post_processing());
+
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perfAttr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskSequential);
+
+  perfAnalyzer->task_run(perfAttr, perfResults);
+  ppc::core::Perf::print_perf_statistic(perfResults);
+
+  ASSERT_EQ(A.size(), out.size());
+}
