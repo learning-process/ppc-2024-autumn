27aea2d8eef5fda23b114c5dcc37dd67ba58b88c
GrayPirat <s.agafeev@mail.ru>
2024-12-12 09:37:27 +0300

    Saving begin

---
+++ tasks/mpi/agafeev_s_linear_lopology/include/lintop_mpi.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <boost/mpi/collectives.hpp>
+#include <boost/mpi/communicator.hpp>
+#include <random>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace agafeev_s_linear_topology {
+
+template <typename T>
+class LinearTopology : public ppc::core::Task {
+ public:
+  explicit LinearTopology(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  boost::mpi::communicator world;
+  std::vector<T> data_;
+};
+
+}  // namespace agafeev_s_linear_topology
---
+++ tasks/mpi/agafeev_s_linear_lopology/src/lintop_mpi.cpp
@@ -0,0 +1,52 @@
+#include "mpi/agafeev_s_linear_lopology/include/lintop_mpi.hpp"
+
+namespace agafeev_s_linear_topology {
+
+template <typename T>
+bool LinearTopology<T>::validation() {
+  internal_order_test();
+
+  if (world.rank() == 0) {
+    return taskData->inputs_count[0] > 0;
+  }
+
+  return true;
+}
+
+template <typename T>
+bool LinearTopology<T>::pre_processing() {
+  internal_order_test();
+
+  if (world.size() == 1) return true;
+
+  if (world.rank() == 0) {
+    auto* temp_ptr = reinterpret_cast<T*>(taskData->inputs[0]);
+    data_.insert(data_.begin(), temp_ptr, temp_ptr + taskData->inputs_count[0]);
+  }
+
+  return true;
+}
+
+template <typename T>
+bool LinearTopology<T>::run() {
+  internal_order_test();
+
+  unsigned int data_size = 0;
+  if (world.rank() == 0) {
+    data_size = taskData->inputs_count[0];
+  }
+
+  boost::mpi::broadcast(world, data_size, 0);
+}
+
+template <typename T>
+bool LinearTopology<T>::post_processing() {
+  internal_order_test();
+
+  if (world.rank() == 0) {
+  }
+
+  return true;
+}
+
+}  // namespace agafeev_s_linear_topology
