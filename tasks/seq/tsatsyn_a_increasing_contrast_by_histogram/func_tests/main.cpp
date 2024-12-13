1edd9535b65f864a887d3b5c00b710ecb54a3beb
KAPUSHA228 <shura_tsatsyn_2003@mail.ru>
2024-12-13 18:50:10 +0300

    fix review 1

--- tasks/seq/tsatsyn_a_increasing_contrast_by_histogram/func_tests/main.cpp
+++ tasks/seq/tsatsyn_a_increasing_contrast_by_histogram/func_tests/main.cpp
@@ -12,7 +12,6 @@ TEST(tsatsyn_a_increasing_contrast_by_histogram_mpi, Test_Sum_1) {
   const int count_size_vector = sizes[0] * sizes[1];
   std::vector<int> in = {64, 67, 10, 152, 152, 106, 117, 10, 155, 78};
   std::vector<int> reference = {94, 100, 0, 249, 249, 168, 188, 0, 255, 119};
-  // in = getRandomVector(count_size_vector, 0, 255);
   std::vector<int> out(count_size_vector);
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
@@ -57,7 +56,6 @@ TEST(tsatsyn_a_increasing_contrast_by_histogram_mpi, Test_Sum_3) {
   const int count_size_vector = sizes[0] * sizes[1];
   std::vector<int> in = {229, 97, 175, 70, 143, 90, 73, 81, 11, 53};
   std::vector<int> reference = {255, 100, 191, 69, 154, 92, 72, 81, 0, 49};
-  // in = getRandomVector(count_size_vector, 0, 255);
   std::vector<int> out(count_size_vector);
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
@@ -81,7 +79,6 @@ TEST(tsatsyn_a_increasing_contrast_by_histogram_mpi, Test_Sum_4) {
   const int count_size_vector = sizes[0] * sizes[1];
   std::vector<int> in = {75, 38, 133, 158, 66, 95, 137, 153, 178, 94};
   std::vector<int> reference = {67, 0, 173, 218, 51, 103, 180, 209, 255, 102};
-  // in = getRandomVector(count_size_vector, 0, 255);
   std::vector<int> out(count_size_vector);
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
@@ -105,7 +102,6 @@ TEST(tsatsyn_a_increasing_contrast_by_histogram_mpi, Test_Sum_5) {
   const int count_size_vector = sizes[0] * sizes[1];
   std::vector<int> in = {145, 85, 38, 40, 241, 161, 64, 94, 240, 57};
   std::vector<int> reference = {134, 59, 0, 2, 255, 154, 32, 70, 253, 23};
-  // in = getRandomVector(count_size_vector, 0, 255);
   std::vector<int> out(count_size_vector);
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
@@ -129,7 +125,6 @@ TEST(tsatsyn_a_increasing_contrast_by_histogram_mpi, Test_Sum_6) {
   const int count_size_vector = sizes[0] * sizes[1];
   std::vector<int> in = {251, 116, 160, 2, 152, 122, 165, 213, 149, 161};
   std::vector<int> reference = {255, 116, 161, 0, 153, 122, 166, 216, 150, 162};
-  // in = getRandomVector(count_size_vector, 0, 255);
   std::vector<int> out(count_size_vector);
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
@@ -153,7 +148,6 @@ TEST(tsatsyn_a_increasing_contrast_by_histogram_mpi, Test_Sum_7) {
   const int count_size_vector = sizes[0] * sizes[1];
   std::vector<int> in = {188, 71, 210, 3, 16, 213, 43, 219, 92, 22};
   std::vector<int> reference = {218, 80, 244, 0, 15, 247, 47, 255, 105, 22};
-  // in = getRandomVector(count_size_vector, 0, 255);
   std::vector<int> out(count_size_vector);
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
@@ -177,7 +171,6 @@ TEST(tsatsyn_a_increasing_contrast_by_histogram_mpi, Test_Sum_8) {
   const int count_size_vector = sizes[0] * sizes[1];
   std::vector<int> in = {59, 135, 129, 4, 28, 254, 74, 183, 15, 71};
   std::vector<int> reference = {56, 133, 127, 0, 24, 255, 71, 182, 11, 68};
-  // in = getRandomVector(count_size_vector, 0, 255);
   std::vector<int> out(count_size_vector);
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
@@ -201,7 +194,6 @@ TEST(tsatsyn_a_increasing_contrast_by_histogram_mpi, Test_Sum_9) {
   const int count_size_vector = sizes[0] * sizes[1];
   std::vector<int> in = {164, 103, 144, 17, 4, 185, 125, 234, 150, 131};
   std::vector<int> reference = {177, 109, 155, 14, 0, 200, 134, 255, 161, 140};
-  // in = getRandomVector(count_size_vector, 0, 255);
   std::vector<int> out(count_size_vector);
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
@@ -225,7 +217,6 @@ TEST(tsatsyn_a_increasing_contrast_by_histogram_mpi, Test_Sum_10) {
   const int count_size_vector = sizes[0] * sizes[1];
   std::vector<int> in = {65, 78, 113, 107, 224, 0, 197, 103, 135, 96};
   std::vector<int> reference = {73, 88, 128, 121, 255, 0, 224, 117, 153, 109};
-  // in = getRandomVector(count_size_vector, 0, 255);
   std::vector<int> out(count_size_vector);
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
