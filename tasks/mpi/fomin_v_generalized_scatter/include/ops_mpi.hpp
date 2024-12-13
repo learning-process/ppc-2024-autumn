0af7e001218ee0ac34956fb844f4921f99ac3622
W-Snack <90415228+W-Snack@users.noreply.github.com>
2024-12-13 18:22:16 +0300

    Фомин Владимир. Задача 2. Вариант 8. Обобщенная передача от одного всем(scatter)

--- tasks/mpi/fomin_v_generalized_scatter/include/ops_mpi.hpp
+++ tasks/mpi/fomin_v_generalized_scatter/include/ops_mpi.hpp
@@ -9,37 +9,31 @@
 
 namespace fomin_v_generalized_scatter {
 
-    // Declaration of TaskData struct
-    struct TaskData {
-        void* inputs[1];
-        int inputs_count[1];
-        void* outputs[1];
-        int outputs_count[1];
-        MPI_Datatype datatype;
-        std::string ops;
-    };
+// Declaration of TaskData struct
+struct TaskData {
+  void* inputs[1];
+  int inputs_count[1];
+  void* outputs[1];
+  int outputs_count[1];
+  MPI_Datatype datatype;
+  std::string ops;
+};
 
-    // Declaration of getRandomVector function
-    std::vector<int> getRandomVector(int sz);
+// Declaration of getRandomVector function
+std::vector<int> getRandomVector(int sz);
+// Declaration of generalized_scatter function
+int generalized_scatter(const void* sendbuf, int sendcount, MPI_Datatype sendtype, void* recvbuf, int recvcount,
+                        MPI_Datatype recvtype, int root, MPI_Comm comm);
 
-    // Declaration of generalized_scatter function
-    int generalized_scatter(const void* sendbuf, int sendcount, MPI_Datatype sendtype,
-                            void* recvbuf, int recvcount, MPI_Datatype recvtype,
-                            int root, MPI_Comm comm);
+// Declaration of GeneralizedScatterTestParallel class
+class GeneralizedScatterTestParallel : public ppc::core::Task {
+ public:
+  bool pre_processing(TaskData* taskData);
+  bool validation(TaskData* taskData);
+  bool run(TaskData* taskData);
+  bool post_processing(TaskData* taskData);
 
-    
+ private:
+};
 
-    // Declaration of GeneralizedScatterTestParallel class
-    class GeneralizedScatterTestParallel : public ppc::core::Task{
-    public:
-        bool pre_processing(TaskData* taskData);
-        bool validation(TaskData* taskData);
-        bool run(TaskData* taskData);
-        bool post_processing(TaskData* taskData);
-    private:
-        // Data members
-    };
-
-} 
-
-#endif 
+}  // namespace fomin_v_generalized_scatter
--- tasks/mpi/fomin_v_generalized_scatter/perf_tests/main.cpp
+++ tasks/mpi/fomin_v_generalized_scatter/perf_tests/main.cpp
@@ -6,89 +6,79 @@
 #include "core/perf/include/perf.hpp"
 #include "mpi/fomin_v_generalized_scatter/include/ops_mpi.hpp"
 
+
 TEST(fomin_v_generalized_scatter_mpi, test_pipeline_run_2000) {
-    const int count = 2000; 
-    boost::mpi::communicator world;
-    std::vector<int> out;
-    std::vector<int> in;
+  const int count = 2000;
+  boost::mpi::communicator world;
+  std::vector<int> out;
+  std::vector<int> in;
 
-    
-    std::shared_ptr<ppc::core::TaskData> taskData = std::make_shared<ppc::core::TaskData>();
+  std::shared_ptr<ppc::core::TaskData> taskData = std::make_shared<ppc::core::TaskData>();
 
-    if (world.rank() == 0) {
-        in = fomin_v_generalized_scatter::getRandomVector(count); 
-        out = std::vector<int>(count / world.size(), 0);
-        taskData->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
-        taskData->inputs_count.emplace_back(count);
-        taskData->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
-        taskData->outputs_count.emplace_back(count / world.size());
-    }
+  if (world.rank() == 0) {
+    in = fomin_v_generalized_scatter::getRandomVector(count);
+    out = std::vector<int>(count / world.size(), 0);
+    taskData->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
+    taskData->inputs_count.emplace_back(count);
+    taskData->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
+    taskData->outputs_count.emplace_back(count / world.size());
+  }
 
-    
-    auto scatterParallel = std::make_shared<fomin_v_generalized_scatter::GeneralizedScatterTestParallel>(taskData, world);
-    ASSERT_EQ(scatterParallel->validation(), true);
-    scatterParallel->pre_processing(); 
-    scatterParallel->run(); 
-    scatterParallel->post_processing(); 
+  auto scatterParallel = std::make_shared<fomin_v_generalized_scatter::GeneralizedScatterTestParallel>(taskData, world);
+  ASSERT_EQ(scatterParallel->validation(), true);
+  scatterParallel->pre_processing();
+  scatterParallel->run();
+  scatterParallel->post_processing();
 
-    
-    auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
-    perfAttr->num_running = 10; 
-    const boost::mpi::timer current_timer;
-    perfAttr->current_timer = [&] { return current_timer.elapsed(); };
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+  const boost::mpi::timer current_timer;
+  perfAttr->current_timer = [&] { return current_timer.elapsed(); };
 
-    
-    auto perfResults = std::make_shared<ppc::core::PerfResults>();
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
 
-   
-    auto perfAnalyzer = std::make_shared<ppc::core::Perf>(scatterParallel);
-    perfAnalyzer->pipeline_run(perfAttr, perfResults); 
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(scatterParallel);
+  perfAnalyzer->pipeline_run(perfAttr, perfResults);
 
-    if (world.rank() == 0) {
-        ppc::core::Perf::print_perf_statistic(perfResults); 
-    }
+  if (world.rank() == 0) {
+    ppc::core::Perf::print_perf_statistic(perfResults);
+  }
 }
 
-
 TEST(fomin_v_generalized_scatter_mpi, test_task_run_2000) {
-    const int count = 2000; 
-    boost::mpi::communicator world;
-    std::vector<int> out;
-    std::vector<int> in;
+  const int count = 2000;
+  boost::mpi::communicator world;
+  std::vector<int> out;
+  std::vector<int> in;
 
-    
-    std::shared_ptr<ppc::core::TaskData> taskData = std::make_shared<ppc::core::TaskData>();
+  std::shared_ptr<ppc::core::TaskData> taskData = std::make_shared<ppc::core::TaskData>();
 
-    if (world.rank() == 0) {
-        in = fomin_v_generalized_scatter::getRandomVector(count); 
-        out = std::vector<int>(count / world.size(), 0);
-        taskData->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
-        taskData->inputs_count.emplace_back(count);
-        taskData->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
-        taskData->outputs_count.emplace_back(count / world.size());
-    }
+  if (world.rank() == 0) {
+    in = fomin_v_generalized_scatter::getRandomVector(count);
+    out = std::vector<int>(count / world.size(), 0);
+    taskData->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
+    taskData->inputs_count.emplace_back(count);
+    taskData->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
+    taskData->outputs_count.emplace_back(count / world.size());
+  }
 
-    
-    auto scatterParallel = std::make_shared<fomin_v_generalized_scatter::GeneralizedScatterTestParallel>(taskData, world);
-    ASSERT_EQ(scatterParallel->validation(), true); 
-    scatterParallel->pre_processing(); 
-    scatterParallel->run(); 
-    scatterParallel->post_processing(); 
+  auto scatterParallel = std::make_shared<fomin_v_generalized_scatter::GeneralizedScatterTestParallel>(taskData, world);
+  ASSERT_EQ(scatterParallel->validation(), true);
+  scatterParallel->pre_processing();
+  scatterParallel->run();
+  scatterParallel->post_processing();
 
-    
-    auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
-    perfAttr->num_running = 30; 
-    const boost::mpi::timer current_timer;
-    perfAttr->current_timer = [&] { return current_timer.elapsed(); };
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 30;
+  const boost::mpi::timer current_timer;
+  perfAttr->current_timer = [&] { return current_timer.elapsed(); };
 
-    
-    auto perfResults = std::make_shared<ppc::core::PerfResults>();
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
 
-    
-    auto perfAnalyzer = std::make_shared<ppc::core::Perf>(scatterParallel);
-    perfAnalyzer->task_run(perfAttr, perfResults); 
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(scatterParallel);
+  perfAnalyzer->task_run(perfAttr, perfResults);
 
-    if (world.rank() == 0) {
-        ppc::core::Perf::print_perf_statistic(perfResults); 
-    }
+  if (world.rank() == 0) {
+    ppc::core::Perf::print_perf_statistic(perfResults);
+  }
 }
