52488403d7654a8a9707c3b0d36293525c830e52
NikKazzzzzz <nikitakazunin@yandex.ru>
2024-12-15 01:04:59 +0300

    func tests rename

--- tasks/mpi/kazunin_n_quicksort_simple_merge/func_tests/main.cpp
+++ tasks/mpi/kazunin_n_quicksort_simple_merge/func_tests/main.cpp
@@ -95,96 +95,96 @@ void template_test(const std::vector<int>& input_data) {
 
 }  // namespace kazunin_n_quicksort_simple_merge_mpi
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_1) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_sorted_ascending) {
   kazunin_n_quicksort_simple_merge_mpi::template_test({1, 2, 3, 4, 5, 6, 8, 9, 5, 4, 3, 2, 1});
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_2) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_almost_sorted_random) {
   kazunin_n_quicksort_simple_merge_mpi::template_test({9, 7, 5, 3, 1, 2, 4, 6, 8, 10});
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_3) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_sorted_descending) {
   kazunin_n_quicksort_simple_merge_mpi::template_test({10, 9, 8, 7, 6, 5, 4, 3, 2, 1});
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_4) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_all_equal_elements) {
   kazunin_n_quicksort_simple_merge_mpi::template_test({5, 5, 5, 5, 5, 5, 5, 5});
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_5) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_negative_numbers) {
   kazunin_n_quicksort_simple_merge_mpi::template_test({0, -1, -2, -3, -4, -5, -6, -7, -8, -9});
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_6) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_mixed_order) {
   kazunin_n_quicksort_simple_merge_mpi::template_test({1, 3, 2, 4, 6, 5, 7, 9, 8, 10});
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_7) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_single_element) {
   std::vector<int> vec = {42};
   kazunin_n_quicksort_simple_merge_mpi::template_test(vec);
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_8) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_empty_vector) {
   kazunin_n_quicksort_simple_merge_mpi::template_test({});
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_9) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_mixed_large_and_small_numbers) {
   kazunin_n_quicksort_simple_merge_mpi::template_test({100, 99, 98, 1, 2, 3, 4, 5});
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_10) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_positive_and_negative_numbers) {
   kazunin_n_quicksort_simple_merge_mpi::template_test({-5, -10, 5, 10, 0});
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_11) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_large_random_numbers) {
   kazunin_n_quicksort_simple_merge_mpi::template_test({123, 456, 789, 321, 654, 987});
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_12) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_random_numbers) {
   kazunin_n_quicksort_simple_merge_mpi::template_test({9, 1, 4, 7, 2, 8, 5, 3, 6});
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_13) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_max_and_min_int) {
   kazunin_n_quicksort_simple_merge_mpi::template_test(
       {std::numeric_limits<int>::max(), std::numeric_limits<int>::min(), 0});
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_14) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_prime_numbers) {
   kazunin_n_quicksort_simple_merge_mpi::template_test({11, 13, 17, 19, 23, 29, 31});
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_15) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_descending_with_negatives) {
   kazunin_n_quicksort_simple_merge_mpi::template_test({50, 40, 30, 20, 10, 0, -10, -20, -30});
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_16) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_pi_digits) {
   kazunin_n_quicksort_simple_merge_mpi::template_test({3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5});
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_17) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_consecutive_mixed_numbers) {
   kazunin_n_quicksort_simple_merge_mpi::template_test({12, 14, 16, 18, 20, 15, 17, 19, 21});
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_18) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_descending_with_zero_and_negatives) {
   kazunin_n_quicksort_simple_merge_mpi::template_test({7, 6, 5, 4, 3, 2, 1, 0, -1, -2});
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_19) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_large_integers) {
   kazunin_n_quicksort_simple_merge_mpi::template_test({1000, 2000, 1500, 2500, 1750});
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_20) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_mixed_digits_and_zero) {
   kazunin_n_quicksort_simple_merge_mpi::template_test({8, 4, 2, 6, 1, 9, 5, 3, 7, 0});
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_21) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_random_generated_10_elements) {
   kazunin_n_quicksort_simple_merge_mpi::template_test(10);
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_22) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_random_generated_20_elements) {
   kazunin_n_quicksort_simple_merge_mpi::template_test(20);
 }
 
-TEST(kazunin_n_quicksort_simple_merge_mpi, test_quick_sort_23) {
+TEST(kazunin_n_quicksort_simple_merge_mpi, test_random_generated_23_elements) {
   kazunin_n_quicksort_simple_merge_mpi::template_test(23);
 }
