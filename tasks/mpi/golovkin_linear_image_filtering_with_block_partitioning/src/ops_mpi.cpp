6b5b1ce9d5e3a7a24918c960997540cfc4567eac
Maksim Golovkin <Mxaqq>
2024-12-14 16:57:29 +0300

    ClangFix5

--- tasks/mpi/golovkin_linear_image_filtering_with_block_partitioning/src/ops_mpi.cpp
+++ tasks/mpi/golovkin_linear_image_filtering_with_block_partitioning/src/ops_mpi.cpp
@@ -103,28 +103,28 @@ void SimpleBlockMPI::exchangeHalo() {
   MPI_Request send_down_req;
   MPI_Request recv_down_req;
 
-  std::vector<MPI_Request> requests;
-
   if (up != MPI_PROC_NULL) {
     MPI_Isend(send_up.data(), width_, MPI_INT, up, 0, world, &send_up_req);
-    requests.push_back(send_up_req);
-
     MPI_Irecv(recv_up.data(), width_, MPI_INT, up, 1, world, &recv_up_req);
-    requests.push_back(recv_up_req);
   }
 
   if (down != MPI_PROC_NULL) {
     MPI_Isend(send_down.data(), width_, MPI_INT, down, 1, world, &send_down_req);
-    requests.push_back(send_down_req);
-
     MPI_Irecv(recv_down.data(), width_, MPI_INT, down, 0, world, &recv_down_req);
-    requests.push_back(recv_down_req);
   }
 
-  if (!requests.empty()) {
-    MPI_Waitall(requests.size(), requests.data(), MPI_STATUSES_IGNORE);
+  // Явно ожидаем каждый запрос
+  if (up != MPI_PROC_NULL) {
+    MPI_Wait(&send_up_req, MPI_STATUS_IGNORE);
+    MPI_Wait(&recv_up_req, MPI_STATUS_IGNORE);
   }
 
+  if (down != MPI_PROC_NULL) {
+    MPI_Wait(&send_down_req, MPI_STATUS_IGNORE);
+    MPI_Wait(&recv_down_req, MPI_STATUS_IGNORE);
+  }
+
+  // Обработка полученных данных
   if (up != MPI_PROC_NULL && local_height_ > 0) {
     local_data_.insert(local_data_.begin(), recv_up.begin(), recv_up.end());
     extended_local_height_++;
