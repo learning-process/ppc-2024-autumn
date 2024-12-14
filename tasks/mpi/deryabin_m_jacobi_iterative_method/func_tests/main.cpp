760bb3646232165377f073b6d4e45f102bd70c41
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-12-14 23:54:30 +0300

    Update main.cpp

--- tasks/mpi/deryabin_m_jacobi_iterative_method/func_tests/main.cpp
+++ tasks/mpi/deryabin_m_jacobi_iterative_method/func_tests/main.cpp
@@ -231,7 +231,6 @@ TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_invalid_matrix_zeros_on
   ASSERT_EQ(testMpiTaskParallel.validation(), false);
 
   if (world.rank() == 0) {
-
     std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
     taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_matrix_.data()));
     taskDataSeq->inputs_count.emplace_back(input_matrix_.size());
