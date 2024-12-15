71c15bb33ab3325bac3c649c1cf97084930b964b
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-12-15 23:44:32 +0300

    Update main.cpp

--- tasks/mpi/deryabin_m_jacobi_iterative_method/func_tests/main.cpp
+++ tasks/mpi/deryabin_m_jacobi_iterative_method/func_tests/main.cpp
@@ -209,35 +209,6 @@ TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_diagonal_elements_are_m
   }
 }
 
-TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_invalid_matrix_zeros_on_diagonal) {
-  boost::mpi::communicator world;
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::uniform_real_distribution<> distrib(1, 10);
-  std::vector<double> input_matrix_(100, distrib(gen));
-  std::vector<double> input_right_vector_(10, distrib(gen));
-  for (unsigned short i = 0; i < 10; i++) {
-    input_matrix_[i * 11] = 0;
-  }
-  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
-  if (world.rank() == 0) {
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_matrix_.data()));
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_right_vector_.data()));
-    taskDataPar->inputs_count.emplace_back(input_right_vector_.size());
-  }
-  deryabin_m_jacobi_iterative_method_mpi::JacobiIterativeMPITaskParallel testMpiTaskParallel(taskDataPar);
-  ASSERT_EQ(testMpiTaskParallel.validation(), false);
-  if (world.rank() == 0) {
-    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_matrix_.data()));
-    taskDataSeq->inputs_count.emplace_back(input_matrix_.size());
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_right_vector_.data()));
-    taskDataSeq->inputs_count.emplace_back(input_right_vector_.size());
-    deryabin_m_jacobi_iterative_method_mpi::JacobiIterativeMPITaskSequential testMpiTaskSequential(taskDataSeq);
-    ASSERT_EQ(testMpiTaskSequential.validation(), false);
-  }
-}
-
 TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_valid_step_matrix) {
   boost::mpi::communicator world;
   std::random_device rd;
@@ -290,5 +261,101 @@ TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_valid_step_matrix) {
     testMpiTaskSequential.post_processing();
 
     ASSERT_EQ(reference_out_x_vec[0], out_x_vec[0]);
+  }
+}
+
+TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_invalid_matrix_zeros_on_diagonal) {
+  boost::mpi::communicator world;
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> distrib(1, 10);
+  std::vector<double> input_matrix_(100, distrib(gen));
+  std::vector<double> input_right_vector_(10, distrib(gen));
+  for (unsigned short i = 0; i < 10; i++) {
+    input_matrix_[i * 11] = 0;
+  }
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+  deryabin_m_jacobi_iterative_method_mpi::JacobiIterativeMPITaskParallel testMpiTaskParallel(taskDataPar);
+  if (world.rank() == 0) {
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_matrix_.data()));
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_right_vector_.data()));
+    taskDataPar->inputs_count.emplace_back(input_right_vector_.size());
+    ASSERT_EQ(testMpiTaskParallel.validation(), false);
+  }
+
+  if (world.rank() == 0) {
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_matrix_.data()));
+    taskDataSeq->inputs_count.emplace_back(input_matrix_.size());
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_right_vector_.data()));
+    taskDataSeq->inputs_count.emplace_back(input_right_vector_.size());
+    deryabin_m_jacobi_iterative_method_mpi::JacobiIterativeMPITaskSequential testMpiTaskSequential(taskDataSeq);
+    ASSERT_EQ(testMpiTaskSequential.validation(), false);
+  }
+}
+
+TEST(deryabin_m_jacobi_iterative_method_mpi, test_random_invalid_matrix_negative_elements) {
+  boost::mpi::communicator world;
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> distrib(1, 10);
+  std::uniform_real_distribution<> distribut(91, 100);
+  std::vector<double> input_matrix_(100, -distrib(gen));
+  std::vector<double> input_right_vector_(10, -distrib(gen));
+  for (unsigned short i = 0; i < 10; i++) {
+    input_matrix_[i * 11] = -distribut(gen);
+  }
+
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+  deryabin_m_jacobi_iterative_method_mpi::JacobiIterativeMPITaskParallel testMpiTaskParallel(taskDataPar);
+  if (world.rank() == 0) {
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_matrix_.data()));
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_right_vector_.data()));
+    taskDataPar->inputs_count.emplace_back(input_right_vector_.size());
+      ASSERT_EQ(testMpiTaskParallel.validation(), false);
+  }
+  
+  if (world.rank() == 0) {
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_matrix_.data()));
+    taskDataSeq->inputs_count.emplace_back(input_matrix_.size());
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_right_vector_.data()));
+    taskDataSeq->inputs_count.emplace_back(input_right_vector_.size());
+
+    deryabin_m_jacobi_iterative_method_mpi::JacobiIterativeMPITaskSequential testMpiTaskSequential(taskDataSeq);
+    ASSERT_EQ(testMpiTaskSequential.validation(), false);
+  }
+}
+
+TEST(deryabin_m_jacobi_iterative_method_mpi,
+     test_random_invalid_matrix_zeros_on_diagonal_and_negative_elements_on_non_diagonal) {
+  boost::mpi::communicator world;
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> distrib(1, 10);
+  std::vector<double> input_matrix_(100, -distrib(gen));
+  std::vector<double> input_right_vector_(10, -distrib(gen));
+  for (unsigned short i = 0; i < 10; i++) {
+    input_matrix_[i * 11] = 0;
+  }
+
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+  deryabin_m_jacobi_iterative_method_mpi::JacobiIterativeMPITaskParallel testMpiTaskParallel(taskDataPar);
+  if (world.rank() == 0) {
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_matrix_.data()));
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_right_vector_.data()));
+    taskDataPar->inputs_count.emplace_back(input_right_vector_.size());
+    ASSERT_EQ(testMpiTaskParallel.validation(), false);
+  }
+  
+  if (world.rank() == 0) {
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_matrix_.data()));
+    taskDataSeq->inputs_count.emplace_back(input_matrix_.size());
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_right_vector_.data()));
+    taskDataSeq->inputs_count.emplace_back(input_right_vector_.size());
+
+    deryabin_m_jacobi_iterative_method_mpi::JacobiIterativeMPITaskSequential testMpiTaskSequential(taskDataSeq);
+    ASSERT_EQ(testMpiTaskSequential.validation(), false);
   }
 }
