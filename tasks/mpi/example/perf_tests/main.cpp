0cf9491e4f633b32e5a15e86397a1478e7c7e98d
kmichaelk <130953568+kmichaelk@users.noreply.github.com>
2024-12-16 21:47:28 +0000

    Check for cluttering only on test teardown

--- tasks/mpi/example/perf_tests/main.cpp
+++ tasks/mpi/example/perf_tests/main.cpp
@@ -95,21 +95,17 @@ int main(int argc, char** argv) {
   if (world.rank() != 0) {
     delete listeners.Release(listeners.default_result_printer());
   }
-  class BufferGarbageDetector : public ::testing::EmptyTestEventListener {
-   public:
-    void OnTestStart(const ::testing::TestInfo& test_info) override { inspect(test_info); }
-    void OnTestEnd(const ::testing::TestInfo& test_info) override { inspect(test_info); }
-
-   private:
-    void inspect(const ::testing::TestInfo& test_info, const char* routine = __builtin_FUNCTION()) {
+  struct BufferGarbageDetector : public ::testing::EmptyTestEventListener {
+    void OnTestEnd(const ::testing::TestInfo& test_info) override {
       world.barrier();
       if (const auto status = world.iprobe(boost::mpi::any_source, boost::mpi::any_tag)) {
-        fprintf(stderr, "[  PROCESS %d  ] %s(%s.%s): MPI buffer is cluttered, unread message tag is %d\n", world.rank(),
-                routine, test_info.test_suite_name(), test_info.name(), status->tag());
+        fprintf(stderr, "[  PROCESS %d  ] %s.%s: MPI buffer is cluttered, unread message tag is %d\n", world.rank(),
+                test_info.test_suite_name(), test_info.name(), status->tag());
         exit(2);
       }
       world.barrier();
     }
+
     boost::mpi::communicator world;
   };
   listeners.Append(new BufferGarbageDetector);
