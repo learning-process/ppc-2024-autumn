3d787908150a7b17633a85718376f0f0def9fcd6
Mironov Arsenii <arseniy.mironov.01@mail.ru>
2024-12-17 22:53:50 +0300

    it works

--- tasks/mpi/mironov_a_quick_sort/func_tests/main.cpp
+++ tasks/mpi/mironov_a_quick_sort/func_tests/main.cpp
@@ -7,7 +7,7 @@
 #include <vector>
 
 #include "mpi/mironov_a_quick_sort/include/ops_mpi.hpp"
-
+using namespace std;
 namespace mironov_a_quick_sort_mpi {
 
 std::vector<int> get_random_vector(int sz) {
@@ -27,7 +27,7 @@ std::vector<int> get_random_vector(int sz) {
 TEST(mironov_a_quick_sort_mpi, Test_Sort_1) {
   boost::mpi::communicator world;
   // Create TaskData
-  const int count = 10000;
+  const int count = 20;
   std::vector<int> global_vec;
   std::vector<int> global_max;
   std::vector<int> gold;
@@ -49,11 +49,122 @@ TEST(mironov_a_quick_sort_mpi, Test_Sort_1) {
     taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_max.data()));
     taskDataPar->outputs_count.emplace_back(global_max.size());
   }
-
+  //cout << "Epoch 1 " << world.rank() << endl;
   mironov_a_quick_sort_mpi::QuickSortMPI testMpiTaskParallel(taskDataPar);
   ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  //cout << "Epoch 2 " << world.rank() << endl;
   testMpiTaskParallel.pre_processing();
+  //cout << "Epoch 3 " << world.rank() << endl;
   testMpiTaskParallel.run();
+  //cout << "Epoch 4 " << world.rank() << endl;
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+    // Create TaskData
+    std::vector<int32_t> reference_max(count);
+
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataSeq->inputs_count.emplace_back(global_vec.size());
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_max.data()));
+    taskDataSeq->outputs_count.emplace_back(reference_max.size());
+
+    // Create Task
+    mironov_a_quick_sort_mpi::QuickSortSequential testMpiTaskSequential(taskDataSeq);
+    ASSERT_EQ(testMpiTaskSequential.validation(), true);
+    testMpiTaskSequential.pre_processing();
+    testMpiTaskSequential.run();
+    testMpiTaskSequential.post_processing();
+
+    ASSERT_EQ(reference_max, gold);
+    ASSERT_EQ(global_max, gold);
+  }
+}
+
+TEST(mironov_a_quick_sort_mpi, Test_Sort_2) {
+  boost::mpi::communicator world;
+  // Create TaskData
+  const int count = 30000;
+  std::vector<int> global_vec;
+  std::vector<int> global_max;
+  std::vector<int> gold;
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    // Create TaskData
+
+    global_vec = mironov_a_quick_sort_mpi::get_random_vector(count);
+    global_max.resize(count);
+    gold = global_vec;
+    sort(gold.begin(), gold.end());
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataPar->inputs_count.emplace_back(global_vec.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_max.data()));
+    taskDataPar->outputs_count.emplace_back(global_max.size());
+  }
+  // cout << "Epoch 1 " << world.rank() << endl;
+  mironov_a_quick_sort_mpi::QuickSortMPI testMpiTaskParallel(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  // cout << "Epoch 2 " << world.rank() << endl;
+  testMpiTaskParallel.pre_processing();
+  // cout << "Epoch 3 " << world.rank() << endl;
+  testMpiTaskParallel.run();
+  // cout << "Epoch 4 " << world.rank() << endl;
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+    // Create TaskData
+    std::vector<int32_t> reference_max(count);
+
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataSeq->inputs_count.emplace_back(global_vec.size());
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_max.data()));
+    taskDataSeq->outputs_count.emplace_back(reference_max.size());
+
+    // Create Task
+    mironov_a_quick_sort_mpi::QuickSortSequential testMpiTaskSequential(taskDataSeq);
+    ASSERT_EQ(testMpiTaskSequential.validation(), true);
+    testMpiTaskSequential.pre_processing();
+    testMpiTaskSequential.run();
+    testMpiTaskSequential.post_processing();
+
+    ASSERT_EQ(reference_max, gold);
+    ASSERT_EQ(global_max, gold);
+  }
+}
+
+TEST(mironov_a_quick_sort_mpi, Test_Sort_3) {
+  boost::mpi::communicator world;
+  // Create TaskData
+  const int count = 5000;
+  std::vector<int> global_vec;
+  std::vector<int> global_max;
+  std::vector<int> gold;
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    // Create TaskData
+
+    global_vec = mironov_a_quick_sort_mpi::get_random_vector(count);
+    global_max.resize(count);
+    gold = global_vec;
+    sort(gold.begin(), gold.end());
+
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
+    taskDataPar->inputs_count.emplace_back(global_vec.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_max.data()));
+    taskDataPar->outputs_count.emplace_back(global_max.size());
+  }
+  // cout << "Epoch 1 " << world.rank() << endl;
+  mironov_a_quick_sort_mpi::QuickSortMPI testMpiTaskParallel(taskDataPar);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  // cout << "Epoch 2 " << world.rank() << endl;
+  testMpiTaskParallel.pre_processing();
+  // cout << "Epoch 3 " << world.rank() << endl;
+  testMpiTaskParallel.run();
+  // cout << "Epoch 4 " << world.rank() << endl;
   testMpiTaskParallel.post_processing();
 
   if (world.rank() == 0) {
--- tasks/mpi/mironov_a_quick_sort/src/ops_mpi.cpp
+++ tasks/mpi/mironov_a_quick_sort/src/ops_mpi.cpp
@@ -4,44 +4,60 @@
 #include <thread>
 #include <utility>
 
-using namespace std::chrono_literals;
+//#define DEBUG
 
+#ifdef DEBUG
+using namespace std;
+#endif
 bool mironov_a_quick_sort_mpi::QuickSortMPI::pre_processing() {
   internal_order_test();
-
+  if (world.rank() == 0) {
+    delta = taskData->inputs_count[0] / world.size() + (taskData->inputs_count[0] % world.size() > 0);
+#ifdef DEBUG
+    cout << "Delta " << delta << endl;
+#endif
+    input_ = std::vector < int>(delta * world.size(), std::numeric_limits<int>::max());
+    result_ = std::vector<int>(taskData->inputs_count[0]);
+    int* it = reinterpret_cast<int*>(taskData->inputs[0]);
+#ifdef DEBUG
+    cout << "sz " << input_.size() << " " << result_.size() << endl;
+#endif
+    for(size_t i = 0; i < taskData->inputs_count[0]; ++i) {
+      input_[i] = it[i];
+    }
+  }
   return true;
 }
 
 bool mironov_a_quick_sort_mpi::QuickSortMPI::validation() {
   internal_order_test();
   // Check count elements input & output
-  return (taskData->inputs_count[0] > 0) && (taskData->outputs_count[0] == taskData->inputs_count[0]);
-}
-
-bool mironov_a_quick_sort_mpi::QuickSortMPI::run() {
-  internal_order_test();
+  if (world.rank() == 0)
+    return (taskData->inputs_count[0] > 0) && (taskData->outputs_count[0] == taskData->inputs_count[0]);
   return true;
 }
 
-bool mironov_a_quick_sort_mpi::QuickSortMPI::post_processing() {
-  internal_order_test();
-  return true;
-}
+static void merge(std::vector<int>& vec1, std::vector<int>& vec2) {
+  std::vector<int> res;
+  res.reserve(vec1.size() + vec2.size());
+  
+  int ptr1 = 0;
+  int ptr2 = 0;
 
-bool mironov_a_quick_sort_mpi::QuickSortSequential::pre_processing() {
-  internal_order_test();
-
-  // Init value for input and output
-  input_ = std::vector<int>(taskData->inputs_count[0]);
-  int* it = reinterpret_cast<int*>(taskData->inputs[0]);
-  std::copy(it, it + taskData->inputs_count[0], input_.begin());
-  return true;
-}
-
-bool mironov_a_quick_sort_mpi::QuickSortSequential::validation() {
-  internal_order_test();
-  // Check count elements input & output
-  return (taskData->inputs_count[0] > 0) && (taskData->outputs_count[0] == taskData->inputs_count[0]);
+  while (ptr1 < vec1.size() && ptr2 < vec2.size()) {
+    if (vec1[ptr1] <= vec2[ptr2]) {
+      res.push_back(vec1[ptr1++]);
+    } else {
+      res.push_back(vec2[ptr2++]);
+    }
+  }
+  while (ptr1 < vec1.size()) {
+    res.push_back(vec1[ptr1++]);
+  }
+  while (ptr2 < vec2.size()) {
+    res.push_back(vec2[ptr2++]);
+  }
+  vec1 = std::move(res);
 }
 
 static void quickSort(std::vector<int>& arr, int start, int end) {
@@ -60,6 +76,118 @@ static void quickSort(std::vector<int>& arr, int start, int end) {
 
   quickSort(arr, start, right);
   quickSort(arr, left, end);
+}
+
+bool mironov_a_quick_sort_mpi::QuickSortMPI::run() {
+  internal_order_test();
+
+  broadcast(world, delta, 0);
+
+  if (world.rank() == 0) {
+    // probably better to use isend
+    for (int proc = 1; proc < world.size(); proc++) {
+      world.send(proc, 0, input_.data() + proc * delta, delta);
+    }
+  };
+
+  std::vector<int> local_input;
+  if (world.rank() == 0) {
+    local_input = std::vector<int>(input_.begin(), input_.begin() + delta);
+  } else {
+    local_input.resize(delta);
+    world.recv(0, 0, local_input.data(), delta);
+  }
+  // cout << "Stage 1 " <<  world.rank() << endl;
+  quickSort(local_input, 0, delta - 1);
+  int world_sz = world.size();
+
+  if (world.rank() == 0) {
+    if (world_sz == 1) {
+      result_ = std::move(local_input);
+      return true;
+    }
+    int curr_rank = 1;
+    std::vector<int> vec2;
+    while (curr_rank < world_sz) {
+
+      vec2.resize(local_input.size());
+#ifdef DEBUG
+      cout << "recv: " << curr_rank << " " << vec2.size() << " " << local_input.size() << " " << world.rank() << endl;
+#endif
+      world.recv(curr_rank, 0, vec2.data(), local_input.size());
+      merge(local_input, vec2);
+#ifdef DEBUG  
+      cout << "MERGED " << local_input.size() << endl;
+#endif
+      curr_rank <<= 1;
+    }
+    result_ = std::move(local_input);
+
+  } else {
+    int rank = world.rank();
+    int curr_rank = world.rank();
+    int need_rank = 1;
+
+    std::vector<int> vec2;
+
+    while (!(curr_rank % 2)) {
+      vec2.resize(local_input.size());
+      if (rank + need_rank < world_sz) {
+        world.recv(rank + need_rank, 0, vec2.data(), local_input.size());
+        merge(local_input, vec2);
+#ifdef DEBUG 
+        cout << "Abort: " << rank + need_rank << " " << local_input.size() << " " << world.rank() << endl;
+        cout << "MERGED_$ " << local_input.size() << " " << world.rank() << endl;
+#endif
+      }
+      else
+      {
+        local_input.resize(local_input.size() * 2, std::numeric_limits<int>::max());
+      }
+      need_rank <<= 1;
+      curr_rank >>= 1;
+    }
+#ifdef DEBUG 
+    if (world.rank() == 4)
+    {
+      cout << "WOWWW: " << need_rank << " " << curr_rank << endl;
+    }
+    int c = world.rank() - need_rank;
+ 
+    cout << "C: " << c << " " << local_input.size() << " " << world.rank() << endl;
+#endif
+    world.send(world.rank() - need_rank, 0, local_input.data(), local_input.size());
+  }
+
+  return true;
+}
+
+bool mironov_a_quick_sort_mpi::QuickSortMPI::post_processing() {
+  internal_order_test();
+  if (world.rank() == 0) {
+    int* it = reinterpret_cast<int*>(taskData->outputs[0]);
+#ifdef DEBUG  
+    cout << "OUTPUT: " <<  taskData->outputs_count[0] << " " << result_.size() << endl;
+#endif
+    std::copy(result_.begin(), result_.begin() + taskData->outputs_count[0], it);
+  }
+  return true;
+}
+
+bool mironov_a_quick_sort_mpi::QuickSortSequential::pre_processing() {
+  internal_order_test();
+
+  // Init value for input and output
+  input_ = std::vector<int>(taskData->inputs_count[0]);
+  int* it = reinterpret_cast<int*>(taskData->inputs[0]);
+  std::copy(it, it + taskData->inputs_count[0], input_.begin());
+  return true;
+}
+
+bool mironov_a_quick_sort_mpi::QuickSortSequential::validation() {
+  internal_order_test();
+  // Check count elements input & output
+  return (taskData->inputs_count[0] > 0) && (taskData->outputs_count[0] == taskData->inputs_count[0]);
 }
 
 bool mironov_a_quick_sort_mpi::QuickSortSequential::run() {
