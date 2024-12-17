adfe542643c11c44ed421ef11c745a092dfbee3f
Sasha Zinoviev <kobychit1@gmail.com>
2024-12-17 19:26:26 +0300

    new

--- tasks/mpi/zinoviev_a_bellman_ford/src/ops_mpi.cpp
+++ tasks/mpi/zinoviev_a_bellman_ford/src/ops_mpi.cpp
@@ -12,6 +12,7 @@ using namespace std::chrono_literals;
 
 bool zinoviev_a_bellman_ford_mpi::BellmanFordMPITaskSequential::pre_processing() {
   internal_order_test();
+  // Init graph
   graph_ = std::vector<int>(taskData->inputs_count[0]);
   auto* tmp_ptr = reinterpret_cast<int*>(taskData->inputs[0]);
   for (unsigned i = 0; i < taskData->inputs_count[0]; i++) {
@@ -25,11 +26,13 @@ bool zinoviev_a_bellman_ford_mpi::BellmanFordMPITaskSequential::pre_processing()
 
 bool zinoviev_a_bellman_ford_mpi::BellmanFordMPITaskSequential::validation() {
   internal_order_test();
+  // Check count elements of output
   return taskData->outputs_count[0] > 0;
 }
 
 bool zinoviev_a_bellman_ford_mpi::BellmanFordMPITaskSequential::run() {
   internal_order_test();
+  // Bellman-Ford algorithm
   for (size_t i = 0; i < distances_.size() - 1; ++i) {
     for (size_t j = 0; j < graph_.size(); j += 3) {
       int u = graph_[j];
@@ -60,6 +63,7 @@ bool zinoviev_a_bellman_ford_mpi::BellmanFordMPITaskParallel::pre_processing() {
   broadcast(world, delta, 0);
 
   if (world.rank() == 0) {
+    // Init graph
     graph_ = std::vector<int>(taskData->inputs_count[0]);
     auto* tmp_ptr = reinterpret_cast<int*>(taskData->inputs[0]);
     for (unsigned i = 0; i < taskData->inputs_count[0]; i++) {
@@ -75,6 +79,7 @@ bool zinoviev_a_bellman_ford_mpi::BellmanFordMPITaskParallel::pre_processing() {
   } else {
     world.recv(0, 0, local_graph_.data(), delta);
   }
+  // Init distances
   distances_ = std::vector<int>(taskData->outputs_count[0], INT_MAX);
   distances_[0] = 0;
   return true;
@@ -83,6 +88,7 @@ bool zinoviev_a_bellman_ford_mpi::BellmanFordMPITaskParallel::pre_processing() {
 bool zinoviev_a_bellman_ford_mpi::BellmanFordMPITaskParallel::validation() {
   internal_order_test();
   if (world.rank() == 0) {
+    // Check count elements of output
     return taskData->outputs_count[0] > 0;
   }
   return true;
@@ -90,6 +96,7 @@ bool zinoviev_a_bellman_ford_mpi::BellmanFordMPITaskParallel::validation() {
 
 bool zinoviev_a_bellman_ford_mpi::BellmanFordMPITaskParallel::run() {
   internal_order_test();
+  // Bellman-Ford algorithm
   for (size_t i = 0; i < distances_.size() - 1; ++i) {
     for (size_t j = 0; j < local_graph_.size(); j += 3) {
       int u = local_graph_[j];
@@ -99,13 +106,11 @@ bool zinoviev_a_bellman_ford_mpi::BellmanFordMPITaskParallel::run() {
         distances_[v] = distances_[u] + weight;
       }
     }
+    // Reduce distances
     std::vector<int> local_distances = distances_;
     boost::mpi::reduce(
         world, local_distances.data(), local_distances.size(), distances_.data(),
-        [](const int* in, const int* in_end, int* out) {
-          std::transform(in, in_end, out, out, [](int a, int b) { return std::min(a, b); });
-        },
-        0);
+        [](int a, int b) { return std::min(a, b); }, 0);
   }
   return true;
 }
