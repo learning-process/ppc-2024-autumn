f06ceb15325d4043c790627d1a0ef6fbc39df921
emilien-gus <emiguseynov@gmail.com>
2024-12-17 12:23:24 +0300

    this is bad

--- tasks/mpi/guseynov_e_marking_comps_of_bin_image/func_tests/main.cpp
+++ tasks/mpi/guseynov_e_marking_comps_of_bin_image/func_tests/main.cpp
@@ -3,16 +3,28 @@
 #include <boost/mpi/communicator.hpp>
 #include <boost/mpi/environment.hpp>
 #include <vector>
+#include <random>
 
 #include "mpi/guseynov_e_marking_comps_of_bin_image/include/ops_mpi.hpp"
 
+std::vector<int> getRandomBinImage(int r, int c) {
+  std::random_device dev;
+  std::mt19937 gen(dev());
+  std::vector<int> vec(r * c);
+  for (int i = 0; i < r * c; i++) {
+    vec[i] = gen() % 2;
+  }
+  return vec;
+}
+
+
 TEST(guseynov_e_marking_comps_of_bin_image_mpi, fixed_test_1) {
   boost::mpi::communicator world;
-  const int rows = 3;
-  const int cols = 3;
-  std::vector<int> image = {1, 0, 1, 0, 1, 0, 1, 0, 1};
+  const int rows = 100;
+  const int cols = 100;
+  std::vector<int> image(rows * cols, 0);
   std::vector<int> global_labeled_image(rows * cols);
-  std::vector<int> expected_label = {1, 2, 1, 2, 1, 3, 1, 3, 1};
+  std::vector<int> expected_label(rows * cols, 2);
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
 
@@ -58,11 +70,11 @@ TEST(guseynov_e_marking_comps_of_bin_image_mpi, fixed_test_1) {
 
 TEST(guseynov_e_marking_comps_of_bin_image_mpi, fixed_test_2) {
   boost::mpi::communicator world;
-  const int rows = 3;
-  const int cols = 3;
-  std::vector<int> image = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+  const int rows = 100;
+  const int cols = 100;
+  std::vector<int> image(rows * cols, 1);
   std::vector<int> global_labeled_image(rows * cols);
-  std::vector<int> expected_label = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+  std::vector<int> expected_label(rows * cols, 1);
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
 
@@ -106,17 +118,17 @@ TEST(guseynov_e_marking_comps_of_bin_image_mpi, fixed_test_2) {
   }
 }
 
-TEST(guseynov_e_marking_comps_of_bin_image_mpi, fixed_test_3) {
+TEST(guseynov_e_marking_comps_of_bin_image_mpi, random_tes_100x100) {
   boost::mpi::communicator world;
-  const int rows = 3;
-  const int cols = 3;
-  std::vector<int> image = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+  const int rows = 10;
+  const int cols = 10;
+  std::vector<int> image;
   std::vector<int> global_labeled_image(rows * cols);
-  std::vector<int> expected_label = {2, 2, 2, 2, 2, 2, 2, 2, 2};
   // Create TaskData
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
 
   if (world.rank() == 0) {
+    image = getRandomBinImage(rows, cols);
     taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(image.data()));
     taskDataPar->inputs_count.emplace_back(rows);
     taskDataPar->inputs_count.emplace_back(cols);
@@ -151,107 +163,19 @@ TEST(guseynov_e_marking_comps_of_bin_image_mpi, fixed_test_3) {
     testMpiTaskSequential.run();
     testMpiTaskSequential.post_processing();
 
-    ASSERT_EQ(reference_labeled_image, expected_label);
-    ASSERT_EQ(reference_labeled_image, global_labeled_image);
-  }
-}
-
-TEST(guseynov_e_marking_comps_of_bin_image_mpi, fixed_test_4) {
-  boost::mpi::communicator world;
-  const int rows = 3;
-  const int cols = 3;
-  std::vector<int> image = {0, 1, 1, 1, 1, 0, 0, 1, 1};
-  std::vector<int> global_labeled_image(rows * cols);
-  std::vector<int> expected_label = {2, 1, 1, 1, 1, 3, 4, 1, 1};
-  // Create TaskData
-  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
-
-  if (world.rank() == 0) {
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(image.data()));
-    taskDataPar->inputs_count.emplace_back(rows);
-    taskDataPar->inputs_count.emplace_back(cols);
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_labeled_image.data()));
-    taskDataPar->outputs_count.emplace_back(rows);
-    taskDataPar->outputs_count.emplace_back(cols);
-  }
-
-  guseynov_e_marking_comps_of_bin_image_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
-  ASSERT_TRUE(testMpiTaskParallel.validation());
-  testMpiTaskParallel.pre_processing();
-  testMpiTaskParallel.run();
-  testMpiTaskParallel.post_processing();
-
-  if (world.rank() == 0) {
-    // Create data
-    std::vector<int> reference_labeled_image(rows * cols);
-
-    // Create TaskData
-    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(image.data()));
-    taskDataSeq->inputs_count.emplace_back(rows);
-    taskDataSeq->inputs_count.emplace_back(cols);
-    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_labeled_image.data()));
-    taskDataSeq->outputs_count.emplace_back(rows);
-    taskDataSeq->outputs_count.emplace_back(cols);
-
-    // Create Task
-    guseynov_e_marking_comps_of_bin_image_mpi::TestMPITaskSequential testMpiTaskSequential(taskDataSeq);
-    ASSERT_TRUE(testMpiTaskSequential.validation());
-    testMpiTaskSequential.pre_processing();
-    testMpiTaskSequential.run();
-    testMpiTaskSequential.post_processing();
-
-    ASSERT_EQ(reference_labeled_image, expected_label);
-    ASSERT_EQ(reference_labeled_image, global_labeled_image);
-  }
-}
-
-TEST(guseynov_e_marking_comps_of_bin_image_mpi, fixed_test_5) {
-  boost::mpi::communicator world;
-  const int rows = 3;
-  const int cols = 3;
-  std::vector<int> image(rows * cols, 0);
-  std::vector<int> global_labeled_image(rows * cols);
-  std::vector<int> expected_label(rows * cols, 2);
-  // Create TaskData
-  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
-
-  if (world.rank() == 0) {
-    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(image.data()));
-    taskDataPar->inputs_count.emplace_back(rows);
-    taskDataPar->inputs_count.emplace_back(cols);
-    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_labeled_image.data()));
-    taskDataPar->outputs_count.emplace_back(rows);
-    taskDataPar->outputs_count.emplace_back(cols);
-  }
-
-  guseynov_e_marking_comps_of_bin_image_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
-  ASSERT_TRUE(testMpiTaskParallel.validation());
-  testMpiTaskParallel.pre_processing();
-  testMpiTaskParallel.run();
-  testMpiTaskParallel.post_processing();
-
-  if (world.rank() == 0) {
-    // Create data
-    std::vector<int> reference_labeled_image(rows * cols);
-
-    // Create TaskData
-    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
-    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(image.data()));
-    taskDataSeq->inputs_count.emplace_back(rows);
-    taskDataSeq->inputs_count.emplace_back(cols);
-    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_labeled_image.data()));
-    taskDataSeq->outputs_count.emplace_back(rows);
-    taskDataSeq->outputs_count.emplace_back(cols);
-
-    // Create Task
-    guseynov_e_marking_comps_of_bin_image_mpi::TestMPITaskSequential testMpiTaskSequential(taskDataSeq);
-    ASSERT_TRUE(testMpiTaskSequential.validation());
-    testMpiTaskSequential.pre_processing();
-    testMpiTaskSequential.run();
-    testMpiTaskSequential.post_processing();
-
-    ASSERT_EQ(reference_labeled_image, expected_label);
+    for (int i = 0; i < rows; i++){
+      for (int j = 0; j < cols; j++){
+        std::cout << global_labeled_image[i * cols + j] << " ";
+      }
+      std::cout << "\n";
+    }
+    std::cout << "\n";
+    for (int i = 0; i < rows; i++){
+      for (int j = 0; j < cols; j++){
+        std::cout << reference_labeled_image[i * cols + j] << " ";
+      }
+      std::cout << "\n";
+    }
     ASSERT_EQ(reference_labeled_image, global_labeled_image);
   }
 }
--- tasks/seq/guseynov_e_marking_comps_of_bin_image/func_tests/main.cpp
+++ tasks/seq/guseynov_e_marking_comps_of_bin_image/func_tests/main.cpp
@@ -1,8 +1,21 @@
 #include <gtest/gtest.h>
 
 #include <vector>
+#include <random>
 
 #include "seq/guseynov_e_marking_comps_of_bin_image/include/ops_seq.hpp"
+
+std::vector<int> getRandomBinImage(int r, int c) {
+  std::random_device dev;
+  std::mt19937 gen(dev());
+  std::uniform_real_distribution<> dist(0, 2);
+  std::vector<int> vec(r * c);
+  for (int i = 0; i < r * c; i++) {
+    vec[i] = gen();
+  }
+  return vec;
+}
+
 
 TEST(guseynov_e_marking_comps_of_bin_image_seq, test_image_is_object) {
   const int rows = 3;
@@ -140,4 +153,78 @@ TEST(guseynov_e_marking_comps_of_bin_image_seq, test_one_column_with_isolated_po
   testTaskSequential.run();
   testTaskSequential.post_processing();
   ASSERT_EQ(expected_out, out);
+}
+
+
+TEST(guseynov_e_marking_comps_of_bin_image_seq, test_one_column_with_isolated_point2) {
+  const int rows = 10;
+  const int columns = 10;
+  std::vector<int> in = getRandomBinImage(rows, columns);
+  std::vector<int> out(rows * columns, -1);
+
+  for (int i = 0; i < rows; i++){
+    for (int j = 0; j < columns; j++){
+      std::cout << in[i * columns] << " ";
+    }
+    std::cout << "\n";
+  }
+
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataSeq->inputs_count.emplace_back(rows);
+  taskDataSeq->inputs_count.emplace_back(columns);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(rows);
+  taskDataSeq->outputs_count.emplace_back(columns);
+
+  guseynov_e_marking_comps_of_bin_image_seq::TestTaskSequential testTaskSequential(taskDataSeq);
+  ASSERT_TRUE(testTaskSequential.validation());
+  testTaskSequential.pre_processing();
+  testTaskSequential.run();
+  testTaskSequential.post_processing();
+  std::cout << "\n";
+    for (int i = 0; i < rows; i++){
+    for (int j = 0; j < columns; j++){
+      std::cout << out[i * columns] << " ";
+    }
+    std::cout << "\n";
+  }
+}
+
+TEST(guseynov_e_marking_comps_of_bin_image_seq, test_one_column_with_isolated_point3) {
+  const int rows = 10;
+  const int columns = 10;
+  std::vector<int> in = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+  std::vector<int> out(rows * columns, -1);
+
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataSeq->inputs_count.emplace_back(rows);
+  taskDataSeq->inputs_count.emplace_back(columns);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(rows);
+  taskDataSeq->outputs_count.emplace_back(columns);
+
+  guseynov_e_marking_comps_of_bin_image_seq::TestTaskSequential testTaskSequential(taskDataSeq);
+  ASSERT_TRUE(testTaskSequential.validation());
+  testTaskSequential.pre_processing();
+  testTaskSequential.run();
+  testTaskSequential.post_processing();
+  std::cout << "\n";
+  for (int i = 0; i < rows; i++){
+    for (int j = 0; j < columns; j++){
+      std::cout << out[i * columns] << " ";
+    }
+    std::cout << "\n";
+  }
+
 }
