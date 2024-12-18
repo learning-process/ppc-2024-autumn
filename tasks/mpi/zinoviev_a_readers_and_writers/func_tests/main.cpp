9c9b7f94a97bf25312c124ef29df461cb4b07212
Sasha Zinoviev <kobychit1@gmail.com>
2024-12-18 13:49:46 +0300

    getRandomVec fix 2

--- tasks/mpi/zinoviev_a_readers_and_writers/func_tests/main.cpp
+++ tasks/mpi/zinoviev_a_readers_and_writers/func_tests/main.cpp
@@ -15,7 +15,7 @@ std::vector<int> getRandomVector(int sz);
 
 using namespace zinoviev_a_readers_and_writers_mpi;
 
-std::vector<int> zinoviev_a_readers_and_writers_mpi::getRandomVec(int sz) {
+std::vector<int> zinoviev_a_readers_and_writers_mpi::getRandomVector(int sz) {
   static std::mt19937 gen(std::random_device{}());
   std::uniform_int_distribution<int> dist(0, 99);
   std::vector<int> vec(sz);
@@ -31,7 +31,7 @@ TEST(zinoviev_a_readers_and_writers_mpi, Test_Readers_Writers) {
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
   if (world.rank() == 0) {
     const int count_size_vector = 120;
-    global_vec = zinoviev_a_readers_and_writers_mpi::getRandomVec(count_size_vector);
+    global_vec = zinoviev_a_readers_and_writers_mpi::getRandomVector(count_size_vector);
     taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
     taskDataPar->inputs_count.emplace_back(global_vec.size());
     taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_result.data()));
@@ -74,7 +74,7 @@ TEST(zinoviev_a_readers_and_writers_mpi, Test_Different_Input_Sizes) {
 
   if (world.rank() == 0) {
     const int count_size_vector = 200;
-    global_vec = zinoviev_a_readers_and_writers_mpi::getRandomVec(count_size_vector);
+    global_vec = zinoviev_a_readers_and_writers_mpi::getRandomVector(count_size_vector);
     taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_vec.data()));
     taskDataPar->inputs_count.emplace_back(global_vec.size());
     taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_result.data()));
