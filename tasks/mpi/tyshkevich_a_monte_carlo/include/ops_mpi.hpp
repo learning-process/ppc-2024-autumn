ebf0e1350750fd157bea5b9497cd7561c9aacc60
artich02 <tar8803@gmail.com>
2024-12-19 01:46:49 +0300

    first

---
+++ tasks/mpi/tyshkevich_a_monte_carlo/include/ops_mpi.hpp
@@ -0,0 +1,92 @@
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include <boost/mpi/collectives.hpp>
+#include <boost/mpi/communicator.hpp>
+#include <random>
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace tyshkevich_a_monte_carlo_mpi {
+
+inline double function_sin_sum(const std::vector<double>& x) {
+  double sum = 0.0;
+  for (double xi : x) {
+    sum += std::sin(xi);
+  }
+  return sum;
+}
+
+inline double function_cos_product(const std::vector<double>& x) {
+  double product = 1.0;
+  for (double xi : x) {
+    product *= std::cos(xi);
+  }
+  return product;
+}
+
+inline double function_gaussian(const std::vector<double>& x) {
+  double sum = 0.0;
+  for (double xi : x) {
+    sum += xi * xi;
+  }
+  return std::exp(-sum);
+}
+
+inline double function_paraboloid(const std::vector<double>& x) {
+  double sum = 0.0;
+  for (double xi : x) {
+    sum += xi * xi;
+  }
+  return sum;
+}
+
+inline double function_exp_sum(const std::vector<double>& x) {
+  double sum = 0.0;
+  for (double xi : x) {
+    sum += std::exp(xi);
+  }
+  return sum;
+}
+
+inline double function_abs_product(const std::vector<double>& x) {
+  double product = 1.0;
+  for (double xi : x) {
+    product *= std::abs(xi);
+  }
+  return product;
+}
+
+inline double function_log_sum_squares(const std::vector<double>& x) {
+  double sum = 0.0;
+  for (double xi : x) {
+    sum += xi * xi;
+  }
+  return std::log(1 + sum);
+}
+
+class MonteCarloParallelMPI : public ppc::core::Task {
+ public:
+  explicit MonteCarloParallelMPI(std::shared_ptr<ppc::core::TaskData> taskData_,
+                                 std::function<double(const std::vector<double>&)> func_)
+      : Task(std::move(taskData_)), func(std::move(func_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  std::function<double(const std::vector<double>&)> func;
+  int dimensions, numPoints;
+  double precision, globalSum, result = 0.0;
+
+  std::vector<std::pair<double, double>> bounds;
+  std::mt19937 gen;
+  std::vector<std::uniform_real_distribution<double>> distributions;
+  boost::mpi::communicator world;
+};
+
+}  // namespace tyshkevich_a_monte_carlo_mpi
