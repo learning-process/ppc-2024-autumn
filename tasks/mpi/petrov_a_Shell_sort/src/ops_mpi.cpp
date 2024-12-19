d92a9b856e1eb45bfba03c1fa5c72a602e7eab4d
Artem <artemon33005022@yandex.ru>
2024-12-19 03:28:50 +0300

    fix_rev

--- tasks/mpi/petrov_a_Shell_sort/src/ops_mpi.cpp
+++ tasks/mpi/petrov_a_Shell_sort/src/ops_mpi.cpp
@@ -2,7 +2,6 @@
 
 #include <mpi.h>
 
-#include <algorithm>
 #include <cstring>
 #include <iostream>
 #include <vector>
@@ -23,19 +22,23 @@ bool TestTaskMPI::validation() {
   if (taskData->inputs.empty() || taskData->inputs_count.empty()) {
     return false;
   }
-
+  if (taskData->outputs.empty() || taskData->outputs_count.empty()) {
+    return false;
+  }
   return true;
 }
 
 bool TestTaskMPI::run() {
   MPI_Comm comm = MPI_COMM_WORLD;
-  int rank, size;
+  int rank;
+  int size;
 
   MPI_Comm_rank(comm, &rank);
   MPI_Comm_size(comm, &size);
 
   int n = data_.size();
   int local_n = n / size;
+  int remainder = n % size;
   std::vector<int> local_data(local_n);
 
   MPI_Scatter(data_.data(), local_n, MPI_INT, local_data.data(), local_n, MPI_INT, 0, comm);
@@ -54,6 +57,25 @@ bool TestTaskMPI::run() {
   MPI_Gather(local_data.data(), local_n, MPI_INT, data_.data(), local_n, MPI_INT, 0, comm);
 
   if (rank == 0) {
+    if (remainder > 0) {
+      std::vector<int> remainder_data(remainder);
+      std::copy(data_.begin() + local_n * size, data_.end(), remainder_data.begin());
+
+      for (int gap = remainder / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < remainder; ++i) {
+          int temp = remainder_data[i];
+          int j;
+          for (j = i; j >= gap && remainder_data[j - gap] > temp; j -= gap) {
+            remainder_data[j] = remainder_data[j - gap];
+          }
+          remainder_data[j] = temp;
+        }
+      }
+
+      std::merge(data_.begin(), data_.begin() + local_n * size, remainder_data.begin(), remainder_data.end(),
+                 data_.begin());
+    }
+
     for (int gap = n / 2; gap > 0; gap /= 2) {
       for (int i = gap; i < n; ++i) {
         int temp = data_[i];
@@ -73,13 +95,7 @@ bool TestTaskMPI::post_processing() {
   size_t output_size = taskData->outputs_count[0];
   auto* raw_output_data = reinterpret_cast<unsigned char*>(taskData->outputs[0]);
   memcpy(raw_output_data, data_.data(), output_size * sizeof(int));
-  if (taskData->inputs.empty() || taskData->inputs_count.empty()) {
-    return false;
-  }
-
-  if (taskData->outputs.empty() || taskData->outputs_count.empty()) {
-    return true;
-  }
+  return true;
 }
 
 }  // namespace petrov_a_Shell_sort_mpi
