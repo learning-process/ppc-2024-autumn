d725be43e271304fbab473b30b3c68705f22d2b5
myffii <132840123+myffii@users.noreply.github.com>
2024-12-20 18:28:55 +0300

    1

--- tasks/mpi/nasedkin_e_strassen_algorithm/src/ops_mpi.cpp
+++ tasks/mpi/nasedkin_e_strassen_algorithm/src/ops_mpi.cpp
@@ -14,7 +14,7 @@ namespace nasedkin_e_strassen_algorithm_mpi {
 
 bool StrassenAlgorithmSequential::pre_processing() {
   internal_order_test();
-  size_t matrix_size = *reinterpret_cast<size_t*>(taskData->inputs[0]);  // Изменено имя переменной
+  size_t matrix_size = *reinterpret_cast<size_t*>(taskData->inputs[0]);
 
   A_.assign(matrix_size, std::vector<double>(matrix_size, 0.0));
   B_.assign(matrix_size, std::vector<double>(matrix_size, 0.0));
@@ -169,7 +169,11 @@ bool StrassenAlgorithmParallel::pre_processing() {
   displs_b.resize(world.size());
 
   if (world.rank() == 0) {
-    size_t matrix_size = *reinterpret_cast<size_t*>(taskData->inputs[0]);  // Изменено имя переменной
+    if (taskData->inputs.size() < 3 || taskData->inputs_count.size() < 3) {
+      return false;
+    }
+
+    size_t matrix_size = *reinterpret_cast<size_t*>(taskData->inputs[0]);
 
     A_.assign(matrix_size, std::vector<double>(matrix_size, 0.0));
     B_.assign(matrix_size, std::vector<double>(matrix_size, 0.0));
@@ -188,6 +192,12 @@ bool StrassenAlgorithmParallel::pre_processing() {
     calculate_distribution(matrix_size, matrix_size, world.size(), sizes_a, displs_a);
     calculate_distribution(matrix_size, matrix_size, world.size(), sizes_b, displs_b);
   }
+
+  boost::mpi::broadcast(world, sizes_a, 0);
+  boost::mpi::broadcast(world, displs_a, 0);
+  boost::mpi::broadcast(world, sizes_b, 0);
+  boost::mpi::broadcast(world, displs_b, 0);
+
   return true;
 }
 
@@ -209,8 +219,8 @@ bool StrassenAlgorithmParallel::run() {
   internal_order_test();
 
   boost::mpi::broadcast(world, sizes_a, 0);
-  boost::mpi::broadcast(world, sizes_b, 0);
   boost::mpi::broadcast(world, displs_a, 0);
+  boost::mpi::broadcast(world, sizes_b, 0);
   boost::mpi::broadcast(world, displs_b, 0);
   boost::mpi::broadcast(world, n, 0);
 
@@ -252,6 +262,10 @@ bool StrassenAlgorithmParallel::run() {
 bool StrassenAlgorithmParallel::post_processing() {
   internal_order_test();
   if (world.rank() == 0) {
+    if (taskData->outputs.size() < 1) {
+      return false;
+    }
+
     auto* C_output = reinterpret_cast<double*>(taskData->outputs[0]);
     for (size_t i = 0; i < n; ++i) {
       for (size_t j = 0; j < n; ++j) {
@@ -264,7 +278,7 @@ bool StrassenAlgorithmParallel::post_processing() {
 
 void StrassenAlgorithmParallel::calculate_distribution(int rows, int cols, int num_proc, std::vector<int>& sizes, std::vector<int>& displs) {
   sizes.resize(num_proc, 0);
-  displs.resize(num_proc, -1);
+  displs.resize(num_proc, 0);
 
   if (num_proc > rows) {
     for (int i = 0; i < rows; ++i) {
@@ -277,13 +291,10 @@ void StrassenAlgorithmParallel::calculate_distribution(int rows, int cols, int n
 
     int offset = 0;
     for (int i = 0; i < num_proc; ++i) {
-      if (b-- > 0) {
-        sizes[i] = (a + 1) * cols;
-      } else {
-        sizes[i] = a * cols;
-      }
+      sizes[i] = (a + (b > 0 ? 1 : 0)) * cols;
       displs[i] = offset;
       offset += sizes[i];
+      b--;
     }
   }
 }
