447b1e1e0f51e091347b81e68a26e50442963580
andrewmeow <91527374+andrewmeow@users.noreply.github.com>
2024-12-20 22:18:52 +0300

    Add files via upload

--- tasks/mpi/zolotareva_a_count_of_words/src/ops_mpi.cpp
+++ tasks/mpi/zolotareva_a_count_of_words/src/ops_mpi.cpp
@@ -1,12 +1,12 @@
 // Copyright 2023 Nesterov Alexander
+// здесь писать саму задачу
 #include "mpi/zolotareva_a_count_of_words/include/ops_mpi.hpp"
 
 #include <string>
 
 bool zolotareva_a_count_of_words_mpi::TestMPITaskSequential::pre_processing() {
   internal_order_test();
-  input_.assign(reinterpret_cast<char *>(taskData->inputs[0]),
-                taskData->inputs_count[0]);
+  input_.assign(reinterpret_cast<char*>(taskData->inputs[0]), taskData->inputs_count[0]);
   res = 0;
   return true;
 }
@@ -27,14 +27,13 @@ bool zolotareva_a_count_of_words_mpi::TestMPITaskSequential::run() {
       in_word = true;
     }
   }
-  if (in_word)
-    ++res;
+  if (in_word) ++res;
   return true;
 }
 
 bool zolotareva_a_count_of_words_mpi::TestMPITaskSequential::post_processing() {
   internal_order_test();
-  reinterpret_cast<int *>(taskData->outputs[0])[0] = res;
+  reinterpret_cast<int*>(taskData->outputs[0])[0] = res;
   return true;
 }
 
@@ -50,24 +49,20 @@ bool zolotareva_a_count_of_words_mpi::TestMPITaskParallel::pre_processing() {
   internal_order_test();
   int world_size = world.size();
   unsigned int delta;
-  if (world.rank() == 0)
-    delta = taskData->inputs_count[0] / world_size;
+  if (world.rank() == 0) delta = taskData->inputs_count[0] / world_size;
   if (world_size == 1) {
-    local_input_.assign(reinterpret_cast<char *>(taskData->inputs[0]),
-                        taskData->inputs_count[0]);
+    local_input_.assign(reinterpret_cast<char*>(taskData->inputs[0]), taskData->inputs_count[0]);
     return true;
   }
   boost::mpi::broadcast(world, delta, 0);
 
   if (world.rank() == 0) {
-    input_.assign(reinterpret_cast<char *>(taskData->inputs[0]),
-                  taskData->inputs_count[0]);
+    input_.assign(reinterpret_cast<char*>(taskData->inputs[0]), taskData->inputs_count[0]);
     unsigned int str_size = taskData->inputs_count[0];
     unsigned int remainder = str_size % world_size;
     local_input_ = input_.substr(0, remainder + delta);
     for (int proc = 1; proc < world_size; proc++) {
-      world.send(proc, 0, input_.data() + remainder + proc * delta - 1,
-                 delta + 1);
+      world.send(proc, 0, input_.data() + remainder + proc * delta - 1, delta + 1);
     }
   } else {
     local_input_ = std::string(delta + 1, '\0');
@@ -90,13 +85,11 @@ bool zolotareva_a_count_of_words_mpi::TestMPITaskParallel::run() {
       in_word = true;
     }
   }
-  if (in_word)
-    ++local_res;
+  if (in_word) ++local_res;
   if (world.rank() != 0 && local_input_[0] != ' ' && in_word) {
     --local_res;
   }
-  if (world.rank() == (world.size() - 1) && local_input_[0] != ' ' &&
-      !in_word) {
+  if (world.rank() == (world.size() - 1) && local_input_[0] != ' ' && !in_word) {
     --local_res;
   }
   boost::mpi::reduce(world, local_res, res, std::plus(), 0);
@@ -106,7 +99,7 @@ bool zolotareva_a_count_of_words_mpi::TestMPITaskParallel::run() {
 bool zolotareva_a_count_of_words_mpi::TestMPITaskParallel::post_processing() {
   internal_order_test();
   if (world.rank() == 0) {
-    reinterpret_cast<int *>(taskData->outputs[0])[0] = res;
+    reinterpret_cast<int*>(taskData->outputs[0])[0] = res;
   }
   return true;
 }
