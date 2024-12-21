eb66894b554a8c3c16ffebba9ac4c5bf6ebb54ce
Matthewsoul124 <132999776+Matthewsoul124@users.noreply.github.com>
2024-12-21 15:57:10 +0300

    toyota_sequoia

--- tasks/seq/example/perf_tests/main.cpp
+++ tasks/seq/example/perf_tests/main.cpp
@@ -6,6 +6,50 @@
 #include "core/perf/include/perf.hpp"
 #include "seq/example/include/ops_seq.hpp"
 
+TEST(sequential_example_perf_test, test_int_task_task_run) {
+  // Create data
+  std::function<double(const std::vector<double> &)> func = test_func_2;
+  std::vector<double> lower_bounds = {0.0};
+  std::vector<double> upper_bounds = {1.0};
+  std::vector<int> intervals = {100};
+  std::vector<double> out(1, 0.0);
+
+  // Create TaskData
+  auto taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<void *>(&func));
+  taskDataSeq->inputs_count.emplace_back(1);
+  taskDataSeq->inputs.emplace_back(lower_bounds.data());
+  taskDataSeq->inputs_count.emplace_back(lower_bounds.size());
+  taskDataSeq->inputs.emplace_back(upper_bounds.data());
+  taskDataSeq->inputs_count.emplace_back(upper_bounds.size());
+  taskDataSeq->inputs.emplace_back(intervals.data());
+  taskDataSeq->inputs_count.emplace_back(intervals.size());
+  taskDataSeq->outputs.emplace_back(out.data());
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  auto testTaskSequential = std::make_shared<TestTaskSequential>(taskDataSeq);
+
+  // Create Perf attributes
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perfAttr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  // Create and init perf results
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
+  // Create Perf analyzer
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskSequential);
+  perfAnalyzer->task_run(perfAttr, perfResults);
+  ppc::core::Perf::print_perf_statistic(perfResults);
+  ASSERT_TRUE(almost_equal(out[0], 0.5));
+}
+
 TEST(sequential_example_perf_test, test_pipeline_run) {
   const int count = 100;
 
@@ -78,9 +122,4 @@ TEST(sequential_example_perf_test, test_task_run) {
   perfAnalyzer->task_run(perfAttr, perfResults);
   ppc::core::Perf::print_perf_statistic(perfResults);
   ASSERT_EQ(count, out[0]);
-}
-
-int main(int argc, char **argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
 }
