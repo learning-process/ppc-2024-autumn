f52a80912a0afbcbf8493f62335705fde8d03f99
Sasha Zinoviev <kobychit1@gmail.com>
2024-12-23 01:46:56 +0300

    Added new seq test func

--- tasks/seq/zinoviev_a_bellman_ford/func_tests/main.cpp
+++ tasks/seq/zinoviev_a_bellman_ford/func_tests/main.cpp
@@ -111,3 +111,24 @@ TEST(zinoviev_a_bellman_ford, Test_Small_Weights_Graph_seq) {
   std::vector<int> expected = {0, 1, 2, 3};
   ASSERT_EQ(shortest_paths, expected);  // Expected shortest paths
 }
+
+TEST(zinoviev_a_bellman_ford, Test_Large_Weights_Graph_seq) {
+  std::vector<int> graph = {0, 1000000, 0, 0, 0, 0, 2000000, 0, 0, 0, 0, 3000000, 0, 0, 0, 0};
+  std::vector<int> shortest_paths(4, 0);
+
+  std::shared_ptr<ppc::core::TaskData> taskData = std::make_shared<ppc::core::TaskData>();
+  taskData->inputs.emplace_back(reinterpret_cast<uint8_t*>(graph.data()));
+  taskData->inputs_count.emplace_back(4);
+  taskData->inputs_count.emplace_back(3);
+  taskData->outputs.emplace_back(reinterpret_cast<uint8_t*>(shortest_paths.data()));
+  taskData->outputs_count.emplace_back(4);
+
+  zinoviev_a_bellman_ford_seq::BellmanFordSeq task(taskData);
+  ASSERT_EQ(task.validation(), true);
+  task.pre_processing();
+  task.run();
+  task.post_processing();
+
+  std::vector<int> expected = {0, 1000000, 3000000, 6000000};
+  ASSERT_EQ(shortest_paths, expected);  // Expected shortest paths
+}
