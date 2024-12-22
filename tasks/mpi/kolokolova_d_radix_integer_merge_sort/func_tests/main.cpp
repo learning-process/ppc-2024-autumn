ba38628fbb764837c899bab3c7576aaf03ea975a
xinod2000 <49196752+xinod2000@users.noreply.github.com>
2024-12-22 18:46:13 +0300

    Fixing codecove report

--- tasks/mpi/kolokolova_d_radix_integer_merge_sort/func_tests/main.cpp
+++ tasks/mpi/kolokolova_d_radix_integer_merge_sort/func_tests/main.cpp
@@ -15,9 +15,9 @@ std::vector<int> kolokolova_d_radix_integer_merge_sort_mpi::getRandomVector(int 
   std::random_device dev;
   std::mt19937 gen(dev());
   std::vector<int> vec(sz);
-  std::uniform_int_distribution<int> dist(-100, 100);
+  std::uniform_int_distribution<int> dist(-10000, 10000);
   for (int i = 0; i < sz; i++) {
-    vec[i] = gen() % 100;
+    vec[i] = dist(gen);
   }
   return vec;
 }
@@ -121,6 +121,54 @@ TEST(kolokolova_d_radix_integer_merge_sort_mpi, Test_Parallel_Sort2) {
 TEST(kolokolova_d_radix_integer_merge_sort_mpi, Test_Parallel_Sort3) {
   boost::mpi::communicator world;
   int size_vector = 100;
+  std::vector<int> unsorted_vector(size_vector);
+  std::vector<int32_t> sorted_vector(int(unsorted_vector.size()), 0);
+  std::vector<int32_t> result(size_vector);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    unsorted_vector = kolokolova_d_radix_integer_merge_sort_mpi::getRandomVector(size_vector);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(unsorted_vector.data()));
+    taskDataPar->inputs_count.emplace_back(unsorted_vector.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(sorted_vector.data()));
+    taskDataPar->outputs_count.emplace_back(sorted_vector.size());
+  }
+
+  kolokolova_d_radix_integer_merge_sort_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+  ASSERT_TRUE(testMpiTaskParallel.validation());
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+    // Create data
+    std::vector<int> reference_res(size_vector);
+
+    // Create TaskData
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t *>(unsorted_vector.data()));
+    taskDataSeq->inputs_count.emplace_back(unsorted_vector.size());
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t *>(reference_res.data()));
+    taskDataSeq->outputs_count.emplace_back(reference_res.size());
+
+    kolokolova_d_radix_integer_merge_sort_mpi::TestMPITaskSequential testTaskSequential(taskDataSeq);
+
+    ASSERT_TRUE(testTaskSequential.validation());
+    testTaskSequential.pre_processing();
+    testTaskSequential.run();
+    testTaskSequential.post_processing();
+
+    for (int i = 0; i < size_vector; i++) {
+      EXPECT_EQ(sorted_vector[i], reference_res[i]);
+    }
+  }
+}
+
+TEST(kolokolova_d_radix_integer_merge_sort_mpi, Test_Parallel_Sort4) {
+  boost::mpi::communicator world;
+  int size_vector = 71;
   std::vector<int> unsorted_vector(size_vector);
   std::vector<int32_t> sorted_vector(int(unsorted_vector.size()), 0);
   std::vector<int32_t> result(size_vector);
