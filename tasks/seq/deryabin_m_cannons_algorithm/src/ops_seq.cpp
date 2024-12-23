b5a2366a92e7f0b13109e7737c0fcc76867d71ea
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-12-23 06:06:06 +0300

    Update ops_seq.cpp

--- tasks/seq/deryabin_m_cannons_algorithm/src/ops_seq.cpp
+++ tasks/seq/deryabin_m_cannons_algorithm/src/ops_seq.cpp
@@ -12,7 +12,7 @@ bool deryabin_m_cannons_algorithm_seq::CannonsAlgorithmTaskSequential::pre_proce
 
 bool deryabin_m_cannons_algorithm_seq::CannonsAlgorithmTaskSequential::validation() {
   internal_order_test();
-  return (taskData->inputs[0])[0].size() == (taskData->inputs[1])[0].size() && taskData->outputs_count[0] == 1;
+  return (taskData->inputs[0])[0].size() == (taskData->inputs[1])[0].size() == pow((unsigned short)sqrt(input_matrix_A.size()), 2) && taskData->outputs_count[0] == 1;
 }
 
 bool deryabin_m_cannons_algorithm_seq::CannonsAlgorithmTaskSequential::run() {
@@ -20,7 +20,7 @@ bool deryabin_m_cannons_algorithm_seq::CannonsAlgorithmTaskSequential::run() {
   unsigned short i = 0;
   unsigned short j = 0;
   unsigned short count = 0;
-  auto dimension = (unsigned short)(sqrt(input_matrix_A.size()));
+  auto dimension = (unsigned short)sqrt(input_matrix_A.size());
   while (i != dimension) {
       while (j != dimension) {
               while (count != dimension) {
