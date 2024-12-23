87208f013d8374bc0c4b72210c4a4281c3bdaea9
Artem <artemon33005022@yandex.ru>
2024-12-23 19:03:08 +0300

    fix again

--- tasks/mpi/petrov_a_Shell_sort/src/ops_mpi.cpp
+++ tasks/mpi/petrov_a_Shell_sort/src/ops_mpi.cpp
@@ -22,7 +22,8 @@ bool TestTaskMPI::validation() {
 }
 
 bool TestTaskMPI::pre_processing() {
-  int world_size, world_rank;
+  int world_size;
+  int world_rank;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
