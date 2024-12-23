51f8bf8482dfcf7d323a93109b5151fc800c2257
dssss12 <125500790+dssss12@users.noreply.github.com>
2024-12-24 01:55:07 +0300

    Volkswagen. Das auto.

--- tasks/seq/sorochkin_d_matrix_col_min/include/ops_seq.hpp
+++ tasks/seq/sorochkin_d_matrix_col_min/include/ops_seq.hpp
@@ -8,15 +8,15 @@ namespace sorochkin_d_matrix_col_min_seq {
 
 class TestTaskSequential : public ppc::core::Task {
  public:
-  explicit TestTaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_)
-      : Task(std::move(taskData_)), rows_(0), cols_(0) {}
+  explicit TestTaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
   bool pre_processing() override;
   bool validation() override;
   bool run() override;
   bool post_processing() override;
 
  private:
-  size_t rows_, cols_;
+  size_t rows_ = 0;
+  size_t cols_ = 0;
   std::vector<int> input_, res_;
 };
 
