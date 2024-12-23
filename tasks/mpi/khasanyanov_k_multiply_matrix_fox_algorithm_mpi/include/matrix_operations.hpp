4589c834165a13b1e57f3968a40aa22b4d4267d7
Kirill Khasanyanov <hasanyanovk2@mail.ru>
2024-12-23 15:13:46 +0000

    add seq version & perf tests for it

--- tasks/mpi/khasanyanov_k_multiply_matrix_fox_algorithm_mpi/include/matrix_operations.hpp
+++ tasks/mpi/khasanyanov_k_multiply_matrix_fox_algorithm_mpi/include/matrix_operations.hpp
@@ -4,6 +4,7 @@
 #include <random>
 #include <stdexcept>
 
+#include "../modules/core/task/include/task.hpp"
 #include "matrix.hpp"
 
 namespace khasanyanov_k_fox_algorithm {
@@ -51,6 +52,59 @@ matrix<T> MatrixOperations::multiply(const matrix<T>& lhs, const matrix<T>& rhs)
     }
   }
   return res;
+}
+
+template <typename DataType>
+class MatrixMultiplication : public ppc::core::Task {
+  matrix<DataType> A, B, C;
+
+ public:
+  explicit MatrixMultiplication(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool validation() override;
+  bool pre_processing() override;
+  bool run() override;
+  bool post_processing() override;
+};
+
+template <typename DataType>
+bool MatrixMultiplication<DataType>::validation() {
+  internal_order_test();
+  return taskData->inputs.size() == 2 && taskData->inputs_count.size() == 6 &&
+         taskData->inputs_count[0] * taskData->inputs_count[1] == taskData->inputs_count[2] &&
+         taskData->inputs_count[3] * taskData->inputs_count[4] == taskData->inputs_count[5] &&
+         taskData->inputs_count[1] == taskData->inputs_count[3] && !taskData->outputs.empty() &&
+         taskData->outputs_count.size() == 3 && taskData->outputs_count[0] == taskData->inputs_count[0] &&
+         taskData->outputs_count[1] == taskData->inputs_count[4] &&
+         taskData->outputs_count[0] * taskData->outputs_count[1] == taskData->outputs_count[2];
+}
+
+template <typename DataType>
+bool MatrixMultiplication<DataType>::pre_processing() {
+  internal_order_test();
+  auto* a = reinterpret_cast<DataType*>(taskData->inputs[0]);
+  auto* b = reinterpret_cast<DataType*>(taskData->inputs[1]);
+  A.data.assign(a, a + taskData->inputs_count[2]);
+  B.data.assign(b, b + taskData->inputs_count[5]);
+  A.rows = taskData->inputs_count[0];
+  A.columns = taskData->inputs_count[1];
+  B.rows = taskData->inputs_count[3];
+  B.columns = taskData->inputs_count[4];
+  C = matrix<DataType>(A.rows, B.columns);
+  return true;
+}
+
+template <typename DataType>
+bool MatrixMultiplication<DataType>::run() {
+  internal_order_test();
+  C = MatrixOperations::multiply(A, B);
+  return true;
+}
+
+template <typename DataType>
+bool MatrixMultiplication<DataType>::post_processing() {
+  internal_order_test();
+  std::copy(C.begin(), C.end(), reinterpret_cast<DataType*>(taskData->outputs[0]));
+  return true;
 }
 
 }  // namespace khasanyanov_k_fox_algorithm
