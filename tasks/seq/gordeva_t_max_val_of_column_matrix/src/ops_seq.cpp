d37a072c155cbadde4feffc7e5b2bb7293e41cfd
TayaGordeeva <tauona.g2004@gmail.com>
2024-12-24 17:09:55 +0300

    end_oh

--- tasks/seq/gordeva_t_max_val_of_column_matrix/src/ops_seq.cpp
+++ tasks/seq/gordeva_t_max_val_of_column_matrix/src/ops_seq.cpp
@@ -25,10 +25,10 @@ bool gordeva_t_max_val_of_column_matrix_seq::TestTaskSequential::pre_processing(
 bool gordeva_t_max_val_of_column_matrix_seq::TestTaskSequential::validation() {
   internal_order_test();
 
-  if ((taskData->inputs.empty() || taskData->outputs.empty()) ||
-      (taskData->inputs_count[0] <= 0 || taskData->inputs_count[1] <= 0) || (taskData->outputs_count.size() != 1) ||
-      (taskData->inputs_count.size() < 2) || (taskData->outputs_count[0] != taskData->inputs_count[1]))
-    return false;
+  if (taskData->outputs_count.size() != 1) return false; (taskData->inputs_count.size() < 2) return false;
+  if (taskData->inputs_count[0] <= 0 || taskData->inputs_count[1] <= 0) return false;
+  if (taskData->outputs_count[0] != taskData->inputs_count[1]) return false;
+  if (taskData->inputs.empty() || taskData->outputs.empty()) return false;
 
   return true;
 }
