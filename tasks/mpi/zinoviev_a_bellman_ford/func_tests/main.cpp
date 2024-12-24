77e0847ffbb324e03f021b30db26e3c31cea3e99
Sasha Zinoviev <kobychit1@gmail.com>
2024-12-25 00:40:31 +0300

    Random fix static

--- tasks/mpi/zinoviev_a_bellman_ford/func_tests/main.cpp
+++ tasks/mpi/zinoviev_a_bellman_ford/func_tests/main.cpp
@@ -3,8 +3,8 @@
 
 #include <boost/mpi/communicator.hpp>
 #include <boost/mpi/environment.hpp>
-#include <vector>
 #include <random>
+#include <vector>
 
 #include "mpi/zinoviev_a_bellman_ford/include/ops_mpi.hpp"
 
