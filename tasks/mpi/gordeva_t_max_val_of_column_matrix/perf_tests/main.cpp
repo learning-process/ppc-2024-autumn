be6a2beecbb2154226cb40f28d51407748629ae8
TayaGordeeva <tauona.g2004@gmail.com>
2024-12-24 11:33:25 +0300

    l1

--- tasks/mpi/gordeva_t_max_val_of_column_matrix/perf_tests/main.cpp
+++ tasks/mpi/gordeva_t_max_val_of_column_matrix/perf_tests/main.cpp
@@ -13,8 +13,8 @@ TEST(gordeva_t_max_val_of_column_matrix_mpi, test_pipeline_run) {
   std::vector<int32_t> max_s;
 
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
-  int rows = 5000;
-  int cols = 5000;
+  size_t rows = 5000;
+  size_t cols = 5000;
 
   if (world.rank() == 0) {
     global_matr = gordeva_t_max_val_of_column_matrix_mpi::TestMPITaskSequential::rand_matr(rows, cols);
@@ -33,13 +33,38 @@ TEST(gordeva_t_max_val_of_column_matrix_mpi, test_pipeline_run) {
 
   ASSERT_EQ(testMpiTaskParallel->validation(), true);
   testMpiTaskParallel->pre_processing();
+
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 100;
+  const boost::mpi::timer current_timer;
+  perfAttr->current_timer = [&] { return current_timer.elapsed(); };
+
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
   testMpiTaskParallel->run();
   testMpiTaskParallel->post_processing();
 
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testMpiTaskParallel);
+  perfAnalyzer->pipeline_run(perfAttr, perfResults);
+
   if (world.rank() == 0) {
-    for (size_t j = 0; j < max_s.size(); ++j) {
-      ASSERT_EQ(max_s[j], 200);
+    std::vector<int> max_example(cols, INT_MIN);
+
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    for (unsigned int i = 0; i < global_matr.size(); i++) {
+      taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_matr[i].data()));
     }
+    taskDataSeq->inputs_count = {rows, cols};
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(max_example.data()));
+    taskDataSeq->outputs_count.emplace_back(max_example.size());
+    gordeva_t_max_val_of_column_matrix_mpi::TestMPITaskSequential testMpiTaskSequential(taskDataSeq);
+
+    ASSERT_EQ(testMpiTaskSequential.validation(), true);
+    testMpiTaskSequential.pre_processing();
+    testMpiTaskSequential.run();
+    testMpiTaskSequential.post_processing();
+    ASSERT_EQ(global_max, max_example);
+    ppc::core::Perf::print_perf_statistic(perfResults);
   }
 }
 
@@ -74,8 +99,22 @@ TEST(gordeva_t_max_val_of_column_matrix_mpi, test_task_run) {
   testMpiTaskParallel->post_processing();
 
   if (world.rank() == 0) {
-    for (size_t j = 0; j < max_s.size(); ++j) {
-      ASSERT_EQ(max_s[j], 200);
+    std::vector<int> max_example(cols, INT_MIN);
+
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    for (unsigned int i = 0; i < global_matr.size(); i++) {
+      taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(global_matr[i].data()));
     }
+    taskDataSeq->inputs_count = {rows, cols};
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(max_example.data()));
+    taskDataSeq->outputs_count.emplace_back(max_example.size());
+    gordeva_t_max_val_of_column_matrix_mpi::TestMPITaskSequential testMpiTaskSequential(taskDataSeq);
+
+    ASSERT_EQ(testMpiTaskSequential.validation(), true);
+    testMpiTaskSequential.pre_processing();
+    testMpiTaskSequential.run();
+    testMpiTaskSequential.post_processing();
+    ASSERT_EQ(global_max, max_example);
+    ppc::core::Perf::print_perf_statistic(perfResults);
   }
 }
