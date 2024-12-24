55164612f454a0db34f8ed06d1305cc2f8bbb035
Budazhapova_e <ka10nnn@yandex.ru>
2024-12-25 02:33:51 +0300

    seq2

---
+++ tasks/seq/budazhapova_betcher/include/odd_even_merge.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include <memory>
+#include <numeric>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace budazhapova_betcher_odd_even_merge_seq {
+
+	class MergeSequential : public ppc::core::Task {
+	public:
+		explicit MergeSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+		bool pre_processing() override;
+		bool validation() override;
+		bool run() override;
+		bool post_processing() override;
+
+	private:
+		std::vector<int> res;
+	};
+}
