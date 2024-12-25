6af01b6d62aca359518e0f1d0a96dcfd6d41291c
shyravina19102004 <shyravina19102004@gmail.com>
2024-12-25 18:54:29 +0300

    jarvis3

--- tasks/mpi/shuravina_o_jarvis_pass/func_tests/main.cpp
+++ tasks/mpi/shuravina_o_jarvis_pass/func_tests/main.cpp
@@ -104,7 +104,8 @@ TEST(shuravina_o_jarvis_pass, Test_Empty_Points) {
   global_hull = jarvis_mpi.get_hull();
 
   if (rank == 0) {
-    EXPECT_EQ(global_hull.size(), size_t(0));
+    std::vector<Point> seq_hull = jarvis_march(global_points);
+    EXPECT_EQ(global_hull.size(), seq_hull.size());
   }
 }
 
@@ -155,82 +156,6 @@ TEST(shuravina_o_jarvis_pass, Test_Triangle_Points) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   std::vector<Point> global_points = {Point(0, 0), Point(3, 0), Point(1, 4)};
-  std::vector<Point> global_hull;
-
-  JarvisPassMPI jarvis_mpi(global_points);
-  ASSERT_TRUE(jarvis_mpi.validation());
-  jarvis_mpi.run();
-  global_hull = jarvis_mpi.get_hull();
-
-  if (rank == 0) {
-    std::vector<Point> seq_hull = jarvis_march(global_points);
-    ASSERT_EQ(global_hull.size(), seq_hull.size());
-    for (size_t i = 0; i < global_hull.size(); i++) {
-      EXPECT_EQ(global_hull[i], seq_hull[i]);
-    }
-  }
-}
-
-TEST(shuravina_o_jarvis_pass, Test_Single_Point) {
-  int rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-  std::vector<Point> global_points = {Point(1, 1)};
-  std::vector<Point> global_hull;
-
-  JarvisPassMPI jarvis_mpi(global_points);
-  ASSERT_FALSE(jarvis_mpi.validation());
-  jarvis_mpi.run();
-  global_hull = jarvis_mpi.get_hull();
-
-  if (rank == 0) {
-    EXPECT_EQ(global_hull.size(), size_t(0));
-  }
-}
-
-TEST(shuravina_o_jarvis_pass, Test_Two_Points) {
-  int rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-  std::vector<Point> global_points = {Point(0, 0), Point(1, 1)};
-  std::vector<Point> global_hull;
-
-  JarvisPassMPI jarvis_mpi(global_points);
-  ASSERT_FALSE(jarvis_mpi.validation());
-  jarvis_mpi.run();
-  global_hull = jarvis_mpi.get_hull();
-
-  if (rank == 0) {
-    EXPECT_EQ(global_hull.size(), size_t(0));
-  }
-}
-
-TEST(shuravina_o_jarvis_pass, Test_Negative_Coordinates) {
-  int rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-  std::vector<Point> global_points = {Point(-1, -1), Point(-2, -2), Point(0, 0), Point(1, 1), Point(2, 2)};
-  std::vector<Point> global_hull;
-
-  JarvisPassMPI jarvis_mpi(global_points);
-  ASSERT_TRUE(jarvis_mpi.validation());
-  jarvis_mpi.run();
-  global_hull = jarvis_mpi.get_hull();
-
-  if (rank == 0) {
-    std::vector<Point> seq_hull = jarvis_march(global_points);
-    ASSERT_EQ(global_hull.size(), seq_hull.size());
-    for (size_t i = 0; i < global_hull.size(); i++) {
-      EXPECT_EQ(global_hull[i], seq_hull[i]);
-    }
-  }
-}
-
-TEST(shuravina_o_jarvis_pass, Test_Large_Coordinates) {
-  int rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-  std::vector<Point> global_points = {Point(1000000, 1000000), Point(2000000, 2000000), Point(0, 0)};
   std::vector<Point> global_hull;
 
   JarvisPassMPI jarvis_mpi(global_points);
