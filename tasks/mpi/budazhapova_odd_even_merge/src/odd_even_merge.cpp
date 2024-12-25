5cad0cf075c7bd09a6f37c8747fd9a988af58fe1
Budazhapova_e <ka10nnn@yandex.ru>
2024-12-25 16:50:59 +0300

    wrong data transmisson

--- tasks/mpi/budazhapova_odd_even_merge/src/odd_even_merge.cpp
+++ tasks/mpi/budazhapova_odd_even_merge/src/odd_even_merge.cpp
@@ -160,7 +160,9 @@ bool budazhapova_betcher_odd_even_merge_mpi::MergeParallel::run() {
     if (world.rank() < res_size) {
       local_res.resize(1);
       for (int i = start; i < end; i++) {
-        local_res[(i - start)] = res[i];
+        for (int j = 0; j < ; j++) {
+          local_res[(i - start) * n_of_send_elements + j] = res[i * n_of_send_elements + j];
+        }
       }
     } else {
       local_res.clear();
@@ -169,7 +171,9 @@ bool budazhapova_betcher_odd_even_merge_mpi::MergeParallel::run() {
   } else {
     local_res.resize(end - start);
     for (int i = start; i < end; i++) {
-      local_res[i - start] = res[i];
+      for (int j = 0; j < ; j++) {
+        local_res[(i - start) * n_of_send_elements + j] = res[i * n_of_send_elements + j];
+      }
     }
   }
 
