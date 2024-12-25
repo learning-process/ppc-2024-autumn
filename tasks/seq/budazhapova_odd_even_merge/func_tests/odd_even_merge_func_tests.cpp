371d269abbc87ca19da3f4824b9fc2212aa1d248
Budazhapova_e <ka10nnn@yandex.ru>
2024-12-25 03:42:10 +0300

    wrong using genrandvec

--- tasks/seq/budazhapova_odd_even_merge/func_tests/odd_even_merge_func_tests.cpp
+++ tasks/seq/budazhapova_odd_even_merge/func_tests/odd_even_merge_func_tests.cpp
@@ -40,7 +40,7 @@ TEST(budazhapova_betcher_odd_even_merge_seq, ordinary_test) {
 }
 
 TEST(budazhapova_betcher_odd_even_merge_seq, random_vector_test) {
-  std::vector<int> input_vector.generateRandomVector(100, 5, 100);
+  std::vector<int> input_vector = generateRandomVector(100, 5, 100);
   std::vector<int> out(100, 0);
 
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
@@ -57,7 +57,7 @@ TEST(budazhapova_betcher_odd_even_merge_seq, random_vector_test) {
 }
 
 TEST(budazhapova_betcher_odd_even_merge_seq, validation_test) {
-  std::vector<int> input_vector.generateRandomVector = {};
+  std::vector<int> input_vector = {};
   std::vector<int> out(1, 0);
 
   std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
