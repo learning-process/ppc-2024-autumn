4c3b9c441725bd25830da25b408f0f02e964a02f
MikasaAkc <daria.komshina@mail.ru>
2024-12-25 06:12:46 +0300

    tasks

--- tasks/mpi/komshina_d_sort_radius_for_real_numbers_with_simple_merge/include/ops_mpi.hpp
+++ tasks/mpi/komshina_d_sort_radius_for_real_numbers_with_simple_merge/include/ops_mpi.hpp
@@ -44,4 +44,6 @@ class TestMPITaskParallel : public ppc::core::Task {
   boost::mpi::communicator world;
   void execute_merge(std::vector<double>& local_data, int rank, int total_processes);
 };
+void SortDoubleByBits(std::vector<double>& data);
+
 }  // namespace komshina_d_sort_radius_for_real_numbers_with_simple_merge_seq_mpi
