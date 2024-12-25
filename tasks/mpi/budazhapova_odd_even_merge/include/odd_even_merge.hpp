35ac61ccba7752ccdecb992aabe00f73282a8982
Budazhapova_e <ka10nnn@yandex.ru>
2024-12-25 04:24:47 +0300

    add mpi

---
+++ tasks/mpi/budazhapova_odd_even_merge/include/odd_even_merge.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include <boost/mpi/collectives.hpp>
+#include <boost/mpi/communicator.hpp>
+#include <memory>
+#include <numeric>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace budazhapova_betcher_odd_even_merge_mpi {
+
+	class MergeSequential : public ppc::core::Task {
+	public:
+		explicit MergeSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+		bool pre_processing() override;
+		bool validation() override;
+		bool run() override;
+		bool post_processing() override;
+
+	private:
+		std::vector<int> res;
+		std::vector<int> local_res;
+	};
+	class MergeParallel : public ppc::core::Task {
+	public:
+		explicit MergeParallel(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+		bool pre_processing() override;
+		bool validation() override;
+		bool run() override;
+		bool post_processing() override;
+
+	private:
+		std::vector<int> res;
+		std::vector<int> local_res;
+
+		boost::mpi::communicator world;
+	};
+}
---
+++ tasks/mpi/budazhapova_odd_even_merge/perf_tests/odd_merge_perf_tests.cpp
@@ -0,0 +1,85 @@
+#include <gtest/gtest.h>
+
+#include "core/perf/include/perf.hpp"
+#include "mpi/budazhapova_odd_even_merge/include/odd_even_merge.hpp"
+
+namespace budazhapova_betcher_odd_even_merge_mpi {
+	std::vector<int> generateRandomVector(int size, int minValue, int maxValue) {
+		std::vector<int> randomVector;
+		randomVector.reserve(size);
+		std::srand(static_cast<unsigned int>(std::time(nullptr)));
+		for (int i = 0; i < size; ++i) {
+			int randomNum = std::rand() % (maxValue - minValue + 1) + minValue;
+			randomVector.push_back(randomNum);
+		}
+		return randomVector;
+	}
+}
+
+TEST(budazhapova_betcher_odd_even_merge_mpi, test_pipeline_run) {
+	boost::mpi::communicator world;
+	std::vector<int> input_vector = budazhapova_betcher_odd_even_merge_mpi::generateRandomVector(10000000, 5, 100);
+	std::vector<int> out(10000000, 0);
+
+	std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+	if (world.rank() == 0) {
+		taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_vector.data()));
+		taskDataPar->inputs_count.emplace_back(input_vector.size());
+		taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
+		taskDataPar->outputs_count.emplace_back(out.size());
+	}
+
+	auto testMpiTaskParallel = std::make_shared<budazhapova_betcher_odd_even_merge_mpi::MergeParallel>(taskDataPar);
+	ASSERT_EQ(testMpiTaskParallel->validation(), true);
+	testMpiTaskParallel->pre_processing();
+	testMpiTaskParallel->run();
+	testMpiTaskParallel->post_processing();
+
+	auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+	perfAttr->num_running = 10;
+	const boost::mpi::timer current_timer;
+	perfAttr->current_timer = [&] { return current_timer.elapsed(); };
+
+	auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
+	auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testMpiTaskParallel);
+	perfAnalyzer->pipeline_run(perfAttr, perfResults);
+	if (world.rank() == 0) {
+		ppc::core::Perf::print_perf_statistic(perfResults);
+	}
+}
+
+TEST(budazhapova_betcher_odd_even_merge_mpi, test_task_run) {
+	boost::mpi::communicator world;
+	std::vector<int> input_vector = budazhapova_betcher_odd_even_merge_mpi::generateRandomVector(10000000, 5, 100);
+	std::vector<int> out(10000000, 0);
+
+	std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+	if (world.rank() == 0) {
+		taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(input_vector.data()));
+		taskDataPar->inputs_count.emplace_back(input_vector.size());
+		taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
+		taskDataPar->outputs_count.emplace_back(out.size());
+	}
+
+	auto testMpiTaskParallel = std::make_shared<budazhapova_betcher_odd_even_merge_mpi::MergeParallel>(taskDataPar);
+	ASSERT_EQ(testMpiTaskParallel->validation(), true);
+	testMpiTaskParallel->pre_processing();
+	testMpiTaskParallel->run();
+	testMpiTaskParallel->post_processing();
+
+	auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+	perfAttr->num_running = 10;
+	const boost::mpi::timer current_timer;
+	perfAttr->current_timer = [&] { return current_timer.elapsed(); };
+
+	auto perfResults = std::make_shared<ppc::core::PerfResults>();
+
+	auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testMpiTaskParallel);
+	perfAnalyzer->task_run(perfAttr, perfResults);
+	if (world.rank() == 0) {
+		ppc::core::Perf::print_perf_statistic(perfResults);
+	}
+}
