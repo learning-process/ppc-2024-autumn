6d775ae09fc00d37443691517564ea510b263ca9
Mikhail Burykin <whitecrazygoose@mail.ru>
2024-12-25 23:15:44 +0300

    added x1 < x0 tests

--- tasks/mpi/burykin_m_strongin/func_tests/main.cpp
+++ tasks/mpi/burykin_m_strongin/func_tests/main.cpp
@@ -58,3 +58,56 @@ TEST(burykin_m_strongin_MPI, TestNumber1) {
     ASSERT_NEAR(ref_res, res, 0.2);
   }
 }
+
+TEST(burykin_m_strongin_MPI, TestNumber2) {
+  boost::mpi::communicator world;
+  double x0 = 1000.0;
+  double x1 = 1.0;
+  double eps = 0.01;
+  double res = 0;
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  if (world.rank() == 0) {
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&x0));
+    taskDataPar->inputs_count.emplace_back(1);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&x1));
+    taskDataPar->inputs_count.emplace_back(1);
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&eps));
+    taskDataPar->inputs_count.emplace_back(1);
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(&res));
+    taskDataPar->outputs_count.emplace_back(1);
+  }
+
+  auto fn = [](double x) { return x * x; };
+  burykin_m_strongin::StronginParallel testMpiTaskParallel(taskDataPar, fn);
+  ASSERT_EQ(testMpiTaskParallel.validation(), true);
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+    // Create data
+    double ref_res = 0;
+
+    // Create TaskData
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&x0));
+    taskDataSeq->inputs_count.emplace_back(1);
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&x1));
+    taskDataSeq->inputs_count.emplace_back(1);
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&eps));
+    taskDataSeq->inputs_count.emplace_back(1);
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(&ref_res));
+    taskDataSeq->outputs_count.emplace_back(1);
+
+    // Create Task
+    burykin_m_strongin::StronginSequential testMpiTaskSequential(taskDataSeq, fn);
+    ASSERT_EQ(testMpiTaskSequential.validation(), true);
+    testMpiTaskSequential.pre_processing();
+    testMpiTaskSequential.run();
+    testMpiTaskSequential.post_processing();
+
+    ASSERT_NEAR(ref_res, res, 0.2);
+  }
+}
