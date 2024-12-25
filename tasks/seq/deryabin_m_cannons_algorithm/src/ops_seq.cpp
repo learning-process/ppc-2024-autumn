5f6995f6709cffe439b8dc777f5344ed71e506c0
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-12-25 14:27:52 +0300

    Update ops_seq.cpp

--- tasks/seq/deryabin_m_cannons_algorithm/src/ops_seq.cpp
+++ tasks/seq/deryabin_m_cannons_algorithm/src/ops_seq.cpp
@@ -12,9 +12,9 @@ bool deryabin_m_cannons_algorithm_seq::CannonsAlgorithmTaskSequential::pre_proce
 
 bool deryabin_m_cannons_algorithm_seq::CannonsAlgorithmTaskSequential::validation() {
   internal_order_test();
-  return (taskData->inputs[0])[0].size() == (taskData->inputs[1])[0].size() ==
-             pow((unsigned short)sqrt((taskData->inputs[0])[0].size()), 2) &&
-         taskData->outputs_count[0] == 1;
+  return (taskData->inputs[0])[0].size() == (taskData->inputs[1])[0].size() &&
+         (taskData->inputs[1])[0].size() == pow((unsigned short)sqrt((taskData->inputs[0])[0].size()), 2) &&
+          taskData->outputs_count[0] == 1;
 }
 
 bool deryabin_m_cannons_algorithm_seq::CannonsAlgorithmTaskSequential::run() {
