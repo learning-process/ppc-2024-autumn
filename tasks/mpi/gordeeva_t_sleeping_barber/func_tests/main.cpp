aed037a4ff9e494502e895a6655ae9921b79330b
TayaGordeeva <tauona.g2004@gmail.com>
2024-12-26 18:28:37 +0300

    2

--- tasks/mpi/gordeeva_t_sleeping_barber/func_tests/main.cpp
+++ tasks/mpi/gordeeva_t_sleeping_barber/func_tests/main.cpp
@@ -168,33 +168,3 @@ TEST(gordeeva_t_sleeping_barber_mpi, Test_End_To_End4) {
     }
   }
 }
-
-TEST(gordeeva_t_sleeping_barber_mpi, Test_End_To_End5) {
-  boost::mpi::communicator world;
-
-  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
-
-  const int max_waiting_chairs = 1024;
-  int global_res = -1;
-
-  taskDataPar->inputs_count.emplace_back(max_waiting_chairs);
-  taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(&global_res));
-  taskDataPar->outputs_count.emplace_back(sizeof(global_res));
-
-  gordeeva_t_sleeping_barber_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
-
-  if (world.size() < 3) {
-    ASSERT_FALSE(testMpiTaskParallel.validation());
-  } else {
-    ASSERT_TRUE(testMpiTaskParallel.validation());
-    ASSERT_TRUE(testMpiTaskParallel.pre_processing());
-    ASSERT_TRUE(testMpiTaskParallel.run());
-    ASSERT_TRUE(testMpiTaskParallel.post_processing());
-
-    world.barrier();
-
-    if (world.rank() == 0) {
-      ASSERT_EQ(global_res, 0);
-    }
-  }
-}
