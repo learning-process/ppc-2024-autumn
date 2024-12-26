bbe05d1716cc8e8f62c9f1a1cf6ac3af23d9b699
KADCHDR <125730153+KADCHDR@users.noreply.github.com>
2024-12-26 04:23:46 +0300

    fix for allnesSeq

--- tasks/seq/kovalchuk_a_max_of_vector_elements/src/ops_seq.cpp
+++ tasks/seq/kovalchuk_a_max_of_vector_elements/src/ops_seq.cpp
@@ -27,24 +27,25 @@ bool kovalchuk_a_max_of_vector_elements_seq::TestSequentialTask::pre_processing(
 bool kovalchuk_a_max_of_vector_elements_seq::TestSequentialTask::validation() {
   internal_order_test();
   // Check count elements of output
-  return taskData->outputs_count[0] == 1;
+  if (taskData->outputs_count[0] != 1) {
+    return false;
+  }
+  // Check that all vectors are not empty
+  for (const auto& vec : input_) {
+    if (vec.empty()) {
+      return false;
+    }
+  }
+  return true;
 }
 
 bool kovalchuk_a_max_of_vector_elements_seq::TestSequentialTask::run() {
   internal_order_test();
-  if (!input_.empty()) {
-    std::vector<int> local_res(input_.size());
-    for (unsigned int i = 0; i < input_.size(); i++) {
-      if (!input_[i].empty()) {
-        local_res[i] = *std::max_element(input_[i].begin(), input_[i].end());
-      } else {
-        local_res[i] = INT_MIN;
-      }
-    }
-    res_ = *std::max_element(local_res.begin(), local_res.end());
-  } else {
-    res_ = INT_MIN;
+  std::vector<int> local_res(input_.size());
+  for (unsigned int i = 0; i < input_.size(); i++) {
+    local_res[i] = *std::max_element(input_[i].begin(), input_[i].end());
   }
+  res_ = *std::max_element(local_res.begin(), local_res.end());
   return true;
 }
 
