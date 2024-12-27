1dc88545e986a116172c98fda7f705a6f51f8226
KorneevaEkaterina <e.korneeva1917@gmail.com>
2024-12-27 05:36:28 +0300

    some changes

--- tasks/mpi/korneeva_e_rectangular_integration_method/func_tests/main.cpp
+++ tasks/mpi/korneeva_e_rectangular_integration_method/func_tests/main.cpp
@@ -231,7 +231,7 @@ TEST(korneeva_e_rectangular_integration_method_mpi, TrigonometricDoubleIntegralO
 }
 TEST(korneeva_e_rectangular_integration_method_mpi, TrigonometricTripleIntegralOneVariable) {
   korneeva_e_rectangular_integration_method_mpi::customTest(
-      korneeva_e_rectangular_integration_method_mpi::trigonometricSingleVar, {{2, 3}, {2, 3}, {2, 3}}, 1e-4);
+      korneeva_e_rectangular_integration_method_mpi::trigonometricSingleVar, {{0.1, 0.2}, {0.1, 0.2}, {0.1, 0.2}}, 1e-4);
 }
 
 TEST(korneeva_e_rectangular_integration_method_mpi, TrigonometricDoubleIntegralTwoVariables) {
