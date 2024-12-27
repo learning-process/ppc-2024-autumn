dfada0d6d0022676af67914fa568e70ce3765195
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-12-27 10:37:26 +0300

    Update ops_mpi.cpp

--- tasks/mpi/deryabin_m_cannons_algorithm/src/ops_mpi.cpp
+++ tasks/mpi/deryabin_m_cannons_algorithm/src/ops_mpi.cpp
@@ -195,27 +195,31 @@ bool deryabin_m_cannons_algorithm_mpi::CannonsAlgorithmMPITaskParallel::run() {
     }
     unsigned short p = 0;
     while (p != block_rows_columns) {
-      if (world.rank() % block_rows_columns == 0) {
-        world.send(world.rank() + block_rows_columns - 1, 2, local_input_matrix_A.data(), block_dimension * block_dimension);
-      } else {
-        world.send(world.rank() - 1, 2, local_input_matrix_A.data(), block_dimension * block_dimension);
-      }
-      if (world.rank() < block_rows_columns) {
-        world.send(world.rank() + block_rows_columns * (block_rows_columns - 1), 3, local_input_matrix_B.data(),
-                   block_dimension * block_dimension);
-      } else {
-        world.send(world.rank() - block_rows_columns, 3, local_input_matrix_B.data(), block_dimension * block_dimension);
-      }
-      if ((world.rank() + 1) % block_rows_columns == 0) {
-        world.recv(world.rank() - block_rows_columns + 1, 2, local_input_matrix_A.data(), block_dimension * block_dimension);
-      } else {
-        world.recv(world.rank() + 1, 2, local_input_matrix_A.data(), block_dimension * block_dimension);
-      }
-      if (world.rank() >= block_rows_columns * (block_rows_columns - 1)) {
-        world.recv(world.rank() - block_rows_columns * (block_rows_columns - 1), 3, local_input_matrix_B.data(),
-                   block_dimension * block_dimension);
-      } else {
-        world.recv(world.rank() + block_rows_columns, 3, local_input_matrix_B.data(), block_dimension * block_dimension);
+      world.sendrecv_replace(local_input_matrix_A.data(), block_dimension * block_dimension, (world.rank() % block_rows_columns == 0) ? (world.rank() + block_rows_columns - 1) : (world.rank() - 1), 2,
+                ((world.rank() + 1) % block_rows_columns == 0) ? (world.rank() - block_rows_columns + 1) : (world.rank() + 1), 2, world, boost::mpi::status());
+      world.sendrecv_replace(local_input_matrix_B.data(), block_dimension * block_dimension, (world.rank() < block_rows_columns) ? (world.rank() + block_rows_columns * (block_rows_columns - 1)) : (world.rank() - block_rows_columns), 3,
+                (world.rank() >= block_rows_columns * (block_rows_columns - 1)) ? (world.rank() - block_rows_columns * (block_rows_columns - 1)) : (world.rank() + block_rows_columns), 3, world, boost::mpi::status());
+      //if (world.rank() % block_rows_columns == 0) {
+        //world.send(world.rank() + block_rows_columns - 1, 2, local_input_matrix_A.data(), block_dimension * block_dimension);
+      //} else {
+        //world.send(world.rank() - 1, 2, local_input_matrix_A.data(), block_dimension * block_dimension);
+      //}
+      //if (world.rank() < block_rows_columns) {
+        //world.send(world.rank() + block_rows_columns * (block_rows_columns - 1), 3, local_input_matrix_B.data(),
+                   //block_dimension * block_dimension);
+      //} else {
+        //world.send(world.rank() - block_rows_columns, 3, local_input_matrix_B.data(), block_dimension * block_dimension);
+      //}
+      //if ((world.rank() + 1) % block_rows_columns == 0) {
+        //world.recv(world.rank() - block_rows_columns + 1, 2, local_input_matrix_A.data(), block_dimension * block_dimension);
+      //} else {
+        //world.recv(world.rank() + 1, 2, local_input_matrix_A.data(), block_dimension * block_dimension);
+      //}
+      //if (world.rank() >= block_rows_columns * (block_rows_columns - 1)) {
+        //world.recv(world.rank() - block_rows_columns * (block_rows_columns - 1), 3, local_input_matrix_B.data(),
+                   //block_dimension * block_dimension);
+      //} else {
+        //world.recv(world.rank() + block_rows_columns, 3, local_input_matrix_B.data(), block_dimension * block_dimension);
       }
       i = 0;
       while (i != block_dimension) {
