18bbb0a3e1598062ed573d57d313f2fa50f77eca
W-Snack <90415228+W-Snack@users.noreply.github.com>
2024-12-27 03:13:02 +0300

    002

--- tasks/mpi/fomin_v_sobel_edges/src/ops_mpi.cpp
+++ tasks/mpi/fomin_v_sobel_edges/src/ops_mpi.cpp
@@ -37,51 +37,44 @@ bool fomin_v_sobel_edges::SobelEdgeDetectionMPI::run() {
   const int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
   const int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
 
-  int blockSize = (height_ - 2) / world.size();
-  int reminder = (height_ - 2) % world.size();
+  int num_procs = world.size();
+  int local_height = (height_ - 2) / num_procs;
+  int reminder = (height_ - 2) % num_procs;
 
   std::vector<unsigned char> local_input;
 
   if (world.rank() == 0) {
-    // Send data to other processes
-    for (int proc = 1; proc < world.size(); ++proc) {
-      int start_row = reminder + blockSize * proc;
-      int local_height = blockSize + 2;  // Include boundary rows
-      if (proc < reminder) {
-        local_height += 1;
-        start_row += proc;
-      }
-      local_input.resize(local_height * width_);
-      for (int y = start_row - 1; y < start_row + blockSize + 1; ++y) {
+    // Prepare and send data to other processes
+    for (int proc = 1; proc < num_procs; ++proc) {
+      int start_row = reminder > 0 ? 1 + proc : 1 + local_height * proc;
+      int proc_local_height = local_height + (proc <= reminder ? 1 : 0);
+      local_input.resize((proc_local_height + 2) * width_);  // Include boundary rows
+
+      for (int y = start_row - 1; y < start_row + proc_local_height + 1; ++y) {
         for (int x = 0; x < width_; ++x) {
-          local_input[(y - start_row + 1) * width_ + x] = input_image_[y * width_ + x];
+          local_input[(y - (start_row - 1)) * width_ + x] = input_image_[y * width_ + x];
         }
       }
-      world.send(proc, 0, local_input.data(), local_height * width_);
+      world.send(proc, 0, local_input.data(), local_input.size());
     }
 
     // Prepare local input for root process
-    int local_height = reminder + 2;  // Include boundary rows
-    local_input.resize(local_height * width_);
-    for (int y = 0; y < local_height; ++y) {
+    local_height = reminder + local_height;
+    local_input.resize((local_height + 2) * width_);
+    for (int y = 0; y < local_height + 2; ++y) {
       for (int x = 0; x < width_; ++x) {
         local_input[y * width_ + x] = input_image_[y * width_ + x];
       }
     }
   } else {
     // Receive data from root process
-    int local_height = blockSize + 2;  // Include boundary rows
-    if (world.rank() <= reminder) {
-      local_height += 1;
-    }
-    local_input.resize(local_height * width_);
-    world.recv(0, 0, local_input.data(), local_height * width_);
+    world.recv(0, 0, local_input.data(), local_input.size());
   }
 
-  int local_height = local_input.size() / width_;
-  std::vector<unsigned char> local_output(local_height * width_, 0);
+  int local_height_effective = local_input.size() / width_ - 2;  // Exclude boundary rows
+  std::vector<unsigned char> local_output(local_height_effective * width_, 0);
 
-  for (int y = 1; y < local_height - 1; ++y) {
+  for (int y = 1; y < local_height_effective + 1; ++y) {
     for (int x = 1; x < width_ - 1; ++x) {
       int sumX = 0;
       int sumY = 0;
@@ -95,22 +88,27 @@ bool fomin_v_sobel_edges::SobelEdgeDetectionMPI::run() {
       }
 
       int gradient = static_cast<int>(std::sqrt(sumX * sumX + sumY * sumY));
-      local_output[y * width_ + x] = static_cast<unsigned char>(std::min(gradient, 255));
+      local_output[(y - 1) * width_ + (x - 1)] = static_cast<unsigned char>(std::min(gradient, 255));
     }
   }
 
-  local_output.erase(local_output.begin(), local_output.begin() + width_);
-  local_output.erase(local_output.end() - width_, local_output.end());
+  std::vector<int> local_sizes(num_procs, local_height * width_);
+  if (world.rank() == 0) {
+    for (int proc = 1; proc < num_procs; ++proc) {
+      local_sizes[proc] = (local_height + (proc <= reminder ? 1 : 0)) * width_;
+    }
+  }
 
-  std::vector<int> local_sizes(world.size(), blockSize * width_);
-  local_sizes[0] += reminder * width_;
+  // Gather results
+  std::vector<unsigned char> gathered_output((height_ - 2) * width_, 0);
+  if (world.rank() == 0) {
+    gathered_output.resize((height_ - 2) * width_);
+  }
 
-  gatherv(world, local_output.data(), local_output.size(), output_image_.data(), local_sizes, 0);
+  gatherv(world, local_output.data(), local_output.size(), gathered_output.data(), local_sizes, 0);
 
   if (world.rank() == 0) {
-    std::vector<unsigned char> zero_row(width_, 0);
-    output_image_.insert(output_image_.begin(), zero_row.begin(), zero_row.end());
-    output_image_.erase(output_image_.end() - width_, output_image_.end());
+    output_image_.assign(gathered_output.begin(), gathered_output.end());
   }
 
   return true;
