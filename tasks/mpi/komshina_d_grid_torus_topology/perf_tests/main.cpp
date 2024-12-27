8f515f149cc1be9aaaa00cd9e6b19e70eeeed8e6
MikasaAkc <daria.komshina@mail.ru>
2024-12-27 19:21:31 +0300

    tasks

--- tasks/mpi/komshina_d_grid_torus_topology/perf_tests/main.cpp
+++ tasks/mpi/komshina_d_grid_torus_topology/perf_tests/main.cpp
@@ -76,6 +76,7 @@ TEST(komshina_d_grid_torus_topology_mpi_perf_test, test_task_run) {
 
   auto gridTorusTask = std::make_shared<komshina_d_grid_torus_topology_mpi::GridTorusTopologyParallel>(taskDataPar);
 
+
   if (world.size() == 1) {
     ASSERT_EQ(gridTorusTask->validation(), false);
   } else {
