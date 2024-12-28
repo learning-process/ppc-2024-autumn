36eb38341fd31f805be5b8fc4986ed499364f680
dangerulya <ardon232@mail.ru>
2024-12-29 01:37:12 +0300

    Update ops_mpi.cpp

--- tasks/mpi/malyshev_v_lent_horizontal/src/ops_mpi.cpp
+++ tasks/mpi/malyshev_v_lent_horizontal/src/ops_mpi.cpp
@@ -117,17 +117,35 @@ bool malyshev_lent_horizontal::TestTaskParallel::run() {
   broadcast(world, ext_, 0);
   broadcast(world, vector_, 0);
 
-  std::vector<int32_t> sizes(world.size(), delta_);
-  for (uint32_t i = 0; i < ext_; i++) {
-    sizes[world.size() - i - 1]++;
+  if (world.rank() == 0) {
+    flat_matrix_.clear();
+    for (const auto& row : matrix_) {
+      flat_matrix_.insert(flat_matrix_.end(), row.begin(), row.end());
+    }
   }
 
-  uint32_t local_rows = sizes[world.rank()];
+  std::vector<int> sendcounts(world.size(), delta_ * vector_.size());
+  for (int i = 0; i < ext_; ++i) {
+    sendcounts[world.size() - i - 1] += vector_.size();
+  }
+
+  std::vector<int> displs(world.size(), 0);
+  for (int i = 1; i < displs.size(); ++i) {
+    displs[i] = displs[i - 1] + sendcounts[i - 1];
+  }
+
+  uint32_t local_rows = sendcounts[world.rank()] / vector_.size();
   local_matrix_.resize(local_rows, std::vector<int32_t>(vector_.size()));
+  std::vector<int32_t> flat_local_matrix(local_rows * vector_.size());
+
+  scatterv(world, flat_matrix_, sendcounts, displs, flat_local_matrix, 0);
+
+  for (uint32_t i = 0; i < local_rows; ++i) {
+    std::copy(flat_local_matrix.begin() + i * vector_.size(), flat_local_matrix.begin() + (i + 1) * vector_.size(),
+              local_matrix_[i].begin());
+  }
+
   local_result_.resize(local_rows);
-
-  scatterv(world, matrix_, sizes, local_matrix_, 0);
-
   for (uint32_t i = 0; i < local_matrix_.size(); i++) {
     local_result_[i] = 0;
     for (uint32_t j = 0; j < vector_.size(); j++) {
@@ -135,7 +153,8 @@ bool malyshev_lent_horizontal::TestTaskParallel::run() {
     }
   }
 
-  gatherv(world, local_result_, result_.data(), sizes, 0);
+  std::vector<int> recvcounts = sendcounts;
+  gatherv(world, local_result_, result_.data(), recvcounts, displs, 0);
 
   return true;
 }
