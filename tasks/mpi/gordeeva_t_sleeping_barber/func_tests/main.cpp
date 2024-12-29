68094dcfda93e92cdf1965cb9a7f7da11ba9b514
TayaGordeeva <tauona.g2004@gmail.com>
2024-12-29 18:04:20 +0300

    correct end

--- tasks/mpi/gordeeva_t_sleeping_barber/func_tests/main.cpp
+++ tasks/mpi/gordeeva_t_sleeping_barber/func_tests/main.cpp
@@ -84,7 +84,7 @@ TEST(gordeeva_t_sleeping_barber_mpi, Test_End_To_End2) {
 
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
 
-  const int max_waiting_chairs = 3;
+  const int max_waiting_chairs = 2;
   int global_res = -1;
 
   taskDataPar->inputs_count.emplace_back(max_waiting_chairs);
@@ -114,7 +114,7 @@ TEST(gordeeva_t_sleeping_barber_mpi, Test_End_To_End3) {
 
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
 
-  const int max_waiting_chairs = 996;
+  const int max_waiting_chairs = 3;
   int global_res = -1;
 
   taskDataPar->inputs_count.emplace_back(max_waiting_chairs);
@@ -140,6 +140,36 @@ TEST(gordeeva_t_sleeping_barber_mpi, Test_End_To_End3) {
 }
 
 TEST(gordeeva_t_sleeping_barber_mpi, Test_End_To_End4) {
+  boost::mpi::communicator world;
+
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  const int max_waiting_chairs = 996;
+  int global_res = -1;
+
+  taskDataPar->inputs_count.emplace_back(max_waiting_chairs);
+  taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(&global_res));
+  taskDataPar->outputs_count.emplace_back(sizeof(global_res));
+
+  gordeeva_t_sleeping_barber_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+
+  if (world.size() < 3) {
+    ASSERT_FALSE(testMpiTaskParallel.validation());
+  } else {
+    ASSERT_TRUE(testMpiTaskParallel.validation());
+    ASSERT_TRUE(testMpiTaskParallel.pre_processing());
+    ASSERT_TRUE(testMpiTaskParallel.run());
+    ASSERT_TRUE(testMpiTaskParallel.post_processing());
+
+    world.barrier();
+
+    if (world.rank() == 0) {
+      ASSERT_EQ(global_res, 0);
+    }
+  }
+}
+
+TEST(gordeeva_t_sleeping_barber_mpi, Test_End_To_End5) {
   boost::mpi::communicator world;
 
   std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
